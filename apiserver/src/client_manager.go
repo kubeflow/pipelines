4159c84b65436a3948d408044b5fd291a2b86185
Yang Pan <yangpa@yangpa-macbookpro2.roam.corp.google.com>
2018-02-26 16:53:41 -0800

    add todo for k8s env variable

--- apiserver/src/client_manager.go
+++ apiserver/src/client_manager.go
@@ -71,6 +71,7 @@ func getConfig(configPath string) Config {
 }
 
 // Get Argo's K8s CRD API client.
+// TODO(yangpa): Pass the env variable from parameter when start the binary instead.
 func getArgoClient() dao.ArgoClientInterface {
 	k8ServiceHost := os.Getenv(k8sServiceHost)
 	if k8ServiceHost == "" {
