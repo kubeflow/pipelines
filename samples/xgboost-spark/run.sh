ab0e7be608e9d502226c134317d63d2a867c0a48
Bradley Jiang <qimingj@google.com>
2018-03-06 15:19:13 -0800

    Update run.sh to handle renamed workflow yaml.

--- samples/xgboost-spark/run.sh
+++ samples/xgboost-spark/run.sh
@@ -1,1 +1,1 @@
-argo submit workflow.yaml -p project=bradley-playground -p output="gs://bradley-playground/sfpdmodel" --entrypoint xgboost-training
+argo submit xgboost-training.yaml -p project=bradley-playground -p output="gs://bradley-playground/sfpdmodel" --entrypoint xgboost-training
