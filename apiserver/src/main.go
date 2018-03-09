64b309945e168ef402fc76d1b45302cc0722fbd8
Yang Pan <yangpa@yangpa-macbookpro2.roam.corp.google.com>
2018-03-09 11:50:43 -0800

    update to use viper

--- apiserver/src/main.go
+++ apiserver/src/main.go
@@ -4,8 +4,10 @@ import (
 	"flag"
 	"fmt"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/golang/glog"
 	"github.com/kataras/iris"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -17,7 +19,25 @@ func main() {
 	flag.Parse()
 	glog.Infof("starting web server")
 
-	clientManager := NewClientManager(*configPath)
+	// Import environment variable
+	viper.AutomaticEnv()
+
+	// Set configuration file name. The format is auto detected in this case.
+	viper.SetConfigName("config")
+	viper.AddConfigPath(*configPath)
+	err := viper.ReadInConfig()
+	if err != nil {
+		glog.Fatalf("Fatal error config file: %s", err)
+	}
+
+	// Watch for configuration change
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		// Read in config again
+		viper.ReadInConfig()
+	})
+
+	clientManager := NewClientManager()
 
 	app := newApp(clientManager)
 
