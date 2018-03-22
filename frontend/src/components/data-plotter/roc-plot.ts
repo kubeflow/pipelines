9311ac9f748ebf234dbbb443481052fef1556a2d
Riley Bauer <34456002+rileyjbauer@users.noreply.github.com>
2018-03-22 12:26:06 -0700

    Fine-tunes the ROC plot's hovertext (#128)
    * Fine-tunes the ROC plot's hovertext so that it smoothly follows the cursor. Also added TPR and FPR to the hovering text, and added a dotted diagonal line to serve as a reference for evaluating the ROC curve.

--- frontend/src/components/data-plotter/roc-plot.ts
+++ frontend/src/components/data-plotter/roc-plot.ts
@@ -12,8 +12,8 @@ export interface RocOptions {
 /**
  * Draws an ROC curve given the data in the following format:
  * [
- *   [FPR, TPR],
- *   [FPR, TPR],
+ *   [FPR, TPR, threshold],
+ *   [FPR, TPR, threshold],
  *   ...
  * ]
  */
@@ -29,6 +29,14 @@ export function drawROC(config: RocOptions) {
     .append('g')
     .attr('transform', `translate(${margin}, ${margin})`);
 
+  // A dotted reference line for baseline comparison of ROC curve.
+  svg.append('line')
+    .attr('class', 'dottedLine')
+    .attr('x1', 0)
+    .attr('y1', height)
+    .attr('x2', width)
+    .attr('y2', 0);
+
   const x = d3.scaleLinear().range([0, width]);
   const y = d3.scaleLinear().range([height, 0]);
 
@@ -40,26 +48,6 @@ export function drawROC(config: RocOptions) {
     .data([config.data as any])
     .attr('class', 'line')
     .attr('d', valueline);
-
-  const cells = svg.append('g').attr('class', 'vors').selectAll('g');
-
-  const cell = cells.data(config.data as any);
-  cell.exit().remove();
-
-  const cellEnter = cell.enter().append('g');
-
-  cellEnter.append('circle')
-    .attr('class', 'dot')
-    .attr('r', 3.5)
-    .attr('cx', (d) => x((d as any)[0]))
-    .attr('cy', (d) => y((d as any)[1]));
-
-  cell.select('path').attr('d', (d) => 'M' + (d as any)[2].join('L') + 'Z');
-
-  cellEnter.append('text').attr('class', 'hidetext')
-    .attr('x', (d) => Math.min(width - 100, x((d as any)[0]))) // Don't go over width
-    .attr('y', (d) => y((d as any)[1]) + 20)
-    .text((d) => 'threshold: ' + (+(d as any)[2]).toFixed(5));
 
   svg.append('g')
     .attr('transform', `translate(0, ${height})`)
@@ -79,4 +67,63 @@ export function drawROC(config: RocOptions) {
     .attr('y', -30)
     .style('font-size', '12px')
     .text('TPR');
+
+  const point = svg.append('g')
+    .attr('class', 'point')
+    .style('display', 'none');
+
+  point.append('circle')
+    .attr('r', 4);
+
+  const textOffsetX = 10;
+  const hoverText = point.append('text')
+    .attr('x', textOffsetX)
+    .attr('y', 5);
+
+  const hoverTextThreshold = hoverText.append('tspan')
+    .attr('dy', 0);
+
+  const hoverTextTPR = hoverText.append('tspan')
+    .attr('x', textOffsetX)
+    .attr('dy', '1.2em');
+
+  const hoverTextFPR = hoverText.append('tspan')
+    .attr('x', textOffsetX)
+    .attr('dy', '1.2em');
+
+  svg.append('rect')
+    .attr('class', 'overlay')
+    .attr('width', width)
+    .attr('height', height)
+    .on('mouseover', () => point.style('display', 'initial'))
+    .on('mouseout', () => point.style('display', 'none'))
+    .on('mousemove', mousemove);
+
+  // Transform the data into an array of x-values (domain) that can be bisected.
+  const xs = (config.data as any[][]).map((a) => a[0]);
+
+  // Provides an interactive dot and label that moves along the plot as the
+  // mouse moves.
+  function mousemove() {
+    // Convert the mouse's horizontal position into a value in the domain.
+    const mouseX = x.invert(d3.mouse(svg.node() as any)[0]);
+
+    // Find the insertion point to the right of the mouse's x-value.
+    const insertionPoint = d3.bisectRight(xs, mouseX);
+
+    // Use the data point that's closest to the mouse, not only the left or only
+    // the right in order to make the movement feel more natural.
+    const dLeft = config.data[insertionPoint - 1] as number[];
+    const dRight = config.data[insertionPoint] as number[];
+    const d = mouseX - dLeft[0] > dRight[0] - mouseX ? dRight : dLeft;
+
+    point.attr('transform', `translate(${x(d[0])}, ${y(d[1])})`);
+
+    // -Math.max here keeps the label from moving outside of the plot element.
+    hoverText.style('transform', `translate(-${Math.max(0, x(d[0]) - width + 100)}px, 12px)`);
+
+    hoverTextThreshold.text('Threshold: ' + (+(d[2])).toFixed(5));
+    hoverTextTPR.text('TPR: ' + (+(d[1])).toFixed(5));
+    hoverTextFPR.text('FPR: ' + (+(d[0])).toFixed(5));
+  }
 }
