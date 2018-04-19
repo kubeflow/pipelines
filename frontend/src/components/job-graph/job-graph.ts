d0c97d43e199feb35e68cf0ab8fe6593857d5538
Riley Bauer <34456002+rileyjbauer@users.noreply.github.com>
2018-04-19 10:51:54 -0700

    Fixes job graphs' layout and node names (#324)
    * Fixes job graphs' layout and node names
    
    Argo's workflows consist of a root node that starts the various steps of
    the workflow via intermediary steps (identified by node type StepGroup,
    and called "virtual nodes" in our code). This means even if a job is
    made up of a sequence of steps, the root node still starts each of them
    via a StepGroup. This results in an unituitive graph when plotted as
    presented in the output yaml/json that Argo returns.
    
    This change removes the root node from the graph, uses the "displayName"
    property of nodes in the graph, and changes the appearance of the
    StepGroup nodes to make them less distracting.
    
    * Fixes indenting, simplifies logic around virtual nodes in Job Graph, addresses other PR comments.

--- frontend/src/components/job-graph/job-graph.ts
+++ frontend/src/components/job-graph/job-graph.ts
@@ -5,7 +5,7 @@ import * as dagre from 'dagre';
 
 import { customElement, property } from 'polymer-decorators/src/decorators';
 import { nodePhaseToIcon } from '../../lib/utils';
-import { Workflow as ArgoTemplate } from '../../model/argo_template';
+import { NodeStatus, Workflow as ArgoTemplate } from '../../model/argo_template';
 
 import './job-graph.html';
 
@@ -34,6 +34,11 @@ interface DisplayNode extends dagre.Node {
   finishedAt: string;
 }
 
+const NODE_WIDTH = 150;
+const NODE_HEIGHT = 50;
+const VIRTUAL_NODE_WIDTH = 30;
+const VIRTUAL_NODE_HEIGHT = 30;
+
 @customElement('job-graph')
 export class JobGraph extends Polymer.Element {
 
@@ -52,26 +57,58 @@ export class JobGraph extends Polymer.Element {
     const g = new dagre.graphlib.Graph();
     g.setGraph({});
     g.setDefaultEdgeLabel(() => ({}));
-    const nodes = this.jobGraph.status.nodes;
-    Object.keys(nodes).forEach((k) =>
-      g.setNode(k, { label: k, width: 150, height: 50 }));
-    Object.keys(nodes).forEach((k) => {
-      if (nodes[k].children) {
-        nodes[k].children.forEach((c) => g.setEdge(k, c));
-      }
-    });
+
+    const workflowNodes = this.jobGraph.status.nodes;
+    const workflowName = this.jobGraph.metadata.name || '';
+
+    // Remove the root node that Argo creates to manage the workflow.
+    delete workflowNodes[workflowName];
+
+    // Create dagre graph nodes from workflow nodes.
+    Object.values(workflowNodes)
+      .forEach((node) => {
+        const isVirtual = this._isVirtual(node);
+        g.setNode(node.id, {
+          height: isVirtual ? VIRTUAL_NODE_HEIGHT : NODE_HEIGHT,
+          label: node.displayName || node.id,
+          virtual: isVirtual,
+          width: isVirtual ? VIRTUAL_NODE_WIDTH : NODE_WIDTH,
+          ...node,
+        });
+      });
+
+    // Connect dagre graph nodes with edges.
+    Object.keys(workflowNodes)
+      .forEach((nodeId) => {
+        if (workflowNodes[nodeId].children) {
+          workflowNodes[nodeId].children.forEach((childNodeId) =>
+            g.setEdge(nodeId, childNodeId));
+        }
+      });
+
+    // Ensure that the exit handler nodes are appended to the graph
+    const onExitHandlerNodeId =
+        Object.keys(workflowNodes).find((id) =>
+            workflowNodes[id].name === `${workflowName}.onExit`);
+    if (onExitHandlerNodeId) {
+      this._getOutboundNodes(this.jobGraph, workflowName).forEach((nodeId) =>
+        g.setEdge(nodeId, onExitHandlerNodeId));
+    }
 
     dagre.layout(g);
 
+    // Creates the array of nodes used that will be rendered and adds additional
+    // metadata to them.
     this._workflowNodes = g.nodes().map((id) => {
       return Object.assign(g.node(id), {
-        finishedAt: nodes[id].finishedAt,
-        icon: nodePhaseToIcon(nodes[id].phase),
-        name: nodes[id].name,
-        startedAt: nodes[id].startedAt,
+        finishedAt: workflowNodes[id].finishedAt,
+        icon: nodePhaseToIcon(workflowNodes[id].phase),
+        name: workflowNodes[id].displayName || id,
+        startedAt: workflowNodes[id].startedAt,
       });
     });
 
+    // Creates the lines that constitute the edges connecting the graph.
     g.edges().forEach((edgeInfo) => {
       const edge = g.edge(edgeInfo);
       const lines: Line[] = [];
@@ -91,5 +128,36 @@ export class JobGraph extends Polymer.Element {
       }
       this.push('_workflowEdges', { from: edgeInfo.v, to: edgeInfo.w, lines });
     });
+  }
+
+  protected _getNodeCssClass(node: NodeStatus) {
+    return this._isVirtual(node) ? 'virtual-node' : 'pipeline-node';
+  }
+
+  // Outbound nodes are roughly those nodes which are the final step of the
+  // workflow's execution. More information can be found in the NodeStatus
+  // interface definition.
+  private _getOutboundNodes(graph: ArgoTemplate, nodeId: string): string[] {
+    const node = graph.status.nodes[nodeId];
+    if (node.type === 'Pod') {
+      return [node.id];
+    }
+    let outbound = Array<string>();
+    for (const outboundNodeID of node.outboundNodes || []) {
+      const outNode = graph.status.nodes[outboundNodeID];
+      if (outNode.type === 'Pod') {
+        outbound.push(outboundNodeID);
+      } else {
+        outbound = outbound.concat(this._getOutboundNodes(graph, outboundNodeID));
+      }
+    }
+    return outbound;
+  }
+
+  // Returns whether or not the given node is one of the intermediate nodes used
+  // by Argo to orchestrate the workflow. Such nodes are not generally
+  // meaningful from a user's perspective.
+  private _isVirtual(node: NodeStatus) {
+    return (node.type === 'StepGroup' || node.type === 'DAG') && !!node.boundaryID;
   }
 }
