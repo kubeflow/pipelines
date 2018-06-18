ddad4aa81fcb8bd46406cf6fc7e2746f39ffe19b
Riley Bauer <34456002+rileyjbauer@users.noreply.github.com>
2018-06-18 16:13:37 -0700

    Adds UI tests for job errors and job graph (#564)

--- frontend/test/ui/visual-regression/job-details.spec.js
+++ frontend/test/ui/visual-regression/job-details.spec.js
@@ -38,6 +38,40 @@ describe('view job details', () => {
     assertDiffs(browser.checkDocument());
   });
 
+  it('clears previous job\'s graph', () => {
+    // Return to job list
+    const backBtnSelector = 'app-shell job-details paper-icon-button';
+    browser.waitForVisible(backBtnSelector);
+    browser.click(backBtnSelector);
+
+    const jobListTabSelector = 'app-shell pipeline-details paper-tab:last-child';
+    browser.waitForVisible(jobListTabSelector);
+    browser.click(jobListTabSelector);
+
+    // Select a job with a different graph.
+    const differentJobSelector =
+        'app-shell pipeline-details job-list item-list paper-item:nth-of-type(3)';
+    browser.waitForVisible(differentJobSelector);
+    browser.doubleClick(differentJobSelector);
+
+    // Return to job list
+    browser.waitForVisible(backBtnSelector);
+    browser.click(backBtnSelector);
+    browser.waitForVisible(jobListTabSelector);
+    browser.click(jobListTabSelector);
+
+    // Select the original job again.
+    const originalJobSelector = 'app-shell pipeline-details job-list item-list paper-item';
+    browser.waitForVisible(originalJobSelector);
+    browser.doubleClick(originalJobSelector);
+
+    const graphSelector = 'app-shell job-details #graph-tab';
+    browser.waitForVisible(graphSelector);
+    browser.click(graphSelector);
+
+    assertDiffs(browser.checkDocument());
+  });
+
   it('opens node details upon click', () => {
     // Select a step that will show in the viewport without scrolling.
     const selector = 'app-shell job-details job-graph .pipeline-node:nth-of-type(5)';
@@ -61,5 +95,80 @@ describe('view job details', () => {
 
     browser.pause(300);
     assertDiffs(browser.checkDocument());
+  });
+
+  describe('error handling', () => {
+
+    beforeAll(() => {
+      browser.url('/pipelines/details/2');
+
+      // Navigate to Job list
+      const selector = 'app-shell pipeline-details paper-tab:last-child';
+      browser.waitForVisible(selector);
+      browser.click(selector);
+    });
+
+    it('displays error message', () => {
+      // Select a job that will show an error message.
+      const failedJobSelector =
+          'app-shell pipeline-details job-list item-list paper-item:nth-of-type(4)';
+
+      browser.waitForVisible(failedJobSelector);
+      browser.doubleClick(failedJobSelector);
+
+      assertDiffs(browser.checkDocument());
+    });
+
+    it('clears previous job graph even if there is an error', () => {
+      // Return to job list
+      const backBtnSelector = 'app-shell job-details paper-icon-button';
+      browser.waitForVisible(backBtnSelector);
+      browser.click(backBtnSelector);
+
+      const jobListTabSelector = 'app-shell pipeline-details paper-tab:last-child';
+      browser.waitForVisible(jobListTabSelector);
+      browser.click(jobListTabSelector);
+
+      // Select a job that succeeded.
+      const successfulJobSelector = 'app-shell pipeline-details job-list item-list paper-item';
+      browser.waitForVisible(successfulJobSelector);
+      browser.doubleClick(successfulJobSelector);
+
+      // Return to job list
+      browser.waitForVisible(backBtnSelector);
+      browser.click(backBtnSelector);
+      browser.waitForVisible(jobListTabSelector);
+      browser.click(jobListTabSelector);
+
+      // Select a job that will show an error message.
+      const failedJobSelector =
+          'app-shell pipeline-details job-list item-list paper-item:nth-of-type(4)';
+      browser.waitForVisible(failedJobSelector);
+      browser.doubleClick(failedJobSelector);
+
+      const graphSelector = 'app-shell job-details #graph-tab';
+      browser.waitForVisible(graphSelector);
+      browser.click(graphSelector);
+
+      assertDiffs(browser.checkDocument());
+    });
+
+    it('clears error message for subsequent successful job pages', () => {
+      // Return to job list
+      const backBtnSelector = 'app-shell job-details paper-icon-button';
+      browser.waitForVisible(backBtnSelector);
+      browser.click(backBtnSelector);
+
+      const jobListTabSelector = 'app-shell pipeline-details paper-tab:last-child';
+      browser.waitForVisible(jobListTabSelector);
+      browser.click(jobListTabSelector);
+
+      // Select a job that succeeded.
+      const successfulJobSelector = 'app-shell pipeline-details job-list item-list paper-item';
+      browser.waitForVisible(successfulJobSelector);
+      browser.doubleClick(successfulJobSelector);
+
+      assertDiffs(browser.checkDocument());
+    });
   });
 });
