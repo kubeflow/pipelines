4af4731ee9a3b896ca0b8bdf5a5e3f3926bb7e1f
Yang Pan <yangpa@google.com>
2018-07-06 15:28:01 -0700

    add prow test script (#627)
    * add flag to prototypes
    
    * fix dependency
    
    * change the order of commands
    
    * update commands
    
    * add prow test script
    
    * print something
    
    * remove argo
    
    * update script
    
    * rename script
    
    * move interpreter annotation to top of file

---
+++ test/presubmit-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo start.....
+
+ls $(dirname $0)
+
+gcloud container clusters get-credentials spinner --zone us-west1-a --project ml-pipeline-test
+
+argo list
+
+echo succeed
+
+# create a argo workflow
