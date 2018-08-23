b83e8aafcf00412e15cc430f0003e7e1054a0a09
Yasser Elsayed <yebrahim@users.noreply.github.com>
2018-08-22 22:18:09 -0700

    License headers for frontend (#906)
    And removing the license-generator.py script that generates these, and the no longer needed .vscode directory. Going forward we'll maintain these manually.
    
    * run autogen on frontend directory, remove .vscode
    
    * fix autogen to include .ts files, run it on .ts files

--- frontend/server/utils.ts
+++ frontend/server/utils.ts
@@ -1,3 +1,17 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 export function equalArrays(a1: any[], a2: any[]): boolean {
   if (!Array.isArray(a1) || !Array.isArray(a2) || a1.length !== a2.length) {
     return false;
