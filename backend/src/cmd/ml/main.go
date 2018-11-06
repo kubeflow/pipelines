204ba0d57bd25e2a24ecaeb7093b55f391b5eec4
Pascal Vicaire <25757005+vicaire@users.noreply.github.com>
2018-11-05 19:16:41 -0800

    First integration test for the ML Pipeline CLI (Pipeline List).

--- backend/src/cmd/ml/main.go
+++ backend/src/cmd/ml/main.go
@@ -12,8 +12,13 @@ const (
 
 func main() {
 	flag.Parse()
+	rootCmd, _ := GetRealRootCommand()
+	rootCmd.Execute()
+}
+
+func GetRealRootCommand() (*cmd.RootCommand, *cmd.ClientFactory) {
 	clientFactory := cmd.NewClientFactory()
 	rootCmd := cmd.NewRootCmd(clientFactory)
 	rootCmd = cmd.CreateSubCommands(rootCmd, defaultPageSize)
-	rootCmd.Execute()
+	return rootCmd, clientFactory
 }
