50646dd5ec60b8211e66938a480a5767027226c4
Bradley Jiang <qimingj@google.com>
2018-11-07 09:43:46 -0800

    Make kubernetes a dependency of kfp package.

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -19,7 +19,8 @@ from setuptools import setup
 NAME = 'kfp'
 VERSION = '0.1'
 
-REQUIRES = ['urllib3 >= 1.15', 'six >= 1.10', 'certifi', 'python-dateutil', 'PyYAML', 'google-cloud-storage >= 1.13.0']
+REQUIRES = ['urllib3 >= 1.15', 'six >= 1.10', 'certifi', 'python-dateutil', 'PyYAML',
+            'google-cloud-storage >= 1.13.0', 'kubernetes == 8.0.0']
 
 setup(
     name=NAME,
