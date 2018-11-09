e639a361de3380e611f4daf1915e1f9c65037bda
nealgao <gaoning777@gmail.com>
2018-11-08 17:34:48 -0800

    add todos for the image tag update for new releases (#138)

--- sdk/python/kfp/dsl/components/kubeflow_tfjob_launcher_op.py
+++ sdk/python/kfp/dsl/components/kubeflow_tfjob_launcher_op.py
@@ -17,7 +17,7 @@ from kfp import dsl
 def kubeflow_tfjob_launcher_op(container_image, command, number_of_workers: int, number_of_parameter_servers: int, tfjob_timeout_minutes: int, output_dir=None, step_name='TFJob-launcher'):
     return dsl.ContainerOp(
         name = step_name,
-        image = 'gcr.io/ml-pipeline/ml-pipeline-kubeflow-tf:0.1.0', 
+        image = 'gcr.io/ml-pipeline/ml-pipeline-kubeflow-tf:0.1.0', #TODO-release: update the release tag for the next release
         arguments = [
             '--workers', number_of_workers,
             '--pss', number_of_parameter_servers,
