2552c7a9defb08aba95ac6b78341bfe7dda80c91
Yang Pan <yangpa@google.com>
2018-11-10 16:11:25 -0800

    Revert "Tests - Testing master+branch instead of just branch (#165)"
    This reverts commit b190e729d28b3d4941f100a3161a61a4734f4fc1.

--- test/imagebuilder/build.sh
+++ test/imagebuilder/build.sh
@@ -60,9 +60,7 @@ BASE_DIR=/ml
 echo "Clone ML pipeline code in COMMIT SHA ${COMMIT_SHA}..."
 git clone https://github.com/kubeflow/pipelines ${BASE_DIR}
 cd ${BASE_DIR}
-git config --local user.name 'K8S Bootstrap'
-git config --local user.email k8s_bootstrap@localhost
-git merge --no-ff ${COMMIT_SHA} -m "Merged PR ${COMMIT_SHA}"
+git checkout ${COMMIT_SHA}
 
 echo "Waiting for dind to start..."
 until docker ps; do sleep 3; done;
