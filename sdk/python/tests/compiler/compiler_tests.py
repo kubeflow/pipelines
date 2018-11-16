3053dcccab20f4cfec39c5daf0b63f8c48986bcb
IronPan <yangpa@google.com>
2018-11-16 03:06:13 -0800

    to list

--- sdk/python/tests/compiler/compiler_tests.py
+++ sdk/python/tests/compiler/compiler_tests.py
@@ -13,8 +13,6 @@
 # limitations under the License.
 
 
-import kfp.compiler as compiler
-import kfp.dsl as dsl
 import os
 import shutil
 import subprocess
@@ -22,7 +20,11 @@ import sys
 import tarfile
 import tempfile
 import unittest
+
+import kfp.compiler as compiler
+import kfp.dsl as dsl
 import yaml
+from kubernetes import client as k8s_client
 
 
 class TestCompiler(unittest.TestCase):
@@ -34,8 +36,15 @@ class TestCompiler(unittest.TestCase):
       msg1 = dsl.PipelineParam('msg1')
       msg2 = dsl.PipelineParam('msg2', value='value2')
       op = dsl.ContainerOp(name='echo', image='image', command=['sh', '-c'],
-                           arguments=['echo %s %s | tee /tmp/message.txt' % (msg1, msg2)],
-                           file_outputs={'merged': '/tmp/message.txt'}, gcp_secret='user-gcp-sa')
+                           arguments=['echo %s %s | tee /tmp/message.txt' % (
+                           msg1, msg2)],
+                           file_outputs={'merged': '/tmp/message.txt'},
+                           volume_mounts=[k8s_client.V1VolumeMount(
+                               mount_path='/secret/gcp-credentials',
+                               name='gcp-credentials')],
+                           env_variables=[k8s_client.V1EnvVar(
+                               name='GOOGLE_APPLICATION_CREDENTIALS',
+                               value='/secret/gcp-credentials/user-gcp-sa.json')])
     golden_output = {
       'container': {
         'image': 'image',
@@ -49,10 +58,10 @@ class TestCompiler(unittest.TestCase):
             'value': '/secret/gcp-credentials/user-gcp-sa.json'
           }
         ],
-        'volumeMounts':[
+        'volumeMounts': [
           {
             'mountPath': '/secret/gcp-credentials',
-            'name': 'echo-gcp-credentials'
+            'name': 'gcp-credentials'
           }
         ]
       },
@@ -66,7 +75,7 @@ class TestCompiler(unittest.TestCase):
         'parameters': [
           {'name': 'echo-merged',
            'valueFrom': {'path': '/tmp/message.txt'}
-          }],
+           }],
         'artifacts': [{
           'name': 'mlpipeline-ui-metadata',
           'path': '/mlpipeline-ui-metadata.json',
@@ -84,7 +93,7 @@ class TestCompiler(unittest.TestCase):
               'name': 'mlpipeline-minio-artifact',
             }
           }
-        },{
+        }, {
           'name': 'mlpipeline-metrics',
           'path': '/mlpipeline-metrics.json',
           's3': {
@@ -144,11 +153,13 @@ class TestCompiler(unittest.TestCase):
     try:
       # First make sure the simple pipeline can be compiled.
       simple_package_path = os.path.join(tmpdir, 'simple.tar.gz')
-      compiler.Compiler().compile(compose.save_most_frequent_word, simple_package_path)
+      compiler.Compiler().compile(compose.save_most_frequent_word,
+                                  simple_package_path)
 
       # Then make sure the composed pipeline can be compiled and also compare with golden.
       compose_package_path = os.path.join(tmpdir, 'compose.tar.gz')
-      compiler.Compiler().compile(compose.download_save_most_frequent_word, compose_package_path)
+      compiler.Compiler().compile(compose.download_save_most_frequent_word,
+                                  compose_package_path)
       with open(os.path.join(test_data_dir, 'compose.yaml'), 'r') as f:
         golden = yaml.load(f)
       compiled = self._get_yaml_from_tar(compose_package_path)
@@ -165,8 +176,8 @@ class TestCompiler(unittest.TestCase):
     """Test invalid pipelines."""
 
     @dsl.pipeline(
-      name='name',
-      description='description'
+        name='name',
+        description='description'
     )
     def invalid_param_defaults(message, outputpath='something'):
       pass
@@ -189,12 +200,14 @@ class TestCompiler(unittest.TestCase):
     cwd = os.getcwd()
     try:
       os.chdir(test_package_dir)
-      subprocess.check_call(['python3', 'setup.py', 'sdist', '--format=gztar', '-d', tmpdir])
+      subprocess.check_call(
+          ['python3', 'setup.py', 'sdist', '--format=gztar', '-d', tmpdir])
       package_path = os.path.join(tmpdir, 'testsample-0.1.tar.gz')
       target_tar = os.path.join(tmpdir, 'compose.tar.gz')
       subprocess.check_call([
-          'dsl-compile', '--package', package_path, '--namespace', 'mypipeline',
-          '--output', target_tar, '--function', 'download_save_most_frequent_word'])
+        'dsl-compile', '--package', package_path, '--namespace', 'mypipeline',
+        '--output', target_tar, '--function',
+        'download_save_most_frequent_word'])
       with open(os.path.join(test_data_dir, 'compose.yaml'), 'r') as f:
         golden = yaml.load(f)
       compiled = self._get_yaml_from_tar(target_tar)
@@ -212,8 +225,9 @@ class TestCompiler(unittest.TestCase):
     try:
       target_tar = os.path.join(tmpdir, file_base_name + '.tar.gz')
       subprocess.check_call([
-          'dsl-compile', '--py', py_file, '--output', target_tar])
-      with open(os.path.join(test_data_dir, file_base_name + '.yaml'), 'r') as f:
+        'dsl-compile', '--py', py_file, '--output', target_tar])
+      with open(os.path.join(test_data_dir, file_base_name + '.yaml'),
+                'r') as f:
         golden = yaml.load(f)
       compiled = self._get_yaml_from_tar(target_tar)
 
@@ -221,7 +235,7 @@ class TestCompiler(unittest.TestCase):
       self.assertEqual(golden, compiled)
     finally:
       shutil.rmtree(tmpdir)
-    
+
   def test_py_compile_basic(self):
     """Test basic sequential pipeline."""
     self._test_py_compile('basic')
@@ -241,4 +255,3 @@ class TestCompiler(unittest.TestCase):
   def test_py_volume(self):
     """Test a pipeline with a volume and volume mount."""
     self._test_py_compile('volume')
-
