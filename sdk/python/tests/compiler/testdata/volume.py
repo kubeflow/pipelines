5f83dc35cbd19153ee9f104a7adc33f83c21d63c
Yang Pan <yangpa@google.com>
2018-11-16 18:36:38 -0800

    Add volume, volumemount and env to container op (#300)
    * [WIP] change deployment platform to gcp
    
    * debug
    
    * revert test
    
    * add volume
    
    * update test
    
    * to list
    
    * fix
    
    * to list
    
    * to list
    
    * to list
    
    * to list
    
    * stage
    
    * update
    
    * update
    
    * Undid style changes
    
    * address comments
    
    * update comments

--- sdk/python/tests/compiler/testdata/volume.py
+++ sdk/python/tests/compiler/testdata/volume.py
@@ -14,23 +14,30 @@
 
 
 import kfp.dsl as dsl
+from kubernetes import client as k8s_client
 
 
 @dsl.pipeline(
-    name='GCP Secret',
-    description='A pipeline with gcp secret.'
+    name='Volume',
+    description='A pipeline with volume.'
 )
-def gcp_secret_pipeline():
+def volume_pipeline():
   op1 = dsl.ContainerOp(
       name='download',
       image='google/cloud-sdk',
       command=['sh', '-c'],
       arguments=['ls | tee /tmp/results.txt'],
-      gcp_secret='user-gcp-sa',
       file_outputs={'downloaded': '/tmp/results.txt'})
+  op1.set_volumes([k8s_client.V1Volume(name='gcp-credentials',
+                                       secret=k8s_client.V1SecretVolumeSource(
+                                           secret_name='user-gcp-sa'))])
+  op1.set_volume_mounts([k8s_client.V1VolumeMount(
+      mount_path='/secret/gcp-credentials', name='gcp-credentials')])
+  op1.set_env_variables([k8s_client.V1EnvVar(
+      name='GOOGLE_APPLICATION_CREDENTIALS',
+      value='/secret/gcp-credentials/user-gcp-sa.json')])
   op2 = dsl.ContainerOp(
       name='echo',
       image='library/bash',
       command=['sh', '-c'],
-      arguments=['echo %s' % op1.output],
-      gcp_secret='admin-gcp-sa')
+      arguments=['echo %s' % op1.output])
