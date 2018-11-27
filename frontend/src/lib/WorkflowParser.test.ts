32708db1a510d1f8ebafc14fcefb3ab8a0925a89
Chris Van Pelt <vanpelt@gmail.com>
2018-11-27 13:29:06 -0800

    Add new tests for parity

--- frontend/src/lib/WorkflowParser.test.ts
+++ frontend/src/lib/WorkflowParser.test.ts
@@ -505,6 +505,22 @@ describe('WorkflowParser', () => {
       });
     });
 
+    it('handles Minio bucket without key', () => {
+      expect(WorkflowParser.parseStoragePath('minio://testbucket/')).toEqual({
+        bucket: 'testbucket',
+        key: '',
+        source: StorageService.MINIO,
+      });
+    });
+
+    it('handles Minio bucket and key', () => {
+      expect(WorkflowParser.parseStoragePath('minio://testbucket/testkey')).toEqual({
+        bucket: 'testbucket',
+        key: 'testkey',
+        source: StorageService.MINIO,
+      });
+    });
+
     it('handles Minio bucket and multi-part key', () => {
       expect(WorkflowParser.parseStoragePath('minio://testbucket/test/key/path')).toEqual({
         bucket: 'testbucket',
