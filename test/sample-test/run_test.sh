15b1505444d7910236c13a68bf43127e813e9b87
Ning Gao <ngao@google.com>
2018-11-27 16:41:03 -0800

    add notebook sample tests for tfx

--- test/sample-test/run_test.sh
+++ test/sample-test/run_test.sh
@@ -257,4 +257,24 @@ elif [ "$TEST_NAME" == "xgboost" ]; then
 
   echo "Copy the test results to GCS ${RESULTS_GCS_DIR}/"
   gsutil cp ${SAMPLE_XGBOOST_TEST_RESULT} ${RESULTS_GCS_DIR}/${SAMPLE_XGBOOST_TEST_RESULT}
+elif [ "$TEST_NAME" == "notebook-tfx" ]; then
+  SAMPLE_NOTEBOOK_TFX_TEST_RESULT=junit_SampleNotebookTFXOutput.xml
+  SAMPLE_NOTEBOOK_TFX_TEST_OUTPUT=${RESULTS_GCS_DIR}
+
+  export LC_ALL=C.UTF-8
+  export LANG=C.UTF-8
+  papermill --prepare-only -p EXPERIMENT_NAME notebook-tfx-test -p OUTPUT_DIR ${RESULTS_GCS_DIR} -p PROJECT_NAME ml-pipeline-test -p BASE_IMAGE gcr.io/ml-pipeline-test/pusherbase:dev -p TARGET_IMAGE gcr.io/ml-pipeline-test/pusher:dev -p TRAIN_DATA gs://ml-pipeline-dataset/sample-test/taxi-cab-classification/train50.csv -p EVAL_DATA gs://ml-pipeline-dataset/sample-test/taxi-cab-classification/eval20.csv -p HIDDEN_LAYER_SIZE 10 -p STEPS 50 KubeFlow\ Pipeline\ Using\ TFX\ OSS\ Components.ipynb notebook-tfx.ipynb
+  jupyter nbconvert --to python notebook-tfx.ipynb
+  ipython notebook-tfx.py
+  python3 check_notebook_results.py --experiment notebook-tfx-test --testname notebooktfx --result $SAMPLE_NOTEBOOK_TFX_TEST_RESULT
+elif [ "$TEST_NAME" == "notebook-lightweight" ]; then
+  SAMPLE_NOTEBOOK_LIGHTWEIGHT_TEST_RESULT=junit_SampleNotebookLightweightOutput.xml
+  SAMPLE_NOTEBOOK_LIGHTWEIGHT_TEST_OUTPUT=${RESULTS_GCS_DIR}
+
+  export LC_ALL=C.UTF-8
+  export LANG=C.UTF-8
+  papermill --prepare-only -p EXPERIMENT_NAME notebook-lightweight -p PROJECT_NAME ml-pipeline-test Lightweight\ Python\ components\ -\ basics.ipynb notebook-lightweight.ipynb
+  jupyter nbconvert --to python notebook-lightweight.ipynb
+  ipython notebook-lightweight.py
+  python3 check_notebook_results.py --experiment notebook-lightweight --testname notebooklightweight --result $SAMPLE_NOTEBOOK_LIGHTWEIGHT_TEST_RESULT
 fi
