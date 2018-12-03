47be2039b40540c14759bc6555d7b42f5cba8858
Ajay Gopinathan <ajaygopinathan@google.com>
2018-12-03 15:03:17 -0800

    Clean up and separate reconciler logic into its own package for future testing.

--- backend/src/crd/controller/viewer/main.go
+++ backend/src/crd/controller/viewer/main.go
@@ -17,31 +17,23 @@
 package main
 
 import (
-	"fmt"
-	"context"
 	"flag"
 	"log"
 
 	"github.com/golang/glog"
 
-	viewerV1alpha1 "github.com/kubeflow/pipelines/backend/src/crd/pkg/apis/viewer/v1alpha1"
-	kubeflowClientSet "github.com/kubeflow/pipelines/backend/src/crd/pkg/client/clientset/versioned"
-	"github.com/kubeflow/pipelines/backend/src/crd/pkg/client/clientset/versioned/typed/viewer/v1alpha1"
+	"github.com/kubeflow/pipelines/backend/src/crd/controller/viewer/reconciler"
 	"github.com/kubeflow/pipelines/backend/src/crd/pkg/signals"
-	"github.com/kubernetes-sigs/controller-runtime/pkg/builder"
 
+	viewerV1alpha1 "github.com/kubeflow/pipelines/backend/src/crd/pkg/apis/viewer/v1alpha1"
+	"github.com/kubernetes-sigs/controller-runtime/pkg/builder"
 	appsv1 "k8s.io/api/apps/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp" // Needed for GCP authentication.
 )
 
 var (
@@ -49,136 +41,37 @@ var (
 	kubecfg   = flag.String("kubecfg", "", "Path to a valid kubeconfig.")
 )
 
-type ViewerReconciler struct {
-	client.Client
-	vcli   v1alpha1.ViewerV1alpha1Interface
-	scheme *runtime.Scheme
-}
-
-type Nothing struct{}
-
-// var _ reconcile.Reconciler = &ViewerReconciler{}
-
-func (v *ViewerReconciler) Reconcile(req reconcile.Request) (reconcile.Result, error) {
-	glog.Infof("Got request: %+v", req)
-
-	instance := &viewerV1alpha1.Viewer{}
-	if err := v.Get(context.TODO(), req.NamespacedName, instance); err != nil {
-		glog.Infof("Got error: %+v", err)
-		if errors.IsNotFound(err) {
-			// Object not found, return.
-			// Created objects are automatically garbage collected.
-			// For additional cleanup logic use finalizers.
-			return reconcile.Result{}, nil
-		}
-		// Error reading the object - requeue the request.
-		return reconcile.Result{}, err
-	}
-	glog.Infof("Got instance: %+v", instance)
-
-	// Set up potential deployment.
-	instanceName := instance.Namespace + "-" + instance.Name
-	deployment := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      instance.Name + "-deployment",
-			Namespace: instance.Namespace,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"deployment": instanceName + "-deployment",
-				},
-			},
-			Template: &corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": "tensorboard",
-						"instance": instanceName,
-					},
-				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						corev1.Container{
-							Name: instance.Name + "-pod",
-							Image: "tensorflow/tensorflow:1.11.0",
-							Args: []string{
-								"tensorboard",
-								fmt.Sprintf("--logdir=%s", instance.Spec.TensorboardSpec.LogDir),
-								fmt.Sprintf("--path_prefix=/tensorboard/%s/", instanceName),
-							},
-							Ports: []corev1.ContainerPort{6006},
-						},
-					},
-				},
-			},
-
-
-			},
-		},
-	}
-
-	// Set the deployment to be owned by the instance.
-	if err := controllerutil.SetControllerReference(instance, deployment, v.scheme); err != nil {
-		// Error means that the deployment is already owned by some other instance.
-		return reconcile.Result{}, err
-	}
-
-	if instance.Spec.Type != viewerV1alpha1.TensorboardViewer {
-		glog.Infof("Unsupported spec type: %q", instance.Spec.Type)
-		// Return nil to indicate nothing more to do here.
-		return reconcile.Result{}, nil
-	}
-	instance.Spec.PodSpec.
-
-	// Assume Tensorboard for now.
-
-	// // i2 := &viewerV1alpha1.Viewer{}
-	// i2, err := v.vcli.Viewers(req.Namespace).Get(req.Name, metav1.GetOptions{})
-	// if err != nil {
-	// 	glog.Infof("Got error: %+v", err)
-	// } else {
-	// 	glog.Infof("Got instance: %+v", *i2)
-	// }
-
-	// Enqueue the
-
-	return reconcile.Result{}, nil
-}
-
 func main() {
 	flag.Parse()
-
-	viewerV1alpha1.AddToScheme(scheme.Scheme)
 
 	cfg, err := clientcmd.BuildConfigFromFlags(*masterURL, *kubecfg)
 	if err != nil {
 		log.Fatalf("Failed to build valid config from supplied flags: %v", err)
 	}
 
-	_ = kubernetes.NewForConfigOrDie(cfg)
-	vcli := kubeflowClientSet.NewForConfigOrDie(cfg).ViewerV1alpha1()
+	// _ = kubernetes.NewForConfigOrDie(cfg)
+	// vcli := kubeflowClientSet.NewForConfigOrDie(cfg).ViewerV1alpha1()
 
 	cli, err := client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to build Kubernetes runtime client: %v", err)
 	}
 
+	viewerV1alpha1.AddToScheme(scheme.Scheme)
+	reconciler := reconciler.New(cli, scheme.Scheme)
 	mgr, err := builder.SimpleController().
 		ForType(&viewerV1alpha1.Viewer{}).
+		Owns(&appsv1.Deployment{}).
+		Owns(&corev1.Service{}).
 		WithConfig(cfg).
-		Build(&ViewerReconciler{Client: cli, vcli: vcli, scheme: scheme.Scheme})
+		Build(reconciler)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// mgr, err := manager.New(cfg, manager.Options{})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	glog.Info("Starting controller!")
+	glog.Info("Starting controller for the Viewer CRD")
 	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
-		log.Fatalf("Failed to start viewer controller: %v", err)
+		log.Fatalf("Failed to start controller: %v", err)
 	}
 }
---
+++ backend/src/crd/controller/viewer/reconciler/reconciler.go
@@ -0,0 +1,204 @@
+// Package reconciler ...
+package reconciler
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/golang/glog"
+	viewerV1alpha1 "github.com/kubeflow/pipelines/backend/src/crd/pkg/apis/viewer/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+const viewerTargetPort = 6006
+
+// Reconciler is ...
+type Reconciler struct {
+	client.Client
+	scheme *runtime.Scheme
+}
+
+// New returns a new Reconciler.
+func New(cli client.Client, scheme *runtime.Scheme) *Reconciler {
+	return &Reconciler{Client: cli, scheme: scheme}
+}
+
+// Reconcile runs the main logic for reconciling the state of a viewer with a
+// corresponding deployment and service allowing users to access the view under
+// a specific path.
+func (r *Reconciler) Reconcile(req reconcile.Request) (reconcile.Result, error) {
+	glog.Infof("Reconcile request: %+v", req)
+
+	view := &viewerV1alpha1.Viewer{}
+	if err := r.Get(context.Background(), req.NamespacedName, view); err != nil {
+		if errors.IsNotFound(err) {
+			// No viewer instance, so this may be the result of a delete.
+			// Nothing to do.
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+	glog.Infof("Got instance: %+v", view)
+
+	// Ignore other viewer types for now.
+	if view.Spec.Type != viewerV1alpha1.ViewerTypeTensorboard {
+		glog.Infof("Unsupported spec type: %q", view.Spec.Type)
+		// Return nil to indicate nothing more to do here.
+		return reconcile.Result{}, nil
+	}
+
+	// Set up potential deployment.
+	dpl, err := deploymentFrom(view)
+	if err != nil {
+		utilruntime.HandleError(err)
+		// User error, don't requeue key.
+		return reconcile.Result{}, nil
+	}
+
+	// Set the deployment to be owned by the view instance. This ensures that
+	// deleting the viewer instance will delete the deployment as well.
+	if err := controllerutil.SetControllerReference(view, dpl, r.scheme); err != nil {
+		// Error means that the deployment is already owned by some other instance.
+		utilruntime.HandleError(err)
+		return reconcile.Result{}, err
+	}
+
+	foundDpl := &appsv1.Deployment{}
+	nsn := types.NamespacedName{Name: dpl.Name, Namespace: dpl.Namespace}
+	if err := r.Client.Get(context.Background(), nsn, foundDpl); err != nil {
+		if errors.IsNotFound(err) {
+			// Create a new instance.
+			if err := r.Client.Create(context.Background(), dpl); err != nil {
+				utilruntime.HandleError(fmt.Errorf("error creating deployment: %v", err))
+				return reconcile.Result{}, err
+			}
+		} else {
+			// Some other error.
+			utilruntime.HandleError(err)
+			return reconcile.Result{}, err
+		}
+	}
+	glog.Infof("Created new deployment with spec: %+v", dpl)
+
+	// Set up a service for the deployment above.
+	svc := serviceFrom(view, dpl.Name)
+	// Set the service to be owned by the view instance as well.
+	if err := controllerutil.SetControllerReference(view, svc, r.scheme); err != nil {
+		// Error means that the service is already owned by some other instance.
+		utilruntime.HandleError(err)
+		return reconcile.Result{}, err
+	}
+
+	foundSvc := &corev1.Service{}
+	nsn = types.NamespacedName{Name: svc.Name, Namespace: svc.Namespace}
+	if err := r.Client.Get(context.Background(), nsn, foundSvc); err != nil {
+		if errors.IsNotFound(err) {
+			// Create a new instance.
+			if err := r.Client.Create(context.Background(), svc); err != nil {
+				utilruntime.HandleError(fmt.Errorf("error creating service: %v", err))
+				return reconcile.Result{}, err
+			}
+		} else {
+			// Some other error.
+			utilruntime.HandleError(err)
+			return reconcile.Result{}, err
+		}
+	}
+	glog.Infof("Created new service with spec: %+v", svc)
+
+	return reconcile.Result{}, nil
+}
+
+func setPodSpecForTensorboard(view *viewerV1alpha1.Viewer, s *corev1.PodSpec) {
+	c := corev1.Container{
+		Name:  view.Name + "-pod",
+		Image: "tensorflow/tensorflow:1.11.0",
+		Args: []string{
+			"tensorboard",
+			fmt.Sprintf("--logdir=%s", view.Spec.TensorboardSpec.LogDir),
+			fmt.Sprintf("--path_prefix=/tensorboard/%s/", view.Name),
+		},
+		Ports: []corev1.ContainerPort{
+			corev1.ContainerPort{ContainerPort: viewerTargetPort},
+		},
+	}
+	s.Containers = append(s.Containers, c)
+}
+
+func deploymentFrom(view *viewerV1alpha1.Viewer) (*appsv1.Deployment, error) {
+	name := view.Name + "-deployment"
+	dpl := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: view.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"deployment": name,
+				},
+			},
+			Template: view.Spec.PodTemplateSpec,
+		},
+	}
+
+	// Add label so we can select this deployment with a service.
+	if dpl.Spec.Template.ObjectMeta.Labels == nil {
+		dpl.Spec.Template.ObjectMeta.Labels = make(map[string]string)
+	}
+	dpl.Spec.Template.ObjectMeta.Labels["deployment"] = name
+
+	switch view.Spec.Type {
+	case viewerV1alpha1.ViewerTypeTensorboard:
+		setPodSpecForTensorboard(view, &dpl.Spec.Template.Spec)
+	default:
+		return nil, fmt.Errorf("unknown viewer type: %q", view.Spec.Type)
+	}
+
+	return dpl, nil
+}
+
+const mappingTpl = `
+---
+apiVersion: ambassador/v0
+kind: Mapping
+name: viewer-mapping-%s
+prefix: %s
+rewrite: %s
+service: %s`
+
+func serviceFrom(v *viewerV1alpha1.Viewer, deploymentName string) *corev1.Service {
+	name := v.Name + "-service"
+	path := fmt.Sprintf("/%s/%s/", v.Spec.Type, v.Name)
+	mapping := fmt.Sprintf(mappingTpl, v.Name, path, path, name)
+
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        name,
+			Namespace:   v.Namespace,
+			Annotations: map[string]string{"getambassador.io/config": mapping},
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				"deployment": deploymentName,
+			},
+			Ports: []corev1.ServicePort{
+				corev1.ServicePort{
+					Protocol:   corev1.ProtocolTCP,
+					Port:       80,
+					TargetPort: intstr.IntOrString{IntVal: viewerTargetPort}},
+			},
+		},
+	}
+}
