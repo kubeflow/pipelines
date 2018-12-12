3b8b360134944cdf5120b0350e1ff67b1a48efd8
Hongye Sun <joccyeye@gmail.com>
2018-12-12 15:57:35 -0800

    Add args for build gpu image for dnntrainer and update build scripts

--- components/kubeflow/dnntrainer/build_image.sh
+++ components/kubeflow/dnntrainer/build_image.sh
@@ -13,11 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-while getopts ":hp:t:i:" opt; do
+while getopts ":hp:t:i:m:" opt; do
   case "${opt}" in
     h) echo "-p: project name"
         echo "-t: tag name"
         echo "-i: image name. If provided, project name and tag name are not necessary"
+        echo "-m: mode. Optional. The value can be 'cpu' or 'gpu'. Defaults to 'cpu'."
         exit
       ;;
     p) PROJECT_ID=${OPTARG}
@@ -26,12 +27,19 @@ while getopts ":hp:t:i:" opt; do
       ;;
     i) IMAGE_NAME=${OPTARG}
       ;;
-    \? ) echo "Usage: cmd [-p] project [-t] tag [-i] image"
+    m) MODE=${OPTARG}
+      if [ "${MODE}" != "cpu" ] && [ "${MODE}" != "gpu" ]; then
+        echo "mode value can only be 'cpu' or 'gpu'."
+        exit
+      fi
+      ;;
+    \? ) echo "Usage: cmd [-p] project [-t] tag [-i] image [-m] mode"
       exit
       ;;
   esac
 done
 
+set -x
 LOCAL_IMAGE_NAME=ml-pipeline-kubeflow-tf-trainer
 
 if [ -z "${PROJECT_ID}" ]; then
@@ -42,12 +50,18 @@ if [ -z "${TAG_NAME}" ]; then
   TAG_NAME=$(date +v%Y%m%d)-$(git describe --tags --always --dirty)-$(git diff | shasum -a256 | cut -c -6)
 fi
 
+TF_VERSION=1.6.0
+if [ "${MODE}" = "gpu" ]; then
+  TF_VERSION="${TF_VERSION}-gpu"
+  LOCAL_IMAGE_NAME="${LOCAL_IMAGE_NAME}-gpu"
+fi
+
 mkdir -p ./build
 rsync -arvp ./src/ ./build/
 cp ../../license.sh ./build
 cp ../../third_party_licenses.csv ./build
 
-docker build -t ${LOCAL_IMAGE_NAME} -f ./Dockerfile.gpu .
+docker build --build-arg TF_VERSION=${TF_VERSION} -t ${LOCAL_IMAGE_NAME} .
 if [ -z "${IMAGE_NAME}" ]; then
   docker tag ${LOCAL_IMAGE_NAME} gcr.io/${PROJECT_ID}/${LOCAL_IMAGE_NAME}:${TAG_NAME}
   docker push gcr.io/${PROJECT_ID}/${LOCAL_IMAGE_NAME}:${TAG_NAME}
