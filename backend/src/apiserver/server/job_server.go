ced451ebdf06b6a6ff188a60f9c8b3d281b7d811
Ajay Gopinathan <ajaygopinathan@google.com>
2018-12-19 09:56:39 -0800

    refactor filter parsing to parseAPIFilter and add tests

--- backend/src/apiserver/server/job_server.go
+++ backend/src/apiserver/server/job_server.go
@@ -16,9 +16,6 @@ package server
 
 import (
 	"context"
-	"encoding/base64"
-
-	"github.com/golang/protobuf/jsonpb"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	api "github.com/kubeflow/pipelines/backend/api/go_client"
@@ -59,20 +56,9 @@ func (s *JobServer) ListJobs(ctx context.Context, request *api.ListJobsRequest) 
 	if request.PageToken != "" {
 		opts, err = list.NewOptionsFromToken(request.PageToken, int(request.PageSize))
 	} else {
-		var f *api.Filter
-		if request.Filter != "" {
-			errorF := func(err error) (*api.ListJobsResponse, error) {
-				return nil, util.NewInvalidInputError("failed to parse valid filter from %q: %v", request.Filter, err)
-			}
-			b, err := base64.StdEncoding.DecodeString(request.Filter)
-			if err != nil {
-				return errorF(err)
-			}
-
-			f = &api.Filter{}
-			if err := jsonpb.UnmarshalString(string(b), f); err != nil {
-				return errorF(err)
-			}
+		f, err := parseAPIFilter(request.Filter)
+		if err != nil {
+			return nil, err
 		}
 
 		opts, err = list.NewOptions(&model.Job{}, int(request.PageSize), request.SortBy, f)
