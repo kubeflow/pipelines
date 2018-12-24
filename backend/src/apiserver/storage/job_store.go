43654064687feb3338c4c3762678fa9a1885f580
Yasser Elsayed <yasser.ebrahim@gmail.com>
2018-12-23 23:49:11 -0800

    job store fix and tests

--- backend/src/apiserver/storage/job_store.go
+++ backend/src/apiserver/storage/job_store.go
@@ -26,7 +26,7 @@ import (
 )
 
 type JobStoreInterface interface {
-	ListJobs(filterContext *common.FilterContext, opts *list.Options) ([]*model.Job, string, error)
+	ListJobs(filterContext *common.FilterContext, opts *list.Options) ([]*model.Job, int, string, error)
 	GetJob(id string) (*model.Job, error)
 	CreateJob(*model.Job) (*model.Job, error)
 	DeleteJob(id string) error
@@ -41,40 +41,73 @@ type JobStore struct {
 }
 
 func (s *JobStore) ListJobs(
-	filterContext *common.FilterContext, opts *list.Options) ([]*model.Job, string, error) {
-	errorF := func(err error) ([]*model.Job, string, error) {
-		return nil, "", util.NewInternalServerError(err, "Failed to list jobs: %v", err)
+	filterContext *common.FilterContext, opts *list.Options) ([]*model.Job, int, string, error) {
+	errorF := func(err error) ([]*model.Job, int, string, error) {
+		return nil, 0, "", util.NewInternalServerError(err, "Failed to list jobs: %v", err)
 	}
 
-	sqlBuilder := s.selectJob()
 	// Add filter condition
-	sqlBuilder, err := s.toFilteredQuery(sqlBuilder, filterContext)
+	sqlBuilder, err := s.toFilteredQuery(s.selectJob(), filterContext)
 	if err != nil {
 		return errorF(err)
 	}
 
-	sql, args, err := opts.AddPaginationToSelect(sqlBuilder).ToSql()
+	// SQL for row count
+	countSql, countArgs, err := sq.Select("count(*)").FromSelect(sqlBuilder, "rows").ToSql()
 	if err != nil {
 		return errorF(err)
 	}
 
-	rows, err := s.db.Query(sql, args...)
+	// SQL for row list
+	rowsSql, rowsArgs, err := opts.AddPaginationToSelect(sqlBuilder).ToSql()
 	if err != nil {
+		return errorF(err)
+	}
+
+	// Use a transaction to make sure we're returning the count of the same rows queried
+	tx, err := s.db.Begin()
+	if err != nil {
+		return errorF(err)
+	}
+
+	rows, err := tx.Query(rowsSql, rowsArgs...)
+	if err != nil {
+		tx.Rollback()
 		return errorF(err)
 	}
 	defer rows.Close()
 
+	countRow, err := tx.Query(countSql, countArgs...)
+	if err != nil {
+		tx.Rollback()
+		return errorF(err)
+	}
+	defer countRow.Close()
+
 	jobs, err := s.scanRows(rows)
 	if err != nil {
+		tx.Rollback()
+		return errorF(err)
+	}
+
+	count, err := s.scanRowToCount(countRow)
+	if err != nil {
+		tx.Rollback()
+		return errorF(err)
+	}
+
+	tx.Commit()
+	if err != nil {
+		tx.Rollback()
 		return errorF(err)
 	}
 
 	if len(jobs) <= opts.PageSize {
-		return jobs, "", nil
+		return jobs, count, "", nil
 	}
 
 	npt, err := opts.NextPageToken(jobs[opts.PageSize])
-	return jobs[:opts.PageSize], npt, err
+	return jobs[:opts.PageSize], count, npt, err
 }
 
 func (s *JobStore) toFilteredQuery(selectBuilder sq.SelectBuilder, filterContext *common.FilterContext) (sq.SelectBuilder, error) {
@@ -127,6 +160,16 @@ func (s *JobStore) selectJob() sq.SelectBuilder {
 		// Append all the resource references for the run as a json column
 		LeftJoin("(select * from resource_references where ResourceType='Job') AS r ON jobs.UUID=r.ResourceUUID").
 		GroupBy("jobs.UUID")
+}
+
+func (s *JobStore) scanRowToCount(rows *sql.Rows) (int, error) {
+	var count int
+	rows.Next()
+	err := rows.Scan(&count)
+	if err != nil {
+		return 0, util.NewInternalServerError(err, "Failed to scan row count")
+	}
+	return count, nil
 }
 
 func (s *JobStore) scanRows(r *sql.Rows) ([]*model.Job, error) {
