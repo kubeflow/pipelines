83907e5693cbbb14fce6cf1cd9b66f40bd2f2b29
Ning Gao <ngao@google.com>
2019-01-03 11:32:52 -0800

    disable gpu

--- samples/kubeflow-tf/kubeflow-training-classification.py
+++ samples/kubeflow-tf/kubeflow-training-classification.py
@@ -102,7 +102,7 @@ def kubeflow_training(output, project,
   # TODO: use the argo job name as the workflow
   workflow = '{{workflow.name}}'
   # set the flag to use GPU trainer
-  use_gpu = True
+  use_gpu = False
 
   preprocess = dataflow_tf_transform_op(train, evaluation, schema, project, preprocess_mode, '', '%s/%s/transformed' % (output, workflow)).apply(gcp.use_gcp_secret('user-gcp-sa'))
   training = kubeflow_tf_training_op(preprocess.output, schema, learning_rate, hidden_layer_size, steps, target, '', '%s/%s/train' % (output, workflow), use_gpu=use_gpu).apply(gcp.use_gcp_secret('user-gcp-sa'))
