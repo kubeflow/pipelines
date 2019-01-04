d4d4f62056dbda4886eb85100125764607126a3a
Yasser Elsayed <yebrahim@users.noreply.github.com>
2019-01-04 11:54:41 -0800

    Compare perf - pure components, disable ROC curve thumbnail animations (#598)
    * pure components, no animations for roc curve thumbnails
    
    * fix snapshot tests
    
    * set state once when loading parameters
    
    * manually check plot card pros/state to make it pure

--- frontend/src/pages/Compare.tsx
+++ frontend/src/pages/Compare.tsx
@@ -117,7 +117,7 @@ class Compare extends Page<{}, CompareState> {
 
       {/* Overview section */}
       <CollapseButton sectionName={overviewSectionName} collapseSections={collapseSections}
-        compareSetState={this.setStateSafe.bind(this)}/>
+        compareSetState={this.setStateSafe.bind(this)} />
       {!collapseSections[overviewSectionName] && (
         <div className={commonCss.noShrink}>
           <RunList onError={this.showPageError.bind(this)} {...this.props}
@@ -130,7 +130,7 @@ class Compare extends Page<{}, CompareState> {
 
       {/* Parameters section */}
       <CollapseButton sectionName={paramsSectionName} collapseSections={collapseSections}
-        compareSetState={this.setStateSafe.bind(this)}/>
+        compareSetState={this.setStateSafe.bind(this)} />
       {!collapseSections[paramsSectionName] && (
         <div className={classes(commonCss.noShrink, css.outputsRow)}>
           <Separator orientation='vertical' />
@@ -142,36 +142,38 @@ class Compare extends Page<{}, CompareState> {
       <Separator orientation='vertical' />
 
       {Array.from(viewersMap.keys()).map((viewerType, i) =>
-        <div key={i}>
-          <CollapseButton collapseSections={collapseSections}
-            compareSetState={this.setStateSafe.bind(this)}
-            sectionName={componentMap[viewerType].prototype.getDisplayName()} />
-          {!collapseSections[componentMap[viewerType].prototype.getDisplayName()] && (
-            <React.Fragment>
-              <div className={classes(commonCss.flex, css.outputsRow)}>
-                {/* If the component allows aggregation, add one more card for
+        !!runsPerViewerType(viewerType).length && (
+          <div key={i}>
+            <CollapseButton collapseSections={collapseSections}
+              compareSetState={this.setStateSafe.bind(this)}
+              sectionName={componentMap[viewerType].prototype.getDisplayName()} />
+            {!collapseSections[componentMap[viewerType].prototype.getDisplayName()] && (
+              <React.Fragment>
+                <div className={classes(commonCss.flex, css.outputsRow)}>
+                  {/* If the component allows aggregation, add one more card for
                 its aggregated view. Only do this if there is more than one
                 output, filtering out any unselected runs. */}
-                {(componentMap[viewerType].prototype.isAggregatable() && (
-                  runsPerViewerType(viewerType).length > 1) && (
-                    <PlotCard configs={
-                      runsPerViewerType(viewerType).map(t => t.config)} maxDimension={400}
-                      title='Aggregated view' />
-                  )
-                )}
+                  {(componentMap[viewerType].prototype.isAggregatable() && (
+                    runsPerViewerType(viewerType).length > 1) && (
+                      <PlotCard configs={
+                        runsPerViewerType(viewerType).map(t => t.config)} maxDimension={400}
+                        title='Aggregated view' />
+                    )
+                  )}
 
-                {runsPerViewerType(viewerType).map((taggedConfig, c) => (
-                  <PlotCard key={c} configs={[taggedConfig.config]} title={taggedConfig.runName}
-                    maxDimension={400} />
-                ))}
-                <Separator />
+                  {runsPerViewerType(viewerType).map((taggedConfig, c) => (
+                    <PlotCard key={c} configs={[taggedConfig.config]} title={taggedConfig.runName}
+                      maxDimension={400} />
+                  ))}
+                  <Separator />
 
-              </div>
-              <Hr />
-            </React.Fragment>
-          )}
-          <Separator orientation='vertical' />
-        </div>
+                </div>
+                <Hr />
+              </React.Fragment>
+            )}
+            <Separator orientation='vertical' />
+          </div>
+        )
       )}
     </div>);
   }
@@ -212,11 +214,13 @@ class Compare extends Page<{}, CompareState> {
       return;
     }
 
+    const selectedIds = runs.map(r => r.run!.id!);
     this.setStateSafe({
-      runs,	
-      selectedIds: runs.map(r => r.run!.id!),	
-      workflowObjects,	
-    }, () => this._loadParameters());
+      runs,
+      selectedIds,
+      workflowObjects,
+    });
+    this._loadParameters(selectedIds);
 
     const outputPathsList = workflowObjects.map(
       workflow => WorkflowParser.loadAllOutputPaths(workflow));
@@ -245,7 +249,8 @@ class Compare extends Page<{}, CompareState> {
   }
 
   protected _selectionChanged(selectedIds: string[]): void {
-    this.setState({ selectedIds }, () => this._loadParameters());
+    this.setState({ selectedIds });
+    this._loadParameters(selectedIds);
   }
 
   private _collapseAllSections(): void {
@@ -257,8 +262,8 @@ class Compare extends Page<{}, CompareState> {
     this.setState({ collapseSections });
   }
 
-  private _loadParameters(): void {
-    const { runs, selectedIds, workflowObjects } = this.state;
+  private _loadParameters(selectedIds: string[]): void {
+    const { runs, workflowObjects } = this.state;
 
     const selectedIndices = selectedIds.map(id => runs.findIndex(r => r.run!.id === id));
     const filteredRuns = runs.filter((_, i) => selectedIndices.indexOf(i) > -1);
