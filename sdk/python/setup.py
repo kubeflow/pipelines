60cb8c1c1d1f555be615c7f1b86e0044696d4cd6
Tim Zaman <timbobel@gmail.com>
2019-01-04 16:38:19 -0800

    Update setup.py

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -47,6 +47,7 @@ setup(
       'Intended Audience :: Science/Research',
       'License :: OSI Approved :: Apache Software License',
       'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.5',
       'Programming Language :: Python :: 3.6',
       'Programming Language :: Python :: 3.7',
       'Topic :: Scientific/Engineering',
