163545b370381408d72bbba176f590ea41ed0670
Ajay Gopinathan <ajay@gopinathan.net>
2019-01-04 17:17:20 -0800

    Use Bazel to build the entire backend and perform API code generation (#609)
    * Use Bazel to build the entire backend.
    
    This also uses Bazel to generate code from the API definition in the
    proto files.
    
    The Makefile is replaced with a script that uses Bazel to first generate
    the code, and then copy them back into the source tree.
    
    Most of the BUILD files were generated automatically using Gazelle.
    
    * Fix indentation in generate_api.sh
    
    * Clean up WORKSPACE
    
    * Add README for building/testing backend.
    
    Also fix the missing licenses in the generated proto files.
    
    * Add license to files under go_http_client

--- backend/api/go_http_client/job_client/job_service/enable_job_responses.go
+++ backend/api/go_http_client/job_client/job_service/enable_job_responses.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Google LLC
+// Copyright 2019 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -68,7 +68,7 @@ func NewEnableJobOK() *EnableJobOK {
 A successful response.
 */
 type EnableJobOK struct {
-	Payload job_model.ProtobufEmpty
+	Payload interface{}
 }
 
 func (o *EnableJobOK) Error() string {
