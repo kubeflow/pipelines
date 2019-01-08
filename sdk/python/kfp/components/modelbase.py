d86ec4fbc23e6b8021e1665e795db470fe99991f
Alexey Volkov <avolkov@google.com>
2019-01-08 11:23:42 -0800

    Added documentation for ModelBase class and functions.

--- sdk/python/kfp/components/modelbase.py
+++ sdk/python/kfp/components/modelbase.py
@@ -16,7 +16,6 @@ __all__ = [
     'ModelBase',
 ]
 
-import copy
 from collections import abc, OrderedDict
 from typing import Any, Callable, Dict, List, Mapping, MutableMapping, MutableSequence, Optional, Sequence, Tuple, Type, TypeVar, Union, cast, get_type_hints
 
@@ -24,7 +23,9 @@ from typing import Any, Callable, Dict, List, Mapping, MutableMapping, MutableSe
 T = TypeVar('T')
 
 
-def check_type(typ: Type[T], x: Any) -> T:
+def verify_object_against_type(x: Any, typ: Type[T]) -> T:
+    '''Verifies that the object is compatible to the specified type (types from the typing package can be used).'''
+    #TODO: Merge with parse_object_from_struct_based_on_type which has almost the same code
     if typ is type(None):
         if x is None:
             return x
@@ -48,7 +49,7 @@ def check_type(typ: Type[T], x: Any) -> T:
                 return x
             for possible_type in possible_types:
                 try:
-                    check_type(possible_type, x)
+                    verify_object_against_type(x, possible_type)
                     return x
                 except Exception as ex:
                     exception_map[possible_type] = ex
@@ -70,7 +71,7 @@ def check_type(typ: Type[T], x: Any) -> T:
             type_args = typ.__args__ if typ.__args__ is not None else (Any, Any) #Workaround for Python <3.7 (where Mapping.__args__ is None)
             inner_type = type_args[0]
             for item in x:
-                check_type(inner_type, item)
+                verify_object_against_type(item, inner_type)
             return x
 
         elif generic_type in [dict, Dict, abc.Mapping, abc.MutableMapping, Mapping, MutableMapping, OrderedDict]:
@@ -80,8 +81,8 @@ def check_type(typ: Type[T], x: Any) -> T:
             inner_key_type = type_args[0]
             inner_value_type = type_args[1]
             for k, v in x.items():
-                check_type(inner_key_type, k)
-                check_type(inner_value_type, v)
+                verify_object_against_type(k, inner_key_type)
+                verify_object_against_type(v, inner_value_type)
             return x
 
         else:
@@ -90,7 +91,10 @@ def check_type(typ: Type[T], x: Any) -> T:
     raise TypeError('Error: Object "{}" is incompatible with type "{}"'.format(x, typ))
 
 
-def parse_typed_object_from_struct(typ: Type[T], struct: Any) -> T:
+def parse_object_from_struct_based_on_type(struct: Any, typ: Type[T]) -> T:
+    '''Constructs an object from structure (usually dict) based on type. Supports list and dict types from the typing package plus Optional[] and Union[] types.
+    If some type is a class that has .from_struct class method, that method is used for object construction.
+    '''
     if typ is type(None):
         if struct is None:
             return None
@@ -125,11 +129,9 @@ def parse_typed_object_from_struct(typ: Type[T], struct: Any) -> T:
             #Hack for Python <3.7 which for some reason "simplifies" Union[bool, int, ...] to just Union[int, ...]
             if int in possible_types:
                 possible_types = possible_types + [bool]
-            if struct == True:
-                exception_map = exception_map
             for possible_type in possible_types:
                 try:
-                    obj = parse_typed_object_from_struct(possible_type, struct)
+                    obj = parse_object_from_struct_based_on_type(struct, possible_type)
                     results[possible_type] = obj
                 except Exception as ex:
                     exception_map[possible_type] = ex
@@ -156,7 +158,7 @@ def parse_typed_object_from_struct(typ: Type[T], struct: Any) -> T:
                 raise TypeError('Error: Structure "{}" is incompatible with type "{}" - it does not have list type.'.format(struct, typ))
             type_args = typ.__args__ if typ.__args__ is not None else (Any, Any) #Workaround for Python <3.7 (where Mapping.__args__ is None)
             inner_type = type_args[0]
-            return [parse_typed_object_from_struct(inner_type, item) for item in struct]
+            return [parse_object_from_struct_based_on_type(item, inner_type) for item in struct]
 
         elif generic_type in [dict, Dict, abc.Mapping, abc.MutableMapping, Mapping, MutableMapping, OrderedDict]: #in Python <3.7 there is a difference between abc.Mapping and typing.Mapping
             if not isinstance(struct, generic_type):
@@ -164,7 +166,7 @@ def parse_typed_object_from_struct(typ: Type[T], struct: Any) -> T:
             type_args = typ.__args__ if typ.__args__ is not None else (Any, Any) #Workaround for Python <3.7 (where Mapping.__args__ is None)
             inner_key_type = type_args[0]
             inner_value_type = type_args[1]
-            return {parse_typed_object_from_struct(inner_key_type, k): parse_typed_object_from_struct(inner_value_type, v) for k, v in struct.items()}
+            return {parse_object_from_struct_based_on_type(k, inner_key_type): parse_object_from_struct_based_on_type(v, inner_value_type) for k, v in struct.items()}
 
         else:
             raise TypeError('Error: Unsupported generic type "{}". type.__origin__ or type.__extra__ == "{}"'.format(typ, generic_type))
@@ -172,17 +174,19 @@ def parse_typed_object_from_struct(typ: Type[T], struct: Any) -> T:
     raise TypeError('Error: Structure "{}" is incompatible with type "{}". Structure is not the instance of the type, the type does not have .from_struct method and is not generic.'.format(struct, typ))
 
 
-def model_to_struct(self, original_names: Mapping[str, str] = {}):
-    """
-    Returns the model properties as a dict
-    """
+def convert_object_to_struct(obj, serialized_names: Mapping[str, str] = {}):
+    '''Converts an object to structure (usually a dict).
+    Serializes all properties that do not start with underscores.
+    If the type of some property is a class that has .to_struct class method, that method is used for conversion.
+    Used by the ModelBase class.
+    '''
     import inspect
-    signature = inspect.signature(self.__init__) #Needed for default values
+    signature = inspect.signature(obj.__init__) #Needed for default values
     result = {}
-    for python_name, value in self.__dict__.items():
+    for python_name, value in obj.__dict__.items(): #TODO: Should we take the fields from the constructor parameters instead? #TODO: Make it possible to specify the field ordering
         if python_name.startswith('_'):
             continue
-        attr_name = original_names.get(python_name, python_name)
+        attr_name = serialized_names.get(python_name, python_name)
         if hasattr(value, "to_struct"):
             result[attr_name] = value.to_struct()
         elif isinstance(value, list):
@@ -197,23 +201,28 @@ def model_to_struct(self, original_names: Mapping[str, str] = {}):
     return result
 
 
-def model_from_struct(cls : Type[T], struct: Mapping, original_names: Mapping[str, str] = {}) -> T:
-    """
-    Parses the model object from a dict
-    """
+def parse_object_from_struct_based_on_class_init(cls : Type[T], struct: Mapping, serialized_names: Mapping[str, str] = {}) -> T:
+    '''Constructs an object of specified class from structure (usually dict) using the class.__init__ method.
+    Converts all constructor arguments to appropriate types based on the __init__ type hints.
+    Used by the ModelBase class.
+
+    Arguments:
+
+    serialized_names: specifies the mapping between __init__ parameter names and the structure key names for cases where these names are different (due to language syntax clashes or style differences).
+    '''
     parameter_types = get_type_hints(cls.__init__) #Properlty resolves forward references
 
-    original_names_to_pythonic = {v: k for k, v in original_names.items()}
+    serialized_names_to_pythonic = {v: k for k, v in serialized_names.items()}
     #If a pythonic name has a different original name, we forbid the pythonic name in the structure. Otherwise, this function would accept "python-styled" structures that should be invalid
-    forbidden_struct_keys = set(original_names_to_pythonic.values()).difference(original_names_to_pythonic.keys())
+    forbidden_struct_keys = set(serialized_names_to_pythonic.values()).difference(serialized_names_to_pythonic.keys())
     args = {}
     for original_name, value in struct.items():
         if original_name in forbidden_struct_keys:
-            raise ValueError('Use "{}" key instead of pythonic key "{}" in the structure: {}.'.format(original_names[original_name], original_name, struct))
-        python_name = original_names_to_pythonic.get(original_name, original_name)
+            raise ValueError('Use "{}" key instead of pythonic key "{}" in the structure: {}.'.format(serialized_names[original_name], original_name, struct))
+        python_name = serialized_names_to_pythonic.get(original_name, original_name)
         param_type = parameter_types.get(python_name, None)
         if param_type is not None:
-            args[python_name] = parse_typed_object_from_struct(param_type, value)
+            args[python_name] = parse_object_from_struct_based_on_type(value, param_type)
         else:
             args[python_name] = value
 
@@ -221,6 +230,30 @@ def model_from_struct(cls : Type[T], struct: Mapping, original_names: Mapping[st
 
 
 class ModelBase:
+    '''Base class for types that can be converted to JSON-like dict structures or constructed from such structures.
+    The object fields, their types and default values are taken from the __init__ method arguments.
+    Override the _serialized_names mapping to control the key names of the serialized structures.
+
+    The derived class objects will have the .from_struct and .to_struct methods for conversion to or from structure. The base class constructor accepts the arguments map, checks the argument types and sets the object field values.
+
+    Example derived class:
+
+    class TaskSpec(ModelBase):
+        _serialized_names = {
+            'component_ref': 'componentRef',
+            'is_enabled': 'isEnabled',
+        }
+
+        def __init__(self,
+            component_ref: ComponentReference,
+            arguments: Optional[Mapping[str, ArgumentType]] = None,
+            is_enabled: Optional[Union[ArgumentType, EqualsPredicate, NotEqualsPredicate]] = None, #Optional property with default value
+        ):
+            super().__init__(locals()) #Calling the ModelBase constructor to check the argument types and set the object field values. 
+    
+    task_spec = TaskSpec.from_struct("{'componentRef': {...}, 'isEnabled: {'and': {...}}}") # = instance of TaskSpec
+    task_struct = task_spec.to_struct() #= "{'componentRef': {...}, 'isEnabled: {'and': {...}}}"
+    '''
     _serialized_names = {}
     def __init__(self, args):
         parameter_types = get_type_hints(self.__class__.__init__)
@@ -228,15 +261,15 @@ class ModelBase:
         for k, v in field_values.items():
             parameter_type = parameter_types.get(k, None)
             if parameter_type is not None:
-                check_type(parameter_type, v)
+                verify_object_against_type(v, parameter_type)
         self.__dict__.update(field_values)
 
     @classmethod
     def from_struct(cls: Type[T], struct: Mapping) -> T:
-        return model_from_struct(cls, struct, original_names=cls._serialized_names)
+        return parse_object_from_struct_based_on_class_init(cls, struct, serialized_names=cls._serialized_names)
 
     def to_struct(self) -> Mapping:
-        return model_to_struct(self, original_names=self._serialized_names)
+        return convert_object_to_struct(self, serialized_names=self._serialized_names)
 
     def __repr__(self):
         return self.__class__.__name__ + '.from_struct(' + str(self.to_struct()) + ')'
