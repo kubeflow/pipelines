b01607a09e0831ee4e806489a0d61ab5300e8b95
Alexey Volkov <alexey.volkov@ark-kun.com>
2019-01-20 14:10:17 -0800

    SDK/Components - Only create ContainerOp in dsl.Pipeline context

--- sdk/python/kfp/components/_components.py
+++ sdk/python/kfp/components/_components.py
@@ -154,11 +154,6 @@ def _try_get_object_by_name(obj_name):
 _created_task_transformation_handler = []
 
 
-#TODO: Move to the dsl.Pipeline context class
-from . import _dsl_bridge
-_created_task_transformation_handler.append(_dsl_bridge.create_container_op_from_task)
-
-
 #TODO: Refactor the function to make it shorter
 def _create_task_factory_from_component_spec(component_spec:ComponentSpec, component_filename=None, component_ref: ComponentReference = None):
     name = component_spec.name or _default_component_name
