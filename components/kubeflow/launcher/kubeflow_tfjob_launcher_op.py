028685edc1d4214f1597775868ba8945dde9cdd6
Ning <gaoning777@gmail.com>
2019-01-28 17:02:24 -0800

    Releasing components fixing xgboost (#747)
    * try remove the deployer cloud build
    
    * add the deployer back
    
    * releasing the components containing the xgboost fix

--- components/kubeflow/launcher/kubeflow_tfjob_launcher_op.py
+++ components/kubeflow/launcher/kubeflow_tfjob_launcher_op.py
@@ -17,7 +17,7 @@ from kfp import dsl
 def kubeflow_tfjob_launcher_op(container_image, command, number_of_workers: int, number_of_parameter_servers: int, tfjob_timeout_minutes: int, output_dir=None, step_name='TFJob-launcher'):
     return dsl.ContainerOp(
         name = step_name,
-        image = 'gcr.io/ml-pipeline/ml-pipeline-kubeflow-tf:2ed60100d1db9efeb38c6c358f90b21c144179be',
+        image = 'gcr.io/ml-pipeline/ml-pipeline-kubeflow-tf:be19cbc2591a48d2ef5ca715c34ecae8223cf454',
         arguments = [
             '--workers', number_of_workers,
             '--pss', number_of_parameter_servers,
