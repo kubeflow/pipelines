9c0085994e3416bac8f69b34e56262779d740765
Hongye Sun <joccyeye@gmail.com>
2019-02-05 16:01:20 -0800

    Revert "Revert "Add gpu pool to test deployment and enable gpu in sample test (#696)""
    This reverts commit 4c2c52bea3d50b11a2c01f06980bd9ffce97612c.

--- test/sample-test/run_test.sh
+++ test/sample-test/run_test.sh
@@ -152,6 +152,7 @@ if [ "$TEST_NAME" == 'tf-training' ]; then
     sed -i -e "s|gcr.io/ml-pipeline/ml-pipeline-dataflow-tf-predict:\([a-zA-Z0-9_.-]\)\+|${DATAFLOW_PREDICT_IMAGE}|g" kubeflow-training-classification.py
     sed -i -e "s|gcr.io/ml-pipeline/ml-pipeline-local-confusion-matrix:\([a-zA-Z0-9_.-]\)\+|${LOCAL_CONFUSIONMATRIX_IMAGE}|g" kubeflow-training-classification.py
   fi
+  sed -i -e "s|use_gpu\s*=\s*False|use_gpu = True|g" kubeflow-training-classification.py
 
   dsl-compile --py kubeflow-training-classification.py --output kubeflow-training-classification.tar.gz
 
