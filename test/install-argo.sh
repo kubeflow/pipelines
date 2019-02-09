749badee01f0c9059e841d2047d949f7172f8a23
IronPan <yangpa@google.com>
2019-02-08 22:39:18 -0800

    fix

--- test/install-argo.sh
+++ test/install-argo.sh
@@ -13,21 +13,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-#kubectl config set-context $(kubectl config current-context) --namespace=default
-#echo "Add necessary cluster role bindings"
-#ACCOUNT=$(gcloud info --format='value(config.account)')
-#kubectl create clusterrolebinding PROW_BINDING --clusterrole=cluster-admin --user=$ACCOUNT
-#kubectl create clusterrolebinding DEFAULT_BINDING --clusterrole=cluster-admin --serviceaccount=default:default
-#
-#echo "install argo"
-#ARGO_VERSION=v2.2.0
-#mkdir -p ~/bin/
-#export PATH=~/bin/:$PATH
-#curl -sSL -o ~/bin/argo https://github.com/argoproj/argo/releases/download/$ARGO_VERSION/argo-linux-amd64
-#chmod +x ~/bin/argo
-#kubectl create ns argo
-#kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/$ARGO_VERSION/manifests/install.yaml
+
+kubectl config set-context $(kubectl config current-context) --namespace=default
+echo "Add necessary cluster role bindings"
+ACCOUNT=$(gcloud info --format='value(config.account)')
+kubectl create clusterrolebinding PROW_BINDING --clusterrole=cluster-admin --user=$ACCOUNT
+kubectl create clusterrolebinding DEFAULT_BINDING --clusterrole=cluster-admin --serviceaccount=default:default
+
+echo "install argo"
+ARGO_VERSION=v2.2.0
+mkdir -p ~/bin/
+export PATH=~/bin/:$PATH
+curl -sSL -o ~/bin/argo https://github.com/argoproj/argo/releases/download/$ARGO_VERSION/argo-linux-amd64
+chmod +x ~/bin/argo
+kubectl create ns argo
+kubectl apply -n argo -f https://raw.githubusercontent.com/argoproj/argo/$ARGO_VERSION/manifests/install.yaml
 
 # Some workflows are deployed to the non-default namespace where the GCP credential secret is stored
 # In this case, the default service account in that namespace doesn't have enough permission
