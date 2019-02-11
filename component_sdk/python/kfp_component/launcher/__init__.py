dccc7792b588c8b684cfd5af0af18aacb7b94ed1
hongye-sun <43763191+hongye-sun@users.noreply.github.com>
2019-02-11 12:38:17 -0800

    Add Launcher module in component SDK (#769)
    * Add launcher module
    
    * Add argparse to wrap file launcher to avoid dup output prints.
    
    * Fix PR comments
    
    * fix test issue

--- component_sdk/python/kfp_component/launcher/__init__.py
+++ component_sdk/python/kfp_component/launcher/__init__.py
@@ -12,4 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import launcher, core
+"""Entrypoint module to launch python module or file dynamically.
+
+This module makes it easier to build kfp component with python code 
+by defining a dynamic entrypoint and generate command line arg parser
+by python-fire module. It can be used as an entrypoint in the 
+container spec to run arbitary python module or code in the local 
+image.
+"""
+
+from .launcher import launch
--- component_sdk/python/kfp_component/launcher/__main__.py
+++ component_sdk/python/kfp_component/launcher/__main__.py
@@ -12,4 +12,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import launcher, core
+import argparse
+import fire
+import importlib
+import sys
+import logging
+from .launcher import launch
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(
+        prog='launcher',
+        description='Launch a python module or file.')
+    parser.add_argument('file_or_module', type=str,
+        help='Either a python file path or a module name.')
+    parser.add_argument('args', nargs=argparse.REMAINDER)
+    args = parser.parse_args()
+    launch(args.file_or_module, args.args)
+
+if __name__ == '__main__':
+    main()
