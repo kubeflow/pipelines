db51acaad4d17580dd9a0badeecce4bd89de2d50
rileyjbauer <rjbauer@google.com>
2019-02-14 16:31:55 -0800

    Significant clean up and updating tests

--- frontend/src/components/Graph.tsx
+++ frontend/src/components/Graph.tsx
@@ -17,24 +17,24 @@
 import * as dagre from 'dagre';
 import * as React from 'react';
 import { classes, stylesheet } from 'typestyle';
-import { fontsize, color } from '../Css';
+import { fontsize, color, fonts } from '../Css';
 
 interface Segment {
   angle: number;
-  distance: number;
-  finalY: number;
+  // finalX and finalY are used for placing the arrowheads
   finalX: number;
-  height?: number;
+  finalY: number;
   left: number;
+  length: number;
   top: number;
 }
 
 interface Edge {
   color?: string;
-  from: string;
-  to: string;
-  segments: Segment[];
   isPlaceholder?: boolean;
+  from: string;
+  segments: Segment[];
+  to: string;
 }
 
 const css = stylesheet({
@@ -50,16 +50,16 @@ const css = stylesheet({
     zIndex: 2,
   },
   icon: {
-    borderRadius: '0px 2px 2px 0px',
     padding: '5px 7px 0px 7px',
   },
   label: {
+    color: color.strong,
     flexGrow: 1,
-    fontSize: 15,
-    lineHeight: '2em',
-    margin: 'auto',
+    fontFamily: fonts.secondary,
+    fontSize: 13,
+    lineHeight: '16px',
+    margin: 10,
     overflow: 'hidden',
-    paddingLeft: 15,
     textOverflow: 'ellipsis',
     whiteSpace: 'nowrap',
   },
@@ -91,7 +91,7 @@ const css = stylesheet({
     margin: 10,
     position: 'absolute',
     // TODO: can this be calculated?
-    transform: 'translate(73px, 16px)'
+    transform: 'translate(71px, 14px)'
   },
   root: {
     backgroundColor: color.graphBg,
@@ -123,7 +123,7 @@ interface GraphState {
 
 export default class Graph extends React.Component<GraphProps, GraphState> {
   private LEFT_OFFSET = 100;
-  private TOP_OFFSET = 42;
+  private TOP_OFFSET = 44;
   private EDGE_THICKNESS = 2;
   private EDGE_X_BUFFER = 30;
 
@@ -135,14 +135,15 @@ export default class Graph extends React.Component<GraphProps, GraphState> {
 
   public render(): JSX.Element | null {
     const { graph } = this.props;
-    const displayEdges: Edge[] = [];
 
     if (!graph.nodes().length) {
       return null;
     }
-    dagre.layout(graph);
 
-    // Creates the lines that constitute the edges connecting the graph
+    dagre.layout(graph);
+    const displayEdges: Edge[] = [];
+    
+    // Creates the lines that constitute the edges connecting the graph.
     graph.edges().forEach((edgeInfo) => {
       const edge = graph.edge(edgeInfo);
       const segments: Segment[] = [];
@@ -150,81 +151,53 @@ export default class Graph extends React.Component<GraphProps, GraphState> {
       if (edge.points.length > 1) {
         for (let i = 1; i < edge.points.length; i++) {
 
-          let x1 = edge.points[i - 1].x;
-          let y1 = edge.points[i - 1].y;
+          let xStart = edge.points[i - 1].x;
+          let yStart = edge.points[i - 1].y;
 
-          // Make all edges start at the bottom center of the node
+          // Adjustments made to first segment for each edge.
           if (i === 1) {
             const sourceNode = graph.node(edgeInfo.v);
-            // If first segment's x value is too far to the right, move it EDGE_X_BUFFER pixels in
-            // from the right end of the destination node
-            const rightmostAcceptableXPos =
-              sourceNode.x + sourceNode.width - this.LEFT_OFFSET - this.EDGE_X_BUFFER;
-            if (rightmostAcceptableXPos <= x1) {
-              x1 = rightmostAcceptableXPos;
-            }
-
-            // If first segment's x value is too far to the left, move it EDGE_X_BUFFER pixels in
-            // from the left end of the destination node
-            const leftmostAcceptableXPos =
-              sourceNode.x - this.LEFT_OFFSET + this.EDGE_X_BUFFER;
-            if (leftmostAcceptableXPos >= x1) {
-              x1 = leftmostAcceptableXPos;
-            }
-            y1 = sourceNode.y + (sourceNode.height / 2) - 3;
+            
+            // Set the edge's first segment to start at the bottom of the source node.
+            yStart = sourceNode.y + (sourceNode.height / 2) - 3;
+            
+            xStart = this._adjustXVal(sourceNode, xStart);
           }
 
-          let x2 = edge.points[i].x;
-          let y2 = edge.points[i].y;
+          let xEnd = edge.points[i].x;
+          let yEnd = edge.points[i].y;
 
-          // Adjustments made to final segment for each edge
+          // Adjustments made to final segment for each edge.
           if (i === edge.points.length - 1) {
             const destinationNode = graph.node(edgeInfo.w);
 
-            // Placeholder nodes never need adjustment because they always have only a single incoming edge
+            // Placeholder nodes never need adjustment because they always have only a single
+            // incoming edge.
             if (!destinationNode.isPlaceholder) {
-              // Set the final segment's y value to point to the top of the destination node
-              y2 = destinationNode.y - this.TOP_OFFSET + 7;
+              // Set the edge's final segment to terminate at the top of the destination node.
+              yEnd = destinationNode.y - this.TOP_OFFSET + 5;
 
-              // If final segment's x value is too far to the right, move it EDGE_X_BUFFER pixels in
-              // from the right end of the destination node
-              const rightmostAcceptableXPos =
-                destinationNode.x + destinationNode.width - this.LEFT_OFFSET - this.EDGE_X_BUFFER;
-              if (rightmostAcceptableXPos <= x2) {
-                x2 = rightmostAcceptableXPos;
-              }
-
-              // If final segment's x value is too far to the left, move it EDGE_X_BUFFER pixels in
-              // from the left end of the destination node
-              const leftmostAcceptableXPos =
-                destinationNode.x - this.LEFT_OFFSET + this.EDGE_X_BUFFER;
-              if (leftmostAcceptableXPos >= x2) {
-                x2 = leftmostAcceptableXPos;
-              }
+              xEnd = this._adjustXVal(destinationNode, xEnd);
             }
           }
+          
+          // For the final segment of the edge, if the segment is diagonal, split it into a diagonal
+          // and a vertical piece so that all edges terminate with a vertical segment.
+          if (i === edge.points.length - 1 && xStart !== xEnd) {
+            const yHalf = (yStart + yEnd) / 2;
+            this._addDiagonalSegment(segments, xStart, yStart, xEnd, yHalf);
 
-          if (i === edge.points.length - 1 && x1 !== x2) {
-            // Diagonal
-            const yHalf = (y1 + y2) / 2;
-            // The + 0.5 at the end of 'distance' helps fill out the elbows of the edges.
-            const distance = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - yHalf, 2)) + 0.5;
-            const xMid = (x1 + x2) / 2;
-            const top = (y1 + yHalf) / 2;
-            const angle = Math.atan2(y1 - yHalf, x1 - x2) * 180 / Math.PI;
-            const left = xMid - (distance / 2);
-            segments.push({ angle, finalY: yHalf, finalX: x2, left, top, distance });
-
-            // Vertical
-            segments.push({ angle: 270, finalY: y2, finalX: x2, left: x2 - 5, top: y2 - 5, distance: y2 - yHalf });
+            // Vertical segment
+            segments.push({
+              angle: 270,
+              finalX: xEnd,
+              finalY: yEnd,
+              left: xEnd - 5,
+              length: yEnd - yHalf,
+              top: yEnd - 5,
+            });
           } else {
-            // The + 0.5 at the end of 'distance' helps fill out the elbows of the edges.
-            const distance = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2)) + 0.5;
-            const xMid = (x1 + x2) / 2;
-            const top = (y1 + y2) / 2;
-            const angle = Math.atan2(y1 - y2, x1 - x2) * 180 / Math.PI;
-            const left = xMid - (distance / 2);
-            segments.push({ angle, finalY: y2, finalX: x2, left, top, distance });
+            this._addDiagonalSegment(segments, xStart, yStart, xEnd, yEnd);
           }
         }
       }
@@ -264,44 +237,80 @@ export default class Graph extends React.Component<GraphProps, GraphState> {
               transition: 'left 0.5s, top 0.5s',
               width: node.width,
             }}>
-            <div className={css.label}>{node.label}</div>
+            {!node.isPlaceholder && (<div className={css.label}>{node.label}</div>)}
             <div className={css.icon} style={{ background: node.statusColoring }}>{node.icon}</div>
           </div>
         ))}
 
         {displayEdges.map((edge, i) => {
-          const edgeColor = this.getEdgeColor(edge, highlightNode);
+          const edgeColor = this._getEdgeColor(edge, highlightNode);
+          const lastSegment = edge.segments[edge.segments.length - 1];
           return (
             <div key={i}>
               {edge.segments.map((segment, l) => (
                 <div className={css.line}
                   key={l} style={{
                     backgroundColor: edgeColor,
-                    // height: segment.height,
                     height: this.EDGE_THICKNESS,
                     left: segment.left,
                     top: segment.top,
-                    transform: `translate(100px, 44px) rotate(${segment.angle}deg)`,
+                    transform: `translate(${this.LEFT_OFFSET}px, ${this.TOP_OFFSET}px) rotate(${segment.angle}deg)`,
                     transition: 'left 0.5s, top 0.5s',
-                    width: segment.distance,
+                    width: segment.length,
                   }} />
               ))}
+              {/* Arrowhead */}
               {!edge.isPlaceholder && (
                 <div className={css.arrowHead} style={{
                   borderTopColor: edgeColor,
-                  left: edge.segments[edge.segments.length - 1].finalX,
-                  top: edge.segments[edge.segments.length - 1].finalY,
-                  transform: `translate(94px, 41px) rotate(${edge.segments[edge.segments.length - 1].angle + 90}deg)`,
+                  left: lastSegment.finalX,
+                  top: lastSegment.finalY,
+                  transform: `translate(${this.LEFT_OFFSET - 6}px, ${this.TOP_OFFSET - 3}px) rotate(${lastSegment.angle + 90}deg)`,
                 }} />
               )}
             </div>
-          );}
-        )}
+          );
+        })}
       </div>
     );
   }
 
-  private getEdgeColor(edge: Edge, highlightNode?: string): string {
+  private _addDiagonalSegment(segments: Segment[], xStart: number, yStart: number, xEnd: number, yEnd: number): void {
+    const xMid = (xStart + xEnd) / 2;
+    // The + 0.5 at the end of 'length' helps fill out the elbows of the edges.
+    const length = Math.sqrt(Math.pow(xStart - xEnd, 2) + Math.pow(yStart - yEnd, 2)) + 0.5;
+    const left = xMid - (length / 2);
+    const top = (yStart + yEnd) / 2;
+    const angle = Math.atan2(yStart - yEnd, xStart - xEnd) * 180 / Math.PI;
+    segments.push({
+      angle,
+      finalX: xEnd,
+      finalY: yEnd,
+      left,
+      length,
+      top,
+    });
+  }
+
+  private _adjustXVal(node: dagre.Node, originalX: number): number {
+    // If the original X value was too far to the right, move it EDGE_X_BUFFER pixels
+    // in from the left end of the node.
+    const rightmostAcceptableLoc = node.x + node.width - this.LEFT_OFFSET - this.EDGE_X_BUFFER;
+    if (rightmostAcceptableLoc <= originalX) {
+      return rightmostAcceptableLoc;
+    }
+
+    // If the original X value was too far to the left, move it EDGE_X_BUFFER pixels
+    // in from the left end of the node.
+    const leftmostAcceptableLoc = node.x - this.LEFT_OFFSET + this.EDGE_X_BUFFER;
+    if (leftmostAcceptableLoc >= originalX) {
+      return leftmostAcceptableLoc;
+    }
+
+    return originalX;
+  }
+
+  private _getEdgeColor(edge: Edge, highlightNode?: string): string {
     if (highlightNode) {
       if (edge.from === highlightNode) {
         return color.theme;
