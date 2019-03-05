25ffdd4d3afc07910a667847323c20f7d4b2065f
Ning <gaoning777@gmail.com>
2019-03-05 09:48:00 -0800

    add core types and type checking function (#886)
    * add core types and type checking function
    
    * fix unit test bug
    
    * avoid defining dynamic classes
    
    * typo fix
    
    * use python struct for the openapi schema
    
    * update param name in the check_type functions
    remove schema validators for GCRPath, and adjust for GCRPath, GCSPath
    change _check_valid_dict to _check_valid_type_dict to avoid confusion
    fix typo in the comments
    adjust function order for readability

---
+++ sdk/python/tests/dsl/type_tests.py
@@ -0,0 +1,81 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from kfp.dsl._types import _instance_to_dict, _str_to_dict, check_types, GCSPath
+import unittest
+
+class TestTypes(unittest.TestCase):
+
+  def test_class_to_dict(self):
+    """Test _class_to_dict function."""
+    gcspath_dict = _instance_to_dict(GCSPath(path_type='file', file_type='csv'))
+    golden_dict = {
+        'GCSPath': {
+            'path_type': 'file',
+            'file_type': 'csv',
+        }
+    }
+    self.assertEqual(golden_dict, gcspath_dict)
+
+  def test_str_to_dict(self):
+    gcspath_str = '{"GCSPath": {"file_type": "csv", "path_type": "file"}}'
+    gcspath_dict = _str_to_dict(gcspath_str)
+    golden_dict = {
+        'GCSPath': {
+            'path_type': 'file',
+            'file_type': 'csv'
+        }
+    }
+    self.assertEqual(golden_dict, gcspath_dict)
+    gcspath_str = '{"file_type": "csv", "path_type": "file"}'
+    with self.assertRaises(ValueError):
+      _str_to_dict(gcspath_str)
+
+  def test_check_types(self):
+    #Core types
+    typeA = GCSPath(path_type='file', file_type='csv')
+    typeB = GCSPath(path_type='file', file_type='csv')
+    self.assertTrue(check_types(typeA, typeB))
+    typeC = GCSPath(path_type='file', file_type='tsv')
+    self.assertFalse(check_types(typeA, typeC))
+
+    # Custom types
+    typeA = {
+        'A':{
+            'X': 'value1',
+            'Y': 'value2'
+        }
+    }
+    typeB = {
+        'B':{
+            'X': 'value1',
+            'Y': 'value2'
+        }
+    }
+    typeC = {
+        'A':{
+            'X': 'value1'
+        }
+    }
+    typeD = {
+        'A':{
+            'X': 'value1',
+            'Y': 'value3'
+        }
+    }
+    self.assertFalse(check_types(typeA, typeB))
+    self.assertFalse(check_types(typeA, typeC))
+    self.assertTrue(check_types(typeC, typeA))
+    self.assertFalse(check_types(typeA, typeD))
