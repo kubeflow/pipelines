d7eed55257a74f5b15acc27ecc8be3c912967abd
Ning Gao <ngao@google.com>
2019-03-26 12:03:08 -0700

    fix file write

--- sdk/python/kfp/_client.py
+++ sdk/python/kfp/_client.py
@@ -189,7 +189,8 @@ class Client(object):
         if len(all_yaml_files) > 1:
           raise ValueError('Invalid package. Multiple yaml files in the package.')
 
-        with zip.extract(all_yaml_files[0]) as f:
+        filename = zip.extract(all_yaml_files[0])
+        with open(filename, 'r') as f:
           return yaml.load(f)
     elif package_file.endswith('.yaml') or package_file.endswith('.yml'):
       with open(package_file, 'r') as f:
