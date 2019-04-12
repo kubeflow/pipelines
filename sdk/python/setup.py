7f8ec0ee516e015ac71ab4754ce60864b7713ef0
Ajay Gopinathan <ajay@gopinathan.net>
2019-04-12 00:20:46 -0700

    Allow later versions of dependencies to be used with Python KFP package. (#1137)

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -12,16 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 from setuptools import setup
-
 
 NAME = 'kfp'
 VERSION = '0.1.16'
 
-REQUIRES = ['urllib3 >= 1.15', 'six >= 1.10', 'certifi', 'python-dateutil', 'PyYAML',
-            'google-cloud-storage == 1.13.0', 'kubernetes == 8.0.0', 'PyJWT==1.6.4',
-            'cryptography==2.4.2', 'google-auth==1.6.1', 'requests_toolbelt==0.8.0']
+REQUIRES = [
+    'urllib3>=1.15',
+    'six >= 1.10',
+    'certifi',
+    'python-dateutil',
+    'PyYAML',
+    'google-cloud-storage>=1.13.0',
+    'kubernetes>=8.0.0',
+    'PyJWT>=1.6.4',
+    'cryptography>=2.4.2',
+    'google-auth>=1.6.1',
+    'requests_toolbelt>=0.8.0',
+]
 
 setup(
     name=NAME,
@@ -30,49 +38,44 @@ setup(
     author='google',
     install_requires=REQUIRES,
     packages=[
-      'kfp',
-      'kfp.compiler',
-      'kfp.components',
-      'kfp.components.structures',
-      'kfp.components.structures.kubernetes',
-      'kfp.dsl',
-      'kfp.notebook',
-      'kfp_experiment',
-      'kfp_experiment.api',
-      'kfp_experiment.models',
-      'kfp_run',
-      'kfp_run.api',
-      'kfp_run.models',
-      'kfp_pipeline',
-      'kfp_pipeline.api',
-      'kfp_pipeline.models',
-      'kfp_uploadpipeline',
-      'kfp_uploadpipeline.api',
-      'kfp_uploadpipeline.models',
-      'kfp_job',
-      'kfp_job.api',
-      'kfp_job.models',
+        'kfp',
+        'kfp.compiler',
+        'kfp.components',
+        'kfp.components.structures',
+        'kfp.components.structures.kubernetes',
+        'kfp.dsl',
+        'kfp.notebook',
+        'kfp_experiment',
+        'kfp_experiment.api',
+        'kfp_experiment.models',
+        'kfp_run',
+        'kfp_run.api',
+        'kfp_run.models',
+        'kfp_pipeline',
+        'kfp_pipeline.api',
+        'kfp_pipeline.models',
+        'kfp_uploadpipeline',
+        'kfp_uploadpipeline.api',
+        'kfp_uploadpipeline.models',
+        'kfp_job',
+        'kfp_job.api',
+        'kfp_job.models',
     ],
     classifiers=[
-      'Intended Audience :: Developers',
-      'Intended Audience :: Education',
-      'Intended Audience :: Science/Research',
-      'License :: OSI Approved :: Apache Software License',
-      'Programming Language :: Python :: 3',
-      'Programming Language :: Python :: 3.5',
-      'Programming Language :: Python :: 3.6',
-      'Programming Language :: Python :: 3.7',
-      'Topic :: Scientific/Engineering',
-      'Topic :: Scientific/Engineering :: Artificial Intelligence',
-      'Topic :: Software Development',
-      'Topic :: Software Development :: Libraries',
-      'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     python_requires='>=3.5.3',
     include_package_data=True,
-    entry_points = {
-      'console_scripts': [ 
-        'dsl-compile = kfp.compiler.main:main',
-      ]
-    }
-)
+    entry_points={'console_scripts': ['dsl-compile = kfp.compiler.main:main',]})
