c7a95b4a2ee775efd8c24dd92049c88ec206eed7
singhan <singhan@us.ibm.com>
2019-04-25 00:36:43 -0700

    simplifying-params

--- samples/ibm-samples/watson/watson_train_serve_pipeline.py
+++ samples/ibm-samples/watson/watson_train_serve_pipeline.py
@@ -34,20 +34,18 @@ deploy_op = components.load_component_from_url('https://raw.githubusercontent.co
     description='Kubeflow pipelines running on WML performing tensorflow image recognition.'
 )
 def kfp_wml_pipeline(
-    GITHUB_TOKEN=dsl.PipelineParam(name='github-token',
-                                   value=''),
-    CONFIG_FILE_URL=dsl.PipelineParam(name='config-file-url',
-                                      value='https://raw.githubusercontent.com/user/repository/branch/creds.ini'),
-    train_code=dsl.PipelineParam(name='train-code', value='tf-model.zip'),
-    execution_command=dsl.PipelineParam(name='execution-command', value='\'python3 convolutional_network.py --trainImagesFile ${DATA_DIR}/train-images-idx3-ubyte.gz --trainLabelsFile ${DATA_DIR}/train-labels-idx1-ubyte.gz --testImagesFile ${DATA_DIR}/t10k-images-idx3-ubyte.gz --testLabelsFile ${DATA_DIR}/t10k-labels-idx1-ubyte.gz --learningRate 0.001 --trainingIters 20000\''),
-    framework= dsl.PipelineParam(name='framework', value='tensorflow'),
-    framework_version = dsl.PipelineParam(name='framework-version', value='1.5'),
-    runtime = dsl.PipelineParam(name='runtime', value='python'),
-    runtime_version = dsl.PipelineParam(name='runtime-version', value='3.5'),
-    run_definition = dsl.PipelineParam(name='run-definition', value='wml-tensorflow-definition'),
-    run_name = dsl.PipelineParam(name='run-name', value='wml-tensorflow-run'),
-    model_name=dsl.PipelineParam(name='model-name', value='wml-tensorflow-mnist'),
-    scoring_payload=dsl.PipelineParam(name='scoring-payload', value='tf-mnist-test-payload.json')
+    GITHUB_TOKEN='',
+    CONFIG_FILE_URL='https://raw.githubusercontent.com/user/repository/branch/creds.ini',
+    train_code='tf-model.zip',
+    execution_command='\'python3 convolutional_network.py --trainImagesFile ${DATA_DIR}/train-images-idx3-ubyte.gz --trainLabelsFile ${DATA_DIR}/train-labels-idx1-ubyte.gz --testImagesFile ${DATA_DIR}/t10k-images-idx3-ubyte.gz --testLabelsFile ${DATA_DIR}/t10k-labels-idx1-ubyte.gz --learningRate 0.001 --trainingIters 20000\'',
+    framework= 'tensorflow',
+    framework_version = '1.5',
+    runtime = 'python',
+    runtime_version = '3.5',
+    run_definition = 'wml-tensorflow-definition',
+    run_name = 'wml-tensorflow-run',
+    model_name='wml-tensorflow-mnist',
+    scoring_payload='tf-mnist-test-payload.json'
 ):
     # op1 - this operation will create the credentials as secrets to be used by other operations
     get_configuration = configuration_op(
