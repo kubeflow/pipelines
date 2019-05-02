f875cad7fc9a1f9790bbdaa5e7902c6ffd191009
singhan <singhan@us.ibm.com>
2019-05-02 00:41:33 -0700

    changing base image

--- components/ibm-components/spark/train_spark/src/spark-submit.sh
+++ components/ibm-components/spark/train_spark/src/spark-submit.sh
@@ -1,10 +1,15 @@
-#!/usr/bin/env bash
-
-# Licensed Materials - Property of IBM
-# 5725-Y50
-# (C) Copyright IBM Corp. 2015, 2017    All Rights Reserved.
-# US Government Users Restricted Rights - Use, duplication or disclosure
-# restricted by GSA ADP Schedule Contract with IBM Corp.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
 
 ###############################################################################
 #
