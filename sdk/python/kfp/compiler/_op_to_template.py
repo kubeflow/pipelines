ec2183bbcb14a987be5e0f96b115de0ac61585c2
Alexey Volkov <avolkov@google.com>
2019-05-07 22:44:36 -0700

    SDK - Stopped hard-coding artifact storage configuration in the pipeline packages
    We should follow Argo's prefferred way to configure the artifact storage: https://github.com/argoproj/argo/blob/master/ARTIFACT_REPO.md#configure-the-default-artifact-repository and use a cluster-local configMap. This way the pipelines remain clean and portable: https://github.com/argoproj/argo/blob/master/examples/artifact-passing.yaml
    Kubeflow deployer has already been pre-installing the configMap for several months: https://github.com/kubeflow/kubeflow/pull/2238

--- sdk/python/kfp/compiler/_op_to_template.py
+++ sdk/python/kfp/compiler/_op_to_template.py
@@ -152,28 +152,6 @@ def _outputs_to_json(op: BaseOp,
     return ret
 
 
-def _build_conventional_artifact(name, path):
-    return {
-        'name': name,
-        'path': path,
-        's3': {
-            # TODO: parameterize namespace for minio service
-            'endpoint': 'minio-service.kubeflow:9000',
-            'bucket': 'mlpipeline',
-            'key': 'runs/{{workflow.uid}}/{{pod.name}}/' + name + '.tgz',
-            'insecure': True,
-            'accessKeySecret': {
-                'name': 'mlpipeline-minio-artifact',
-                'key': 'accesskey',
-            },
-            'secretKeySecret': {
-                'name': 'mlpipeline-minio-artifact',
-                'key': 'secretkey'
-            }
-        },
-    }
-
-
 # TODO: generate argo python classes from swagger and use convert_k8s_obj_to_json??
 def _op_to_template(op: BaseOp):
     """Generate template given an operator inherited from BaseOp."""
@@ -189,7 +167,7 @@ def _op_to_template(op: BaseOp):
         output_artifact_paths.setdefault('mlpipeline-metrics', '/mlpipeline-metrics.json')
 
         output_artifacts = [
-            _build_conventional_artifact(name, path)
+            {'name': name, 'path': path}
             for name, path in output_artifact_paths.items()
         ]
 
