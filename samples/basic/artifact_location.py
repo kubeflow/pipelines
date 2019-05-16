bb65b7ed624c5ae702a38b4ec0e7df72df4c1590
eterna2 <eterna2@hotmail.com>
2019-05-16 08:40:32 +0800

    Sample for pipeline level artifact location only

--- samples/basic/artifact_location.py
+++ samples/basic/artifact_location.py
@@ -20,7 +20,7 @@ from kubernetes.client import V1SecretKeySelector
 @dsl.pipeline(
     name="custom_artifact_location_pipeline",
     description="""A pipeline to demonstrate how to configure the artifact 
-    location at a pipeline level and container op level.""",
+    location for all the ops in the pipeline.""",
 )
 def custom_artifact_location(
     tag: str, namespace: str = "kubeflow", bucket: str = "somebucket"
@@ -29,31 +29,14 @@ def custom_artifact_location(
     # configures artifact location
     pipeline_artifact_location = dsl.ArtifactLocation.s3(
         bucket=bucket,
-        endpoint="minio-service.%s:9000"
-        % namespace,  # parameterize minio-service endpoint
+        endpoint="minio-service.%s:9000" % namespace,  # parameterize minio-service endpoint
         insecure=True,
         access_key_secret=V1SecretKeySelector(name="minio", key="accesskey"),
         secret_key_secret={"name": "minio", "key": "secretkey"},  # accepts dict also
     )
 
-    # configures artifact location using AWS IAM role (no access key provided)
-    artifact_location = dsl.ArtifactLocation.s3(
-        bucket=bucket,
-        endpoint="s3.amazonaws.com",
-        region="ap-southeast-1",
-        insecure=False,
-    )
-
     # set pipeline level artifact location
     dsl.get_pipeline_conf().set_artifact_location(pipeline_artifact_location)
 
-    # pipeline level artifact location (to minio)
-    op1 = dsl.ContainerOp(name="foo", image="busybox:%s" % tag)
-
-    # op level artifact location (to s3 bucket)
-    op2 = dsl.ContainerOp(
-        name="foo",
-        image="busybox:%s" % tag,
-        # configures artifact location
-        artifact_location=artifact_location,
-    )
+    # artifacts in this op are stored to endpoint `minio-service.<namespace>:9000`
+    op = dsl.ContainerOp(name="foo", image="busybox:%s" % tag)
