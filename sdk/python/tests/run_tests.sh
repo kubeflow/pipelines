784249195f6c655dee262e82f6950c42f6395b32
Alexey Volkov <avolkov@google.com>
2019-06-06 16:37:59 -0700

    SDK - Improved test script compatibility with editable package installation (#1200)

--- sdk/python/tests/run_tests.sh
+++ sdk/python/tests/run_tests.sh
@@ -13,5 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cd $(dirname $0)
-python3 -m unittest discover --verbose --start-dir tests --top-level-directory=..
+cd "$(dirname "$0")/.."
+python3 -m unittest discover --verbose --start-dir tests --top-level-directory=.
