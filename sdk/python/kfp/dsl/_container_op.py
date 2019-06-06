f43da869b8ca25b30de03cd9a3cf7a540672518f
Ning Gao <ngao@google.com>
2019-06-06 16:15:40 -0700

    add timeout in dsl

--- sdk/python/kfp/dsl/_container_op.py
+++ sdk/python/kfp/dsl/_container_op.py
@@ -687,6 +687,7 @@ class BaseOp(object):
         self.pod_annotations = {}
         self.pod_labels = {}
         self.num_retries = 0
+        self.timeout = 0
         self.sidecars = sidecars or []
 
         # attributes specific to `BaseOp`
@@ -804,6 +805,16 @@ class BaseOp(object):
         """
 
         self.num_retries = num_retries
+        return self
+
+    def set_timeout(self, seconds: int):
+        """Sets the timeout for the task in seconds.
+
+        Args:
+          seconds: Number of seconds.
+        """
+
+        self.timeout = seconds
         return self
 
     def add_sidecar(self, sidecar: Sidecar):
