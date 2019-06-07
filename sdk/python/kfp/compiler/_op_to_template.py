7d69cda69c68bc38f896967edc8dc7e5d3ae798a
Alexey Volkov <avolkov@google.com>
2019-06-06 17:36:32 -0700

    Frontend - Show customized task display names (#1463)
    * Frontend - Show customized task display names
    
    * Added customized name test
    
    * Added ContainerOp.set_display_name(name) method
    
    * Stopped writing human_name to display_name annotation for now
    Reason: It's a change to existing pipelines.
    
    * Added test for op.set_display_name
    
    * Fix for tests that have workflows with status nodes, but without any spec or templates
    
    * Fixed the test workflow
    
    * Fix linter error
    Error: "The key 'metadata' is not sorted alphabetically"

--- sdk/python/kfp/compiler/_op_to_template.py
+++ sdk/python/kfp/compiler/_op_to_template.py
@@ -249,4 +249,8 @@ def _op_to_template(op: BaseOp):
     if processed_op.sidecars:
         template['sidecars'] = processed_op.sidecars
 
+    # Display name
+    if processed_op.display_name:
+        template.setdefault('metadata', {}).setdefault('annotations', {})['kubeflow.org/pipelines/task_display_name'] = processed_op.display_name
+
     return template
