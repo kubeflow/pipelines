9aa5da7d07e4c47121be6f95aef68a39f7283b46
Alexey Volkov <alexey.volkov@ark-kun.com>
2019-06-19 12:05:38 -0700

    Updated the sample

--- sdk/python/tests/compiler/testdata/input_artifact_raw_value.py
+++ sdk/python/tests/compiler/testdata/input_artifact_raw_value.py
@@ -19,18 +19,28 @@ from pathlib import Path
 
 sys.path.insert(0, __file__ + '/../../../../')
 
-import kfp.dsl as dsl
+import kfp
+from kfp import dsl
+
+
+def component_with_inline_input_artifact(text: str):
+    return dsl.ContainerOp(
+        name='component_with_inline_input_artifact',
+        image='alpine',
+        command=['cat', dsl.InputArtifactArgument(text, path='/tmp/inputs/text/data', input='text')], # path and input are optional
+    )
+
 
 def component_with_input_artifact(text):
     '''A component that passes text as input artifact'''
 
-    text_input_path = '/inputs/text/data'
     return dsl.ContainerOp(
         name='component_with_input_artifact',
-        input_artifact_paths={'text': text_input_path},
-        input_artifact_arguments={'text': text},
+        input_artifact_arguments=[
+            dsl.InputArtifactArgument(argument=text, path='/tmp/inputs/text/data', input='text'), # path and input are optional
+        ],
         image='alpine',
-        command=['cat', text_input_path],
+        command=['cat', '/tmp/inputs/text/data'],
     )
 
 def component_with_hardcoded_input_artifact_value():
@@ -43,18 +53,17 @@ def component_with_input_artifact_value_from_file(file_path):
     return component_with_input_artifact(Path(file_path).read_text())
 
 
-file_path = str(Path(__file__).parent.joinpath('input_artifact_raw_value.txt'))
-
-
 @dsl.pipeline(
-  name='Pipeline with artifact input raw argument value.',
-  description='Pipeline shows how to define artifact inputs and pass raw artifacts to them.'
+    name='Pipeline with artifact input raw argument value.',
+    description='Pipeline shows how to define artifact inputs and pass raw artifacts to them.'
 )
 def retry_sample_pipeline():
+    component_with_inline_input_artifact('Constant artifact value')
     component_with_input_artifact('Constant artifact value')
     component_with_hardcoded_input_artifact_value()
+
+    file_path = str(Path(__file__).parent.joinpath('input_artifact_raw_value.txt'))
     component_with_input_artifact_value_from_file(file_path)
 
 if __name__ == '__main__':
-    import kfp.compiler as compiler
-    compiler.Compiler().compile(retry_sample_pipeline, __file__ + '.tar.gz')
+    kfp.compiler.Compiler().compile(retry_sample_pipeline, __file__ + '.yaml')
