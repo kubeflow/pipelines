bd65d5448780219616c26af934c2382daaca025b
Ning Gao <ngao@google.com>
2019-06-24 17:24:17 -0700

    remove debug code

--- sdk/python/tests/dsl/pipeline_volume_tests.py
+++ sdk/python/tests/dsl/pipeline_volume_tests.py
@@ -65,9 +65,7 @@ class TestPipelineVolume(unittest.TestCase):
         """Test PipelineVolume creation when omitting "name"."""
         vol1 = PipelineVolume(pvc="foo")
         vol2 = PipelineVolume(name="provided", pvc="foo")
-        name1 = ("pvolume-109601a31098e6c92e2ad294fee3fae234e3f52398d3b077b0e0"
-                 "690e4cece686")
+        name1 = "pvolume-"
         name2 = "provided"
-        print(vol1.name)
-        self.assertEqual(vol1.name, name1)
+        self.assertTrue(vol1.name.startswith(name1))
         self.assertEqual(vol2.name, name2)
