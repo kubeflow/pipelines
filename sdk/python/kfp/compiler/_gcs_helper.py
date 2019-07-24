a236e6955d1d9d5bf7f6a1b126f096e3e912cd10
Ning Gao <ngao@google.com>
2019-07-24 12:17:43 -0700

    add import

--- sdk/python/kfp/compiler/_gcs_helper.py
+++ sdk/python/kfp/compiler/_gcs_helper.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from pathlib import PurePath
+
 class GCSHelper(object):
   """ GCSHelper manages the connection with the GCS storage """
 
