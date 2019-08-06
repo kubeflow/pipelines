5a51bf335ffe4be0f305bdb87bc2f9fe2ab7cc2c
IronPan <yangpa@google.com>
2019-08-06 12:57:44 -0700

    Update deploy-kubeflow.sh

--- test/deploy-kubeflow.sh
+++ test/deploy-kubeflow.sh
@@ -45,7 +45,7 @@ function clean_up {
   # delete the storage
   gcloud deployment-manager --project=${PROJECT} deployments delete ${KFAPP}-storage --quiet
 }
-# trap clean_up EXIT SIGINT SIGTERM
+trap clean_up EXIT SIGINT SIGTERM
 
 cd ${DIR}
 ${KUBEFLOW_SRC}/scripts/kfctl.sh init ${KFAPP} --platform ${PLATFORM} --project ${PROJECT} --skipInitProject
