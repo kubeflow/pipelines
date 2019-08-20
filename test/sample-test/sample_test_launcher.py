797b618520d78ccb1e07dfc2ef01a3dbc076d699
numerology <jxzheng@google.com>
2019-08-19 17:28:54 -0700

    Refactor the logic of run_test.sh into sample_test_launcher.py

--- test/sample-test/sample_test_launcher.py
+++ test/sample-test/sample_test_launcher.py
@@ -22,15 +22,31 @@ import importlib
 import io
 import json
 import os
+import papermill as pm
 import sys
+import subprocess
 import tarfile
 import logging
 import utils
+import uuid
 
 from datetime import datetime
+from google.cloud import storage
 from kfp import Client
 
+# List of notebook samples' test names and corresponding file names.
+NOTEBOOK_SAMPLES = {
+    'kubeflow_pipeline_using_TFX_OSS_components': 'KubeFlow Pipeline Using TFX OSS Components.ipynb',
+    'lightweight_component': 'Lightweight Python components - basics.ipynb',
+    'dsl_static_type_checking': 'DSL Static Type Checking.ipynb'
+}
 
+
+PROJECT_NAME = 'ml-pipeline-test'
+PAPERMILL_ERR_MSG = 'An Exception was encountered at'
+
+
+#TODO(numerology): Add unit-test for classes.
 class SampleTest(object):
   """Launch a KFP sample_test provided its name.
 
@@ -50,155 +66,145 @@ class SampleTest(object):
                          namespace='kubeflow'):
     self._test_name = test_name
     self._results_gcs_dir = test_results_gcs_dir
+    #(TODO: numerology) target_image_prefix seems to be only used for post-submit
+    # check.
     self._target_image_prefix = target_image_prefix
     self._namespace = namespace
-    self._sample_test_result = 'junit_Sample' + self._test_name + 'Output.xml'
+    self._sample_test_result = 'junit_Sample%sOutput.xml' % self._test_name
     self._sample_test_output = self._results_gcs_dir
     self._work_dir = self.BASE_DIR + '/samples/core/' + self._test_name
 
     self._run_test()
 
+  def check_result(self):
+    os.chdir(self.TEST_DIR)
+    subprocess.call([
+        'python3',
+        'run_sample_test.py',
+        '--input',
+        input,
+        '--result',
+        self._sample_test_result,
+        '--output',
+        self._sample_test_output,
+        '--testname',
+        self._test_name,
+        '--namespace',
+        self._namespace
+    ])
+    print('Copy the test results to GCS %s/' % self._results_gcs_dir)
+    storage_client = storage.Client()
+    working_bucket = PROJECT_NAME
+
+    src_bucket = storage_client.get_bucket(working_bucket)
+    dest_bucket =src_bucket # Currently copy to the same bucket.
+    src_bucket.copy_blob(
+        self._sample_test_result,
+        dest_bucket,
+        self._results_gcs_dir + '/' + self._sample_test_result)
+
+  def check_notebook_result(self):
+    # Workaround because papermill does not directly return exit code.
+    exit_code = PAPERMILL_ERR_MSG in open('%s.ipynb' % self._test_name).read()
+    if self._test_name == 'dsl_static_type_checking':
+      subprocess.call([
+          'python3',
+          'check_notebook_results.py',
+          '--testname',
+          self._test_name,
+          '--result',
+          self._sample_test_result,
+          '--exit-code',
+          exit_code
+      ])
+    else:
+      subprocess.call([
+          'python3',
+          'check_notebook_results.py',
+          '--testname',
+          self._test_name,
+          '--result',
+          self._sample_test_result,
+          '--namespace',
+          self._namespace,
+          '--exit-code',
+          exit_code
+      ])
+
+    print('Copy the test results to GCS %s/' % self._results_gcs_dir)
+    storage_client = storage.Client()
+    working_bucket = PROJECT_NAME
+
+    src_bucket = storage_client.get_bucket(working_bucket)
+    dest_bucket =src_bucket # Currently copy to the same bucket.
+    src_bucket.copy_blob(
+        self._sample_test_result,
+        dest_bucket,
+        self._results_gcs_dir + '/' + self._sample_test_result)
+
   def _run_test(self):
     if len(self._results_gcs_dir) == 0:
       return 1
-
-    os.chdir(self._work_dir)
-
-
-
-
 
     # variables needed for sample test logic.
     input = '%s/%s.yaml' % (self._work_dir, self._test_name)
     test_cases = [] # Currently, only capture run-time error, no result check.
     sample_test_name = self._test_name + ' Sample Test'
 
-    ###### Initialization ######
-    host = 'ml-pipeline.%s.svc.cluster.local:8888' % self._namespace
-    client = Client(host=host)
+    os.chdir(self._work_dir)
+    print('Run the sample tests...')
 
-    ###### Check Input File ######
-    utils.add_junit_test(test_cases, 'input generated yaml file',
-                         os.path.exists(input), 'yaml file is not generated')
-    if not os.path.exists(input):
-      utils.write_junit_xml(sample_test_name, self._sample_test_result, test_cases)
-      print('Error: job not found.')
-      exit(1)
-
-    ###### Create Experiment ######
-    experiment_name = self._test_name + ' sample experiment'
-    response = client.create_experiment(experiment_name)
-    experiment_id = response.id
-    utils.add_junit_test(test_cases, 'create experiment', True)
-
-    ###### Create Job ######
-    job_name = self._test_name + '_sample'
-    ###### Test-specific arguments #######
-    if self._test_name == 'tfx_cab_classification':
-      params = {
-          'output':
-            self._sample_test_output,
-          'project':
-            'ml-pipeline-test',
-          'column-names':
-            'gs://ml-pipeline-dataset/sample-test/taxi-cab-classification/column-names.json',
-          'evaluation':
-            'gs://ml-pipeline-dataset/sample-test/taxi-cab-classification/eval20.csv',
-          'train':
-            'gs://ml-pipeline-dataset/sample-test/taxi-cab-classification/train50.csv',
-          'hidden-layer-size':
-            '5',
-          'steps':
-            '5'
-      }
-    elif self._test_name == 'kubeflow_training_classification':
-      params = {
-          'output': self._sample_test_output,
-          'project': 'ml-pipeline-test',
-          'evaluation': 'gs://ml-pipeline-dataset/sample-test/flower/eval15.csv',
-          'train': 'gs://ml-pipeline-dataset/sample-test/flower/train30.csv',
-          'hidden-layer-size': '10,5',
-          'steps': '5'
-      }
-    elif self._test_name == 'xgboost_training_cm':
-      params = {
-          'output': self._sample_test_output,
-          'project': 'ml-pipeline-test',
-          'train-data': 'gs://ml-pipeline-dataset/sample-test/sfpd/train_50.csv',
-          'eval-data': 'gs://ml-pipeline-dataset/sample-test/sfpd/eval_20.csv',
-          'schema': 'gs://ml-pipeline-dataset/sample-test/sfpd/schema.json',
-          'rounds': '20',
-          'workers': '2'
-      }
-    else:
-      # Basic tests require no additional params.
-      params = {}
-
-    response = client.run_pipeline(experiment_id, job_name, input, params)
-    run_id = response.id
-    utils.add_junit_test(test_cases, 'create pipeline run', True)
-
-    ###### Monitor Job ######
-    try:
-      start_time = datetime.now()
-      if self._test_name == 'xgboost_training_cm':
-        response = client.wait_for_run_completion(run_id, 1800)
-      else:
-        response = client.wait_for_run_completion(run_id, 1200)
-      succ = (response.run.status.lower() == 'succeeded')
-      end_time = datetime.now()
-      elapsed_time = (end_time - start_time).seconds
-      utils.add_junit_test(test_cases, 'job completion', succ,
-                           'waiting for job completion failure', elapsed_time)
-    finally:
-      ###### Output Argo Log for Debugging ######
-      workflow_json = client._get_workflow_json(run_id)
-      workflow_id = workflow_json['metadata']['name']
-      argo_log, _ = utils.run_bash_command('argo logs -n {} -w {}'.format(
-          self._namespace, workflow_id))
-      print('=========Argo Workflow Log=========')
-      print(argo_log)
-
-    if not succ:
-      utils.write_junit_xml(sample_test_name, self._sample_test_result, test_cases)
-      exit(1)
-
-    ###### Validate the results for specific test cases ######
-    #TODO: Add result check for tfx-cab-classification after launch.
-    if self._test_name == 'kubeflow_training_classification':
-      cm_tar_path = './confusion_matrix.tar.gz'
-      utils.get_artifact_in_minio(workflow_json, 'confusion-matrix', cm_tar_path,
-                                  'mlpipeline-ui-metadata')
-      with tarfile.open(cm_tar_path) as tar_handle:
-        file_handles = tar_handle.getmembers()
-        assert len(file_handles) == 1
-
-        with tar_handle.extractfile(file_handles[0]) as f:
-          cm_data = json.load(io.TextIOWrapper(f))
-          utils.add_junit_test(
-              test_cases, 'confusion matrix format',
-              (len(cm_data['outputs'][0]['schema']) == 3),
-              'the column number of the confusion matrix output is not equal to three'
+    # For presubmit check, do not do any image injection as for now.
+    # Notebook samples need to be papermilled first.
+    if self._test_name == 'kubeflow_pipeline_using_TFX_OSS_components':
+      bucket_prefix = 'gs://ml-pipeline-dataset/sample-test/taxi-cab-classification/'
+      pm.execute_notebook(
+          input_path='KubeFlow Pipeline Using TFX OSS Components.ipynb',
+          output_path='%s.ipynb' % self._test_name,
+          prepare_only=False,
+          parameters=dict(
+              EXPERIMENT_NAME='%s-test' % self._test_name,
+              OUTPUT_DIR=self._results_gcs_dir,
+              PROJECT_NAME=PROJECT_NAME,
+              BASE_IMAGE='%spusherbase:dev' % self._target_image_prefix,
+              TARGET_IMAGE='%spusher:dev' % self._target_image_prefix,
+              TARGET_IMAGE_TWO='%spusher_two:dev' % self._target_image_prefix,
+              KFP_PACKAGE='tmp/kfp.tar.gz',
+              DEPLOYER_MODEL='Notebook_tfx_taxi_%s' % uuid.uuid1(),
+              TRAINING_DATA=bucket_prefix + 'train50.csv',
+              EVAL_DATA=bucket_prefix + 'eval20.csv',
+              HIDDEN_LAYER_SIZE=10,
+              STEPS=50
           )
-    elif self._test_name == 'xgboost_training_cm':
-      cm_tar_path = './confusion_matrix.tar.gz'
-      utils.get_artifact_in_minio(workflow_json, 'confusion-matrix', cm_tar_path,
-                                  'mlpipeline-ui-metadata')
-      with tarfile.open(cm_tar_path) as tar_handle:
-        file_handles = tar_handle.getmembers()
-        assert len(file_handles) == 1
+      )
+      self.check_notebook_result()
+    elif self._test_name == 'lightweight_component':
+      pm.execute_notebook(
+          input_path='Lightweight Python components - basics.ipynb',
+          output_path='%s.ipynb' % self._test_name,
+          prepare_only=False,
+          parameters=dict(
+              EXPERIMENT_NAME='%s-test' % self._test_name,
+              PROJECT_NAME=PROJECT_NAME,
+              KFP_PACKAGE='tmp/kfp.tar.gz',
+          )
+      )
+      self.check_notebook_result()
+    elif self._test_name == 'dsl_static_type_checking':
+      pm.execute_notebook(
+          input_path='DSL Static Type Checking.ipynb',
+          output_path='%s.ipynb' % self._test_name,
+          prepare_only=False,
+          parameters=dict(
+              KFP_PACKAGE='tmp/kfp.tar.gz',
+          )
+      )
+      self.check_notebook_result()
+    else:
+      subprocess.call(['dsl-compile', '--py', '%s.py' % self._test_name,
+                       '--output', '%s.yaml' % self._test_name])
+      self.check_result()
 
-        with tar_handle.extractfile(file_handles[0]) as f:
-          cm_data = f.read()
-          utils.add_junit_test(test_cases, 'confusion matrix format',
-                               (len(cm_data) > 0),
-                               'the confusion matrix file is empty')
-
-    ###### Delete Job ######
-    #TODO: add deletion when the backend API offers the interface.
-
-    ###### Write out the test result in junit xml ######
-    utils.write_junit_xml(sample_test_name, self._sample_test_result, test_cases)
 
 class ComponentTest(SampleTest):
   """ Launch a KFP sample test as component test provided its name.
@@ -209,6 +215,7 @@ class ComponentTest(SampleTest):
       result_gcs_dir, target_image_prefix, dataflow_tft_image,
       namespace='kubeflow'):
     super().__init__(test_name, input, result, output, namespace)
+    pass
 
 def main():
   """Launches either KFP sample test or component test as a command entrypoint.
