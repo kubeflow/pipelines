06b5c9d3f347aa9dbf4968259139743277624b6e
cclauss <cclauss@me.com>
2019-08-21 10:06:41 +0200

    Lint Python code for undefined names

--- backend/src/apiserver/visualization/tfdv.py
+++ backend/src/apiserver/visualization/tfdv.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# flake8: noqa TODO
+
 import tensorflow_data_validation as tfdv
 
 # The following variables are provided through dependency injection. These
