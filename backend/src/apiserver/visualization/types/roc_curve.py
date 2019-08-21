debf697dc5b45d45b2287248298599e3dbd30db4
numerology <jxzheng@google.com>
2019-08-21 15:11:01 -0700

    Squash and merge from master

--- backend/src/apiserver/visualization/types/roc_curve.py
+++ backend/src/apiserver/visualization/types/roc_curve.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# flake8: noqa TODO
+
 import json
 from pathlib import Path
 from bokeh.layouts import row
