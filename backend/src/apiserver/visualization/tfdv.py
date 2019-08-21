a416d12da7365cf5659a4bcf5281a17273141526
cclauss <cclauss@me.com>
2019-08-21 11:11:38 +0200

    Exclude tfdv.py to workaround an overzealous pytest

--- backend/src/apiserver/visualization/tfdv.py
+++ backend/src/apiserver/visualization/tfdv.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# flake8: noqa TODO
-
 import tensorflow_data_validation as tfdv
 
 # The following variables are provided through dependency injection. These
