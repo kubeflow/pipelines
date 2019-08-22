b9f2d900e867d38bba86598caf564a613caf8056
Kirin Patel <kirinpatel@gmail.com>
2019-08-21 18:30:33 -0700

    Add visualization-server service to lightweight deployment (#1844)
    * Add visualization-server service to lightweight deployment
    
    * Addressed PR suggestions
    
    * Added field to determine if visualization service is active and fixed unit tests for visualization_server.go
    
    * Additional small fixes
    
    * port change from 88888 -> 8888
    * version change from 0.1.15 -> 0.1.26
    * removed visualization-server from base/kustomization.yaml
    
    * Fixed visualization_server_test.go to reflect new changes
    
    * Changed implementation to be fail fast
    
    * Changed host name to be constant provided by environment
    
    * Added retry and extracted isVisualizationServiceAlive logic to function
    
    * Fixed deployment.yaml file
    
    * Fixed serviceURL configuration issuse
    
    serviceURL is now properly obtained from the environment, the service ip address and port are used rather than service name and namespace
    
    * Added log message to indicate when visualization service is unreachable
    
    * Addressed PR comments
    
    * Removed _HTTP

--- backend/src/apiserver/server/visualization_server.go
+++ backend/src/apiserver/server/visualization_server.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/cenkalti/backoff"
+	"github.com/golang/glog"
 	"github.com/kubeflow/pipelines/backend/api/go_client"
 	"github.com/kubeflow/pipelines/backend/src/apiserver/resource"
 	"github.com/kubeflow/pipelines/backend/src/common/util"
@@ -11,11 +13,13 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 )
 
 type VisualizationServer struct {
-	resourceManager *resource.ResourceManager
-	serviceURL      string
+	resourceManager    *resource.ResourceManager
+	serviceURL         string
+	isServiceAvailable bool
 }
 
 func (s *VisualizationServer) CreateVisualization(ctx context.Context, request *go_client.CreateVisualizationRequest) (*go_client.Visualization, error) {
@@ -56,6 +60,12 @@ func (s *VisualizationServer) validateCreateVisualizationRequest(request *go_cli
 // service to generate HTML visualizations from a request.
 // It returns the generated HTML as a string and any error that is encountered.
 func (s *VisualizationServer) generateVisualizationFromRequest(request *go_client.CreateVisualizationRequest) ([]byte, error) {
+	if !s.isServiceAvailable {
+		return nil, util.NewInternalServerError(
+			fmt.Errorf("service not available"),
+			"Service not available",
+		)
+	}
 	visualizationType := strings.ToLower(go_client.Visualization_Type_name[int32(request.Visualization.Type)])
 	arguments := fmt.Sprintf("--type %s --source %s --arguments '%s'", visualizationType, request.Visualization.Source, request.Visualization.Arguments)
 	resp, err := http.PostForm(s.serviceURL, url.Values{"arguments": {arguments}})
@@ -73,6 +83,33 @@ func (s *VisualizationServer) generateVisualizationFromRequest(request *go_clien
 	return body, nil
 }
 
-func NewVisualizationServer(resourceManager *resource.ResourceManager) *VisualizationServer {
-	return &VisualizationServer{resourceManager: resourceManager, serviceURL: "http://visualization-service.kubeflow"}
+func isVisualizationServiceAlive(serviceURL string, initConnectionTimeout time.Duration) bool {
+	var operation = func() error {
+		_, err := http.Get(serviceURL)
+		if err != nil {
+			glog.Error("Unable to verify visualization service is alive!", err)
+			return err
+		}
+		return nil
+	}
+	b := backoff.NewExponentialBackOff()
+	b.MaxElapsedTime = initConnectionTimeout
+	err := backoff.Retry(operation, b)
+	return err == nil
+}
+
+func NewVisualizationServer(resourceManager *resource.ResourceManager, serviceHost string, servicePort string, initConnectionTimeout time.Duration) *VisualizationServer {
+	serviceURL := fmt.Sprintf("http://%s:%s", serviceHost, servicePort)
+	isServiceAvailable := isVisualizationServiceAlive(serviceURL, initConnectionTimeout)
+	return &VisualizationServer{
+		resourceManager:    resourceManager,
+		serviceURL:         serviceURL,
+		// TODO: isServiceAvailable is used to determine if the new visualization
+		//  service is alive. If this is true, then the service is alive and
+		//  requests can be made to it. Otherwise, if it is false, the service is
+		//  not alive and requests should not be made. This prevents timeouts and
+		//  counteracts current instabilities with the service. This should be
+		//  removed after the visualization service is deemed stable.
+		isServiceAvailable: isServiceAvailable,
+	}
 }
