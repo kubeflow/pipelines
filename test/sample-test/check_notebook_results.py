f491b1dc0f6244ad87ba0687d05215601b7b4f1c
numerology <jxzheng@google.com>
2019-08-23 17:04:40 -0700

    Refactor check_notebook_results into class.

--- test/sample-test/check_notebook_results.py
+++ test/sample-test/check_notebook_results.py
@@ -13,89 +13,72 @@
 # limitations under the License.
 
 import argparse
-from kfp import Client
 import utils
 
-###### Input/Output Instruction ######
-# args:
-#   experiment: where the test run belong, only necessary when a job is submitted.
-#   namespace: where the pipeline system is deployed.
-#   testname: test name in the json xml
-#   result: name of the file that stores the test result
-#   exit_code: the exit code of the bash command that runs the test.
-
-# Parsing the input arguments
-
-class CheckNotebookResults(object):
-  def __init__(self):
-    pass
+from kfp import Client
 
 
-def parse_arguments():
-  """Parse command line arguments."""
+_RUN_LIST_PAGE_SIZE = 1000
+_TEST_TIMEOUT = 1200
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--experiment',
-                      type=str,
-                      help='The experiment name')
-  parser.add_argument('--namespace',
-                      type=str,
-                      default='kubeflow',
-                      help="namespace of the deployed pipeline system. Default: kubeflow")
-  parser.add_argument('--testname',
-                      type=str,
-                      required=True,
-                      help="Test name")
-  parser.add_argument('--result',
-                      type=str,
-                      required=True,
-                      help='The path of the test result that will be exported.')
-  parser.add_argument('--exit-code',
-                      type=str,
-                      required=True,
-                      help='The exit code of the bash command that runs the test.')
-  args = parser.parse_args()
-  return args
+class NoteBookChecker(object):
+  def __init__(self, testname, result, exit_code,
+      experiment=None, namespace='kubeflow'):
+    """ Util class for checking notebook sample test running results.
 
-def main():
-  args = parse_arguments()
-  test_cases = []
-  test_name = args.testname + ' Sample Test'
+    :param testname: test name in the json xml.
+    :param result: name of the file that stores the test result
+    :param exit_code: the exit code of the notebook run. 0 for passed test.
+    :param experiment: where the test run belong, only necessary when a job is submitted.
+    :param namespace: where the pipeline system is deployed.
+    """
+    self._testname = testname
+    self._result = result
+    self._exit_code = exit_code
+    self._experiment = experiment
+    self._namespace = namespace
 
-  ###### Write the script exit code log ######
-  utils.add_junit_test(test_cases, 'test script execution', (args.exit_code == '0'), 'test script failure with exit code: ' + args.exit_code)
+  def check(self):
+    test_cases = []
+    test_name = self._testname + ' Sample Test'
 
-  if args.experiment is not None:
-    ###### Initialization ######
-    host = 'ml-pipeline.%s.svc.cluster.local:8888' % args.namespace
-    client = Client(host=host)
+    ###### Write the script exit code log ######
+    utils.add_junit_test(test_cases, 'test script execution',
+                         (self._exit_code == '0'),
+                         'test script failure with exit code: '
+                         + self._exit_code)
 
-    ###### Get experiments ######
-    experiment_id = client.get_experiment(experiment_name=args.experiment).id
+    if self._experiment is not None:  # Bypassing dsl type check sample.
+      ###### Initialization ######
+      host = 'ml-pipeline.%s.svc.cluster.local:8888' % self._namespace
+      client = Client(host=host)
 
-    ###### Get runs ######
-    list_runs_response = client.list_runs(page_size=1000, experiment_id=experiment_id)
+      ###### Get experiments ######
+      experiment_id = client.get_experiment(experiment_name=self._experiment).id
 
-    ###### Check all runs ######
-    for run in list_runs_response.runs:
-      run_id = run.id
-      response = client.wait_for_run_completion(run_id, 1200)
-      succ = (response.run.status.lower()=='succeeded')
-      utils.add_junit_test(test_cases, 'job completion', succ, 'waiting for job completion failure')
+      ###### Get runs ######
+      list_runs_response = client.list_runs(page_size=_RUN_LIST_PAGE_SIZE,
+                                            experiment_id=experiment_id)
 
-      ###### Output Argo Log for Debugging ######
-      workflow_json = client._get_workflow_json(run_id)
-      workflow_id = workflow_json['metadata']['name']
-      argo_log, _ = utils.run_bash_command('argo logs -n {} -w {}'.format(args.namespace, workflow_id))
-      print("=========Argo Workflow Log=========")
-      print(argo_log)
+      ###### Check all runs ######
+      for run in list_runs_response.runs:
+        run_id = run.id
+        response = client.wait_for_run_completion(run_id, _TEST_TIMEOUT)
+        succ = (response.run.status.lower()=='succeeded')
+        utils.add_junit_test(test_cases, 'job completion',
+                             succ, 'waiting for job completion failure')
 
-      if not succ:
-        utils.write_junit_xml(test_name, args.result, test_cases)
-        exit(1)
+        ###### Output Argo Log for Debugging ######
+        workflow_json = client._get_workflow_json(run_id)
+        workflow_id = workflow_json['metadata']['name']
+        argo_log, _ = utils.run_bash_command(
+            'argo logs -n {} -w {}'.format(self._namespace, workflow_id))
+        print("=========Argo Workflow Log=========")
+        print(argo_log)
 
-  ###### Write out the test result in junit xml ######
-  utils.write_junit_xml(test_name, args.result, test_cases)
+        if not succ:
+          utils.write_junit_xml(test_name, self._result, test_cases)
+          exit(1)
 
-if __name__ == "__main__":
-  main()
+    ###### Write out the test result in junit xml ######
+    utils.write_junit_xml(test_name, self._result, test_cases)
