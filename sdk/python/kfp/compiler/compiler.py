38a8dd329bc48a8a0a8d74a863c3042849e1819c
IronPan <yangpa@google.com>
2019-08-29 13:56:35 -0700

    Update compiler.py
    revert python change for backward compatibility

--- sdk/python/kfp/compiler/compiler.py
+++ sdk/python/kfp/compiler/compiler.py
@@ -641,6 +641,7 @@ class Compiler(object):
         'entrypoint': pipeline_name,
         'templates': templates,
         'arguments': {'parameters': input_params},
+        'serviceAccountName': 'pipeline-runner'
       }
     }
     # set ttl after workflow finishes
