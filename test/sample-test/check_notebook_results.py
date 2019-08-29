2cbc666b35dfea0adb4480570f12eabc56a64d07
numerology <jxzheng@google.com>
2019-08-29 16:34:13 -0700

    Lint check_notebook_results.py

--- test/sample-test/check_notebook_results.py
+++ test/sample-test/check_notebook_results.py
@@ -20,73 +20,73 @@ from kfp import Client
 
 
 class NoteBookChecker(object):
-    def __init__(self, testname, result, exit_code,
-                 experiment=None, namespace='kubeflow'):
-        """ Util class for checking notebook sample test running results.
+  def __init__(self, testname, result, exit_code,
+      experiment=None, namespace='kubeflow'):
+    """ Util class for checking notebook sample test running results.
 
-        :param testname: test name in the json xml.
-        :param result: name of the file that stores the test result
-        :param exit_code: the exit code of the notebook run. 0 for passed test.
-        :param experiment: where the test run belong, only necessary when a job is submitted.
-        :param namespace: where the pipeline system is deployed.
-        """
-        self._testname = testname
-        self._result = result
-        self._exit_code = exit_code
-        self._experiment = experiment
-        self._namespace = namespace
+    :param testname: test name in the json xml.
+    :param result: name of the file that stores the test result
+    :param exit_code: the exit code of the notebook run. 0 for passed test.
+    :param experiment: where the test run belong, only necessary when a job is submitted.
+    :param namespace: where the pipeline system is deployed.
+    """
+    self._testname = testname
+    self._result = result
+    self._exit_code = exit_code
+    self._experiment = experiment
+    self._namespace = namespace
 
-    def check(self):
-        test_cases = []
-        test_name = self._testname + ' Sample Test'
+  def check(self):
+    test_cases = []
+    test_name = self._testname + ' Sample Test'
 
-        ###### Write the script exit code log ######
-        utils.add_junit_test(test_cases, 'test script execution',
-                             (self._exit_code == '0'),
-                             'test script failure with exit code: '
-                             + self._exit_code)
+    ###### Write the script exit code log ######
+    utils.add_junit_test(test_cases, 'test script execution',
+                         (self._exit_code == '0'),
+                         'test script failure with exit code: '
+                         + self._exit_code)
 
-        try:
-            with open(DEFAULT_CONFIG, 'r') as f:
-                raw_args = yaml.safe_load(f)
-        except yaml.YAMLError as yamlerr:
-            raise RuntimeError('Illegal default config:{}'.format(yamlerr))
-        except OSError as ose:
-            raise FileExistsError('Default config not found:{}'.format(ose))
-        else:
-            test_timeout = raw_args['test_timeout']
+    try:
+      with open(DEFAULT_CONFIG, 'r') as f:
+        raw_args = yaml.safe_load(f)
+    except yaml.YAMLError as yamlerr:
+      raise RuntimeError('Illegal default config:{}'.format(yamlerr))
+    except OSError as ose:
+      raise FileExistsError('Default config not found:{}'.format(ose))
+    else:
+      test_timeout = raw_args['test_timeout']
 
-        if self._experiment is not None:  # Bypassing dsl type check sample.
-            ###### Initialization ######
-            host = 'ml-pipeline.%s.svc.cluster.local:8888' % self._namespace
-            client = Client(host=host)
+    if self._experiment is not None:  # Bypassing dsl type check sample.
+      ###### Initialization ######
+      host = 'ml-pipeline.%s.svc.cluster.local:8888' % self._namespace
+      client = Client(host=host)
 
-            ###### Get experiments ######
-            experiment_id = client.get_experiment(experiment_name=self._experiment).id
+      ###### Get experiments ######
+      experiment_id = client.get_experiment(experiment_name=self._experiment).id
 
-            ###### Get runs ######
-            list_runs_response = client.list_runs(page_size=RUN_LIST_PAGE_SIZE,
-                                                  experiment_id=experiment_id)
+      ###### Get runs ######
+      list_runs_response = client.list_runs(page_size=RUN_LIST_PAGE_SIZE,
+                                            experiment_id=experiment_id)
 
-            ###### Check all runs ######
-            for run in list_runs_response.runs:
-                run_id = run.id
-                response = client.wait_for_run_completion(run_id, test_timeout)
-                succ = (response.run.status.lower()=='succeeded')
-                utils.add_junit_test(test_cases, 'job completion',
-                                     succ, 'waiting for job completion failure')
+      ###### Check all runs ######
+      for run in list_runs_response.runs:
+        run_id = run.id
+        response = client.wait_for_run_completion(run_id, test_timeout)
+        succ = (response.run.status.lower() == 'succeeded')
+        utils.add_junit_test(test_cases, 'job completion',
+                             succ, 'waiting for job completion failure')
 
-                ###### Output Argo Log for Debugging ######
-                workflow_json = client._get_workflow_json(run_id)
-                workflow_id = workflow_json['metadata']['name']
-                argo_log, _ = utils.run_bash_command(
-                    'argo logs -n {} -w {}'.format(self._namespace, workflow_id))
-                print("=========Argo Workflow Log=========")
-                print(argo_log)
+        ###### Output Argo Log for Debugging ######
+        workflow_json = client._get_workflow_json(run_id)
+        workflow_id = workflow_json['metadata']['name']
+        argo_log, _ = utils.run_bash_command(
+            'argo logs -n {} -w {}'.format(self._namespace, workflow_id))
+        print("=========Argo Workflow Log=========")
+        print(argo_log)
 
-                if not succ:
-                    utils.write_junit_xml(test_name, self._result, test_cases)
-                    exit(1)
+        if not succ:
+          utils.write_junit_xml(test_name, self._result, test_cases)
+          exit(1)
 
-        ###### Write out the test result in junit xml ######
-        utils.write_junit_xml(test_name, self._result, test_cases)
+    ###### Write out the test result in junit xml ######
+    utils.write_junit_xml(test_name, self._result, test_cases)
