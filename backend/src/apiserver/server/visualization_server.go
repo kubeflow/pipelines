fe7303f5a99d8e24074624cae77b6fa17b31691b
Kirin Patel <kirinpatel@gmail.com>
2019-08-30 12:48:30 -0700

    Changed isVisualizationServiceAlive implementation (#2004)

--- backend/src/apiserver/server/visualization_server.go
+++ backend/src/apiserver/server/visualization_server.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/cenkalti/backoff"
 	"github.com/golang/glog"
 	"github.com/kubeflow/pipelines/backend/api/go_client"
 	"github.com/kubeflow/pipelines/backend/src/apiserver/resource"
@@ -13,13 +12,11 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
-	"time"
 )
 
 type VisualizationServer struct {
 	resourceManager    *resource.ResourceManager
 	serviceURL         string
-	isServiceAvailable bool
 }
 
 func (s *VisualizationServer) CreateVisualization(ctx context.Context, request *go_client.CreateVisualizationRequest) (*go_client.Visualization, error) {
@@ -63,7 +60,7 @@ func (s *VisualizationServer) validateCreateVisualizationRequest(request *go_cli
 // service to generate HTML visualizations from a request.
 // It returns the generated HTML as a string and any error that is encountered.
 func (s *VisualizationServer) generateVisualizationFromRequest(request *go_client.CreateVisualizationRequest) ([]byte, error) {
-	if !s.isServiceAvailable {
+	if !isVisualizationServiceAlive(s.serviceURL) {
 		return nil, util.NewInternalServerError(
 			fmt.Errorf("service not available"),
 			"Service not available",
@@ -90,33 +87,19 @@ func (s *VisualizationServer) generateVisualizationFromRequest(request *go_clien
 	return body, nil
 }
 
-func isVisualizationServiceAlive(serviceURL string, initConnectionTimeout time.Duration) bool {
-	var operation = func() error {
-		_, err := http.Get(serviceURL)
-		if err != nil {
-			glog.Error("Unable to verify visualization service is alive!", err)
-			return err
-		}
-		return nil
+func isVisualizationServiceAlive(serviceURL string) bool {
+	resp, err := http.Get(serviceURL)
+	if err != nil {
+		glog.Error("Unable to verify visualization service is alive!", err)
+		return false
 	}
-	b := backoff.NewExponentialBackOff()
-	b.MaxElapsedTime = initConnectionTimeout
-	err := backoff.Retry(operation, b)
-	return err == nil
+	return resp.StatusCode == http.StatusOK
 }
 
-func NewVisualizationServer(resourceManager *resource.ResourceManager, serviceHost string, servicePort string, initConnectionTimeout time.Duration) *VisualizationServer {
+func NewVisualizationServer(resourceManager *resource.ResourceManager, serviceHost string, servicePort string) *VisualizationServer {
 	serviceURL := fmt.Sprintf("http://%s:%s", serviceHost, servicePort)
-	isServiceAvailable := isVisualizationServiceAlive(serviceURL, initConnectionTimeout)
 	return &VisualizationServer{
 		resourceManager:    resourceManager,
 		serviceURL:         serviceURL,
-		// TODO: isServiceAvailable is used to determine if the new visualization
-		//  service is alive. If this is true, then the service is alive and
-		//  requests can be made to it. Otherwise, if it is false, the service is
-		//  not alive and requests should not be made. This prevents timeouts and
-		//  counteracts current instabilities with the service. This should be
-		//  removed after the visualization service is deemed stable.
-		isServiceAvailable: isServiceAvailable,
 	}
 }
