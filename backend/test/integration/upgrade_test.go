88fe2a3447bfdbbfad015ec44ca8050e39166c4b
Yuan Gong <gongyuan94@gmail.com>
2019-09-03 04:20:08 +0000

    Restrucutre upgrade_test.go to focus the test on upgrade verification

--- backend/test/integration/upgrade_test.go
+++ backend/test/integration/upgrade_test.go
@@ -1,7 +1,6 @@
 package integration
 
 import (
-	"fmt"
 	"testing"
 
 	"time"
@@ -16,16 +15,17 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-type ExperimentsTest struct {
+// Methods are organized into two types: "prepare" and "verify".
+// "prepare" tests setup resources before upgrade
+// "verify" tests verifies resources are expected after upgrade
+type UpgradeTests struct {
 	suite.Suite
 	namespace        string
 	experimentClient *api_server.ExperimentClient
 }
 
 // Check the namespace have ML job installed and ready
-func (s *ExperimentsTest) SetupSuite() {
-	fmt.Println("SetupSuite started")
-
+func (s *UpgradeTests) SetupSuite() {
 	// Integration tests also run these tests to first ensure they work, so that
 	// when integration tests pass and upgrade tests fail, we know for sure
 	// upgrade process went wrong somehow.
@@ -46,125 +46,63 @@ func (s *ExperimentsTest) SetupSuite() {
 	}
 }
 
-func (s *ExperimentsTest) TestPrepareExperiments() {
-	fmt.Println("upgrade prepare test start")
+func (s *UpgradeTests) TestPrepareExperiments() {
 	t := s.T()
 
 	/* ---------- Clean up ---------- */
 	test.DeleteAllExperiments(s.experimentClient, t)
 
-	/* ---------- Verify no experiment exist ---------- */
-	experiments, totalSize, _, err := s.experimentClient.List(&params.ListExperimentParams{})
-	assert.Nil(t, err)
-	assert.Equal(t, 0, totalSize)
-	assert.True(t, len(experiments) == 0)
-
 	/* ---------- Create a new experiment ---------- */
 	experiment := &experiment_model.APIExperiment{Name: "training", Description: "my first experiment"}
-	trainingExperiment, err := s.experimentClient.Create(&params.CreateExperimentParams{
+	_, err := s.experimentClient.Create(&params.CreateExperimentParams{
 		Body: experiment,
 	})
 	assert.Nil(t, err)
-	expectedTrainingExperiment := &experiment_model.APIExperiment{
-		ID: trainingExperiment.ID, Name: experiment.Name,
-		Description: experiment.Description, CreatedAt: trainingExperiment.CreatedAt}
-	assert.Equal(t, expectedTrainingExperiment, trainingExperiment)
 
 	/* ---------- Create a few more new experiment ---------- */
-	// 1 second interval. This ensures they can be sorted by create time in expected order.
+	// This ensures they can be sorted by create time in expected order.
 	time.Sleep(1 * time.Second)
 	experiment = &experiment_model.APIExperiment{Name: "prediction", Description: "my second experiment"}
 	_, err = s.experimentClient.Create(&params.CreateExperimentParams{
 		Body: experiment,
 	})
+	assert.Nil(t, err)
+
 	time.Sleep(1 * time.Second)
-	experiment = &experiment_model.APIExperiment{Name: "moonshot", Description: "my second experiment"}
+	experiment = &experiment_model.APIExperiment{Name: "moonshot", Description: "my third experiment"}
 	_, err = s.experimentClient.Create(&params.CreateExperimentParams{
 		Body: experiment,
 	})
 	assert.Nil(t, err)
 }
 
-func (s *ExperimentsTest) TestVerifyExperiments() {
-	fmt.Println("verify start")
+func (s *UpgradeTests) TestVerifyExperiments() {
 	t := s.T()
 
-	/* ---------- Verify list experiments works ---------- */
-	experiments, totalSize, nextPageToken, err := s.experimentClient.List(&params.ListExperimentParams{})
-	assert.Nil(t, err)
-	assert.Equal(t, 3, totalSize)
-	assert.Equal(t, 3, len(experiments))
-	var trainingExperiment *experiment_model.APIExperiment
-	for _, e := range experiments {
-		// Sampling one of the experiments and verify the result is expected.
-		if e.Name == "training" {
-			trainingExperiment = e
-		}
-	}
-	assert.NotNil(t, trainingExperiment)
-	assert.Equal(t, trainingExperiment.Description, "my first experiment")
-
-	/* ---------- Verify list experiments sorted by names ---------- */
-	experiments, totalSize, nextPageToken, err = s.experimentClient.List(&params.ListExperimentParams{
-		PageSize: util.Int32Pointer(2), SortBy: util.StringPointer("name")})
-	assert.Nil(t, err)
-	assert.Equal(t, 3, totalSize)
-	fmt.Println("len = ", len(experiments))
-	assert.Equal(t, 2, len(experiments))
-	assert.Equal(t, "moonshot", experiments[0].Name)
-	assert.Equal(t, "prediction", experiments[1].Name)
-	assert.NotEmpty(t, nextPageToken)
-
-	experiments, totalSize, nextPageToken, err = s.experimentClient.List(&params.ListExperimentParams{
-		PageToken: util.StringPointer(nextPageToken), PageSize: util.Int32Pointer(2), SortBy: util.StringPointer("name")})
-	assert.Nil(t, err)
-	assert.Equal(t, 3, totalSize)
-	assert.Equal(t, 1, len(experiments))
-	assert.Equal(t, "training", experiments[0].Name)
-	assert.Empty(t, nextPageToken)
-
 	/* ---------- Verify list experiments sorted by creation time ---------- */
-	experiments, totalSize, nextPageToken, err = s.experimentClient.List(&params.ListExperimentParams{
-		PageSize: util.Int32Pointer(2), SortBy: util.StringPointer("created_at")})
+	experiments, _, _, err := s.experimentClient.List(
+		&params.ListExperimentParams{SortBy: util.StringPointer("created_at")})
 	assert.Nil(t, err)
-	assert.Equal(t, 3, totalSize)
-	assert.Equal(t, 2, len(experiments))
+	// after upgrade, default experiment may be inserted, but the oldest 3
+	// experiments should be the ones created in this test
+	assert.True(t, len(experiments) >= 3)
+
 	assert.Equal(t, "training", experiments[0].Name)
+	assert.Equal(t, "my first experiment", experiments[0].Description)
+	assert.NotEmpty(t, experiments[0].ID)
+	assert.NotEmpty(t, experiments[0].CreatedAt)
+
 	assert.Equal(t, "prediction", experiments[1].Name)
-	assert.NotEmpty(t, nextPageToken)
+	assert.Equal(t, "my second experiment", experiments[1].Description)
+	assert.NotEmpty(t, experiments[1].ID)
+	assert.NotEmpty(t, experiments[1].CreatedAt)
 
-	experiments, totalSize, nextPageToken, err = s.experimentClient.List(&params.ListExperimentParams{
-		PageToken: util.StringPointer(nextPageToken), PageSize: util.Int32Pointer(2), SortBy: util.StringPointer("created_at")})
-	assert.Nil(t, err)
-	assert.Equal(t, 3, totalSize)
-	assert.Equal(t, 1, len(experiments))
-	assert.Equal(t, "moonshot", experiments[0].Name)
-	assert.Empty(t, nextPageToken)
-
-	/* ---------- List experiments sorted by names descend order ---------- */
-	experiments, totalSize, nextPageToken, err = s.experimentClient.List(&params.ListExperimentParams{
-		PageSize: util.Int32Pointer(2), SortBy: util.StringPointer("name desc")})
-	assert.Nil(t, err)
-	assert.Equal(t, 3, totalSize)
-	assert.Equal(t, 2, len(experiments))
-	assert.Equal(t, "training", experiments[0].Name)
-	assert.Equal(t, "prediction", experiments[1].Name)
-	assert.NotEmpty(t, nextPageToken)
-
-	experiments, totalSize, nextPageToken, err = s.experimentClient.List(&params.ListExperimentParams{
-		PageToken: util.StringPointer(nextPageToken), PageSize: util.Int32Pointer(2), SortBy: util.StringPointer("name desc")})
-	assert.Nil(t, err)
-	assert.Equal(t, 3, totalSize)
-	assert.Equal(t, 1, len(experiments))
-	assert.Equal(t, "moonshot", experiments[0].Name)
-	assert.Empty(t, nextPageToken)
-
-	/* ---------- Verify get experiment works ---------- */
-	experiment, err := s.experimentClient.Get(&params.GetExperimentParams{ID: trainingExperiment.ID})
-	assert.Nil(t, err)
-	assert.Equal(t, trainingExperiment, experiment)
+	assert.Equal(t, "moonshot", experiments[2].Name)
+	assert.Equal(t, "my third experiment", experiments[2].Description)
+	assert.NotEmpty(t, experiments[2].ID)
+	assert.NotEmpty(t, experiments[2].CreatedAt)
 }
 
-func TestExperiments(t *testing.T) {
-	suite.Run(t, new(ExperimentsTest))
+func TestUpgrade(t *testing.T) {
+	suite.Run(t, new(UpgradeTests))
 }
