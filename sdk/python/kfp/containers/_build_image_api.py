65314754f2198f3b4f49d5d9ca4d83e72c49dfe6
Alexey Volkov <avolkov@google.com>
2019-09-03 18:41:11 -0700

    Switched the default base image to Google Deep Learning container image as requested by @hongye-sun The size of this image is 4.35GB which really concerns me. The GPU image size is 6.45GB.

--- sdk/python/kfp/containers/_build_image_api.py
+++ sdk/python/kfp/containers/_build_image_api.py
@@ -35,7 +35,8 @@ def get_python_image_for_current_version() -> str:
     return 'python' + ':{}.{}.{}'.format(sys.version_info[0], sys.version_info[1], sys.version_info[2])
 
 
-default_base_image = get_python_image_for_current_version()
+#default_base_image = get_python_image_for_current_version()
+default_base_image = 'gcr.io/deeplearning-platform-release/tf-cpu.1-14' # The size of this image is 4.35GB which really concerns me. The GPU image size is 6.45GB.
 
 
 _container_work_dir = '/python_env'
@@ -78,7 +79,7 @@ def build_image_from_working_dir(image_name : str = None, working_dir : str = No
         working_dir: Optional. The directory that will be captured. The current directory will be used if omitted.
         file_filter_re: Optional. A regular expression that will be used to decide which files to include in the container building context.
         timeout: Optional. The image building timeout in seconds.
-        base_image: Optional. The container image to use as the base for the new image. If not set, the python:x.y.z image corresponding to the current python version will be used.
+        base_image: Optional. The container image to use as the base for the new image. If not set, the Google Deep Learning Tensorflow CPU image will be used.
         builder: Optional. An instance of ContainerBuilder or compatible class that will be used to build the image.
     Returns:
         The full name of the container image including the hash digest. E.g. gcr.io/my-org/my-image@sha256:86c1...793c.
@@ -100,9 +101,11 @@ def build_image_from_working_dir(image_name : str = None, working_dir : str = No
         src_dockerfile_path = os.path.join(current_dir, 'Dockerfile')
         dst_dockerfile_path = os.path.join(context_dir, 'Dockerfile')
         if os.path.exists(src_dockerfile_path):
+            if base_image:
+                raise ValueError('Cannot specify base_image when using custom Dockerfile (which already specifies the base image).')
             shutil.copy(src_dockerfile_path, dst_dockerfile_path)
         else:
-            dockerfile_text = generate_dockerfile_text(context_dir, dst_dockerfile_path)
+            dockerfile_text = generate_dockerfile_text(context_dir, dst_dockerfile_path, base_image)
             with open(dst_dockerfile_path, 'w') as f:
                 f.write(dockerfile_text)
 
