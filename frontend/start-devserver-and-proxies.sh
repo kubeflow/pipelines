654267e377d7f8394491d6a3325fe5b8570f8012
Yuan Gong <gongyuan94@gmail.com>
2019-09-16 09:50:39 +0800

    Add error handling for pod discovery

--- frontend/start-devserver-and-proxies.sh
+++ frontend/start-devserver-and-proxies.sh
@@ -14,9 +14,18 @@ echo "Preparing dev env for KFP frontend"
 
 echo "Detecting api server pod names..."
 METADATA_ENVOY_POD=$(kubectl get pods -n $NAMESPACE -l component=metadata-envoy -o=custom-columns=:.metadata.name --no-headers)
+if [ -z "$METADATA_ENVOY_POD" ]; then
+  echo "Couldn't get metadata envoy pod in namespace $NAMESPACE, double check the cluster your kubectl talks to."
+  exit 1
+fi
 echo "Metadata envoy pod is $METADATA_ENVOY_POD"
-BACKEND_POD=$(kubectl get pods -n $NAMESPACE -l app=ml-pipeline -o=custom-columns=:.metadata.name --no-headers)
-echo "Ml pipeline backend api pod is $BACKEND_POD"
+
+PIPELINE_API_POD=$(kubectl get pods -n $NAMESPACE -l app=ml-pipeline -o=custom-columns=:.metadata.name --no-headers)
+if [ -z "$PIPELINE_API_POD" ]; then
+  echo "Couldn't get pipeline api pod in namespace $NAMESPACE, double check the cluster your kubectl talks to."
+  exit 1
+fi
+echo "Ml pipeline PIPELINE_API api pod is $PIPELINE_API_POD"
 
 echo "Compiling node server..."
 pushd server
@@ -34,7 +43,7 @@ popd
 
 echo "Starting to port forward backend apis..."
 kubectl port-forward -n kubeflow $METADATA_ENVOY_POD 9090:9090 &
-kubectl port-forward -n kubeflow $BACKEND_POD 3002:8888 &
+kubectl port-forward -n kubeflow $PIPELINE_API_POD 3002:8888 &
 ML_PIPELINE_SERVICE_PORT=3002 npm run mock:server 3001 &
 
 echo "Starting to run webpack dev server..."
