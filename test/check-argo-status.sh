189edc85c2979a6a441555bfed2490e46069a05c
Alexey Volkov <avolkov@google.com>
2019-09-19 17:22:18 -0700

    Further improved argo logging in case of failure or timeout

--- test/check-argo-status.sh
+++ test/check-argo-status.sh
@@ -30,32 +30,36 @@ do
   echo $WORKFLOW_STATUS | grep ${WORKFLOW_COMPLETE_KEYWORD} && s=0 && break || s=$? && printf "Workflow ${ARGO_WORKFLOW} is not finished.\n${WORKFLOW_STATUS}\nSleep for 20 seconds...\n" && sleep 20
 done
 
-# Check whether the argo workflow finished or not and exit if not.
+if [[ $s == 0 ]]; then
+  echo "Argo workflow finished."
+  if [[ ! -z "$TEST_RESULT_FOLDER" ]]; then
+    echo "Copy test result"
+    mkdir -p $ARTIFACT_DIR
+    gsutil cp -r "${TEST_RESULTS_GCS_DIR}"/* "${ARTIFACT_DIR}" || true
+  fi
+  argo get ${ARGO_WORKFLOW} -n ${NAMESPACE}
+  exit 0
+fi
+
+# Handling failed workflow
 if [[ $s != 0 ]]; then
-  echo "Prow job Failed: Argo workflow timeout.."
-  echo "=========Argo Workflow Logs========="
-  argo logs -w ${ARGO_WORKFLOW} -n ${NAMESPACE}
-  echo "===================================="
-  argo get ${ARGO_WORKFLOW} -n ${NAMESPACE}
-  exit $s
-fi
-
-echo "Argo workflow finished."
-
-if [[ ! -z "$TEST_RESULT_FOLDER" ]]
-then
-  echo "Copy test result"
-  mkdir -p $ARTIFACT_DIR
-  gsutil cp -r "${TEST_RESULTS_GCS_DIR}"/* "${ARTIFACT_DIR}" || true
-fi
-
-if [[ $WORKFLOW_STATUS = *"${WORKFLOW_FAILED_KEYWORD}"* ]]; then
-  echo "Test workflow failed."
-  echo "=========Argo Workflow Logs========="
-  argo logs -w ${ARGO_WORKFLOW} -n ${NAMESPACE}
-  echo "===================================="
-  argo get ${ARGO_WORKFLOW} -n ${NAMESPACE}
-  exit 1
+  echo "Argo workflow timed out."
 else
-  argo get ${ARGO_WORKFLOW} -n ${NAMESPACE}
+  echo "Argo workflow failed."
 fi
+
+echo "=========Argo Workflow Logs========="
+argo logs -w ${ARGO_WORKFLOW} -n ${NAMESPACE}
+
+echo "=========Main workflow=============="
+argo get ${ARGO_WORKFLOW} -n ${NAMESPACE}
+
+echo "========Child workflows============="
+
+argo --namespace ${NAMESPACE} list --output=name |
+  while read workflow_id; do
+    echo "========${workflow_id}============="
+    argo get "${workflow_id}"" -n "${NAMESPACE}""
+  done
+
+exit 1
