2e3618b0515c807096455c7563269b34f928819a
Alexey Volkov <avolkov@google.com>
2019-09-25 14:32:34 -0700

    Renamed task_id_placeholder to execution_id_placeholder

--- sdk/python/kfp/dsl/__init__.py
+++ sdk/python/kfp/dsl/__init__.py
@@ -26,5 +26,5 @@ from ._ops_group import OpsGroup, ExitHandler, Condition, ParallelFor
 from ._component import python_component, graph_component, component
 from ._artifact_location import ArtifactLocation
 
-task_id_placeholder = '{{workflow.uid}}-{{pod.name}}'
+execution_id_placeholder = '{{workflow.uid}}-{{pod.name}}'
 run_id_placeholder = '{{workflow.uid}}'
