5bfcfb6930bfcd915cd0a75913ccd21e94643c38
Tommy Li <Tommy.chaoping.li@ibm.com>
2019-10-07 15:41:11 -0700

    Extend KFServing component with autoscaling and server mode (#2315)
    * extend kfs component with custom framework and autoscaling
    
    * minor update on the server code
    
    * Update pip to pip3
    
    * update naming convention
    
    * update status description

--- components/kubeflow/kfserving/sample-pipeline.py
+++ components/kubeflow/kfserving/sample-pipeline.py
@@ -25,10 +25,14 @@ def kfservingPipeline(
     action = 'create',
     model_name='tensorflow-sample',
     default_model_uri='gs://kfserving-samples/models/tensorflow/flowers',
-    canary_model_uri='gs://kfserving-samples/models/tensorflow/flowers',
+    canary_model_uri='gs://kfserving-samples/models/tensorflow/flowers-2',
     canary_model_traffic_percentage='10',
     namespace='kubeflow',
-    framework='tensorflow'
+    framework='tensorflow',
+    default_custom_model_spec='{}',
+    canary_custom_model_spec='{}',
+    autoscaling_target=0,
+    kfserving_endpoint=''
 ):
 
     # define workflow
@@ -38,7 +42,11 @@ def kfservingPipeline(
                              canary_model_uri=canary_model_uri,
                              canary_model_traffic_percentage=canary_model_traffic_percentage,
                              namespace=namespace,
-                             framework=framework)
+                             framework=framework,
+                             default_custom_model_spec=default_custom_model_spec,
+                             canary_custom_model_spec=canary_custom_model_spec,
+                             autoscaling_target=autoscaling_target,
+                             kfserving_endpoint=kfserving_endpoint)
 
 if __name__ == '__main__':
     import kfp.compiler as compiler
