c0b6b82e8f17a2f7ea46f3222bf3cddc70bd3789
Tommy Li <Tommy.chaoping.li@ibm.com>
2019-10-07 15:41:11 -0700

    Extend KFServing component with autoscaling and server mode (#2315)
    * extend kfs component with custom framework and autoscaling
    
    * minor update on the server code
    
    * Update pip to pip3
    
    * update naming convention
    
    * update status description

--- components/kubeflow/kfserving/src/kfservingdeployer.py
+++ components/kubeflow/kfserving/src/kfservingdeployer.py
@@ -14,6 +14,8 @@
 import json
 import argparse
 import os
+import requests
+import re
 
 from kubernetes import client
 
@@ -70,32 +72,12 @@ def kfserving_deployment(metadata, default_model_spec, canary_model_spec=None, c
                                                         canary_traffic_percent=canary_model_traffic))
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--action', type=str, help='Action to execute on KFServing', default='create')
-    parser.add_argument('--model-name', type=str, help='Name to give to the deployed model', default="")
-    parser.add_argument('--default-model-uri', type=str, help='Path of the S3, GCS or PVC directory containing default model.')
-    parser.add_argument('--canary-model-uri', type=str, help='Optional path of the S3, GCS or PVC directory containing canary model.', default="")
-    parser.add_argument('--canary-model-traffic', type=str, help='Optional Traffic to be sent to the default model', default='0')
-    parser.add_argument('--namespace', type=str, help='Kubernetes namespace where the KFServing service is deployed.', default='kubeflow')
-    parser.add_argument('--framework', type=str, help='Model Serving Framework', default='tensorflow')
-    parser.add_argument('--default-custom-model-spec', type=json.loads, help='Custom runtime default custom model container spec', default={})
-    parser.add_argument('--canary-custom-model-spec', type=json.loads, help='Custom runtime canary custom model container spec', default={})
-    parser.add_argument('--output_path', type=str, help='Path to store URI output')
-    args = parser.parse_args()
-
-    action = args.action.lower()
-    model_name = args.model_name
-    default_model_uri = args.default_model_uri
-    canary_model_uri = args.canary_model_uri
-    canary_model_traffic = int(args.canary_model_traffic)
-    namespace = args.namespace
-    framework = args.framework.lower()
-    output_path = args.output_path
-    default_custom_model_spec = args.default_custom_model_spec
-    canary_custom_model_spec = args.canary_custom_model_spec
-
-    metadata = client.V1ObjectMeta(name=model_name, namespace=namespace)
+def deploy_model(action, model_name, default_model_uri, canary_model_uri, canary_model_traffic, namespace, framework, default_custom_model_spec, canary_custom_model_spec, autoscaling_target=0):
+    if int(autoscaling_target) != 0:
+        annotations = {"autoscaling.knative.dev/target": str(autoscaling_target)}
+    else:
+        annotations = None
+    metadata = client.V1ObjectMeta(name=model_name, namespace=namespace, annotations=annotations)
     if framework != 'custom':
         default_model_spec = ModelSpec(framework, default_model_uri)
     else:
@@ -122,8 +104,76 @@ if __name__ == "__main__":
         raise("Error: No matching action: " + action)
 
     model_status = KFServing.get(model_name, namespace=namespace)
-    print(model_status)
+    return model_status
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--action', type=str, help='Action to execute on KFServing', default='create')
+    parser.add_argument('--model-name', type=str, help='Name to give to the deployed model', default="")
+    parser.add_argument('--default-model-uri', type=str, help='Path of the S3, GCS or PVC directory containing default model.')
+    parser.add_argument('--canary-model-uri', type=str, help='Optional path of the S3, GCS or PVC directory containing canary model.', default="")
+    parser.add_argument('--canary-model-traffic', type=str, help='Optional Traffic to be sent to the default model', default='0')
+    parser.add_argument('--namespace', type=str, help='Kubernetes namespace where the KFServing service is deployed.', default='kubeflow')
+    parser.add_argument('--framework', type=str, help='Model Serving Framework', default='tensorflow')
+    parser.add_argument('--default-custom-model-spec', type=json.loads, help='Custom runtime default custom model container spec', default={})
+    parser.add_argument('--canary-custom-model-spec', type=json.loads, help='Custom runtime canary custom model container spec', default={})
+    parser.add_argument('--kfserving-deployer-api', type=str, help='kfserving remote deployer api endpoint', default='')
+    parser.add_argument('--autoscaling-target', type=str, help='Autoscaling target number', default='0')
+    parser.add_argument('--output_path', type=str, help='Path to store URI output')
+    args = parser.parse_args()
+
+    url = re.compile(r"https?://")
+
+    action = args.action.lower()
+    model_name = args.model_name
+    default_model_uri = args.default_model_uri
+    canary_model_uri = args.canary_model_uri
+    canary_model_traffic = int(args.canary_model_traffic)
+    namespace = args.namespace
+    framework = args.framework.lower()
+    output_path = args.output_path
+    default_custom_model_spec = args.default_custom_model_spec
+    canary_custom_model_spec = args.canary_custom_model_spec
+    kfserving_deployer_api = url.sub('', args.kfserving_deployer_api)
+    autoscaling_target = int(args.autoscaling_target)
+
+    if kfserving_deployer_api:
+        formData = {
+            "action": action,
+            "model_name": model_name,
+            "default_model_uri": default_model_uri,
+            "canary_model_uri": canary_model_uri,
+            "canary_model_traffic": canary_model_traffic,
+            "namespace": namespace,
+            "framework": framework,
+            "default_custom_model_spec": default_custom_model_spec,
+            "canary_custom_model_spec": canary_custom_model_spec,
+            "autoscaling_target": autoscaling_target
+            }
+        response = requests.post("http://" + kfserving_deployer_api + "/deploy-model", json=formData)
+        model_status = response.json()
+    else:
+        model_status = deploy_model(
+            action=action,
+            model_name=model_name,
+            default_model_uri=default_model_uri,
+            canary_model_uri=canary_model_uri,
+            canary_model_traffic=canary_model_traffic,
+            namespace=namespace,
+            framework=framework,
+            default_custom_model_spec=default_custom_model_spec,
+            canary_custom_model_spec=canary_custom_model_spec,
+            autoscaling_target=autoscaling_target
+        )
+    print(model_status)
+    try:
+        print(model_status['status']['url'] + ' is the knative domain header. $ISTIO_INGRESS_ENDPOINT are defined in the below commands')
+        print('Sample test commands: ')
+        print('# Note: If Istio Ingress gateway is not served with LoadBalancer, use $CLUSTER_NODE_IP:31380 as the ISTIO_INGRESS_ENDPOINT')
+        print('ISTIO_INGRESS_ENDPOINT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath=\'{.status.loadBalancer.ingress[0].ip}\')')
+        print('curl -X GET -H "Host: ' + url.sub('', model_status['status']['url']) + '" $ISTIO_INGRESS_ENDPOINT')
+    except:
+        print('Model is not ready, check the logs for the Knative URL status.')
     if not os.path.exists(os.path.dirname(output_path)):
         os.makedirs(os.path.dirname(output_path))
     with open(output_path, "w") as report:
