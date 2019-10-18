aab5a501207e6b9b02813cdd9b0b10c3ef5d09e0
hongye-sun <43763191+hongye-sun@users.noreply.github.com>
2019-10-18 12:19:08 -0700

    Update sdk release version (#2434)
    * Update sdk release version
    
    * Update the server image version

--- sdk/python/kfp/__init__.py
+++ sdk/python/kfp/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.1.31.2'
+__version__ = '0.1.32'
 
 from . import components
 from . import containers
