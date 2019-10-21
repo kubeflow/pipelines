5043ec78afc63506ce70fca01d2936928cb0fedb
Alexey Volkov <avolkov@google.com>
2019-10-21 15:45:23 -0700

    SDK - Setup - Added cloudpickle to requirements (#2437)
    * SDK - Setup - Added cloudpickle to requirements
    
    * Specifying the exact cloudpickle version

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -31,7 +31,7 @@ REQUIRES = [
     'cryptography>=2.4.2',
     'google-auth>=1.6.1',
     'requests_toolbelt>=0.8.0',
-    'cloudpickle',
+    'cloudpickle==1.1.1',
     'kfp-server-api >= 0.1.18, <= 0.1.25',  #Update the upper version whenever a new version of the kfp-server-api package is released. Update the lower version when there is a breaking change in kfp-server-api.
     'argo-models == 2.2.1a',  #2.2.1a is equivalent to argo 2.2.1
     'jsonschema >= 3.0.1',
