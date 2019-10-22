8433cb9f46f17d0813d28969a37c82930e4ea3cc
Yuan Gong <gongyuan94@gmail.com>
2019-10-22 17:17:16 +0800

    Add frontend format check to travis CI, pin prettier version

---
+++ frontend/check-format-error-info.js
@@ -0,0 +1,8 @@
+console.error();
+console.error('=================');
+console.error('Please use `npm run format` to format your code.');
+console.error(
+  'Refer to https://github.com/kubeflow/pipelines/tree/master/frontend#code-style for our code style guidance.',
+);
+console.error();
+process.exit(1);
