8e8eddde74522b0921d06350fddc6e52849bb96b
Yuan (Bob) Gong <gongyuan94@gmail.com>
2019-11-14 00:17:12 +0800

    Release 9670cc1aadfbbed9c52b84ea859ea97aa81213ad (#2599)
    * Updated component images to version 9670cc1aadfbbed9c52b84ea859ea97aa81213ad
    
    * Updated components to version caa2dc56f29b0dce5216bec390b1685fc0cdc4b7

--- components/gcp/bigquery/query/README.md
+++ components/gcp/bigquery/query/README.md
@@ -89,7 +89,7 @@ KFP_PACKAGE = 'https://storage.googleapis.com/ml-pipeline/release/0.1.14/kfp.tar
 import kfp.components as comp
 
 bigquery_query_op = comp.load_component_from_url(
-    'https://raw.githubusercontent.com/kubeflow/pipelines/02c991dd265054b040265b3dfa1903d5b49df859/components/gcp/bigquery/query/component.yaml')
+    'https://raw.githubusercontent.com/kubeflow/pipelines/caa2dc56f29b0dce5216bec390b1685fc0cdc4b7/components/gcp/bigquery/query/component.yaml')
 help(bigquery_query_op)
 ```
 
--- components/kubeflow/launcher/kubeflow_tfjob_launcher_op.py
+++ components/kubeflow/launcher/kubeflow_tfjob_launcher_op.py
@@ -17,7 +17,7 @@ from kfp import dsl
 def kubeflow_tfjob_launcher_op(container_image, command, number_of_workers: int, number_of_parameter_servers: int, tfjob_timeout_minutes: int, output_dir=None, step_name='TFJob-launcher'):
     return dsl.ContainerOp(
         name = step_name,
-        image = 'gcr.io/ml-pipeline/ml-pipeline-kubeflow-tf:d6d9d8da19f7110fff3a5ba713710402edaeee65',
+        image = 'gcr.io/ml-pipeline/ml-pipeline-kubeflow-tf:9670cc1aadfbbed9c52b84ea859ea97aa81213ad',
         arguments = [
             '--workers', number_of_workers,
             '--pss', number_of_parameter_servers,
