9d1fbf1f1893b2d2de1559c5d00159f4a855219a
numerology <jxzheng@google.com>
2019-12-19 13:32:38 -0800

    update

--- components/gcp/container/component_sdk/python/kfp_component/launcher/__main__.py
+++ components/gcp/container/component_sdk/python/kfp_component/launcher/__main__.py
@@ -28,18 +28,18 @@ def main():
     parser.add_argument('file_or_module', type=str,
         help='Either a python file path or a module name.')
     parser.add_argument(
-        '--ui-metadata-path',
+        'ui_metadata_path',
         type=str,
         default='/mlpipeline-ui-metadata.json',
         help='Path for the file where the mlpipeline-ui-metadata.json data '
              'should be written.')
-    parser.add_argument('args', nargs=argparse.REMAINDER)
+    parser.add_argument('args', args=argparse.REMAINDER)
     args = parser.parse_args()
 
     if args.ui_metadata_path:
         os.environ['KFP_UI_METADATA_PATH'] = args.ui_metadata_path
 
-    launch(args.file_or_module, args.args)
+    launch(args.file_or_module, [args.args, args.ui_metadata_path])
 
 if __name__ == '__main__':
     main()
