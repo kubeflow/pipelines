60af173b735a8ea3c6d5fc3158c2c3d58b0711c8
drewbutlerbb4 <Andrew.Butler@ibm.com>
2020-01-03 15:33:24 -0800

    Add EnhancedTextField sub component

--- frontend/src/components/NewRunParameters.tsx
+++ frontend/src/components/NewRunParameters.tsx
@@ -30,10 +30,6 @@ export interface NewRunParametersProps {
   handleParamChange: (index: number, value: string) => void;
 }
 
-export interface NewRunParametersState {
-  isBeingEdited: { [key: number]: boolean };
-}
-
 const css = stylesheet({
   key: {
     color: color.strong,
@@ -51,13 +47,9 @@ const css = stylesheet({
   },
 });
 
-class NewRunParameters extends React.Component<NewRunParametersProps, NewRunParametersState> {
+class NewRunParameters extends React.Component<NewRunParametersProps> {
   constructor(props: any) {
     super(props);
-
-    this.state = {
-      isBeingEdited: {},
-    };
   }
 
   public render(): JSX.Element | null {
@@ -70,116 +62,148 @@ class NewRunParameters extends React.Component<NewRunParametersProps, NewRunPara
         {!!initialParams.length && (
           <div>
             {initialParams.map((param, i) => {
-              let isJson = true;
-              let displayValue = param.value || '';
-              try {
-                displayValue = JSON.parse(param.value || '');
-                // Nulls, booleans, strings, and numbers can all be parsed as JSON, but we don't care
-                // about rendering. Note that `typeOf null` returns 'object'
-                if (displayValue === null || typeof displayValue !== 'object') {
-                  throw new Error(
-                    'Parsed JSON was neither an array nor an object. Using default renderer',
-                  );
-                }
-
-                if (typeof this.state.isBeingEdited[i] === 'undefined') {
-                  this.state.isBeingEdited[i] = false;
-                }
-              } catch (err) {
-                isJson = false;
-              }
-              if (isJson || typeof this.state.isBeingEdited[i] !== 'undefined') {
-                return (
-                  <div key={i}>
-                    <TextField
-                      id={`newRunPipelineParam${i}`}
-                      disabled={this.state.isBeingEdited[i]}
-                      variant='outlined'
-                      label={param.name}
-                      value={param.value || ''}
-                      onChange={ev => handleParamChange(i, ev.target.value || '')}
-                      style={{ maxWidth: 600 }}
-                      className={commonCss.textField}
-                      InputProps={{
-                        classes: { disabled: css.nonEditableInput },
-                        endAdornment: (
-                          <InputAdornment position='end'>
-                            <Button
-                              color='secondary'
-                              id='chooseExperimentBtn'
-                              onClick={() => {
-                                if (isJson) {
-                                  if (this.state.isBeingEdited[i]) {
-                                    handleParamChange(i, JSON.stringify(displayValue) || '');
-                                  } else {
-                                    handleParamChange(
-                                      i,
-                                      JSON.stringify(displayValue, null, 2) || '',
-                                    );
-                                  }
-                                }
-
-                                if (this.state.isBeingEdited[i]) {
-                                  this.setState({
-                                    isBeingEdited: {
-                                      ...this.state.isBeingEdited,
-                                      [i]: false,
-                                    },
-                                  });
-                                } else {
-                                  this.setState({
-                                    isBeingEdited: {
-                                      ...this.state.isBeingEdited,
-                                      [i]: true,
-                                    },
-                                  });
-                                }
-                              }}
-                              style={{ padding: '3px 5px', margin: 0 }}
-                            >
-                              {this.state.isBeingEdited[i] ? 'Close Editor' : 'Open Editor'}
-                            </Button>
-                          </InputAdornment>
-                        ),
-                        readOnly: false,
-                      }}
-                    />
-                    {this.state.isBeingEdited[i] && (
-                      <div className={css.row}>
-                        <Editor
-                          width='100%'
-                          height='300px'
-                          mode='json'
-                          theme='github'
-                          highlightActiveLine={true}
-                          showGutter={true}
-                          readOnly={false}
-                          onChange={text => handleParamChange(i, text || '')}
-                          value={param.value || ''}
-                        />
-                      </div>
-                    )}
-                  </div>
-                );
-              } else {
-                return (
-                  <TextField
-                    id={`newRunPipelineParam${i}`}
-                    key={i}
-                    variant='outlined'
-                    label={param.name}
-                    value={param.value || ''}
-                    onChange={ev => handleParamChange(i, ev.target.value || '')}
-                    style={{ maxWidth: 600 }}
-                    className={commonCss.textField}
-                  />
-                );
-              }
+              return (
+                <EnhancedTextField
+                  key={i}
+                  param={param}
+                  index={i}
+                  handleParamChange={handleParamChange}
+                />
+              );
             })}
           </div>
         )}
       </div>
     );
+  }
+}
+
+interface EnhancedTextFieldProps {
+  param: ApiParameter;
+  index: number;
+  handleParamChange: (index: number, value: string) => void;
+}
+
+interface EnhancedTextFieldState {
+  isEditorOpen: boolean;
+  isInJsonForm: boolean;
+  isJsonField: boolean;
+}
+
+class EnhancedTextField extends React.Component<EnhancedTextFieldProps, EnhancedTextFieldState> {
+
+  public static getDerivedStateFromProps(nextProps: EnhancedTextFieldProps, prevState: EnhancedTextFieldState): EnhancedTextFieldState {
+    let isJson = true;
+    try {
+      const displayValue = JSON.parse(nextProps.param.value || '');
+      // Nulls, booleans, strings, and numbers can all be parsed as JSON, but we don't care
+      // about rendering. Note that `typeOf null` returns 'object'
+      if (displayValue === null || typeof displayValue !== 'object') {
+        throw new Error(
+          'Parsed JSON was neither an array nor an object. Using default renderer',
+        );
+      }
+    } catch (err) {
+      isJson = false;
+    }
+    return {
+      isEditorOpen: prevState.isEditorOpen,
+      isInJsonForm: isJson,
+      isJsonField: prevState.isJsonField || isJson
+    };
+  }
+
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      isEditorOpen: false,
+      isInJsonForm: false,
+      isJsonField: false
+    };
+  }
+
+  public render(): JSX.Element | null {
+    const {param, index, handleParamChange} = this.props;
+
+    if (this.state.isJsonField){
+      return (
+        <>
+          <TextField
+            id={`newRunPipelineParam${index}`}
+            disabled={this.state.isEditorOpen}
+            variant='outlined'
+            label={param.name}
+            value={param.value || ''}
+            onChange={ev => handleParamChange(index, ev.target.value || '')}
+            style={{ maxWidth: 600 }}
+            className={commonCss.textField}
+            InputProps={{
+              classes: { disabled: css.nonEditableInput },
+              endAdornment: (
+                <InputAdornment position='end'>
+                  <Button
+                    color='secondary'
+                    id='chooseExperimentBtn'
+                    onClick={() => {
+                      if (this.state.isInJsonForm) {
+                        const displayValue = JSON.parse(param.value || '');
+                        if (this.state.isEditorOpen) {
+                          handleParamChange(index, JSON.stringify(displayValue) || '');
+                        } else {
+                          handleParamChange(
+                            index,
+                            JSON.stringify(displayValue, null, 2) || '',
+                          );
+                        }
+                      }
+                      this.setState({
+                        isEditorOpen: !this.state.isEditorOpen,
+                        isInJsonForm: this.state.isInJsonForm,
+                        isJsonField: this.state.isJsonField
+                      });
+                    }}
+                    style={{ padding: '3px 5px', margin: 0 }}
+                  >
+                    {this.state.isEditorOpen ? 'Close Editor' : 'Open Editor'}
+                  </Button>
+                </InputAdornment>
+              ),
+              readOnly: false,
+            }}
+          />
+          {this.state.isEditorOpen && 
+            <div className={css.row}>
+              <Editor
+                width='100%'
+                minLines={3}
+                maxLines={20}
+                mode='json'
+                theme='github'
+                highlightActiveLine={true}
+                showGutter={true}
+                readOnly={false}
+                onChange={text => handleParamChange(index, text || '')}
+                value={param.value || ''}
+              />
+            </div>
+          }
+        </>
+      );
+    } else {
+      return (
+        <TextField
+          id={`newRunPipelineParam${index}`}
+          key={index}
+          variant='outlined'
+          label={param.name}
+          value={param.value || ''}
+          onChange={ev => handleParamChange(index, ev.target.value || '')}
+          style={{ maxWidth: 600 }}
+          className={commonCss.textField}
+        />
+      );
+    }
   }
 }
 
