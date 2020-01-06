dd207b1bcf1138aadeea8844de80b8d339f606fd
Ning <ngao@google.com>
2020-01-06 13:15:17 -0800

    release 0.1.39 (#2799)

--- sdk/python/kfp/__init__.py
+++ sdk/python/kfp/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.1.38'
+__version__ = '0.1.39'
 
 from . import components
 from . import containers
