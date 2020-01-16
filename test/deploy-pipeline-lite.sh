e8f409511415f48fd479e3ada49a64ba07d000e9
IronPan <panyang06231989@gmail.com>
2020-01-16 09:20:25 -0800

    Install application CRD and add pipeline application CR to pipeline standalone (#2585)
    * install application CRD and add pipeline application CR
    
    * add labels and let application manager to set ownerref
    
    * fix
    
    * address comments
    
    * update test
    
    * update test
    
    * update test
    
    * update readme
    
    * fix test
    
    * update
    
    * update
    
    * update
    
    * Update application-crd.yaml
    
    * fix
    
    * fix
    
    * Update .release.cloudbuild.yaml
    
    * update tests
    
    * Update kustomization.yaml
    
    * Update deploy-pipeline-lite.sh
    
    * Update ml-pipeline-viewer-crd-sa.yaml
    
    * update tests
    
    * update tests
    
    * update tests

--- test/deploy-pipeline-lite.sh
+++ test/deploy-pipeline-lite.sh
@@ -37,7 +37,14 @@ fi
 kubectl delete namespace argo --wait || echo "No argo installed"
 
 KFP_MANIFEST_DIR=${DIR}/manifests
-pushd ${KFP_MANIFEST_DIR}
+
+pushd ${KFP_MANIFEST_DIR}/crd
+kustomize build . | kubectl apply -f -
+popd
+
+kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
+
+pushd ${KFP_MANIFEST_DIR}/dev
 
 # This is the recommended approach to do this.
 # reference: https://github.com/kubernetes-sigs/kustomize/blob/master/docs/eschewedFeatures.md#build-time-side-effects-from-cli-args-or-env-variables
