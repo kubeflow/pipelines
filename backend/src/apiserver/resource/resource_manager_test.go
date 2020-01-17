bf9a78ad006b864dfc922c5d02716c6b51e3c7d2
eterna2 <eterna2@hotmail.com>
2020-01-17 09:28:39 +0800

    fix fake bad object store.

--- backend/src/apiserver/resource/resource_manager_test.go
+++ backend/src/apiserver/resource/resource_manager_test.go
@@ -43,6 +43,11 @@ func initEnvVars() {
 
 type FakeBadObjectStore struct{}
 
+
+func (m *FakeBadObjectStore) GetPipelineKey(pipelineID string) string {
+	return pipelineID
+}
+
 func (m *FakeBadObjectStore) AddFile(template []byte, filePath string) error {
 	return util.NewInternalServerError(errors.New("Error"), "bad object store")
 }
