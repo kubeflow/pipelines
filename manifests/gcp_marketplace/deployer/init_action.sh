2a81213fd406bd9edbf95c19fd170631010971b6
numerology <jxzheng@google.com>
2020-01-29 14:22:46 -0800

    add gcs creation and configmap

--- manifests/gcp_marketplace/deployer/init_action.sh
+++ manifests/gcp_marketplace/deployer/init_action.sh
@@ -14,8 +14,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Helper script for auto-provision bucket in KFP MKP deployment.
+function deploy_bucket() {
+  # Helper function to deploy bucket with a unique name. The unique name is ${BASE_NAME} + random
+  # unique string.
+  #
+  # Usage:
+  # deploy_bucket BASE_NAME NUM_RETRIES
+  BASE_NAME=$1
+  NUM_RETRIES=$2
+  for i in $(seq 1 ${NUM_RETRIES})
+  do
+    bucket_is_set=true
+    bucket_name="${BASE_NAME}-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)"
+    gsutil mb "gs://${bucket_name}/" || bucket_is_set=false
+    if [ ! "$bucket_is_set" = true ]; then
+      continue
+    fi
+    # Populate configmap, with name gcs-config
+    kubectl create configmap -n "gcs-config" "${NAMESPACE}" --from-literal bucket_name="${bucket_name}"
+    break
+  done
+  if [ ! "bucket_is_set" = true ]; then
+    echo "Cannot successfully create bucket after ${NUM_RETRIES} attempts."
+    return 1
+  fi
+}
 
+# Helper script for auto-provision bucket in KFP MKP deployment.
+NAME="$(/bin/print_config.py \
+    --xtype NAME \
+    --values_mode raw)"
+NAMESPACE="$(/bin/print_config.py \
+    --xtype NAMESPACE \
+    --values_mode raw)"
+export NAME
+export NAMESPACE
+
+deploy_bucket "${NAME}-default" 10
 
 # Invoke normal deployer routine.
 /bin/bash /bin/deploy.sh
