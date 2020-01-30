762e3f31637ef6002282b7e5a3db27ae981ab53c
Rui Fang <31815555+rui5i@users.noreply.github.com>
2020-01-30 11:38:18 -0800

    [Fix][Test] Fix post submit test on mkp deployment (#2939)
    * Add mkp deployment for test
    
    * Add mkp deployment script
    
    This commit adds mkp deployment script including cluster deployment,
    argo installation and kfp mkp deployment.
    
    * Add sleep 60 after kfp deployment
    
    This commits adds sleep 60 after kfp deployment to make sure kfp
    resources are ready when running argo submit.
    
    * Typo fix
    This commit fixes zone variable and deletes extra comments.
    
    * Remove cluster deployment and argo installation in mkp script
    
    This commit removes cluster deployment and argo installation in mkp
    script. Those logic are the same with before and can use existing
    script.
    
    * Revert unrelated changes
    
    This commit reverts unrelated changes on
    test/manifests/kustomization.yaml.
    
    * Add kfp deployment options param in post submit script
    
    This commit adds kfp deployment options in post submit script. Current
    options are standalone and mkp deployment.
    
    * Remove WI set up on mkp deployment
    
    This commit removes WI set up on mkp deployment.
    
    * Fix on naming and syntax
    
    This commit fixes small naming and syntax problem pointed on PR review.
    
    * Read marketplace tools image from dockerfile
    
    This commit changes marketplace tool image name from hard-coded to be
    read from manifest.
    
    * Fix post submit test
    
    This commit fixes post submit on mkp deployment when running docker. The
    base image didn't set up avoiding using sudo when running docker.

--- test/deploy-pipeline-mkp-cli.sh
+++ test/deploy-pipeline-mkp-cli.sh
@@ -42,12 +42,12 @@ KFP_MANIFEST_DIR=${DIR}/../manifests/gcp_marketplace
 
 if ! which mpdev; then
   echo "Install mpdev"
-  docker pull gcr.io/cloud-marketplace-tools/k8s/dev
+  sudo docker pull gcr.io/cloud-marketplace-tools/k8s/dev
   mkdir -p $HOME/bin/
   touch $BIN_FILE
   export PATH=$HOME/bin:$PATH
   MKP_TOOLS_IMAGE=`grep -i "FROM" ${KFP_MANIFEST_DIR}/deployer/Dockerfile | sed 's/FROM //'`
-  docker run ${MKP_TOOLS_IMAGE} cat /scripts/dev > "$BIN_FILE"
+  sudo docker run ${MKP_TOOLS_IMAGE} cat /scripts/dev > "$BIN_FILE"
   chmod +x "$BIN_FILE"
 fi
 
@@ -61,9 +61,9 @@ export REGISTRY=gcr.io/$(gcloud config get-value project | tr ':' '/')
 export APP_NAME=$COMMIT_SHA
 export DEPLOYER_NAME=$REGISTRY/$APP_NAME/deployer
 
-docker build --tag $DEPLOYER_NAME -f deployer/Dockerfile .
+sudo docker build --tag $DEPLOYER_NAME -f deployer/Dockerfile .
 
-docker push $DEPLOYER_NAME:$GCR_IMAGE_TAG
+sudo docker push $DEPLOYER_NAME:$GCR_IMAGE_TAG
 
 # Copy rest images and rename current images
 export MKP_VERSION=$GCR_IMAGE_TAG
