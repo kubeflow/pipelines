edcbe8d08cd39fea9871c5ac70178efb20ecd0ed
Sina Chavoshi <chavoshi@google.com>
2020-01-31 09:41:14 -0800

    Updating component with execution flag

--- components/diagnostics/diagnose_me/component.py
+++ components/diagnostics/diagnose_me/component.py
@@ -1,15 +1,43 @@
+# Copyright 2020 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import NamedTuple
 
 
-def run_diagnose_me(target_apis: str, bucket: str) -> NamedTuple('Outputs', [('project_id', str), ('bucket', str)]):
-    """ Performs enviroment verification specific to this pipeline.
+def run_diagnose_me(
+    bucket: str, execution_mode: str, target_apis: str
+) -> NamedTuple('Outputs', [('bucket', str), ('project_id', str)]):
+    """ Performs environment verification specific to this pipeline.
 
-    Raises:
-        RuntimeError: If configuration is not setup properly and
-        HALT_ON_ERROR flag is set.
-    """
+      args:
+          bucket:
+              string name of the bucket to be checked. Must be of the format
+              gs://bucket_root/any/path/here/is/ignored where any path beyond root
+              is ignored.
+          execution_mode:
+              If set to HALT_ON_ERROR will case any error to raise an exception.
+              This is intended to stop the data processing of a pipeline. Can set
+              to False to only report Errors/Warnings.
+          target_apis: 
+              String consisting of a comma separated list of apis to be verified.
+      Raises:
+          RuntimeError: If configuration is not setup properly and
+          HALT_ON_ERROR flag is set.
+      """
 
-    # Installing pip3 and kfp, since the base image 'google/cloud-sdk:276.0.0' does not come with pip3 pre-installed.
+    # Installing pip3 and kfp, since the base image 'google/cloud-sdk:276.0.0'
+    # does not come with pip3 pre-installed.
     import subprocess
     subprocess.run(
         ['curl', 'https://bootstrap.pypa.io/get-pip.py', '-o', 'get-pip.py'],
@@ -36,16 +64,15 @@ def run_diagnose_me(target_apis: str, bucket: str) -> NamedTuple('Outputs', [('p
     project_id = ''
     if not project_config.has_error:
         project_id = project_config.parsed_output['core']['project']
-        print('GCP credentials are configured with access to project: %s ...\n' %
-              (project_id))
+        print('GCP credentials are configured with access to project: %s ...\n' % (project_id))
         print('Following account(s) are active under this pipeline:\n')
         subprocess.run(['gcloud', 'auth', 'list'])
         print('\n')
     else:
-        print('Project configuration is not accessible with error  %s\n' %
-              (project_config.stderr) + 'Follow the instructions at\n' +
-              'https://github.com/kubeflow/pipelines/blob/master/manifests/gcp_marketplace/guide.md#gcp-service-account-credentials \n'
-              + 'to verify you have configured the required gcp secret.', file=sys.stderr)
+        print(
+            'Project configuration is not accessible with error  %s\n' %
+            (project_config.stderr),
+            file=sys.stderr)
         config_error_observed = True
 
     # Get project buckets
@@ -53,41 +80,41 @@ def run_diagnose_me(target_apis: str, bucket: str) -> NamedTuple('Outputs', [('p
         gcp.Commands.GET_STORAGE_BUCKETS, human_readable=False)
 
     if get_project_bucket_results.has_error:
-        print('could not retrieve project buckets with error: %s' %
-              (get_project_bucket_results.stderr), file=sys.stderr)
+        print(
+            'could not retrieve project buckets with error: %s' %
+            (get_project_bucket_results.stderr),
+            file=sys.stderr)
         config_error_observed = True
 
     # Get the root of the user provided bucket i.e. gs://root.
     bucket_root = '/'.join(bucket.split('/')[0:3])
 
-    print('Checking to see if the provided GCS bucket\n  %s\nis accessible ...\n' % (bucket))
+    print(
+        'Checking to see if the provided GCS bucket\n  %s\nis accessible ...\n' %
+        (bucket))
 
     if bucket_root in get_project_bucket_results.json_output:
-        print('Provided bucket \n   %s\nis accessible within the project\n   %s.' % (
-            bucket, project_id))
-        print('\n\n')
+        print('Provided bucket \n   %s\nis accessible within the project\n   %s\n' %
+              (bucket, project_id))
 
     else:
-        print('''
-    Could not find the bucket %s in project %s
-    Please verify that you have provided the correct GCS bucket name.
-
-    Only the following buckets are visible in this project:
-    \n%s
-                '''
-              % (bucket, project_id, get_project_bucket_results.parsed_output), file=sys.stderr)
+        print('Could not find the bucket %s in project %s' % (bucket, project_id) +
+              'Please verify that you have provided the correct GCS bucket name.\n' +
+              'Only the following buckets are visible in this project:\n%s' %
+              (get_project_bucket_results.parsed_output),
+              file=sys.stderr)
         config_error_observed = True
 
     # Verify APIs that are required are enabled
-    api_config_results = gcp.get_gcp_configuration(
-        gcp.Commands.GET_APIS)
+    api_config_results = gcp.get_gcp_configuration(gcp.Commands.GET_APIS)
 
     api_status = {}
 
     if api_config_results.has_error:
-        raise RuntimeError('could not retrieve API status with error: %s' % (
-            api_config_results.stderr))
-
+        raise RuntimeError('could not retrieve API status with error: %s' %
+                           (api_config_results.stderr))
+    
+    print('Checking APIs status ...')
     for item in api_config_results.parsed_output:
         api_status[item['config']['name']] = item['state']
         # printing the results in stdout for logging purposes
@@ -100,13 +127,28 @@ def run_diagnose_me(target_apis: str, bucket: str) -> NamedTuple('Outputs', [('p
         if 'ENABLED' != api_status.get(api, 'DISABLED'):
             api_check_results = False
             error_list.append(
-                'API \"%s\" is not enabled. To enable this api go to https://pantheon.corp.google.com/apis/library/%s?project=%s' % (api, api, project_id))
+                'API \"%s\" is not enabled. To enable this api go to ' % (api) +
+                'https://pantheon.corp.google.com/apis/library/%s?project=%s' %
+                (api, project_id))
 
     if not api_check_results:
         config_error_observed = True
-        print('Required APIs are not enabled:\n' +
-              '\n'.join(error_list), file=sys.stderr)
+        print(
+            'Required APIs are not enabled:\n' + '\n'.join(error_list),
+            file=sys.stderr)
 
+    if 'HALT_ON_ERROR' in execution_mode and config_error_observed:
+        raise RuntimeError('There was an error in your environment configuration. See above for details.\n'+
+            'Note that resolving such issues generally require a deep knowledge of Kubernetes.\n'+
+            '\n'+
+            'We highly recommend that you recreate the cluster and check "Allow access ..." \n'+
+            'checkbox during cluster creation to have the cluster configured automatically.\n'+ 
+            'For more information on this and other troubleshooting instructions refer to\n'+
+            'our troubleshooting guide.\n'+
+            '\n'+
+            'If you have intentionally modified the cluster configuration, you may\n'+ 
+            'bypass this error by removing the execution_mode HALT_ON_ERROR flag.\n')
+        
     return (project_id, bucket)
 
 
