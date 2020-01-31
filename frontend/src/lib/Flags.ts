4709c6b42f678a3fb3a26d79cb7b08b7ec768f12
Yuan (Bob) Gong <gongyuan94@gmail.com>
2020-02-01 04:35:20 +0800

    [UI] Getting started page for hosted pipelines (#2935)
    * Implement getting started page.
    
    * Add feature flag to only show getting started page on hosted pipelines
    
    * Add tests
    
    * Fix format
    
    * Implement requested layout in getting started page
    
    * Minor adjust layout
    
    * Fix tests
    
    * Fix snapshots
    
    * Update page title

--- frontend/src/lib/Flags.ts
+++ frontend/src/lib/Flags.ts
@@ -1,11 +1,18 @@
 export enum Deployments {
   KUBEFLOW = 'KUBEFLOW',
+  MARKETPLACE = 'MARKETPLACE',
 }
 
 export const KFP_FLAGS = {
   DEPLOYMENT:
     // tslint:disable-next-line:no-string-literal
-    window && window['KFP_FLAGS'] && window['KFP_FLAGS']['DEPLOYMENT'] === Deployments.KUBEFLOW
-      ? Deployments.KUBEFLOW
+    window && window['KFP_FLAGS']
+      ? // tslint:disable-next-line:no-string-literal
+        window['KFP_FLAGS']['DEPLOYMENT'] === Deployments.KUBEFLOW
+        ? Deployments.KUBEFLOW
+        : // tslint:disable-next-line:no-string-literal
+        window['KFP_FLAGS']['DEPLOYMENT'] === Deployments.MARKETPLACE
+        ? Deployments.MARKETPLACE
+        : undefined
       : undefined,
 };
