cb4f4ba59e4feb06080cbd1b11b75f370f19c263
Realsen <30911434+Realsen@users.noreply.github.com>
2020-02-03 03:23:21 -0800

    Add support for automatic visualization of TFX artifacts (#2957)
    * Basic prototype
    
    * Get kfpPodName from RunDetails page
    
    * disable tfma visualization
    
    * Refactor more stuff
    
    * Remove MLMD protos
    
    * Adapt to kubeflow/frontend PR #29
    
    * Don't throw errors if artifacts and contexts are not in MLMD
    
    * Revert unnecessary changes
    
    * Update package.json to include the latest kubeflow/frontend commit
    
    * Restore changes to package.json and package-lock.json
    
    * Address comments
    
    * Use new TS syntax for concatenating lists, as per @Bobgy's suggestion
    
    * Use @Bobgy's suggestion to re-throw exceptions with error messages
    
    * Catch all exceptions and return an empty list for them.
    
    * npm run format
    
    * use === instead of ==
    
    * Apply suggestions from code review
    
    * Fix @kubeflow/frontend by using a forked fix
    
    * Fix unit test failure
    
    * Fixed error handling and code clean up
    
    * Restructure output artifact loader to change detailed implementation as local functions instead of public static methods.
    
    Co-authored-by: Yuan (Bob) Gong <gongyuan94@gmail.com>

--- frontend/src/pages/ExecutionDetails.tsx
+++ frontend/src/pages/ExecutionDetails.tsx
@@ -198,6 +198,8 @@ function parseEventsByType(
     [Event.Type.INPUT]: [],
     [Event.Type.DECLARED_OUTPUT]: [],
     [Event.Type.OUTPUT]: [],
+    [Event.Type.INTERNAL_INPUT]: [],
+    [Event.Type.INTERNAL_OUTPUT]: [],
   };
 
   if (!response) {
