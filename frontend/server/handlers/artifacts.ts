c1e3225f70715c2590bc5173293a1f33c3b727ac
eterna2 <eterna2@hotmail.com>
2020-02-06 01:08:26 +0800

    artifact endpoints supports peeking.

--- frontend/server/handlers/artifacts.ts
+++ frontend/server/handlers/artifacts.ts
@@ -30,6 +30,8 @@ interface ArtifactsQueryStrings {
   bucket: string;
   /** artifact key/path that is uri encoded.  */
   key: string;
+  /** return only the first x characters or bytes. */
+  peek?: number;
 }
 
 /**
@@ -44,7 +46,9 @@ export function getArtifactsHandler(artifactsConfigs: {
 }): Handler {
   const { aws, http, minio } = artifactsConfigs;
   return async (req, res) => {
-    const { source, bucket, key: encodedKey } = req.query as Partial<ArtifactsQueryStrings>;
+    const { source, bucket, key: encodedKey, peek = -1 } = req.query as Partial<
+      ArtifactsQueryStrings
+    >;
     if (!source) {
       res.status(500).send('Storage source is missing from artifact request');
       return;
@@ -61,31 +65,38 @@ export function getArtifactsHandler(artifactsConfigs: {
     console.log(`Getting storage artifact at: ${source}: ${bucket}/${key}`);
     switch (source) {
       case 'gcs':
-        getGCSArtifactHandler({ bucket, key })(req, res);
+        getGCSArtifactHandler({ bucket, key }, peek)(req, res);
         break;
 
       case 'minio':
-        getMinioArtifactHandler({
-          bucket,
-          client: new MinioClient(minio),
-          key,
-        })(req, res);
+        getMinioArtifactHandler(
+          {
+            bucket,
+            client: new MinioClient(minio),
+            key,
+          },
+          peek,
+        )(req, res);
         break;
 
       case 's3':
-        getS3ArtifactHandler({
-          bucket,
-          client: await createMinioClient(aws),
-          key,
-        })(req, res);
+        getS3ArtifactHandler(
+          {
+            bucket,
+            client: await createMinioClient(aws),
+            key,
+          },
+          peek,
+        )(req, res);
         break;
 
       case 'http':
       case 'https':
-        getHttpArtifactsHandler(getHttpUrl(source, http.baseUrl || '', bucket, key), http.auth)(
-          req,
-          res,
-        );
+        getHttpArtifactsHandler(
+          getHttpUrl(source, http.baseUrl || '', bucket, key),
+          http.auth,
+          peek,
+        )(req, res);
         break;
 
       default:
@@ -114,6 +125,7 @@ function getHttpArtifactsHandler(
     key: string;
     defaultValue: string;
   } = { key: '', defaultValue: '' },
+  peek: number = -1,
 ) {
   return async (req: Request, res: Response) => {
     const headers = {};
@@ -126,11 +138,14 @@ function getHttpArtifactsHandler(
     }
     const response = await fetch(url, { headers });
     const content = await response.buffer();
-    res.send(content);
+    res.send(peek > 0 ? content.slice(0, peek) : content);
   };
 }
 
-function getS3ArtifactHandler(options: { bucket: string; key: string; client: MinioClient }) {
+function getS3ArtifactHandler(
+  options: { bucket: string; key: string; client: MinioClient },
+  peek: number = -1,
+) {
   return async (_: Request, res: Response) => {
     try {
       const stream = await getObjectStream(options);
@@ -140,17 +155,38 @@ function getS3ArtifactHandler(options: { bucket: string; key: string; client: Mi
           .status(500)
           .send(`Failed to get object in bucket ${options.bucket} at path ${options.key}: ${err}`),
       );
-      stream.pipe(res);
+      if (peek > 0) {
+        let preview = '';
+        let sent = false;
+        stream.on('data', buffer => {
+          preview += buffer.toString();
+          if (preview.length >= peek) {
+            sent = true;
+            res.send(preview.slice(0, peek));
+          }
+        });
+        stream.on('end', () => {
+          if (!sent) {
+            res.send(preview);
+          }
+        });
+      } else {
+        stream.pipe(res);
+      }
     } catch (err) {
       res.send(`Failed to get object in bucket ${options.bucket} at path ${options.key}: ${err}`);
     }
   };
 }
 
-function getMinioArtifactHandler(options: { bucket: string; key: string; client: MinioClient }) {
+function getMinioArtifactHandler(
+  options: { bucket: string; key: string; client: MinioClient },
+  peek: number = -1,
+) {
   return async (_: Request, res: Response) => {
     try {
-      res.send(await getTarObjectAsString(options));
+      const content = await getTarObjectAsString(options);
+      res.send(peek > 0 ? content.slice(0, peek) : content);
     } catch (err) {
       res
         .status(500)
@@ -159,7 +195,7 @@ function getMinioArtifactHandler(options: { bucket: string; key: string; client:
   };
 }
 
-function getGCSArtifactHandler(options: { key: string; bucket: string }) {
+function getGCSArtifactHandler(options: { key: string; bucket: string }, peek: number = -1) {
   const { key, bucket } = options;
   return async (_: Request, res: Response) => {
     try {
@@ -207,7 +243,7 @@ function getGCSArtifactHandler(options: { key: string; bucket: string }) {
                 .toString()
                 .trim() + '\n';
             if (i === matchingFiles.length - 1) {
-              res.send(contents);
+              res.send(peek > 0 ? contents.slice(0, peek) : contents);
             }
           })
           .on('error', () => res.status(500).send('Failed to read file: ' + f.name));
