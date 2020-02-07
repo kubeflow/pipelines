f0726b5e478f5955d8e33821832356060c50f4ce
dldaisy <dldaisy98@gmail.com>
2020-02-07 20:23:15 +0800

    rm debug print

--- samples/contrib/versioned-pipeline-ci-samples/mnist-ci-sample/create_pipeline_version_and_run.py
+++ samples/contrib/versioned-pipeline-ci-samples/mnist-ci-sample/create_pipeline_version_and_run.py
@@ -22,14 +22,10 @@ version_body = {"name": args.commit_sha, \
 "code_source_url": args.code_source_url, \
 "package_url": {"pipeline_url": package_url}, \
 "resource_references": [{"key": {"id": args.pipeline_id, "type":3}, "relationship":1}]}
-print('version body: {}'.format(version_body))
 response = client.pipelines.create_pipeline_version(version_body)
 
-print('args are: {}'.format(args))
-print('Now start to create a run...')
 version_id = response.id
 # create run
-print('version response: {}'.format(response))
 run_name = args.run_name if args.run_name else 'run' + version_id
 resource_references = [{"key": {"id": version_id, "type":4}, "relationship":2}]
 if args.experiment_id:
@@ -37,7 +33,6 @@ if args.experiment_id:
 run_body={"name":run_name,
           "pipeline_spec":{"parameters": [{"name": "storage_bucket", "value": args.bucket_name}]},
           "resource_references": resource_references}
-print('run body is :{}'.format(run_body))
 try:
     client.runs.create_run(run_body)
 except:
