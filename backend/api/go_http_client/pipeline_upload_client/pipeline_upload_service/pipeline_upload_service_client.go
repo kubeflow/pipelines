b6dccde2a6f18d849b5b17344b614f5b550bffbe
jingzhang36 <jingzhangjz@google.com>
2020-02-08 12:23:53 +0800

    Swagger generated code for UploadPipelineVersion method (#3010)

--- backend/api/go_http_client/pipeline_upload_client/pipeline_upload_service/pipeline_upload_service_client.go
+++ backend/api/go_http_client/pipeline_upload_client/pipeline_upload_service/pipeline_upload_service_client.go
@@ -67,6 +67,35 @@ func (a *Client) UploadPipeline(params *UploadPipelineParams, authInfo runtime.C
 
 }
 
+/*
+UploadPipelineVersion upload pipeline version API
+*/
+func (a *Client) UploadPipelineVersion(params *UploadPipelineVersionParams, authInfo runtime.ClientAuthInfoWriter) (*UploadPipelineVersionOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewUploadPipelineVersionParams()
+	}
+
+	result, err := a.transport.Submit(&runtime.ClientOperation{
+		ID:                 "UploadPipelineVersion",
+		Method:             "POST",
+		PathPattern:        "/apis/v1beta1/pipelines/upload_version",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"multipart/form-data"},
+		Schemes:            []string{"http", "https"},
+		Params:             params,
+		Reader:             &UploadPipelineVersionReader{formats: a.formats},
+		AuthInfo:           authInfo,
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return result.(*UploadPipelineVersionOK), nil
+
+}
+
 // SetTransport changes the transport on the client
 func (a *Client) SetTransport(transport runtime.ClientTransport) {
 	a.transport = transport
