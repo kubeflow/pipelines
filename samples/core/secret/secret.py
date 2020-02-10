453780cd1a0c9677a2e8e99053299447472bdcc6
Yuan Gong <gongyuan94@gmail.com>
2020-02-10 19:12:54 +0800

    Fix secret sample using python3

--- samples/core/secret/secret.py
+++ samples/core/secret/secret.py
@@ -37,8 +37,8 @@ def use_gcp_api_op():
         image='google/cloud-sdk:279.0.0',
         command=[
             'sh', '-c',
-            'pip install google-cloud-storage && "$0" "$*"',
-            'python', '-c', '''
+            'pip3 install google-cloud-storage && "$0" "$*"',
+            'python3', '-c', '''
 from google.cloud import storage
 storage_client = storage.Client()
 buckets = storage_client.list_buckets()
