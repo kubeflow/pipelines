0448a12482b66d492c25fd0680d7a612ee586066
rui5i <fang.ru@husky.neu.edu>
2020-02-10 15:28:32 -0800

    Add const

--- backend/execution_cache/admission_controller/main.go
+++ backend/execution_cache/admission_controller/main.go
@@ -7,21 +7,26 @@ import (
 )
 
 const (
-	tlsDir      = `/run/secrets/tls`
-	tlsCertFile = `tls.crt`
-	tlsKeyFile  = `tls.key`
+	TlsDir      string = "/run/secrets/tls"
+	TlsCertFile string = "tls.crt"
+	TlsKeyFile  string = "tls.key"
+)
+
+const (
+	MutateApi   string = "/mutate"
+	WebhookPort string = ":8443"
 )
 
 func main() {
-	certPath := filepath.Join(tlsDir, tlsCertFile)
-	keyPath := filepath.Join(tlsDir, tlsKeyFile)
+	certPath := filepath.Join(TlsDir, TlsCertFile)
+	keyPath := filepath.Join(TlsDir, TlsKeyFile)
 
 	mux := http.NewServeMux()
-	mux.Handle("/mutate", admitFuncHandler(applyPodOutput))
+	mux.Handle(MutateApi, admitFuncHandler(applyPodOutput))
 	server := &http.Server{
 		// We listen on port 8443 such that we do not need root privileges or extra capabilities for this server.
 		// The Service object will take care of mapping this port to the HTTPS port 443.
-		Addr:    ":8443",
+		Addr:    WebhookPort,
 		Handler: mux,
 	}
 	log.Fatal(server.ListenAndServeTLS(certPath, keyPath))
