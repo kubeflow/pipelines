887996080dec5a6a223fadecb8c16dd1ea9164be
sina chavoshi <sina.chavoshi@gmail.com>
2020-02-11 14:20:07 -0800

    Adding auth list support for diagnose_me (#3052)
    * Adding auth list support for diagnose_me
    
    * correcting formatting

--- sdk/python/kfp/cli/diagnose_me/gcp.py
+++ sdk/python/kfp/cli/diagnose_me/gcp.py
@@ -32,6 +32,7 @@ class Commands(enum.Enum):
   GET_SERVICE_ACCOUNTS = 9
   GET_STORAGE_BUCKETS = 10
   GET_GCLOUD_VERSION = 11
+  GET_AUTH_LIST = 12
 
 
 _command_string = {
@@ -46,6 +47,7 @@ _command_string = {
     Commands.GET_SERVICE_ACCOUNTS: 'iam service-accounts list',
     Commands.GET_STORAGE_BUCKETS: 'ls',
     Commands.GET_GCLOUD_VERSION: 'version',
+    Commands.GET_AUTH_LIST: 'auth list',
 }
 
 
