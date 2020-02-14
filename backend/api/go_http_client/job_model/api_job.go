32bb149d3d220b8d31fa561f461479df7478ebd7
Yuan Gong <gongyuan94@gmail.com>
2020-02-14 10:12:37 +0000

    Update generated backend api

--- backend/api/go_http_client/job_model/api_job.go
+++ backend/api/go_http_client/job_model/api_job.go
@@ -33,11 +33,6 @@ import (
 // swagger:model apiJob
 type APIJob struct {
 
-	// Optional input field. Whether the job should catch up if behind schedule.
-	// If false, the job will only schedule the latest interval if behind schedule.
-	// If true, the job will catch up on each past interval.
-	Catchup bool `json:"catchup,omitempty"`
-
 	// Output. The time this job is created.
 	// Format: date-time
 	CreatedAt strfmt.DateTime `json:"created_at,omitempty"`
@@ -65,6 +60,11 @@ type APIJob struct {
 
 	// Required input field. Job name provided by user. Not unique.
 	Name string `json:"name,omitempty"`
+
+	// Optional input field. Whether the job should catch up if behind schedule.
+	// If true, the job will only schedule the latest interval if behind schedule.
+	// If false, the job will catch up on each past interval.
+	NoCatchup bool `json:"no_catchup,omitempty"`
 
 	// Required input field.
 	// Describing what the pipeline manifest and parameters to use
