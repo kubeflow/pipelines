b7e891b6160eb31e36734b6e0cfd39f5aeca8da2
numerology <jxzheng@google.com>
2020-02-19 08:56:56 -0800

    init

---
+++ samples/core/iris/iris_pipeline.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Iris flowers example using TFX. Fork from https://github.com/tensorflow/tfx/blob/master/tfx/examples/iris/iris_pipeline_native_keras.py"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import kfp
+from typing import Text
+
+import absl
+import tensorflow_model_analysis as tfma
+
+from tfx.components import CsvExampleGen
+from tfx.components import Evaluator
+from tfx.components import ExampleValidator
+from tfx.components import SchemaGen
+from tfx.components import StatisticsGen
+from tfx.components import Trainer
+from tfx.components import Transform
+from tfx.components.base import executor_spec
+from tfx.components.trainer.executor import GenericExecutor
+from tfx.orchestration import data_types
+from tfx.orchestration import pipeline
+from tfx.orchestration.kubeflow import kubeflow_dag_runner
+from tfx.proto import trainer_pb2
+from tfx.utils.dsl_utils import external_input
+
+_pipeline_name = 'iris_native_keras'
+
+# This example assumes that Iris flowers data is stored in ~/iris/data and the
+# utility function is in ~/iris. Feel free to customize as needed.
+_data_root_param = data_types.RuntimeParameter(
+    name='data-root',
+    default='gs://ml-pipeline-playground/iris/data',
+    ptype=Text,
+)
+
+# Python module file to inject customized logic into the TFX components. The
+# Transform and Trainer both require user-defined functions to run successfully.
+_module_file_param = data_types.RuntimeParameter(
+    name='module-file',
+    default='gs://ml-pipeline-playground/iris/modules/iris_utils.py',
+    ptype=Text,
+)
+
+# Directory and data locations.  This example assumes all of the flowers
+# example code and metadata library is relative to $HOME, but you can store
+# these files anywhere on your local filesystem.
+_pipeline_root = os.path.join(
+    'gs://jxzheng-helloworld', 'tfx_iris', kfp.dsl.RUN_ID_PLACEHOLDER
+)
+
+
+def _create_pipeline(
+    pipeline_name: Text, pipeline_root: Text
+) -> pipeline.Pipeline:
+  """Implements the Iris flowers pipeline with TFX."""
+  examples = external_input(_data_root_param)
+
+  # Brings data into the pipeline or otherwise joins/converts training data.
+  example_gen = CsvExampleGen(input=examples)
+
+  # Computes statistics over data for visualization and example validation.
+  statistics_gen = StatisticsGen(examples=example_gen.outputs['examples'])
+
+  # Generates schema based on statistics files.
+  infer_schema = SchemaGen(
+      statistics=statistics_gen.outputs['statistics'], infer_feature_shape=True
+  )
+
+  # Performs anomaly detection based on statistics and data schema.
+  validate_stats = ExampleValidator(
+      statistics=statistics_gen.outputs['statistics'],
+      schema=infer_schema.outputs['schema']
+  )
+
+  # Performs transformations and feature engineering in training and serving.
+  transform = Transform(
+      examples=example_gen.outputs['examples'],
+      schema=infer_schema.outputs['schema'],
+      module_file=_module_file_param
+  )
+
+  # Uses user-provided Python function that trains a model using TF-Learn.
+  trainer = Trainer(
+      module_file=_module_file_param,
+      custom_executor_spec=executor_spec.ExecutorClassSpec(GenericExecutor),
+      examples=transform.outputs['transformed_examples'],
+      transform_graph=transform.outputs['transform_graph'],
+      schema=infer_schema.outputs['schema'],
+      train_args=trainer_pb2.TrainArgs(num_steps=100),
+      eval_args=trainer_pb2.EvalArgs(num_steps=50)
+  )
+
+  # Uses TFMA to compute a evaluation statistics over features of a model.
+  model_analyzer = Evaluator(
+      examples=example_gen.outputs['examples'],
+      model=trainer.outputs['model'],
+      eval_config=tfma.EvalConfig(
+          model_specs=[tfma.ModelSpec(label_key='variety')],
+          slicing_specs=[tfma.SlicingSpec(feature_keys=['sepal_length'])]
+      )
+  )
+
+  return pipeline.Pipeline(
+      pipeline_name=pipeline_name,
+      pipeline_root=pipeline_root,
+      components=[
+          example_gen,
+          statistics_gen,
+          infer_schema,
+          validate_stats,
+          transform,
+          trainer,
+          model_analyzer,
+      ],
+      enable_cache=True,
+  )
+
+
+# To run this pipeline from the python CLI:
+#   $python iris_pipeline_native_keras.py
+if __name__ == '__main__':
+  absl.logging.set_verbosity(absl.logging.INFO)
+  # Make sure the version of TFX image used is consistent with the version of
+  # TFX SDK. Here we use tfx:0.15.0 image.
+  config = kubeflow_dag_runner.KubeflowDagRunnerConfig(
+      kubeflow_metadata_config=kubeflow_dag_runner.
+      get_default_kubeflow_metadata_config(),
+      pipeline_operator_funcs=list(
+          filter(
+              lambda operator: operator.__name__.find('gcp_secret') == -1,
+              kubeflow_dag_runner.get_default_pipeline_operator_funcs()
+          )
+      ),
+      tfx_image='tensorflow/tfx:0.21.0',
+  )
+  kfp_runner = kubeflow_dag_runner.KubeflowDagRunner(
+      output_filename=__file__ + '.yaml', config=config
+  )
+  kfp_runner.run(
+      _create_pipeline(
+          pipeline_name=_pipeline_name, pipeline_root=_pipeline_root
+      )
+  )
