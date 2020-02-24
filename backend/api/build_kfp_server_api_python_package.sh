58162b3718a161da246fa41690c259c37fa030e6
Jing Zhang <jingzhangjz@google.com>
2020-02-24 17:18:21 +0800

    Reverse another local change

--- backend/api/build_kfp_server_api_python_package.sh
+++ backend/api/build_kfp_server_api_python_package.sh
@@ -34,7 +34,7 @@ if [ -z "$VERSION" ]; then
     exit 1
 fi
 
-codegen_file=~/tmp/swagger-codegen-cli.jar
+codegen_file=/tmp/swagger-codegen-cli.jar
 codegen_uri=http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.7/swagger-codegen-cli-2.4.7.jar
 if ! [ -f "$codegen_file" ]; then
     wget --no-verbose "$codegen_uri" -O "$codegen_file"
