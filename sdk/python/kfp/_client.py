74e05935aa6437d20f56c8bbc163f22b6c5f86ac
Niklas Hansson <niklas.sven.hansson@gmail.com>
2020-02-27 18:05:28 +0100

    Update to fix bug in kip.client for scheduling (#3173)
    * update to fetch remote
    
    * missed to add the description

--- sdk/python/kfp/_client.py
+++ sdk/python/kfp/_client.py
@@ -409,7 +409,8 @@ class Client(object):
     run_info = self.run_pipeline(experiment.id, run_name, pipeline_file, arguments, namespace=namespace)
     return RunPipelineResult(self, run_info)
 
-  def schedule_pipeline(self, experiment_id, job_name, pipeline_package_path=None, params={}, pipeline_id=None, namespace=None):
+  def schedule_pipeline(self, experiment_id, job_name, pipeline_package_path=None, params={}, pipeline_id=None, 
+    namespace=None, cron_schedule=None, description=None):
     """Schedule pipeline on kubeflow to run based upon a cron job
     
     Arguments:
@@ -449,12 +450,12 @@ class Client(object):
         workflow_manifest=pipeline_json_string,
         parameters=api_params)
     
-    trigger = kfp_server_api.models.api_cron_schedule.ApiCronSchedule(cron="0 0 9 ? * 2-6")
+    trigger = kfp_server_api.models.api_cron_schedule.ApiCronSchedule(cron=cron_schedule) #Example:cron_schedule="0 0 9 ? * 2-6"
     job_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
     schedule_body = kfp_server_api.models.ApiJob(
         id=job_id,
-        name="TestScheduling",
-        description="Schedule the pipeline using the API",
+        name=job_name,
+        description=description,
         pipeline_spec=spec,
         resource_references=resource_references,
         max_concurrency=10,
