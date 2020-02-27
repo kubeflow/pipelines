688aa08d428a36e51ac348e770c3cd724b922de0
Yuan (Bob) Gong <gongyuan94@gmail.com>
2020-02-27 15:59:19 +0800

    Release 0.2.5 - e66dcb18607406330f953bf99b04fe7c3ed1a4a8 (#3179)
    * Updated component images to version e66dcb18607406330f953bf99b04fe7c3ed1a4a8
    
    * Updated components to version 3f4b80127f35e40760eeb1813ce1d3f641502222
    
    * Update setup.py
    
    * Fix swagger codegen download url
    
    * Update versions

--- sdk/python/kfp/__init__.py
+++ sdk/python/kfp/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = '0.2.4'
+__version__ = '0.2.5'
 
 from . import components
 from . import containers
