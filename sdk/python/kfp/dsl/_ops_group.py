244b690255637df438a4cc0270de73e0e9f90c60
Xiaohan Huang <xhuang@argo.ai>
2020-03-03 18:07:52 -0800

    add an optional name for dsl.Condition (kubeflow#3210)

--- sdk/python/kfp/dsl/_ops_group.py
+++ sdk/python/kfp/dsl/_ops_group.py
@@ -137,18 +137,18 @@ class Condition(OpsGroup):
 
   Example usage:
   ```python
-  with Condition(param1=='pizza'):
+  with Condition(param1=='pizza', '[param1 is pizza]'):
     op1 = ContainerOp(...)
     op2 = ContainerOp(...)
   ```
   """
 
-  def __init__(self, condition):
+  def __init__(self, condition, name = None):
     """Create a new instance of Condition.
     Args:
       condition (ConditionOperator): the condition.
     """
-    super(Condition, self).__init__('condition')
+    super(Condition, self).__init__('condition', name)
     self.condition = condition
 
 
