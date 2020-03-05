3bfb65656987ce83b24082596b9c1ae8d4b16f22
numerology <jxzheng@google.com>
2020-03-05 09:50:57 -0800

    pin two requirements.txt

--- backend/update_requirements.sh
+++ backend/update_requirements.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 IMAGE=${1:-"python:3.5"}
 exec docker run -v "$PWD:/src" -w /src --rm "$IMAGE" bash \
-  -c 'pip3 install pip-tools==4.5.0 > /dev/null 2>&1 && pip-compile -v requirements.in'
+  -c 'pip3 install pip setuptools --upgrade && pip3 install pip-tools==4.5.0 > /dev/null 2>&1 && pip-compile -v requirements.in'
