7e610d1a3298891c76e76777560068ecfac56b36
Kubernetes Prow Robot <k8s-ci-robot@users.noreply.github.com>
2020-03-05 01:54:38 -0800

    backend: Pin python dependencies (#3161)
    * backend: Pin python dependencies
    
    Builds/tests failed due to broken transident dependencies. This pins the
    python dependencies to prevent this in the future.
    
    * Add README to test/sample-test

---
+++ backend/update_requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euo pipefail
+IMAGE=${1:-"python:3.5"}
+exec docker run -v "$PWD:/src" -w /src --rm "$IMAGE" bash \
+  -c 'pip3 install pip-tools==4.5.0 > /dev/null 2>&1 && pip-compile -v requirements.in'
