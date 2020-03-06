0c42ead6207791cc5e9adee8c3fd660d610d416f
numerology <jxzheng@google.com>
2020-03-06 09:53:41 -0800

    apply suggestion

--- frontend/src/lib/OutputArtifactLoader.ts
+++ frontend/src/lib/OutputArtifactLoader.ts
@@ -472,10 +472,11 @@ async function getExecutionInContextWithPodName(
     return undefined; // Not found, this is expected to happen normally when there's no mlmd data.
   }
   const state = foundExecution.getPropertiesMap().get('state');
-  if (!state || (state.getStringValue() !== 'complete' && state.getStringValue() !== 'cached')) {
-    return undefined; // Execution doesn't have a valid state, or it has not finished.
+  if (state && state.getStringValue() === 'complete' || state.getStringValue() === 'cached')) {
+    return foundExecution
   }
-  return foundExecution;
+  // No valid execution found.
+  return undefined;
 }
 
 /**
