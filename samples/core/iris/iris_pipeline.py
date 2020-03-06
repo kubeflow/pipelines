9b3dc7439dda24f514d6a10921fc8ebb992ba4b7
numerology <jxzheng@google.com>
2020-03-06 14:18:35 -0800

    update iris sample to keras native version

--- samples/core/iris/iris_pipeline.py
+++ samples/core/iris/iris_pipeline.py
@@ -23,20 +23,31 @@ import kfp
 from typing import Text
 
 import absl
+import tensorflow_model_analysis as tfma
 
 from tfx.components import CsvExampleGen
 from tfx.components import Evaluator
 from tfx.components import ExampleValidator
+from tfx.components import Pusher
+from tfx.components import ResolverNode
 from tfx.components import SchemaGen
 from tfx.components import StatisticsGen
 from tfx.components import Trainer
+from tfx.components import Transform
+from tfx.components.base import executor_spec
+from tfx.components.trainer.executor import GenericExecutor
+from tfx.dsl.experimental import latest_blessed_model_resolver
 from tfx.orchestration import data_types
 from tfx.orchestration import pipeline
 from tfx.orchestration.kubeflow import kubeflow_dag_runner
 from tfx.proto import trainer_pb2
+from tfx.proto import pusher_pb2
+from tfx.types import Channel
+from tfx.types.standard_artifacts import Model
+from tfx.types.standard_artifacts import ModelBlessing
 from tfx.utils.dsl_utils import external_input
 
-_pipeline_name = 'iris_native_beam'
+_pipeline_name = 'iris_native_keras'
 
 # This example assumes that Iris flowers data is stored in GCS and the
 # utility function is in iris_utils.py. Feel free to customize as needed.
@@ -50,7 +61,8 @@ _data_root_param = data_types.RuntimeParameter(
 # Transform and Trainer both require user-defined functions to run successfully.
 _module_file_param = data_types.RuntimeParameter(
     name='module-file',
-    default='gs://ml-pipeline-playground/iris/modules/iris_utils.py',
+    default=
+    'gs://ml-pipeline-playground/iris/modules/iris_utils_native_keras.py',
     ptype=Text,
 )
 
@@ -88,17 +100,83 @@ def _create_pipeline(
       schema=infer_schema.outputs['schema']
   )
 
+  # Performs transformations and feature engineering in training and serving.
+  transform = Transform(
+      examples=example_gen.outputs['examples'],
+      schema=infer_schema.outputs['schema'],
+      module_file=_module_file_param
+  )
+
   # Uses user-provided Python function that implements a model using TF-Learn.
   trainer = Trainer(
       module_file=_module_file_param,
-      examples=example_gen.outputs['examples'],
+      custom_executor_spec=executor_spec.ExecutorClassSpec(GenericExecutor),
+      examples=transform.outputs['transformed_examples'],
+      transform_graph=transform.outputs['transform_graph'],
       schema=infer_schema.outputs['schema'],
       train_args=trainer_pb2.TrainArgs(num_steps=100),
-      eval_args=trainer_pb2.EvalArgs(num_steps=50))
+      eval_args=trainer_pb2.EvalArgs(num_steps=50)
+  )
+
+  # Get the latest blessed model for model validation.
+  model_resolver = ResolverNode(
+      instance_name='latest_blessed_model_resolver',
+      resolver_class=latest_blessed_model_resolver.LatestBlessedModelResolver,
+      model=Channel(type=Model),
+      model_blessing=Channel(type=ModelBlessing)
+  )
+
+  # Uses TFMA to compute an evaluation statistics over features of a model and
+  # perform quality validation of a candidate model (compared to a baseline).
+  eval_config = tfma.EvalConfig(
+      model_specs=[
+          tfma.ModelSpec(name='candidate', label_key='variety'),
+          tfma.ModelSpec(
+              name='baseline', label_key='variety', is_baseline=True
+          )
+      ],
+      slicing_specs=[tfma.SlicingSpec()],
+      metrics_specs=[
+          tfma.MetricsSpec(
+              metrics=[
+                  tfma.MetricConfig(
+                      class_name='SparseCategoricalAccuracy',
+                      threshold=tfma.config.MetricThreshold(
+                          value_threshold=tfma.GenericValueThreshold(
+                              lower_bound={'value': 0.9}
+                          ),
+                          change_threshold=tfma.GenericChangeThreshold(
+                              direction=tfma.MetricDirection.HIGHER_IS_BETTER,
+                              absolute={'value': -1e-10}
+                          )
+                      )
+                  )
+              ]
+          )
+      ]
+  )
 
   # Uses TFMA to compute a evaluation statistics over features of a model.
   model_analyzer = Evaluator(
-      examples=example_gen.outputs['examples'], model=trainer.outputs['model'])
+      examples=example_gen.outputs['examples'],
+      model=trainer.outputs['model'],
+      baseline_model=model_resolver.outputs['model'],
+      # Change threshold will be ignored if there is no baseline (first run).
+      eval_config=eval_config
+  )
+
+  # Checks whether the model passed the validation steps and pushes the model
+  # to a file destination if check passed.
+  pusher = Pusher(
+      model=trainer.outputs['model'],
+      model_blessing=model_analyzer.outputs['blessing'],
+      push_destination=pusher_pb2.PushDestination(
+          filesystem=pusher_pb2.PushDestination.Filesystem(
+              base_directory=os.path.
+              join(str(pipeline.ROOT_PARAMETER), 'model_serving')
+          )
+      )
+  )
 
   # TODO(numerology): Add pusher and model validator.
 
@@ -106,8 +184,8 @@ def _create_pipeline(
       pipeline_name=pipeline_name,
       pipeline_root=pipeline_root,
       components=[
-          example_gen, statistics_gen, infer_schema, validate_stats, trainer,
-          model_analyzer,
+          example_gen, statistics_gen, infer_schema, validate_stats, transform,
+          trainer, model_resolver, model_analyzer, pusher
       ],
       enable_cache=True,
   )
@@ -116,11 +194,11 @@ def _create_pipeline(
 if __name__ == '__main__':
   absl.logging.set_verbosity(absl.logging.INFO)
   # Make sure the version of TFX image used is consistent with the version of
-  # TFX SDK. Here we use tfx:0.21.0 image.
+  # TFX SDK. Here we use tfx:0.21.1 image.
   config = kubeflow_dag_runner.KubeflowDagRunnerConfig(
       kubeflow_metadata_config=kubeflow_dag_runner.
       get_default_kubeflow_metadata_config(),
-      tfx_image='tensorflow/tfx:0.21.0',
+      tfx_image='tensorflow/tfx:0.21.1',
   )
   kfp_runner = kubeflow_dag_runner.KubeflowDagRunner(
       output_filename=__file__ + '.yaml', config=config
