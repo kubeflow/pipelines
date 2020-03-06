f62f5b2800da6815b9bc726cf0fdacad960651f8
numerology <jxzheng@google.com>
2020-03-06 14:39:16 -0800

    add readme

--- samples/core/iris/iris_pipeline.py
+++ samples/core/iris/iris_pipeline.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Iris flowers example using TFX. Fork from https://github.com/tensorflow/tfx/blob/master/tfx/examples/iris/iris_pipeline_native_keras.py"""
+"""Iris flowers example using TFX. Based on https://github.com/tensorflow/tfx/blob/master/tfx/examples/iris/iris_pipeline_native_keras.py"""
 
 from __future__ import absolute_import
 from __future__ import division
