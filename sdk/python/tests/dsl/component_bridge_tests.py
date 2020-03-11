a48241694c3964850544265c188a25af69726472
Alexey Volkov <alexey.volkov@ark-kun.com>
2020-03-11 15:50:35 -0700

    Indented the component texts

--- sdk/python/tests/dsl/component_bridge_tests.py
+++ sdk/python/tests/dsl/component_bridge_tests.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 
+import textwrap
 import unittest
 import kfp
 from kfp.components import load_component_from_text
@@ -29,26 +30,28 @@ class TestComponentBridge(unittest.TestCase):
         kfp.components._components._container_task_constructor = self.old_container_task_constructor
 
     def test_conversion_to_container_op(self):
-        component_text = '''\
-name: Custom component
-implementation:
-  container:
-    image: busybox
-'''
+        component_text = textwrap.dedent('''\
+            name: Custom component
+            implementation:
+                container:
+                    image: busybox
+            '''
+        )
         task_factory1 = load_component_from_text(component_text)
         task1 = task_factory1()
 
         self.assertEqual(task1.human_name, 'Custom component')
 
     def test_passing_env_to_container_op(self):
-        component_text = '''\
-implementation:
-  container:
-    image: busybox
-    env:
-      key1: value 1
-      key2: value 2
-'''
+        component_text = textwrap.dedent('''\
+            implementation:
+                container:
+                    image: busybox
+                    env:
+                        key1: value 1
+                        key2: value 2
+            '''
+        )
         task_factory1 = load_component_from_text(component_text)
         
         task1 = task_factory1()
@@ -57,16 +60,17 @@ implementation:
         self.assertDictEqual(expected_env, actual_env)
 
     def test_input_path_placeholder_with_constant_argument(self):
-        component_text = '''\
-inputs:
-- {name: input 1}
-implementation:
-  container:
-    image: busybox
-    command:
-      - --input-data
-      - {inputPath: input 1}
-'''
+        component_text = textwrap.dedent('''\
+            inputs:
+            - {name: input 1}
+            implementation:
+                container:
+                    image: busybox
+                    command:
+                    - --input-data
+                    - {inputPath: input 1}
+            '''
+        )
         task_factory1 = load_component_from_text(component_text)
         task1 = task_factory1('Text')
 
@@ -74,16 +78,17 @@ implementation:
         self.assertEqual(task1.artifact_arguments, {'input 1': 'Text'})
 
     def test_passing_component_metadata_to_container_op(self):
-        component_text = '''\
-metadata:
-  annotations:
-    key1: value1
-  labels:
-    key1: value1
-implementation:
-  container:
-    image: busybox
-'''
+        component_text = textwrap.dedent('''\
+            metadata:
+                annotations:
+                    key1: value1
+                labels:
+                    key1: value1
+            implementation:
+                container:
+                    image: busybox
+            '''
+        )
         task_factory1 = load_component_from_text(text=component_text)
 
         task1 = task_factory1()
@@ -91,22 +96,24 @@ implementation:
         self.assertEqual(task1.pod_labels['key1'], 'value1')
 
     def test_type_compatibility_check_not_failing_when_disabled(self):
-        component_a = '''\
-outputs:
-  - {name: out1, type: type_A}
-implementation:
-  container:
-    image: busybox
-    command: [bash, -c, 'mkdir -p "$(dirname "$0")"; date > "$0"', {outputPath: out1}]
-'''
-        component_b = '''\
-inputs:
-  - {name: in1, type: type_Z}
-implementation:
-  container:
-    image: busybox
-    command: [echo, {inputValue: in1}]
-'''
+        component_a = textwrap.dedent('''\
+            outputs:
+            - {name: out1, type: type_A}
+            implementation:
+                container:
+                    image: busybox
+                    command: [bash, -c, 'mkdir -p "$(dirname "$0")"; date > "$0"', {outputPath: out1}]
+            '''
+        )
+        component_b = textwrap.dedent('''\
+            inputs:
+            - {name: in1, type: type_Z}
+            implementation:
+                container:
+                    image: busybox
+                    command: [echo, {inputValue: in1}]
+            '''
+        )
         kfp.TYPE_CHECK = False
         task_factory_a = load_component_from_text(component_a)
         task_factory_b = load_component_from_text(component_b)
@@ -115,22 +122,24 @@ implementation:
         kfp.TYPE_CHECK = True
 
     def test_type_compatibility_check_not_failing_when_type_is_ignored(self):
-        component_a = '''\
-outputs:
-  - {name: out1, type: type_A}
-implementation:
-  container:
-    image: busybox
-    command: [bash, -c, 'mkdir -p "$(dirname "$0")"; date > "$0"', {outputPath: out1}]
-'''
-        component_b = '''\
-inputs:
-  - {name: in1, type: type_Z}
-implementation:
-  container:
-    image: busybox
-    command: [echo, {inputValue: in1}]
-'''
+        component_a = textwrap.dedent('''\
+            outputs:
+            - {name: out1, type: type_A}
+            implementation:
+                container:
+                    image: busybox
+                    command: [bash, -c, 'mkdir -p "$(dirname "$0")"; date > "$0"', {outputPath: out1}]
+            '''
+        )
+        component_b = textwrap.dedent('''\
+            inputs:
+            - {name: in1, type: type_Z}
+            implementation:
+                container:
+                    image: busybox
+                    command: [echo, {inputValue: in1}]
+            '''
+        )
         task_factory_a = load_component_from_text(component_a)
         task_factory_b = load_component_from_text(component_b)
         a_task = task_factory_a()
