492ee058bf7350b58087f5c7553b8fb22b187b30
Jing Zhang <jingzhangjz@google.com>
2020-03-24 14:46:59 +0800

    add new field in db schema and api schema

--- backend/src/apiserver/model/experiment.go
+++ backend/src/apiserver/model/experiment.go
@@ -6,6 +6,7 @@ type Experiment struct {
 	Description    string `gorm:"column:Description; not null"`
 	CreatedAtInSec int64  `gorm:"column:CreatedAtInSec; not null"`
 	Namespace      string `gorm:"column:Namespace; not null; unique_index:idx_name_namespace"`
+	StorageState   string `gorm:"column:StorageState; not null;"`
 }
 
 func (e Experiment) GetValueOfPrimaryKey() string {
@@ -27,11 +28,12 @@ func (e *Experiment) DefaultSortField() string {
 }
 
 var experimentAPIToModelFieldMap = map[string]string{
-	"id":          "UUID",
-	"name":        "Name",
-	"created_at":  "CreatedAtInSec",
-	"description": "Description",
-	"namespace":   "Namespace",
+	"id":            "UUID",
+	"name":          "Name",
+	"created_at":    "CreatedAtInSec",
+	"description":   "Description",
+	"namespace":     "Namespace",
+	"storage_state": "StorageState",
 }
 
 // APIToModelFieldMap returns a map from API names to field names for model
