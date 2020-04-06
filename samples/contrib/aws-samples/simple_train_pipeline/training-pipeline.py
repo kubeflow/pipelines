fbed22f65b16835165b38fa56bd3fad1948d82cd
Kartik Kalamadi <kalamadi@amazon.com>
2020-04-06 15:26:57 -0700

    change s3 bucket to match other sample pipelines

--- samples/contrib/aws-samples/simple_train_pipeline/training-pipeline.py
+++ samples/contrib/aws-samples/simple_train_pipeline/training-pipeline.py
@@ -17,27 +17,27 @@ def training(
         image='382416733822.dkr.ecr.us-east-1.amazonaws.com/kmeans:1',
         training_input_mode='File',
         hyperparameters='{"k": "10", "feature_dim": "784"}',
-        channels='[                                                               \
-                    {                                                             \
-                      "ChannelName": "train",                                     \
-                      "DataSource": {                                             \
-                        "S3DataSource": {                                         \
-                          "S3Uri": "s3://m-kfp-mnist/mnist_kmeans_example/data",  \
-                          "S3DataType": "S3Prefix",                               \
-                          "S3DataDistributionType": "FullyReplicated"             \
-                        }                                                         \
-                      },                                                          \
-                      "ContentType": "",                                          \
-                      "CompressionType": "None",                                  \
-                      "RecordWrapperType": "None",                                \
-                      "InputMode": "File"                                         \
-                    }                                                             \
+        channels='[                                                                          \
+                    {                                                                        \
+                      "ChannelName": "train",                                                \
+                      "DataSource": {                                                        \
+                        "S3DataSource": {                                                    \
+                          "S3Uri": "s3://kubeflow-pipeline-data/mnist_kmeans_example/data",  \
+                          "S3DataType": "S3Prefix",                                          \
+                          "S3DataDistributionType": "FullyReplicated"                        \
+                        }                                                                    \
+                      },                                                                     \
+                      "ContentType": "",                                                     \
+                      "CompressionType": "None",                                             \
+                      "RecordWrapperType": "None",                                           \
+                      "InputMode": "File"                                                    \
+                    }                                                                        \
                   ]',
         instance_type='ml.p2.xlarge',
         instance_count='1',
         volume_size='50',
         max_run_time='3600',
-        model_artifact_path='s3://m-kfp-mnist/mnist_kmeans_example/output',
+        model_artifact_path='s3://kubeflow-pipeline-data/mnist_kmeans_example/data',
         output_encryption_key='',
         network_isolation='True',
         traffic_encryption='False',
