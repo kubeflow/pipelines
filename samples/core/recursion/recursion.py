22e8ae471312ebc30fa0ff0a0858b9f467c4ca2e
rui5i <fang.ru@husky.neu.edu>
2020-04-08 13:38:46 -0700

    add comments

--- samples/core/recursion/recursion.py
+++ samples/core/recursion/recursion.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Notice: caching is tricky when recursion is involved. Please be careful and 
+# set proper max_cache_staleness in case of infinite loop.
 
 import kfp
 from kfp import dsl
