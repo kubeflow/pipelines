7753ec66b659779ee05d330c1074f9f761536f11
Yuan Gong <gongyuan94@gmail.com>
2020-04-10 15:52:14 +0800

    Parralelize image buidling for api server and others

--- test/build-images.sh
+++ test/build-images.sh
@@ -18,10 +18,9 @@ set -ex
 
 IMAGES_BUILDING=false
 
-function has_images_been_built {
+function has_batch_images_been_built {
   local images=$(gcloud container images list --repository=${GCR_IMAGE_BASE_DIR})
   local result=1
-  echo "$images" | grep api-server && \
   echo "$images" | grep frontend && \
   echo "$images" | grep scheduledworkflow && \
   echo "$images" | grep persistenceagent && \
@@ -34,39 +33,62 @@ function has_images_been_built {
   return $result
 }
 
-# Image caching can be turned off by setting $DISABLE_IMAGE_CACHING env flag.
-# Note that GCR_IMAGE_BASE_DIR contains commit hash, so whenever there's a code
-# change, we won't use caches for sure.
-if
-  test -z "$DISABLE_IMAGE_CACHING" && has_images_been_built
-then
-  echo "docker images for api-server, frontend, scheduledworkflow, \
-    persistenceagent, viewer-crd-controller, inverse-proxy-agent, metadata-writer, cache-server, \
-    cache-deployer and visualization-server are already built in ${GCR_IMAGE_BASE_DIR}."
-else
+function has_api_server_image_been_built {
+  local images=$(gcloud container images list --repository=${GCR_IMAGE_BASE_DIR})
+  local result=1
+  echo "$images" | grep api-server && result=0
+  return $result
+}
+
+BUILD_IDS=()
+function build_image {
+  local build_target = $1
   # sleep randomly to reduce chance of submitting two cloudbuild jobs at the same time
-  sleep $((RANDOM%30))
+  sleep $((RANDOM%10))
 
   # The object name is inited as ${TEST_RESULTS_GCS_DIR} below, so it always has ${COMMIT_SHA} in it.
-  ongoing_build_ids=($(gcloud builds list --filter='source.storageSource.object~'${COMMIT_SHA}' AND (status=QUEUED OR status=WORKING)' --format='value(id)'))
+  local ongoing_build_ids=($(gcloud builds list \
+    --filter='source.storageSource.object~'${COMMIT_SHA}.*/${build_target}' \
+    AND (status=QUEUED OR status=WORKING)' --format='value(id)'))
   if [ "${#ongoing_build_ids[@]}" -gt "0" ]; then
     echo "There is an existing cloud build job, wait for it: id=${ongoing_build_ids[0]}"
-    IMAGES_BUILDING=true
-    BUILD_IDS=(${ongoing_build_ids[0]})
+    BUILD_IDS+="${ongoing_build_ids[0]}"
   else
-    echo "submitting cloud build to build docker images for commit ${COMMIT_SHA}..."
-    IMAGES_BUILDING=true
-    CLOUD_BUILD_COMMON_ARGS=( \
+    echo "submitting cloud build to build docker images for commit ${COMMIT_SHA} and target ${build_target}..."
+    local common_args=( \
       $remote_code_archive_uri \
       --async \
       --format='value(id)' \
       --substitutions=_GCR_BASE=${GCR_IMAGE_BASE_DIR} \
     )
-    BUILD_ID_BATCH=$(gcloud builds submit ${CLOUD_BUILD_COMMON_ARGS[@]} \
-      --config ${DIR}/cloudbuild/batch_build.yaml \
-      --gcs-source-staging-dir ${TEST_RESULTS_GCS_DIR}/cloudbuild)
+    local build_id=$(gcloud builds submit ${common_args[@]} \
+      --config ${DIR}/cloudbuild/${build_target}.yaml \
+      --gcs-source-staging-dir "${TEST_RESULTS_GCS_DIR}/cloudbuild/${build_target}")
 
-    BUILD_IDS=("${BUILD_ID_BATCH}")
-    echo "Submitted the following cloud build jobs: ${BUILD_IDS[@]}"
+    BUILD_IDS+="${build_id}"
+    echo "Submitted the following cloud build job: ${build_id}"
   fi
+}
+
+# Image caching can be turned off by setting $DISABLE_IMAGE_CACHING env flag.
+# Note that GCR_IMAGE_BASE_DIR contains commit hash, so whenever there's a code
+# change, we won't use caches for sure.
+if
+  test -z "$DISABLE_IMAGE_CACHING" && has_batch_images_been_built
+then
+  echo "docker images for frontend, scheduledworkflow, \
+    persistenceagent, viewer-crd-controller, inverse-proxy-agent, metadata-writer, cache-server, \
+    cache-deployer and visualization-server are already built in ${GCR_IMAGE_BASE_DIR}."
+else
+  IMAGES_BUILDING=true
+  build_image "batch_build"
+fi
+
+if
+  test -z "$DISABLE_IMAGE_CACHING" && has_api_server_image_been_built
+then
+  echo "docker image for api-server is already built in ${GCR_IMAGE_BASE_DIR}."
+else
+  IMAGES_BUILDING=true
+  build_image "api_server"
 fi
