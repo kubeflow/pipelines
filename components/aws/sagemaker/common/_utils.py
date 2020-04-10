573ef1bb411d6205f83cf2bfb0057bac21014060
Suraj Kota <surakota@amazon.com>
2020-04-10 21:31:32 +0530

    add component version according to license file

--- components/aws/sagemaker/common/_utils.py
+++ components/aws/sagemaker/common/_utils.py
@@ -67,7 +67,7 @@ def add_default_client_arguments(parser):
 def get_sagemaker_client(region, endpoint_url=None):
     """Builds a client to the AWS SageMaker API."""
     session_config = botocore.config.Config(
-        user_agent='sagemaker-on-kubeflow-pipelines'
+        user_agent='sagemaker-on-kubeflow-pipelines-v0.2'
     )
     client = boto3.client('sagemaker', region_name=region, endpoint_url=endpoint_url, config=session_config)
     return client
