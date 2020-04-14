d78429e9e50a8303f177f5c8d82d30263b84f3a2
Kartik Kalamadi <kalamadi@amazon.com>
2020-04-13 19:10:21 -0700

    Small changes to readme of ground truth sample pipeline

--- components/aws/sagemaker/common/_utils.py
+++ components/aws/sagemaker/common/_utils.py
@@ -59,14 +59,17 @@ def nullable_string_argument(value):
         return None
     return value
 
+
 def add_default_client_arguments(parser):
     parser.add_argument('--region', type=str.strip, required=True, help='The region where the training job launches.')
     parser.add_argument('--endpoint_url', type=nullable_string_argument, required=False, help='The URL to use when communicating with the Sagemaker service.')
+
 
 def get_sagemaker_client(region, endpoint_url=None):
     """Builds a client to the AWS SageMaker API."""
     client = boto3.client('sagemaker', region_name=region, endpoint_url=endpoint_url)
     return client
+
 
 def create_training_job_request(args):
     ### Documentation: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sagemaker.html#SageMaker.Client.create_training_job
