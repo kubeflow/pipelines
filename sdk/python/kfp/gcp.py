7b5f4450ac6921068aec9efc5843c248a1bce41a
Shotaro Kohama <khmshtr28@gmail.com>
2020-04-30 21:00:33 -0700

    Make sure of the google coding style with yapf

--- sdk/python/kfp/gcp.py
+++ sdk/python/kfp/gcp.py
@@ -122,7 +122,8 @@ def use_preemptible_nodepool(toleration: V1Toleration = V1Toleration(effect='NoS
 
   return _set_preemptible
 
-def use_gpu_nodepool(toleration: V1Toleration = V1Toleration(effect='NoSchedule', key='nvidia.com/gpu', operator='Equal', value='true')):
+def use_gpu_nodepool(toleration: V1Toleration = V1Toleration(
+    effect='NoSchedule', key='nvidia.com/gpu', operator='Equal', value='true')):
     """An operator that configures the GKE GPU nodes in a container op.
 
     Args:
