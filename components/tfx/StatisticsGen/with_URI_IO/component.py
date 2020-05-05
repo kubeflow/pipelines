d5d4b4675103b1a9ebd30ec01583f4b5d079112b
Alexey Volkov <alexey.volkov@ark-kun.com>
2020-05-05 00:10:10 -0700

    Addressed the review feedback

--- components/tfx/StatisticsGen/with_URI_IO/component.py
+++ components/tfx/StatisticsGen/with_URI_IO/component.py
@@ -18,7 +18,6 @@ def StatisticsGen(
     import os
     import tempfile
     import tensorflow
-    import uuid
     from google.protobuf import json_format, message
     from tfx.types import channel_utils, artifact_utils
     from tfx.components.base import base_executor
@@ -28,20 +27,22 @@ def StatisticsGen(
     component_class_args = {}
 
     for name, execution_parameter in component_class.SPEC_CLASS.PARAMETERS.items():
-        argument_value_obj = argument_value = arguments.get(name, None)
+        argument_value = arguments.get(name, None)
         if argument_value is None:
             continue
         parameter_type = execution_parameter.type
-        if isinstance(parameter_type, type) and issubclass(parameter_type, message.Message): # Maybe FIX: execution_parameter.type can also be a tuple
+        if isinstance(parameter_type, type) and issubclass(parameter_type, message.Message):
             argument_value_obj = parameter_type()
             json_format.Parse(argument_value, argument_value_obj)
+        else:
+            argument_value_obj = argument_value
         component_class_args[name] = argument_value_obj
 
     for name, channel_parameter in component_class.SPEC_CLASS.INPUTS.items():
         artifact_path = arguments.get(name + '_uri', None) or arguments.get(name + '_path', None)
         if artifact_path:
             artifact = channel_parameter.type()
-            artifact.uri = artifact_path + '/' # ?
+            artifact.uri = artifact_path.rstrip('/') + '/'  # Some TFX components require that the artifact URIs end with a slash
             if channel_parameter.type.PROPERTIES and 'split_names' in channel_parameter.type.PROPERTIES:
                 # Recovering splits
                 subdirs = tensorflow.io.gfile.listdir(artifact_path)
@@ -75,7 +76,7 @@ def StatisticsGen(
     executor_context = base_executor.BaseExecutor.Context(
         beam_pipeline_args=beam_pipeline_args,
         tmp_dir=tempfile.gettempdir(),
-        unique_id=uuid.uuid4(),
+        unique_id='tfx_component',
     )
     executor = component_class_instance.executor_spec.executor_class(executor_context)
     executor.Do(
