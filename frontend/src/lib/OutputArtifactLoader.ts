028267a16d0dd039b78cd78f9ff1209df8483f6f
Yuan (Bob) Gong <gongyuan94@gmail.com>
2020-05-08 16:11:42 +0800

    [UI] Improve TFX artifact visualization speed (#3712)
    * Improve TFX artifact visualization speed
    
    * Update OutputArtifactLoader.ts
    
    * Fix loading
    
    * Add nodeId back as an identifier

--- frontend/src/lib/OutputArtifactLoader.ts
+++ frontend/src/lib/OutputArtifactLoader.ts
@@ -239,12 +239,12 @@ export class OutputArtifactLoader {
    * @returns config array, also returns empty array when expected erros happen
    */
   public static async buildTFXArtifactViewerConfig({
-    argoPodName,
     namespace,
+    execution,
     reportProgress = () => null,
   }: {
     namespace: string;
-    argoPodName: string;
+    execution: Execution;
     reportProgress: (progress: number) => void;
   }): Promise<HTMLViewerConfig[]> {
     // Error handling assumptions:
@@ -260,33 +260,15 @@ export class OutputArtifactLoader {
     // Since artifact types don't change per run, this can be optimized further so
     // that we don't fetch them on every page load.
     reportProgress(10);
-    const artifactTypes = await getArtifactTypes();
-    if (artifactTypes.length === 0) {
-      // There are no artifact types data.
+    const [artifactTypes, artifacts] = await Promise.all([
+      getArtifactTypes(),
+      getOutputArtifactsInExecution(execution),
+    ]);
+    if (artifactTypes.length === 0 || artifacts.length === 0) {
+      // There are no artifact types data or no artifacts.
       return [];
     }
-    reportProgress(20);
-
-    const context = await getMlmdContext(argoPodName);
-    if (!context) {
-      // Failed finding corresponding MLMD context.
-      return [];
-    }
-    reportProgress(40);
-
-    const execution = await getExecutionInContextWithPodName(argoPodName, context);
-    if (!execution) {
-      // Failed finding corresponding MLMD execution.
-      return [];
-    }
-    reportProgress(60);
-
-    const artifacts = await getOutputArtifactsInExecution(execution);
-    if (artifacts.length === 0) {
-      // There are no artifacts in this execution.
-      return [];
-    }
-    reportProgress(80);
+    reportProgress(70);
 
     // TODO: Visualize non-TFDV artifacts, such as ModelEvaluation using TFMA
     let viewers: Array<Promise<HTMLViewerConfig>> = [];
