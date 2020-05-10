8d45e7db8d6dd7059f6d96cebae0564f34bcbdb9
Alexey Volkov <alexey.volkov@ark-kun.com>
2020-05-10 14:50:12 -0700

    SDK - Made outputs with original names available in ContainerOp.outputs
    Previously, ContainerOp had strict requirements for the output names, so we had to convert all the names before passing them to the ContainerOp constructor. Outputs with non-pythonic names could not be accessed using their original names.
    Now ContainerOp supports any output names, so we're now using the original output names.
    However to support legacy pipelines, we're also adding output references with pythonic names.

--- sdk/python/kfp/dsl/_container_op.py
+++ sdk/python/kfp/dsl/_container_op.py
@@ -1098,9 +1098,11 @@ class ContainerOp(BaseOp):
                 name: _pipeline_param.PipelineParam(name, op_name=self.name)
                 for name in file_outputs.keys()
             }
-
-        if len(self.outputs) == 1:
-            self.output = list(self.outputs.values())[0]
+        
+        # Syntactic sugar: Add task.output attribute if the component has a single output.
+        unique_outputs = set(self.outputs.values())
+        if len(unique_outputs) == 1:
+            self.output = list(unique_outputs)[0]
         else:
             self.output = _MultipleOutputsError()
 
@@ -1163,9 +1165,6 @@ class ContainerOp(BaseOp):
                     if output_meta.name == output:
                         output_type = output_meta.type
                 self.outputs[output].param_type = output_type
-
-            if len(self.outputs) == 1:
-                self.output = list(self.outputs.values())[0]
 
     def add_pvolumes(self,
                      pvolumes: Dict[str, V1Volume] = None):
