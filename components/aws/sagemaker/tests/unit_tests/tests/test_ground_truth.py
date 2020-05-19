e8a07f00a1e3d36f6c65552f7d0b65a41e25b757
Kartik Kalamadi <kalamadi@amazon.com>
2020-05-18 20:46:44 -0700

    add more tests for deploy and ground_truth components

--- components/aws/sagemaker/tests/unit_tests/tests/test_ground_truth.py
+++ components/aws/sagemaker/tests/unit_tests/tests/test_ground_truth.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 
-from unittest.mock import patch, Mock, MagicMock
+from unittest.mock import patch, call, Mock, MagicMock, mock_open
 from botocore.exceptions import ClientError
 from datetime import datetime
 
@@ -31,12 +31,61 @@ class GroundTruthTestCase(unittest.TestCase):
     parser = ground_truth.create_parser()
     cls.parser = parser
 
-  def test_sample(self):
-    args = self.parser.parse_args(required_args)
-    response = _utils.create_labeling_job_request(vars(args))
-    self.assertEqual(response['LabelingJobName'], 'test_job')
+  def test_create_parser(self):
+    self.assertIsNotNone(self.parser)
 
-  def test_empty_string(self):
-    args = self.parser.parse_args(required_args)
-    response = _utils.create_labeling_job_request(vars(args))
-    test_utils.check_empty_string_values(response)
+  def test_main(self):
+    # Mock out all of utils except parser
+    ground_truth._utils = MagicMock()
+    ground_truth._utils.add_default_client_arguments = _utils.add_default_client_arguments
+
+    # Set some static returns
+    ground_truth._utils.get_labeling_job_outputs.return_value = ('s3://fake-bucket/output', 'arn:aws:sagemaker:us-east-1:999999999999:labeling-job')
+
+    with patch('builtins.open', mock_open()) as file_open:
+      ground_truth.main(required_args)
+
+    # Check if correct requests were created and triggered
+    ground_truth._utils.create_labeling_job.assert_called()
+    ground_truth._utils.wait_for_labeling_job.assert_called()
+    ground_truth._utils.get_labeling_job_outputs.assert_called()
+
+    # Check the file outputs
+    file_open.assert_has_calls([
+      call('/tmp/output_manifest_location.txt', 'w'),
+      call('/tmp/active_learning_model_arn.txt', 'w')
+    ], any_order=True)
+
+    file_open().write.assert_has_calls([
+      call('s3://fake-bucket/output'),
+      call('arn:aws:sagemaker:us-east-1:999999999999:labeling-job')
+    ], any_order=False)
+
+  def test_ground_truth(self):
+    mock_client = MagicMock()
+    mock_args = self.parser.parse_args(required_args)
+    response = _utils.create_labeling_job(mock_client, vars(mock_args))
+
+    mock_client.create_labeling_job.assert_called_once_with(
+      HumanTaskConfig={'WorkteamArn': None, 'UiConfig': {'UiTemplateS3Uri': 's3://fake-bucket/ui_template'},
+                       'PreHumanTaskLambdaArn': '', 'TaskTitle': 'fake-image-labelling-work',
+                       'TaskDescription': 'fake job', 'NumberOfHumanWorkersPerDataObject': 1,
+                       'TaskTimeLimitInSeconds': 180,
+                       'AnnotationConsolidationConfig': {'AnnotationConsolidationLambdaArn': ''}},
+      InputConfig={'DataSource': {'S3DataSource': {'ManifestS3Uri': 's3://fake-bucket/manifest'}}},
+      LabelAttributeName='test_job', LabelCategoryConfigS3Uri='', LabelingJobName='test_job',
+      OutputConfig={'S3OutputPath': 's3://fake-bucket/output', 'KmsKeyId': ''},
+      RoleArn='arn:aws:iam::123456789012:user/Development/product_1234/*', Tags=[]
+    )
+
+    self.assertEqual(response, 'test_job')
+
+  def test_sagemaker_exception_in_ground_truth(self):
+    mock_client = MagicMock()
+    mock_exception = ClientError({"Error": {"Message": "SageMaker broke"}}, "ground_truth")
+    mock_client.create_labeling_job.side_effect = mock_exception
+    mock_args = self.parser.parse_args(required_args)
+
+    with self.assertRaises(Exception):
+      _utils.get_labeling_job_outputs(mock_client, vars(mock_args))
+
