98b59aa26ea0d6e466ba16a39a2bb6aaecbcb411
Yuan Gong <gongyuan94@gmail.com>
2020-05-27 12:01:26 +0800

    Add old components release script back

--- components/release.sh
+++ components/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2020 Google LLC
+# Copyright 2018 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,11 +14,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This script automated the process to update the component images.
+# This script automated the process to release the component images.
 # To run it, find a good release candidate commit SHA from ml-pipeline-test project,
 # and provide a full github COMMIT SHA to the script. E.g.
-# ./update-for-release.sh 2118baf752d3d30a8e43141165e13573b20d85b8
+# ./release.sh 2118baf752d3d30a8e43141165e13573b20d85b8
 # The script copies the images from test to prod, and update the local code.
+# You can then send a PR using your local branch.
 
 set -xe
 
@@ -38,39 +39,62 @@ images=(
   "ml-pipeline-gcp"
 )
 
-TAG_NAME=$1
+COMMIT_SHA=$1
 FROM_GCR_PREFIX='gcr.io/ml-pipeline-test/'
 TO_GCR_PREFIX='gcr.io/ml-pipeline/'
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
+REPO=kubeflow/pipelines
 
-if [ -z "$TAG_NAME" ]; then
-  echo "Usage: release.sh <tag-name>" >&2
+if [ -z "$COMMIT_SHA" ]; then
+  echo "Usage: release.sh <commit-SHA>" >&2
   exit 1
 fi
 
-# KFP repo root
-pushd "$DIR/.."
+# Checking out the repo
+clone_dir=$(mktemp -d)
+git clone "git@github.com:${REPO}.git" "$clone_dir"
+cd "$clone_dir"
+branch="release-$COMMIT_SHA"
+# Creating the release branch from the specified commit
+release_head=$COMMIT_SHA
+git checkout "$release_head" -b "$branch"
 
-# Update setup.py VERSION
-sed -i.bak -e "s|VERSION =.\+'|VERSION = '${TAG_NAME}'|g" "components/gcp/container/component_sdk/python/setup.py"
-
-# Updating components and samples.
+# Releasing the container images to public. Updating components and samples.
 for image in "${images[@]}"
 do
   TARGET_IMAGE_BASE=${TO_GCR_PREFIX}${image}
-  TARGET_IMAGE=${TARGET_IMAGE_BASE}:${TAG_NAME}
+  TARGET_IMAGE=${TARGET_IMAGE_BASE}:${COMMIT_SHA}
+
+  # Move image from test to prod GCR
+  gcloud container images add-tag --quiet \
+  ${FROM_GCR_PREFIX}${image}:${COMMIT_SHA} ${TARGET_IMAGE}
 
   # Update the code
-  find components samples -type f | while read file; do
-    sed -i -e "s|${TARGET_IMAGE_BASE}:\([a-zA-Z0-9_.-]\)\+|${TARGET_IMAGE}|g" "$file"
-  done
+  find components samples -type f | while read file; do sed -i -e "s|${TARGET_IMAGE_BASE}:\([a-zA-Z0-9_.-]\)\+|${TARGET_IMAGE}|g" "$file"; done
 done
 
+# Checking-in the container image changes
+git add --all
+git commit --message "Updated component images to version $COMMIT_SHA"
+image_update_commit_sha=$(git rev-parse HEAD)
+
 # Updating the samples to use the updated components
-git diff --name-only | while read component_file; do
+git diff HEAD~1 HEAD --name-only | while read component_file; do
     echo $component_file
     find components samples -type f | while read file; do
-      sed -i -E "s|(https://raw.githubusercontent.com/kubeflow/pipelines/)[^/]+(/$component_file)|\1${TAG_NAME}\2|g" "$file";
+      sed -i -E "s|(https://raw.githubusercontent.com/kubeflow/pipelines/)[^/]+(/$component_file)|\1${image_update_commit_sha}\2|g" "$file";
     done
 done
-popd
+
+# Checking-in the component changes
+git add --all
+git commit --message "Updated components to version $image_update_commit_sha"
+component_update_commit_sha=$(git rev-parse HEAD)
+
+# Pushing the changes upstream
+read -p "Do you want to push the new branch to upstream to create a PR? [y|n]"
+if [ "$REPLY" != "y" ]; then
+   exit
+fi
+git push --set-upstream origin "$branch"
+
+sensible-browser "https://github.com/${REPO}/compare/master...$branch"
