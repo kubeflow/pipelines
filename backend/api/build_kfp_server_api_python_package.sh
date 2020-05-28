3c6f3124150a9b2d2ce5c4007cf7907ebbbf137c
Yuan Gong <gongyuan94@gmail.com>
2020-05-28 12:50:30 +0800

    Add more information to generated swagger json and a python client template

--- backend/api/build_kfp_server_api_python_package.sh
+++ backend/api/build_kfp_server_api_python_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2018 Google LLC
+# Copyright 2018-2020 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,6 +27,8 @@
 # brew cask install caskroom/versions/java8
 # brew install jq
 
+set -e
+
 VERSION="$1"
 
 if [ -z "$VERSION" ]; then
@@ -38,7 +40,8 @@ codegen_file=/tmp/swagger-codegen-cli.jar
 # Browse all versions in: https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.7/
 codegen_uri=https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.7/swagger-codegen-cli-2.4.7.jar
 if ! [ -f "$codegen_file" ]; then
-    wget --no-verbose "$codegen_uri" -O "$codegen_file"
+    # curl is more portable than wget, because macOS doesn't have wget by default
+    curl -L "$codegen_uri" -o "$codegen_file"
 fi
 
 pushd "$(dirname "$0")"
@@ -47,15 +50,19 @@ DIR=$(mktemp -d)
 
 swagger_file=$(mktemp)
 
+# Documentation for info object can be found at https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#infoObject
 echo "Merging all Swagger API definitions to $swagger_file."
 jq -s '
     reduce .[] as $item ({}; . * $item) |
     .info.title = "KF Pipelines API" |
-    .info.description = "Generated python client for the KF Pipelines server API"
+    .info.description = "Generated python client for the KF Pipelines server API" |
+    .info.version = "'$VERSION'" |
+    .info.contact = { "name": "google", "email": "kubeflow-pipelines@google.com", "url": "https://www.google.com" } |
+    .info.license = { "name": "Apache 2.0", "url": "https://raw.githubusercontent.com/kubeflow/pipelines/master/LICENSE" }
 ' ./swagger/{run,job,pipeline,experiment,pipeline.upload}.swagger.json > "$swagger_file"
 
 echo "Generating python code from swagger json in $DIR."
-java -jar "$codegen_file" generate -l python -i "$swagger_file" -o "$DIR" -c <(echo '{
+java -jar "$codegen_file" generate -l python -i "$swagger_file" -t "./python_client_template" -o "$DIR" -c <(echo '{
     "packageName": "kfp_server_api",
     "projectName": "kfp-server-api",
     "packageVersion": "'"$VERSION"'",
