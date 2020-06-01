854d0ff8cd3039182d1535ea67da6965f22df368
Kartik Kalamadi <kalamadi@amazon.com>
2020-06-01 16:22:05 -0700

    change docker build context integ test

--- samples/contrib/aws-samples/simple_train_pipeline/training-pipeline.py
+++ samples/contrib/aws-samples/simple_train_pipeline/training-pipeline.py
@@ -5,12 +5,17 @@
 
 import kfp
 import json
+import os
 import copy
 from kfp import components
 from kfp import dsl
 from kfp.aws import use_aws_secret
 
-sagemaker_train_op = components.load_component_from_url('https://raw.githubusercontent.com/akartsky/pipelines/integ_for_sample/components/aws/sagemaker/train/component.yaml')
+
+cur_file_dir = os.path.dirname(__file__)
+components_dir = os.path.join(cur_file_dir, '../../../../components/aws/sagemaker/')
+
+sagemaker_train_op = components.load_component_from_file(components_dir + '/train/component.yaml')
 
 channelObjList = []
 
