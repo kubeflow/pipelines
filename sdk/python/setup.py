886566f7268c246e50280983dc4e5bf3d8625918
Yuan Gong <gongyuan94@gmail.com>
2020-06-05 03:06:17 +0000

    kfp sdk requirement updated to kfp_api_server<2.0.0

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -27,10 +27,11 @@ REQUIRES = [
     'requests_toolbelt>=0.8.0',
     'cloudpickle',
     # Update the upper version whenever a new major version of the
-    # kfp-server-api package is released. Update the lower version when there is
-    # a breaking change in kfp-server-api, or kfp sdk depends on new api changes
-    # in kfp-server-api.
-    'kfp-server-api>=0.2.5, <0.6.0',
+    # kfp-server-api package is released.
+    # Update the lower version when kfp sdk depends on new apis/fields in
+    # kfp-server-api.
+    # Note, please also update ./requirements.in
+    'kfp-server-api>=0.2.5, <2.0.0',
     'jsonschema >= 3.0.1',
     'tabulate',
     'click',
