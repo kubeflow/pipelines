bc94cd8d6a8d1294b452632dbb182b6ec281b9f9
Alexey Volkov <alexey.volkov@ark-kun.com>
2020-06-15 14:35:04 -0700

    Finalized the sample

--- components/CatBoost/_samples/sample_pipeline.py
+++ components/CatBoost/_samples/sample_pipeline.py
@@ -3,6 +3,7 @@ from kfp import components
 
 
 chicago_taxi_dataset_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/e3337b8bdcd63636934954e592d4b32c95b49129/components/datasets/Chicago%20Taxi/component.yaml')
+pandas_transform_csv_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/e69a6694/components/pandas/Transform_DataFrame/in_CSV_format/component.yaml')
 
 catboost_train_classifier_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/f97ad2/components/CatBoost/Train_classifier/from_CSV/component.yaml')
 catboost_train_regression_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/f97ad2/components/CatBoost/Train_regression/from_CSV/component.yaml')
@@ -12,9 +13,6 @@ catboost_predict_class_probabilities_op = components.load_component_from_url('ht
 catboost_to_apple_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/f97ad2/components/CatBoost/convert_CatBoostModel_to_AppleCoreMLModel/component.yaml')
 catboost_to_onnx_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/f97ad2/components/CatBoost/convert_CatBoostModel_to_ONNX/component.yaml')
 
-convert_csv_to_apache_parquet_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/d737c448723b9f541a3543012b4414c17b2eab5c/components/_converters/ApacheParquet/from_CSV/component.yaml')
-pandas_transform_parquet_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/e69a6694/components/pandas/Transform_DataFrame/in_ApacheParquet_format/component.yaml')
-pandas_transform_csv_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/e69a6694/components/pandas/Transform_DataFrame/in_CSV_format/component.yaml')
 
 def catboost_pipeline():
     training_data_in_csv = chicago_taxi_dataset_op(
@@ -23,7 +21,10 @@ def catboost_pipeline():
         limit=10000,
     ).output
 
-    evaluation_data_in_csv = training_data_in_csv
+    training_data_for_classification_in_csv = pandas_transform_csv_op(
+        table=training_data_in_csv,
+        transform_code='''df.insert(0, "was_tipped", df["tips"] > 0); del df["tips"]''',
+    ).output
 
     catboost_train_regression_task = catboost_train_regression_op(
         training_data=training_data_in_csv,
@@ -32,31 +33,7 @@ def catboost_pipeline():
         num_iterations=200,
     )
 
-    catboost_predict_values_op(
-        data=evaluation_data_in_csv,
-        model=catboost_train_regression_task.outputs['model'],
-        label_column=0,
-    )
-
-    catboost_to_apple_op(catboost_train_regression_task.outputs['model'])
-
-    catboost_to_onnx_op(catboost_train_regression_task.outputs['model'])
-
-    training_data_in_parquet = convert_csv_to_apache_parquet_op(training_data_in_csv).output
-
-    training_data_for_classification_in_parquet = pandas_transform_parquet_op(
-        table=training_data_in_parquet,
-        #transform_code='''df["was_tipped"] = df["tips"] > 0''',
-        transform_code='''df.insert(0, "was_tipped", df["tips"] > 0); del df["tips"]''',
-    ).output
-
-    training_data_for_classification_in_csv = pandas_transform_csv_op(
-        table=training_data_in_csv,
-        #transform_code='''df["was_tipped"] = df["tips"] > 0''',
-        transform_code='''df.insert(0, "was_tipped", df["tips"] > 0); del df["tips"]''',
-    ).output
-    
-    evaluation_data_for_classification_in_csv = training_data_for_classification_in_csv
+    regression_model = catboost_train_regression_task.outputs['model']
 
     catboost_train_classifier_task = catboost_train_classifier_op(
         training_data=evaluation_data_for_classification_in_csv,
@@ -64,17 +41,34 @@ def catboost_pipeline():
         num_iterations=200,
     )
 
+    classification_model = catboost_train_classifier_task.outputs['model']
+
+    evaluation_data_for_regression_in_csv = training_data_in_csv
+    evaluation_data_for_classification_in_csv = training_data_for_classification_in_csv
+
+    catboost_predict_values_op(
+        data=evaluation_data_for_regression_in_csv,
+        model=regression_model,
+        label_column=0,
+    )
+
     catboost_predict_classes_op(
-        data=evaluation_data_in_csv,
-        model=catboost_train_classifier_task.outputs['model'],
+        data=evaluation_data_for_classification_in_csv,
+        model=classification_model,
         label_column=0,
     )
 
     catboost_predict_class_probabilities_op(
         data=evaluation_data_for_classification_in_csv,
-        model=catboost_train_classifier_task.outputs['model'],
+        model=classification_model,
         label_column=0,
     )
+
+    catboost_to_apple_op(regression_model)
+    catboost_to_apple_op(classification_model)
+
+    catboost_to_onnx_op(regression_model)
+    catboost_to_onnx_op(classification_model)
 
 
 if __name__ == '__main__':
