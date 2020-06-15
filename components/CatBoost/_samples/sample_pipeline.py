e1ac95f05d6469bc7aaa3610ced21714ff4f0bd3
Alexey Volkov <alexey.volkov@ark-kun.com>
2020-06-14 22:39:19 -0700

    Updated the sample pipeline

--- components/CatBoost/_samples/sample_pipeline.py
+++ components/CatBoost/_samples/sample_pipeline.py
@@ -2,9 +2,14 @@ import kfp
 from kfp import components
 
 
-chicago_taxi_dataset_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/5e3d9aa791fe684962d16be4f0f767feed6222b2/components/datasets/Chicago_Taxi_Trips/component.yaml')
-catboost_train_classifier_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/beea69058b71cc44fb491c5728a6efa99a4a5bee/components/CatBoost/Train_classifier/component.yaml')
-catboost_predict_class_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/beea69058b71cc44fb491c5728a6efa99a4a5bee/components/CatBoost/Predict_class/component.yaml')
+chicago_taxi_dataset_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/ce4ce20a/components/datasets/Chicago_Taxi_Trips/component.yaml')
+catboost_train_classifier_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/ce4ce20a/components/CatBoost/Train_classifier/component.yaml')
+catboost_train_regression_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/ce4ce20a/components/CatBoost/Train_regression/component.yaml')
+catboost_predict_classes_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/ce4ce20a/components/CatBoost/Predict_classes/component.yaml')
+catboost_predict_values_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/ce4ce20a/components/CatBoost/Predict_values/component.yaml')
+catboost_predict_class_probabilities_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/ce4ce20a/components/CatBoost/Predict_class_probabilities/component.yaml')
+catboost_to_apple_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/ce4ce20a/components/CatBoost/convert_CatBoostModel_to_AppleCoreMLModel/component.yaml')
+catboost_to_onnx_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/ce4ce20a/components/CatBoost/convert_CatBoostModel_to_ONNX/component.yaml')
 
 
 def catboost_pipeline():
@@ -14,13 +19,34 @@ def catboost_pipeline():
         limit=10000,
     )
     
-    catboost_train_task = catboost_train_classifier_op(
+    catboost_train_regression_task = catboost_train_regression_op(
         training_data=get_training_data_task.output,
         label_column=0,
         num_iterations=200,
     )
     
-    catboost_predict_class_op(
+    catboost_predict_values_op(
+        data=get_training_data_task.output,
+        model=catboost_train_classifier_task.outputs['model'],
+        label_column=0,
+    )
+
+    catboost_to_apple_op(catboost_train_classifier_task.outputs['model'])
+    catboost_to_onnx_op(catboost_train_classifier_task.outputs['model'])
+
+    catboost_train_classifier_task = catboost_train_classifier_op(
+        training_data=get_training_data_task.output,
+        label_column=0,
+        num_iterations=200,
+    )
+    
+    catboost_predict_classes_op(
+        data=get_training_data_task.output,
+        model=catboost_train_task.outputs['model'],
+        label_column=0,
+    )
+    
+    catboost_predict_class_probabilities_op(
         data=get_training_data_task.output,
         model=catboost_train_task.outputs['model'],
         label_column=0,
