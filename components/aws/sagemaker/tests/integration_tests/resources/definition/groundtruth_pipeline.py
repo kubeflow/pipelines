9286bd0b7a97342a1047294009849fd8f56504a4
Suraj Kota <surakota@amazon.com>
2020-06-16 22:56:04 +0530

    [AWS Sagemaker] aws-samples kmeans-hpo pipeline test (#3905)
    * aws-samples kmeans-hpo pipeline test
    	- code clean up
    	- removed unused args
    
    * add test dependency
    
    * Trigger Build

--- components/aws/sagemaker/tests/integration_tests/resources/definition/groundtruth_pipeline.py
+++ components/aws/sagemaker/tests/integration_tests/resources/definition/groundtruth_pipeline.py
@@ -1,9 +1,6 @@
 import kfp
-import json
-import copy
 from kfp import components
 from kfp import dsl
-from kfp.aws import use_aws_secret
 
 sagemaker_gt_op = components.load_component_from_file(
     "../../ground_truth/component.yaml"
@@ -34,7 +31,7 @@ def ground_truth_test(
     workteam_arn="",
 ):
 
-    ground_truth_train = sagemaker_gt_op(
+    sagemaker_gt_op(
         region=region,
         role=role,
         job_name=ground_truth_train_job_name,
