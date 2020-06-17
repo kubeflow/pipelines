6802c2ef6bb96fcc4c5e916bf5374f7fe30fc957
Nicholas Thomson <nithomso@amazon.com>
2020-06-17 00:01:39 +0000

    Update processing integration test

--- components/aws/sagemaker/tests/integration_tests/scripts/kmeans_preprocessing.py
+++ components/aws/sagemaker/tests/integration_tests/scripts/kmeans_preprocessing.py
@@ -4,29 +4,20 @@ import numpy
 import io
 from sagemaker.amazon.common import write_numpy_to_dense_tensor
 
-print("Starting processing")
-
+print("Extracting MNIST data set")
 # Load the dataset
 with gzip.open('/opt/ml/processing/input/mnist.pkl.gz', 'rb') as f:
     train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
 
 # process the data
 # Convert the training data into the format required by the SageMaker KMeans algorithm
-buf = io.BytesIO()
-write_numpy_to_dense_tensor(buf, train_set[0], train_set[1])
-buf.seek(0)
-
 print("Writing training data")
-with open('/opt/ml/processing/output_train/train_data', 'wb') as train_data_file:
-    pickle.dump(buf, train_data_file)
-
-# Convert the test data into the format required by the SageMaker KMeans algorithm
-write_numpy_to_dense_tensor(buf, test_set[0], test_set[1])
-buf.seek(0)
+with open('/opt/ml/processing/output_train/train_data', 'wb') as train_file:
+    write_numpy_to_dense_tensor(train_file, train_set[0], train_set[1])
 
 print("Writing test data")
-with open('/opt/ml/processing/output_test/test_data', 'wb') as test_data_file:
-    pickle.dump(buf, test_data_file)
+with open('/opt/ml/processing/output_test/test_data', 'wb') as test_file:
+    write_numpy_to_dense_tensor(test_file, test_set[0], test_set[1])
 
 print("Writing validation data")
 # Convert the valid data into the format required by the SageMaker KMeans algorithm
