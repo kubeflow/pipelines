796430be115baa7cffb9fcdd657c27831ec2a7a9
Niklas Hansson <niklas.hansson@sandvik.com>
2020-07-08 12:37:10 +0200

    feat(sdk): CLI - Allow upload_version by the pipeline's name. Fixes #3901 (#4087)
    * Update _client.py
    
    * Allow for passing name instead of only pipeline_id
    
    * fixed old rebase issue
    
    * protobuf fix
    
    * raise error and remove f-string
    
    * moved file
    
    * updated python proto packages versions
    
    * Update sdk/python/kfp/_client.py
    
    * restructured and added to build
    
    * changes structure of import
    
    * Updated the files
    
    * further updates of client and filter.proto
    
    * alternative
    
    * clean up
    
    * clean up
    
    * remove helperfiles
    
    * futher clean up
    
    * clean up or eaither name or id
    
    * update doc strings
    
    * remove page_size
    
    * Update sdk/python/kfp/_client.py
    
    Co-authored-by: Jiaxiao Zheng <jxzheng@google.com>
    
    * Update sdk/python/kfp/cli/pipeline.py
    
    Co-authored-by: Jiaxiao Zheng <jxzheng@google.com>
    
    * updated to classical string formattin
    
    Co-authored-by: Jiaxiao Zheng <jxzheng@google.com>

--- sdk/python/kfp/_client.py
+++ sdk/python/kfp/_client.py
@@ -39,7 +39,15 @@ from kfp._auth import get_auth_token, get_gcp_access_token
 # initialized with host=<inverse proxy endpoint>.
 # Set to 55 mins to provide some safe margin.
 _GCP_ACCESS_TOKEN_TIMEOUT = datetime.timedelta(minutes=55)
-
+# Operators on scalar values. Only applies to one of |int_value|,
+# |long_value|, |string_value| or |timestamp_value|.
+_FILTER_OPERATIONS = {"UNKNOWN": 0,
+    "EQUALS" : 1,
+    "NOT_EQUALS" : 2,
+    "GREATER_THAN": 3,
+    "GREATER_THAN_EQUALS": 5,
+    "LESS_THAN": 6,
+    "LESS_THAN_EQUALS": 7}
 
 def _add_generated_apis(target_struct, api_module, api_client):
   '''Initializes a hierarchical API object based on the generated API module.
@@ -303,6 +311,29 @@ class Client(object):
           % (self._get_url_prefix(), experiment.id))
       IPython.display.display(IPython.display.HTML(html))
     return experiment
+
+  def get_pipeline_id(self, name):
+    """Returns the pipeline id if a pipeline with the name exsists.
+    Args:
+      name: pipeline name
+    Returns:
+      A response object including a list of experiments and next page token.
+    """
+    pipeline_filter = json.dumps({
+      "predicates": [
+        {
+          "op":  _FILTER_OPERATIONS["EQUALS"],
+          "key": "name",
+          "stringValue": name,
+        }
+      ]
+    })
+    result = self._pipelines_api.list_pipelines(filter=pipeline_filter)
+    if len(result.pipelines)==1:
+      return result.pipelines[0].id
+    elif len(result.pipelines)>1:
+      raise ValueError("Multiple pipelines with the name: {} found, the name needs to be unique".format(name))
+    return None
 
   def list_experiments(self, page_token='', page_size=10, sort_by='', namespace=None):
     """List experiments.
