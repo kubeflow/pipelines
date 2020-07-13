84a2dd762722ad627c94052b96edabb2069052cf
Nicholas Thomson <nithomso@amazon.com>
2020-07-13 20:09:35 +0000

    Add client assume role functionality

--- components/aws/sagemaker/batch_transform/src/batch_transform.py
+++ components/aws/sagemaker/batch_transform/src/batch_transform.py
@@ -52,7 +52,7 @@ def main(argv=None):
   args = parser.parse_args(argv)
 
   logging.getLogger().setLevel(logging.INFO)
-  client = _utils.get_sagemaker_client(args.region, args.endpoint_url)
+  client = _utils.get_sagemaker_client(args.region, args.endpoint_url, assume_role_arn=args.assume_role)
   logging.info('Submitting Batch Transformation request to SageMaker...')
   batch_job_name = _utils.create_transform_job(client, vars(args))
 
@@ -68,7 +68,7 @@ def main(argv=None):
   except:
     raise
   finally:
-    cw_client = _utils.get_cloudwatch_client(args.region)
+    cw_client = _utils.get_cloudwatch_client(args.region, assume_role_arn=args.assume_role)
     _utils.print_logs_for_job(cw_client, '/aws/sagemaker/TransformJobs', batch_job_name)
 
   _utils.write_output(args.output_location_output_path, args.output_location)
