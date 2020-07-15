1afcfbe19ea988de9ed48d4d396a70353e78b294
Alex Latchford <alexla@zillowgroup.com>
2020-07-15 16:37:31 -0700

    Update SDK docs for KFP extensions to follow Sphinx guidelines

--- docs/conf.py
+++ docs/conf.py
@@ -106,7 +106,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
