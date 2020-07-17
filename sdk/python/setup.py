9c16eff256f489744ce9440201872a17b7aad67f
Alexey Volkov <alexey.volkov@ark-kun.com>
2020-07-16 19:59:00 -0700

    SDK - Components - Temporary removed Kubernetes options from ComponentSpec (#4230)
    * SDK - Components - Replaced Kubernetes options with generic launcher options
    
    This reduces the schema size and makes the task launcher options more flexible.
    
    * Removed the launcherOptions for now

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -69,7 +69,6 @@ setup(
         'kfp.compiler',
         'kfp.components',
         'kfp.components.structures',
-        'kfp.components.structures.kubernetes',
         'kfp.containers',
         'kfp.dsl',
         'kfp.dsl.extensions',
