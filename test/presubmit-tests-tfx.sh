4f1e35db86fe0dfb2451fc211a9ccc5bda62eb44
Alexey Volkov <alexey.volkov@ark-kun.com>
2020-07-16 20:25:13 -0700

    Testing - Fixed the presubmit TFX test by switching from protoc to Bazel

--- test/presubmit-tests-tfx.sh
+++ test/presubmit-tests-tfx.sh
@@ -33,12 +33,11 @@ python3 -m pip install -e .
 popd # Changing the current directory to the repo root for correct coverall paths
 
 # Test against TFX
-# Compile and setup protobuf
-PROTOC_ZIP=protoc-3.7.1-linux-x86_64.zip
-curl -OL -sS https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/$PROTOC_ZIP
-unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
-unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
-rm -f $PROTOC_ZIP
+# Compile and setup bazel for compiling the protos
+# Instruction from https://docs.bazel.build/versions/master/install-ubuntu.html
+curl -sSL https://github.com/bazelbuild/bazel/releases/download/3.4.1/bazel-3.4.1-installer-linux-x86_64.sh -o bazel_installer.sh
+chmod +x bazel_installer.sh
+./bazel_installer.sh
 
 # Install TFX from head
 cd $source_root
