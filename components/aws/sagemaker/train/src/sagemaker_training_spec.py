dd861edaec69aaadd60eeb59c2d4906fe2d12850
Nicholas Thomson <nithomso@amazon.com>
2020-07-24 16:28:13 +0000

    Update pydocs with formatter

--- components/aws/sagemaker/train/src/sagemaker_training_spec.py
+++ components/aws/sagemaker/train/src/sagemaker_training_spec.py
@@ -1,4 +1,4 @@
-"""Specification for the SageMaker training component"""
+"""Specification for the SageMaker training component."""
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
