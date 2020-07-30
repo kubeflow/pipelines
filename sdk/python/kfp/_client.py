da52c0793881c6783689f7cb0c04010081280666
Renmin <renming@google.com>
2020-07-30 11:38:30 +0800

    fix(sdk/client): reserve the host protocal (http or https) so that http host can work. Fixes #4277 (#4285)

--- sdk/python/kfp/_client.py
+++ sdk/python/kfp/_client.py
@@ -122,9 +122,9 @@ class Client(object):
 
     host = host or ''
     # Preprocess the host endpoint to prevent some common user mistakes.
-    # This should only be done for non-IAP cases (when client_id is None). IAP requires preserving the protocol.
     if not client_id:
-      host = re.sub(r'^(http|https)://', '', host).rstrip('/')
+      # always preserving the protocol (http://localhost requires it)
+      host = host.rstrip('/')
 
     if host:
       config.host = host
