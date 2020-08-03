dc20bf86b1fa03aa05ddb62bdb1c978638ad9782
Yuan Gong <gongyuan94@gmail.com>
2020-08-03 03:41:57 +0000

    updates

--- backend/src/apiserver/visualization/update_requirements.sh
+++ backend/src/apiserver/visualization/update_requirements.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-../../../update_requirements.sh tensorflow/tensorflow:2.1.0-py3 <requirements.in >requirements.txt
+# This image should be in sync with Dockerfile.visualization.
+IMAGE=tensorflow/tensorflow:2.1.0-py3
+../../../update_requirements.sh $IMAGE <requirements.in >requirements.txt
