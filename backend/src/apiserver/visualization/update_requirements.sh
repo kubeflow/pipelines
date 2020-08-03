6320dbfe33ab49692b3f85df5595ff9cb0f7d81d
Yuan Gong <gongyuan94@gmail.com>
2020-08-03 03:32:54 +0000

    chore: pin visualization server python dependencies

--- backend/src/apiserver/visualization/update_requirements.sh
+++ backend/src/apiserver/visualization/update_requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../../../update_requirements.sh tensorflow/tensorflow:2.1.0-py3 <requirements.in >requirements-new.txt
+../../../update_requirements.sh tensorflow/tensorflow:2.1.0-py3 <requirements.in >requirements.txt
