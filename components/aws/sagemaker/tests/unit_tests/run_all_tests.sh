5127d579582c2db655ccbc76f5a926712cbb1e6d
Nicholas Thomson <nithomso@amazon.com>
2020-08-05 19:24:26 +0000

    Update unit test automation

--- components/aws/sagemaker/tests/unit_tests/run_all_tests.sh
+++ components/aws/sagemaker/tests/unit_tests/run_all_tests.sh
@@ -1,4 +1,6 @@
-export PYTHONPATH=../../
+pushd ../..
 
-coverage run -m pytest --junitxml ./unit_tests.log
+coverage run -m pytest tests/unit_tests/tests --junitxml ./unit_tests.log
 coverage report -m --omit "*/usr/*,tests/*,*__init__*,*/Python/*"
+
+popd
