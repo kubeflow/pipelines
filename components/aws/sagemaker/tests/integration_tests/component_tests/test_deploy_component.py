57ad03f3b6b066834bb91f6f8fd7e4ea68da79a3
Nicholas Thomson <nithomso@amazon.com>
2020-09-02 18:28:15 +0000

    Add try/catch cleanup for integ resources

--- components/aws/sagemaker/tests/integration_tests/component_tests/test_deploy_component.py
+++ components/aws/sagemaker/tests/integration_tests/component_tests/test_deploy_component.py
@@ -13,8 +13,8 @@ from utils import sagemaker_utils
 
 
 def run_predict_mnist(boto3_session, endpoint_name, download_dir):
-    """ https://github.com/awslabs/amazon-sagemaker-examples/blob/a8c20eeb72dc7d3e94aaaf28be5bf7d7cd5695cb
-        /sagemaker-python-sdk/1P_kmeans_lowlevel/kmeans_mnist_lowlevel.ipynb """
+    """https://github.com/awslabs/amazon-sagemaker-examples/blob/a8c20eeb72dc7d3e94aaaf28be5bf7d7cd5695cb
+    /sagemaker-python-sdk/1P_kmeans_lowlevel/kmeans_mnist_lowlevel.ipynb"""
     # Download and load dataset
     region = boto3_session.region_name
     download_path = os.path.join(download_dir, "mnist.pkl.gz")
@@ -35,7 +35,9 @@ def run_predict_mnist(boto3_session, endpoint_name, download_dir):
     payload = np2csv(train_set[0][30:31])
 
     response = runtime.invoke_endpoint(
-        EndpointName=endpoint_name, ContentType="text/csv", Body=payload,
+        EndpointName=endpoint_name,
+        ContentType="text/csv",
+        Body=payload,
     )
     return json.loads(response["Body"].read().decode())
 
@@ -67,19 +69,20 @@ def test_create_endpoint(
     ] = test_params["Arguments"]["endpoint_name"] = input_endpoint_name = (
         utils.generate_random_string(5) + "-" + test_params["Arguments"]["model_name"]
     )
-    print(f"running test with model/endpoint name: {input_endpoint_name}")
-
-    _, _, workflow_json = kfp_client_utils.compile_run_monitor_pipeline(
-        kfp_client,
-        experiment_id,
-        test_params["PipelineDefinition"],
-        test_params["Arguments"],
-        download_dir,
-        test_params["TestName"],
-        test_params["Timeout"],
-    )
 
     try:
+        print(f"running test with model/endpoint name: {input_endpoint_name}")
+
+        _, _, workflow_json = kfp_client_utils.compile_run_monitor_pipeline(
+            kfp_client,
+            experiment_id,
+            test_params["PipelineDefinition"],
+            test_params["Arguments"],
+            download_dir,
+            test_params["TestName"],
+            test_params["Timeout"],
+        )
+
         outputs = {"sagemaker-deploy-model": ["endpoint_name"]}
 
         output_files = minio_utils.artifact_download_iterator(
@@ -110,5 +113,10 @@ def test_create_endpoint(
         )
         utils.remove_dir(download_dir)
     finally:
-        # delete endpoint
-        sagemaker_utils.delete_endpoint(sagemaker_client, input_endpoint_name)
+        endpoints = sagemaker_utils.list_endpoints(
+            sagemaker_client, name_contains=input_endpoint_name
+        )["Endpoints"]
+        endpoint_names = list(map((lambda x: x["EndpointName"]), endpoints))
+        # Check endpoint was successfully created
+        if input_endpoint_name in endpoint_names:
+            sagemaker_utils.delete_endpoint(sagemaker_client, input_endpoint_name)
