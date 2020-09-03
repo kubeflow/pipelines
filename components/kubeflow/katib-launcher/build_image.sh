bc311e34beaaaa633d91cfaf6ae66bead276f245
Yuan Gong <gongyuan94@gmail.com>
2020-09-03 17:34:55 +0800

    build: remove our own scripts to comply with pypi package licenses

--- components/kubeflow/katib-launcher/build_image.sh
+++ components/kubeflow/katib-launcher/build_image.sh
@@ -36,9 +36,6 @@ mkdir -p ./build
 rsync -arvp ./src/ ./build/
 rsync -arvp ../common/ ./build/
 
-cp ../../license.sh ./build
-cp ../../third_party_licenses.csv ./build
-
 LOCAL_LAUNCHER_IMAGE_NAME=ml-pipeline-kubeflow-experiment
 
 docker build -t ${LOCAL_LAUNCHER_IMAGE_NAME} .
