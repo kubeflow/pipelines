7e1c88b04559cef13244abb0664f4e0b010606f8
NikeNano <niklas.hansson@sandvik.com>
2020-09-10 20:00:23 +0200

    added config default

--- backend/src/apiserver/common/config.go
+++ backend/src/apiserver/common/config.go
@@ -30,7 +30,12 @@ const (
 	DefaultPipelineRunnerServiceAccount string = "DefaultPipelineRunnerServiceAccount"
 	KubeflowUserIDHeader                string = "KUBEFLOW_USERID_HEADER"
 	KubeflowUserIDPrefix                string = "KUBEFLOW_USERID_PREFIX"
+	UpdatePipelineVersionByDefault      string = "UpdatePipelineVersionByDefault"
 )
+
+func IsPipelineVersionUpdatedByDefault() string {
+	return GetStringConfigWithDefault(UpdatePipelineVersionByDefault, "true")
+}
 
 func GetStringConfig(configName string) string {
 	if !viper.IsSet(configName) {
