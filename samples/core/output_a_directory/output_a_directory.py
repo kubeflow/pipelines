a3dedb31762bbf94199836eb486861cdfacfa093
Alexey Volkov <alexey.volkov@ark-kun.com>
2020-09-21 01:43:07 -0700

    Added the license header

--- samples/core/output_a_directory/output_a_directory.py
+++ samples/core/output_a_directory/output_a_directory.py
@@ -1,3 +1,17 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This sample shows how components can output directories
 # Outputting a directory is performed the same way as outputting a file:
 # component receives an output path, writes data at that path and the system takes that data and makes it available for the downstream components.
