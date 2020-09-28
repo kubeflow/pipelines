825278e49ab483d712dfa18b82b85329171caabd
NikeNano <niklas.sven.hansson@gmail.com>
2020-09-28 14:37:22 +0200

    fixed issue

--- backend/src/crd/controller/scheduledworkflow/util/periodic_schedule.go
+++ backend/src/crd/controller/scheduledworkflow/util/periodic_schedule.go
@@ -17,7 +17,6 @@ package util
 import (
 	"fmt"
 	"math"
-	"time"
 
 	swfapi "github.com/kubeflow/pipelines/backend/src/crd/pkg/apis/scheduledworkflow/v1beta1"
 	log "github.com/sirupsen/logrus"
@@ -43,20 +42,12 @@ func NewPeriodicSchedule(periodicSchedule *swfapi.PeriodicSchedule) *PeriodicSch
 // scheduled.
 func (s *PeriodicSchedule) GetNextScheduledEpoch(lastJobEpoch *int64,
 	defaultStartEpoch int64) int64 {
-	fmt.Println("Default start")
-	fmt.Println(time.Unix(defaultStartEpoch, 0).UTC())
 	effectiveLastJobEpoch := defaultStartEpoch
 	if lastJobEpoch != nil {
 		effectiveLastJobEpoch = *lastJobEpoch
-		fmt.Println("lastJobEpoch")
-		fmt.Println(time.Unix(effectiveLastJobEpoch, 0))
 	} else if s.StartTime != nil {
-		fmt.Println("Start time?")
 		effectiveLastJobEpoch = s.StartTime.Unix()
 	}
-	fmt.Println("Effective last epoch")
-	fmt.Println(effectiveLastJobEpoch)
-	fmt.Println(time.Unix(effectiveLastJobEpoch, 0).UTC())
 	return s.getNextScheduledEpoch(effectiveLastJobEpoch)
 }
 
@@ -104,8 +95,5 @@ func (s *PeriodicSchedule) GetNextScheduledEpochNoCatchup(
 		}
 		return nowEpoch
 	}
-	fmt.Println("HERE HERE HERE")
-	fmt.Println("HERE WE GO!!!")
-	fmt.Println(nextScheduledEpoch)
 	return nextScheduledEpoch
 }
--- backend/src/crd/controller/scheduledworkflow/util/scheduled_workflow.go
+++ backend/src/crd/controller/scheduledworkflow/util/scheduled_workflow.go
@@ -240,20 +240,12 @@ func (s *ScheduledWorkflow) getNextScheduledEpoch(nowEpoch int64) (int64, error)
 	}
 	// Periodic schedule
 	if s.Spec.Trigger.PeriodicSchedule != nil {
-		fmt.Println("SCHEDULE")
 		schedule := NewPeriodicSchedule(s.Spec.Trigger.PeriodicSchedule)
 		if catchup {
-			fmt.Println("CATCH UP")
-			fmt.Println(commonutil.ToInt64Pointer(s.Status.Trigger.LastTriggeredTime))
-			fmt.Println(commonutil.ToInt64Pointer(s.Status.Trigger.LastTriggeredTime))
-			fmt.Println(time.Unix(36960, 0).UTC())
-			fmt.Println("CREATION")
-			fmt.Println(time.Unix(s.creationEpoch(), 0).UTC())
 			return schedule.GetNextScheduledEpoch(
 				commonutil.ToInt64Pointer(s.Status.Trigger.LastTriggeredTime),
 				s.creationEpoch()), nil
 		} else {
-			fmt.Println("IN HERE")
 			return schedule.GetNextScheduledEpochNoCatchup(
 				commonutil.ToInt64Pointer(s.Status.Trigger.LastTriggeredTime),
 				s.creationEpoch(), nowEpoch), nil
@@ -340,14 +332,9 @@ func (s *ScheduledWorkflow) UpdateStatus(updatedEpoch int64, workflow *commonuti
 	s.setLabel(commonutil.LabelKeyScheduledWorkflowStatus, string(conditionType))
 
 	if workflow != nil {
-		nextTriggerTime, _ := s.getNextScheduledEpoch(0)
 		s.updateLastTriggeredTime(scheduledEpoch)
 		s.Status.Trigger.LastIndex = commonutil.Int64Pointer(s.nextIndex())
-		// Next triggered time in label should not be related to now epoch.
-		fmt.Println("Inside")
-		fmt.Println(nextTriggerTime)
-		fmt.Println(time.Unix(nextTriggerTime, 0).UTC())
-		fmt.Println("HERE HERE HERE")
+		nextTriggerTime, _ := s.getNextScheduledEpoch(0)
 		s.updateNextTriggeredTime(nextTriggerTime)
 	} else {
 		// LastTriggeredTime is unchanged.
