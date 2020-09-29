d487ea59058e5d9492042af66d9525de4de8c126
Alexey Volkov <alexey.volkov@ark-kun.com>
2020-09-29 01:18:01 -0700

    SDK - Preserve parameter arguments and input names
    ContainerOp has no concept of inputs, so it looses any information about them such as input names and in some cases even the passed argument values (which are just injected into the command line).
    This commit fixes that issue by preserving the paramater arguments map and ultimately storing it in an Argo template annotation.
    
    Fixes https://github.com/kubeflow/pipelines/issues/4556

--- sdk/python/kfp/dsl/_container_op.py
+++ sdk/python/kfp/dsl/_container_op.py
@@ -1033,7 +1033,7 @@ class ContainerOp(BaseOp):
                 category=DeprecationWarning,
             )
 
-        self.attrs_with_pipelineparams = BaseOp.attrs_with_pipelineparams + ['_container', 'artifact_arguments'] #Copying the BaseOp class variable!
+        self.attrs_with_pipelineparams = BaseOp.attrs_with_pipelineparams + ['_container', 'artifact_arguments', '_parameter_arguments'] #Copying the BaseOp class variable!
 
         input_artifact_paths = {}
         artifact_arguments = {}
@@ -1113,6 +1113,7 @@ class ContainerOp(BaseOp):
             warnings.warn('The output_artifact_paths parameter is deprecated since SDK v0.1.32. Use the file_outputs parameter instead. file_outputs now supports outputting big data.', DeprecationWarning)
 
         self._metadata = None
+        self._parameter_arguments = None
 
         self.execution_options = ExecutionOptionsSpec(
             caching_strategy=CachingStrategySpec(),
