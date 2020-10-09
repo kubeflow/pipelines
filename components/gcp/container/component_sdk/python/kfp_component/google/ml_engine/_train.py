d115d467aef58fde17e4043f783b2290d426ea16
Chen Sun <chensun@users.noreply.github.com>
2020-10-09 14:45:39 -0700

    fix(components): fix ml_engine/train interface (#4593)
    * Fix missing `job_dir_output_path`
    
    * fix test

--- components/gcp/container/component_sdk/python/kfp_component/google/ml_engine/_train.py
+++ components/gcp/container/component_sdk/python/kfp_component/google/ml_engine/_train.py
@@ -15,45 +15,60 @@
 from fire import decorators
 from ._create_job import create_job
 
+
 @decorators.SetParseFns(python_version=str, runtime_version=str)
-def train(project_id, job_id_output_path, python_module=None, package_uris=None, 
-    region=None, args=None, job_dir=None, python_version=None, 
-    runtime_version=None, master_image_uri=None, worker_image_uri=None, 
-    training_input=None, job_id_prefix=None, job_id=None, wait_interval=30):
+def train(project_id,
+          job_id_output_path,
+          job_dir_output_path,
+          python_module=None,
+          package_uris=None,
+          region=None,
+          args=None,
+          job_dir=None,
+          python_version=None,
+          runtime_version=None,
+          master_image_uri=None,
+          worker_image_uri=None,
+          training_input=None,
+          job_id_prefix=None,
+          job_id=None,
+          wait_interval=30):
     """Creates a MLEngine training job.
 
     Args:
         project_id (str): Required. The ID of the parent project of the job.
-        python_module (str): Required. The Python module name to run after 
+        job_id_output_path (str): Required. Path for the ID of the created job.
+        job_dir_output_path (str): Required. Path for the directory of the job.
+        python_module (str): Required. The Python module name to run after
             installing the packages.
-        package_uris (list): Required. The Google Cloud Storage location of 
-            the packages with the training program and any additional 
+        package_uris (list): Required. The Google Cloud Storage location of
+            the packages with the training program and any additional
             dependencies. The maximum number of package URIs is 100.
-        region (str): Required. The Google Compute Engine region to run the 
+        region (str): Required. The Google Compute Engine region to run the
             training job in
         args (list): Command line arguments to pass to the program.
-        job_dir (str): A Google Cloud Storage path in which to store training 
-            outputs and other data needed for training. This path is passed 
-            to your TensorFlow program as the '--job-dir' command-line 
-            argument. The benefit of specifying this field is that Cloud ML 
-            validates the path for use in training.
-        python_version (str): Optional. The version of Python used in training. 
-            If not set, the default version is '2.7'. Python '3.5' is 
-            available when runtimeVersion is set to '1.4' and above. 
-            Python '2.7' works with all supported runtime versions.
-        runtime_version (str): Optional. The Cloud ML Engine runtime version 
-            to use for training. If not set, Cloud ML Engine uses the 
-            default stable version, 1.0. 
-        master_image_uri (str): The Docker image to run on the master replica. 
+        job_dir (str): A Google Cloud Storage path in which to store training
+            outputs and other data needed for training. This path is passed to
+            your TensorFlow program as the '--job-dir' command-line argument.
+            The benefit of specifying this field is that Cloud ML validates the
+            path for use in training.
+        python_version (str): Optional. The version of Python used in
+            training. If not set, the default version is '2.7'. Python '3.5' is
+            available when runtimeVersion is set to '1.4' and above. Python
+            '2.7' works with all supported runtime versions.
+        runtime_version (str): Optional. The Cloud ML Engine runtime version
+            to use for training. If not set, Cloud ML Engine uses the default
+            stable version, 1.0.
+        master_image_uri (str): The Docker image to run on the master replica.
             This image must be in Container Registry.
-        worker_image_uri (str): The Docker image to run on the worker replica. 
+        worker_image_uri (str): The Docker image to run on the worker replica.
             This image must be in Container Registry.
         training_input (dict): Input parameters to create a training job.
         job_id_prefix (str): the prefix of the generated job id.
         job_id (str): the created job_id, takes precedence over generated job
             id if set.
-        wait_interval (int): optional wait interval between calls
-            to get job status. Defaults to 30.
+        wait_interval (int): optional wait interval between calls to get job
+            status. Defaults to 30.
     """
     if not training_input:
         training_input = {}
@@ -79,9 +94,7 @@ def train(project_id, job_id_output_path, python_module=None, package_uris=None,
         if 'workerConfig' not in training_input:
             training_input['workerConfig'] = {}
         training_input['workerConfig']['imageUri'] = worker_image_uri
-    job = {
-        'trainingInput': training_input
-    }
+    job = {'trainingInput': training_input}
     return create_job(
         project_id=project_id,
         job=job,
@@ -89,4 +102,4 @@ def train(project_id, job_id_output_path, python_module=None, package_uris=None,
         job_id=job_id,
         wait_interval=wait_interval,
         job_id_output_path=job_id_output_path,
-    )
+        job_dir_output_path=job_dir_output_path)
