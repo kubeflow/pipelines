ee84255be814899339edca36bb4634199592d8e1
Alexey Volkov <alexey.volkov@ark-kun.com>
2020-10-11 15:57:43 -0700

    Dropped support for Python 3.5
    Fixes https://github.com/kubeflow/pipelines/issues/4584

--- backend/update_requirements.sh
+++ backend/update_requirements.sh
@@ -3,7 +3,7 @@
 # Usage: update_requirements.sh <requirements.in >requirements.txt
 
 set -euo pipefail
-IMAGE=${1:-"python:3.5"}
+IMAGE=${1:-"python:3.6"}
 docker run --interactive --rm "$IMAGE" sh -c '
   python3 -m pip install pip setuptools --upgrade --quiet
   python3 -m pip install pip-tools==5.0.0 --quiet
