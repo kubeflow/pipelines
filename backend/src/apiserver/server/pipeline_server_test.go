d74adefe772ef2cfd1382b8ab89c52997592ad55
NikeNano <niklas.sven.hansson@gmail.com>
2020-10-17 10:12:07 +0200

    Updated the test

--- backend/src/apiserver/server/pipeline_server_test.go
+++ backend/src/apiserver/server/pipeline_server_test.go
@@ -265,44 +265,47 @@ func TestListPipelineVersion_NoResourceKey(t *testing.T) {
 
 func TestCreatePipelineVersionDontUpdateDefault(t *testing.T) {
 	viper.Set(common.UpdatePipelineVersionByDefault, "false")
-	defer viper.Set(common.UpdatePipelineVersionByDefault, "false")
+	defer viper.Set(common.UpdatePipelineVersionByDefault, "true")
 	httpServer := getMockServer(t)
 	// Close the server when test finishes
 	defer httpServer.Close()
-
-	clientManager := resource.NewFakeClientManagerOrFatal(
-		util.NewFakeTimeForEpoch())
+	clientManager := resource.NewFakeClientManagerOrFatal(util.NewFakeTimeForEpoch())
 	resourceManager := resource.NewResourceManager(clientManager)
 
-	pipelineServer := PipelineServer{
-		resourceManager: resourceManager, httpClient: httpServer.Client(), options: &PipelineServerOptions{CollectMetrics: false}}
+	pipelineServer := PipelineServer{resourceManager: resourceManager, httpClient: httpServer.Client(), options: &PipelineServerOptions{CollectMetrics: false}}
+	pipeline, err := pipelineServer.CreatePipeline(context.Background(), &api.CreatePipelineRequest{
+		Pipeline: &api.Pipeline{
+			Url:         &api.Url{PipelineUrl: httpServer.URL + "/arguments_tarball/arguments.tar.gz"},
+			Name:        "argument-parameters",
+			Description: "pipeline description",
+		}})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, pipeline)
+
+	clientManager.UpdateUUID(util.NewFakeUUIDGeneratorOrFatal("123e4567-e89b-12d3-a456-526655440001", nil))
+	resourceManager = resource.NewResourceManager(clientManager)
+
+	pipelineServer = PipelineServer{resourceManager: resourceManager, httpClient: httpServer.Client(), options: &PipelineServerOptions{CollectMetrics: false}}
 	pipelineVersion, err := pipelineServer.CreatePipelineVersion(
 		context.Background(), &api.CreatePipelineVersionRequest{
 			Version: &api.PipelineVersion{
 				PackageUrl: &api.Url{
 					PipelineUrl: httpServer.URL + "/arguments-parameters.yaml"},
-				Name: "argument-parameters",
+				Name: "argument-parameters-update",
 				ResourceReferences: []*api.ResourceReference{
 					&api.ResourceReference{
 						Key: &api.ResourceKey{
-							Id:   "pipeline",
 							Type: api.ResourceType_PIPELINE,
+							Id:   pipeline.Id,
 						},
 						Relationship: api.Relationship_OWNER,
 					}}}})
+	assert.Nil(t, err)
 
-	assert.Nil(t, err)
-	assert.NotNil(t, pipelineVersion)
-	assert.Equal(t, "argument-parameters", pipelineVersion.Name)
-	newPipelineVersion, err := resourceManager.GetPipelineVersion(
-		pipelineVersion.Id)
-	assert.Nil(t, err)
-	assert.NotNil(t, newPipelineVersion)
-	var params []api.Parameter
-	err = json.Unmarshal([]byte(newPipelineVersion.Parameters), &params)
-	assert.Nil(t, err)
-	assert.Equal(t, []api.Parameter{
-		{Name: "param1", Value: "hello"}, {Name: "param2"}}, params)
+	pipelines, err := pipelineServer.GetPipeline(context.Background(), &api.GetPipelineRequest{Id: pipeline.Id})
+	assert.NotNil(t, pipelineVersion.Id)
+	assert.NotEqual(t, pipelines.DefaultVersion.Id, pipelineVersion.Id)
 }
 
 func getMockServer(t *testing.T) *httptest.Server {
