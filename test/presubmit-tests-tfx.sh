060cf02ee7fa77ee61b29e8026fdbb2f961cc8e2
Jiaxiao Zheng <jxzheng@google.com>
2020-11-12 11:32:24 -0800

    test: Improve TFX related unittests (#4766)

--- test/presubmit-tests-tfx.sh
+++ test/presubmit-tests-tfx.sh
@@ -41,13 +41,26 @@ chmod +x bazel_installer.sh
 
 # Install TFX from head
 cd $source_root
-git clone --branch v0.23.0-rc0 --depth 1 https://github.com/tensorflow/tfx.git
+git clone --depth 1 https://github.com/tensorflow/tfx.git
 cd $source_root/tfx
-python3 -m pip install . --upgrade --use-feature=2020-resolver
+python3 -m pip install . --upgrade --use-feature=2020-resolver \
+  --extra-index-url https://pypi-nightly.tensorflow.org/simple
 
 # Three KFP-related unittests
 cd $source_root/tfx/tfx/orchestration/kubeflow
 python3 kubeflow_dag_runner_test.py
+python3 base_component_test.py
+cd $source_root/tfx/tfx/orchestration/kubeflow/v2
+python3 compiler_utils_test.py
+python3 kubeflow_v2_dag_runner_test.py
+python3 parameter_utils_test.py
+python3 pipeline_builder_test.py
+python3 step_builder_test.py
+cd $source_root/tfx/tfx/orchestration/kubeflow/v2/container
+python3 kubeflow_v2_entrypoint_utils_test.py
+python3 kubeflow_v2_run_executor_test.py
+cd $source_root/tfx/tfx/orchestration/kubeflow/v2/file_based_example_gen
+python3 driver_test.py
 cd $source_root/tfx/tfx/examples/chicago_taxi_pipeline
 python3 taxi_pipeline_kubeflow_gcp_test.py
 python3 taxi_pipeline_kubeflow_local_test.py
