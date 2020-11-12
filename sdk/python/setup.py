0f4d9c7aa1030061867f2f12cf0ef7b34f3bbba7
Chen Sun <chensun@users.noreply.github.com>
2020-11-11 16:32:28 -0800

    chore(sdk): pin kfp-pipeline-spec version (#4748)
    * pin kfp-pipeline-spec version
    
    * kfp 1.1.1

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -38,12 +38,12 @@ REQUIRES = [
     'Deprecated',
     'strip-hints',
     'docstring-parser>=0.7.3',
-    'kfp-pipeline-spec',
+    'kfp-pipeline-spec>=0.1.0, <0.2.0',
 ]
 
 TESTS_REQUIRE = [
     'mock',
-    'kfp-pipeline-spec',
+    'kfp-pipeline-spec>=0.1.0, <0.2.0',
 ]
 
 
