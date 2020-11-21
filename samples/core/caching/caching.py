60b9fe46de04ca0cdbeb427bbd9ed06902462a4f
Alexey Volkov <alexey.volkov@ark-kun.com>
2020-11-21 15:42:12 -0800

    Switched to 60-second work time to avoid rare but possible flakyness

--- samples/core/caching/caching.py
+++ samples/core/caching/caching.py
@@ -8,7 +8,7 @@ from kfp.components import create_component_from_func
 
 
 @create_component_from_func
-def do_work_op(seconds: int = 30) -> str:
+def do_work_op(seconds: int = 60) -> str:
     import datetime
     import time
     print(f"Working for {seconds} seconds.")
@@ -19,7 +19,7 @@ def do_work_op(seconds: int = 30) -> str:
     return datetime.datetime.now().isoformat()
 
 
-def caching_pipeline(seconds: int = 30):
+def caching_pipeline(seconds: int = 60):
     # All outputs of successfull executions are cached
     work_task = do_work_op(seconds)
 
@@ -27,12 +27,12 @@ def caching_pipeline(seconds: int = 30):
 # Test 1
 # Running the pipeline for the first time.
 # The pipeline performs work and the results are cached.
-# The pipeline run time should be ~30 seconds.
+# The pipeline run time should be ~60 seconds.
 print("Starting test 1")
 start_time = datetime.datetime.now()
 kfp.Client(host=kfp_endpoint).create_run_from_pipeline_func(
     caching_pipeline,
-    arguments=dict(seconds=30),
+    arguments=dict(seconds=60),
 ).wait_for_run_completion(timeout=999)
 elapsed_time = datetime.datetime.now() - start_time
 print(f"Total run time: {int(elapsed_time.total_seconds())} seconds")
@@ -41,17 +41,17 @@ print(f"Total run time: {int(elapsed_time.total_seconds())} seconds")
 # Test 2
 # Running the pipeline the second time.
 # The pipeline should reuse the cached results and complete faster.
-# The pipeline run time should be <30 seconds.
+# The pipeline run time should be <60 seconds.
 print("Starting test 2")
 start_time = datetime.datetime.now()
 kfp.Client(host=kfp_endpoint).create_run_from_pipeline_func(
     caching_pipeline,
-    arguments=dict(seconds=30),
+    arguments=dict(seconds=60),
 ).wait_for_run_completion(timeout=999)
 elapsed_time = datetime.datetime.now() - start_time
 print(f"Total run time: {int(elapsed_time.total_seconds())} seconds")
 
-if elapsed_time.total_seconds() > 30:
+if elapsed_time.total_seconds() > 60:
     raise RuntimeError("The cached execution was not re-used or pipeline run took to long to complete.")
 
 
@@ -62,7 +62,7 @@ if elapsed_time.total_seconds() > 30:
 # Cached results that are older than the specified time span, are not reused.
 # In this case, the pipeline should not reuse the cached result, since they will be stale.
 
-def caching_pipeline3(seconds: int = 30):
+def caching_pipeline3(seconds: int = 60):
     # All outputs of successfull executions are cached
     work_task = do_work_op(seconds)
     # TODO(Ark-kun): Fix handling non-zero periods in the backend
@@ -74,10 +74,10 @@ print("Starting test 3")
 start_time = datetime.datetime.now()
 kfp.Client(host=kfp_endpoint).create_run_from_pipeline_func(
     caching_pipeline3,
-    arguments=dict(seconds=30),
+    arguments=dict(seconds=60),
 ).wait_for_run_completion(timeout=999)
 elapsed_time = datetime.datetime.now() - start_time
 print(f"Total run time: {int(elapsed_time.total_seconds())} seconds")
 
-if elapsed_time.total_seconds() < 30:
+if elapsed_time.total_seconds() < 60:
     raise RuntimeError("The cached execution was apparently re-used, but that should not happen.")
