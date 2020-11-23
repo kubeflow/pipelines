3b07951a3ae4293de44bc7116213bd1028b49f65
numerology <jxzheng@google.com>
2020-11-23 11:00:51 -0800

    recover cli.py

--- sdk/python/kfp/cli/cli.py
+++ sdk/python/kfp/cli/cli.py
@@ -34,7 +34,12 @@ from .output import OutputFormat
               help='The formatting style for command output.')
 @click.pass_context
 def cli(ctx, endpoint, iap_client_id, namespace, other_client_id, other_client_secret, output):
-    """kfp is the command line interface to KFP service."""
+    """kfp is the command line interface to KFP service.
+
+    Feature stage:
+    [Alpha](https://github.com/kubeflow/pipelines/blob/07328e5094ac2981d3059314cc848fbb71437a76/docs/release/feature-stages.md#alpha)
+
+    """
     if ctx.invoked_subcommand == 'diagnose_me':
         # Do not create a client for diagnose_me
         return
