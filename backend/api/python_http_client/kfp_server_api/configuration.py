bd86072a8c87a3e0cb219a0faad56809eba8577b
hilcj <desmliu@umich.edu>
2020-11-26 04:20:10 +0000

    Revert "chore(release): bumped version to 1.1.1.beta.1"
    This reverts commit 5928a2659b2f6be9dc496af2fa71bd5d51fbb0dd.

--- backend/api/python_http_client/kfp_server_api/configuration.py
+++ backend/api/python_http_client/kfp_server_api/configuration.py
@@ -365,8 +365,8 @@ conf = kfp_server_api.Configuration(
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 1.1.1.beta.1\n"\
-               "SDK Package Version: 1.1.1.beta.1".\
+               "Version of the API: 1.1.0-alpha.1\n"\
+               "SDK Package Version: 1.1.0-alpha.1".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
