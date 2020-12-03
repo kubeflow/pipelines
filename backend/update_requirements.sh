a78d6541eb38ab50610b28ff9da84f367da67031
Rui Fang <fang.ru@husky.neu.edu>
2020-12-02 20:36:21 -0800

    Update requirement.txt and it's scripts

--- backend/update_requirements.sh
+++ backend/update_requirements.sh
@@ -6,6 +6,6 @@ set -euo pipefail
 IMAGE=${1:-"python:3.5"}
 docker run --interactive --rm "$IMAGE" sh -c '
   python3 -m pip install pip setuptools --upgrade --quiet
-  python3 -m pip install pip-tools==5.0.0 --quiet
+  python3 -m pip install pip-tools==5.4.0 --quiet
   pip-compile --verbose --output-file - -
 '
