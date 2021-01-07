8463992aaafb2decefcc5527477291f4c8ccbd83
Chen Sun <chensun@users.noreply.github.com>
2021-01-06 17:09:46 -0800

    fix(components): Update GCP component container to Python 3.7. Fixes #4959  (#4960)
    * Update GCP component container to Python 3.7, fix Dataflow client
    
    * fix test
    
    * add back the http patch

--- components/gcp/container/component_sdk/python/setup.py
+++ components/gcp/container/component_sdk/python/setup.py
@@ -36,8 +36,6 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
