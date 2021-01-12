280ac94b92039f19ca502d76ce70756eece4c728
NikeNano <niklas.sven.hansson@gmail.com>
2021-01-12 18:52:17 +0100

    the latest test

--- backend/api/generate_api.sh
+++ backend/api/generate_api.sh
@@ -25,7 +25,7 @@ cd ../..
 # Generate API
 docker run  --interactive --rm \
     --mount type=bind,source="$(pwd)",target=/app/pipelines \
-    builder /app/pipelines/backend/api/generator.sh
+    testy /app/pipelines/backend/api/generator.sh
 
 # Change owner to user for generate files, explenation of command: https://askubuntu.com/questions/829537/how-do-i-change-owner-to-current-user-on-folder-and-containing-folders-inside-my
 sudo find backend/api -user root -exec sudo chown $USER: {} +
