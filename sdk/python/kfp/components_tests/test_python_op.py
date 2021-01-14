d629397654624a291264299cedaf86112d139318
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-01-14 13:59:31 -0800

    feat(sdk): Components - Support annotations when creating components from python (#4996)
    The component specification has always supported component annotations, but there was no way to specify them for the components generated from python. This PR fixes that.

--- sdk/python/kfp/components_tests/test_python_op.py
+++ sdk/python/kfp/components_tests/test_python_op.py
@@ -933,6 +933,17 @@ class PythonOpTestCase(unittest.TestCase):
         with self.assertRaises(Exception):
             self.helper_test_component_using_local_call(task_factory2, arguments={}, expected_output_values={})
 
+    def test_component_annotations(self):
+        def some_func():
+            pass
+
+        annotations = {
+            'key1': 'value1',
+            'key2': 'value2',
+        }
+        task_factory = comp.create_component_from_func(some_func, annotations=annotations)
+        component_spec = task_factory.component_spec
+        self.assertEqual(component_spec.metadata.annotations, annotations)
 
     def test_end_to_end_python_component_pipeline(self):
         #Defining the Python function
