c4dd37745711563edf9681eca079df71c83c86ed
DavidSpek <vanderspek.david@gmail.com>
2021-01-20 17:06:11 +0100

    fix removing of node_modules folders

--- create_dependabot.sh
+++ create_dependabot.sh
@@ -4,13 +4,15 @@ rm .github/dependabot.yml
 cp .github/base_dependabot.yml.tmp .github/dependabot.yml
 
 for directory in $(dirname $(find . -type f -name "*ockerfile*" -not -path "./components/deprecated*") | sort -u | cut -c2-); do
-     yq eval -i ".updates += {\"package-ecosystem\":\"docker\",\"directory\":\"${directory}\",\"schedule\":{\"interval\":\"daily\"},\"open-pull-requests-limit\":10}" .github/dependabot.yml
+    if [[ ${directory} != *"node_modules"* ]]; then
+        yq eval -i ".updates += {\"package-ecosystem\":\"docker\",\"directory\":\"${directory}\",\"schedule\":{\"interval\":\"daily\"},\"open-pull-requests-limit\":10}" .github/dependabot.yml
+    fi
 done
 
 for directory in $(dirname $(find . -type f -name "package*.json" -not -path "./*node_modules*") | sort -u | cut -c2-); do
     yq eval -i ".updates += {\"package-ecosystem\":\"npm\",\"directory\":\"${directory}\",\"schedule\":{\"interval\":\"daily\"},\"open-pull-requests-limit\":10}" .github/dependabot.yml
 done
 
-for directory in $(dirname $(find . -type f -name "*requirements.txt") | sort -u | cut -c2-); do
+for directory in $(dirname $(find . -type f -name "*requirements.txt" -not -path "./*node_modules*") | sort -u | cut -c2-); do
      yq eval -i ".updates += {\"package-ecosystem\":\"pip\",\"directory\":\"${directory}\",\"schedule\":{\"interval\":\"daily\"},\"open-pull-requests-limit\":10}" .github/dependabot.yml
 done
