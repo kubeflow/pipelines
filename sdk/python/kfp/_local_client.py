9d2c15a70c0924724e1f644563a02a9e93d7783a
Lin Yiming <linyiming@ainnovation.com>
2021-01-26 11:10:01 +0800

    Add some brief docstring about DAG used in local client

--- sdk/python/kfp/_local_client.py
+++ sdk/python/kfp/_local_client.py
@@ -24,27 +24,56 @@ import kfp
 
 
 class _Dag:
-    def __init__(self, nodes):
-        self.graph = {node: [] for node in nodes}
-        self.reverse_graph = {node: [] for node in nodes}
+    """DAG stands for Direct Acyclic Graph.
 
-    def add_edge(self, edge_from, edge_to):
-        self.graph[edge_from].append(edge_to)
-        self.reverse_graph[edge_to].append(edge_from)
+    DAG here is used to decide the order to execute pipeline ops.
 
-    def get_follows(self, node):
-        return self.graph.get(node, [])
+    For more information on DAG, please refer to `wiki <https://en.wikipedia.org/wiki/Directed_acyclic_graph>`_.
 
-    def get_dependencies(self, node):
-        return self.reverse_graph.get(node, [])
+    """
 
-    def topological_sort(self):
+    def __init__(self, nodes: List[str]) -> None:
+        """
+
+        Args::
+          nodes: List of DAG nodes, each node is identified by an unique name.
+        """
+        self._graph = {node: [] for node in nodes}
+        self._reverse_graph = {node: [] for node in nodes}
+
+    def add_edge(self, edge_source: str, edge_target: str) -> None:
+        """Add an edge between DAG nodes.
+
+        Args::
+          edge_source: the source node of the edge
+          edge_target: the target node of the edge
+        """
+        self._graph[edge_source].append(edge_target)
+        self._reverse_graph[edge_target].append(edge_source)
+
+    def get_follows(self, source_node) -> List[str]:
+        """Get all target nodes start from the specified source node
+
+        Args::
+          node: the source node
+        """
+        return self._graph.get(source_node, [])
+
+    def get_dependencies(self, target_node) -> List[str]:
+        """Get all source nodes end with the specified target node
+
+        Args::
+          node: the target node
+        """
+        return self._reverse_graph.get(target_node, [])
+
+    def topological_sort(self) -> List[str]:
         """ List DAG nodes in topological order. """
 
-        in_degree = {node: 0 for node in self.graph.keys()}
+        in_degree = {node: 0 for node in self._graph.keys()}
 
-        for i in self.graph:
-            for j in self.graph[i]:
+        for i in self._graph:
+            for j in self._graph[i]:
                 in_degree[j] += 1
 
         queue = []
@@ -58,7 +87,7 @@ class _Dag:
             u = queue.pop(0)
             top_order.append(u)
 
-            for node in self.graph[u]:
+            for node in self._graph[u]:
                 in_degree[node] -= 1
 
                 if in_degree[node] == 0:
