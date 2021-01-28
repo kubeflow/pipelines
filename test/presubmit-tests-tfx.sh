d4b2100d01ab60548915b7cc0e971813a3c8659d
Chen Sun <chensun@users.noreply.github.com>
2021-01-28 12:05:22 -0800

    Update presubmit-tests-tfx.sh

--- test/presubmit-tests-tfx.sh
+++ test/presubmit-tests-tfx.sh
@@ -15,8 +15,8 @@
 
 source_root=$(pwd)
 
-# TODO(#4853) Skipping pip 20.3 due to a bad version resolution logic.
-python3 -m pip install --upgrade pip
+# TODO(#5051) Unpin pip version once we figure out how to make the new dependency resolver in pip 20.3+ work in our case.
+python3 -m pip install --upgrade pip==20.2.4
 python3 -m pip install -r "$source_root/sdk/python/requirements.txt"
 # Additional dependencies
 #pip3 install coverage==4.5.4 coveralls==1.9.2 six>=1.13.0
@@ -44,7 +44,7 @@ chmod +x bazel_installer.sh
 cd $source_root
 git clone --depth 1 https://github.com/tensorflow/tfx.git
 cd $source_root/tfx
-python3 -m pip install . --upgrade \
+python3 -m pip install . --upgrade --use-feature=2020-resolver \
   --extra-index-url https://pypi-nightly.tensorflow.org/simple
 
 # Three KFP-related unittests
