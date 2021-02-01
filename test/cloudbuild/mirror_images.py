28992bd27ee28341e002d0a157550cac31cd6eb8
Yuan Gong <gongyuan94@gmail.com>
2021-02-01 10:50:57 +0800

    wip

---
+++ test/cloudbuild/mirror_images.py
@@ -0,0 +1,64 @@
+# %%
+from typing import NamedTuple
+
+import kfp
+from kfp.components import func_to_container_op, InputPath, OutputPath
+
+# %%
+# Mirror Image
+
+
+def mirror_image(
+        image: str,
+        source_registry: str,
+        destination_registry: str,
+        tag: str = '',
+):
+    source_image = '{}/{}:{}'.format(source_registry, image, tag)
+    destination_image = '{}/{}:{}'.format(destination_registry, image, tag)
+    import subprocess
+    subprocess.run([
+        'gcloud', 'container', 'images', 'add-tag', source_image,
+        destination_image
+    ])
+
+
+mirror_image_component = kfp.components.create_component_from_func(
+    mirror_image_imp, base_image='google/cloud-sdk:alpine'
+)
+
+
+# Combining all pipelines together in a single pipeline
+def mirror_images_pipeline(
+        version: str = '1.3.0',
+        source_registry: str = 'gcr.io/ml-pipeline',
+        destination_registry: str = 'gcr.io/gongyuan-pipeline-test/dev'
+):
+    images = [
+        'persistenceagent', 'scheduledworkflow', 'frontend',
+        'viewer-crd-controller', 'visualization-server', 'inverse-proxy-agent',
+        'metadata-writer', 'cache-server', 'cache-deployer', 'metadata-envoy'
+    ]
+    with kfp.dsl.ParallelFor(images) as image:
+        mirror_image_task = mirror_image_component(
+            image=image,
+            source_registry=source_registry,
+            destination_registry=destination_registry,
+            tag=version,
+        )
+
+
+# %%
+if __name__ == '__main__':
+    # Submit the pipeline for execution:
+    kfp.Client(
+        host=
+        'https://1fee7064345e6fc9-dot-asia-east1.pipelines.googleusercontent.com'
+    ).create_run_from_pipeline_func(
+        mirror_images_pipeline, arguments={'version': '1.3.0'}
+    )
+
+    # Compiling the pipeline
+    # kfp.compiler.Compiler().compile(mirror_images_pipeline, __file__ + '.yaml')
+
+# %%
---
+++ test/cloudbuild/vulnz_check.py
@@ -0,0 +1,124 @@
+# %%
+from typing import NamedTuple, List
+
+import kfp
+from kfp.components import func_to_container_op, InputPath, OutputPath
+
+# %%
+# Vulnerability checking
+
+
+def fetch_image_digest(image: str) -> str:
+    """ Fetch digest of an image.
+
+    Args:
+        image (str): image url
+
+    Returns:
+        str: full image url with sha256 digest
+    """
+    import subprocess
+    import sys
+    digest = subprocess.check_output([
+        'gcloud', 'container', 'images', 'describe', image,
+        "--format=value(image_summary.fully_qualified_digest)"
+    ]).decode(sys.stdout.encoding)
+    return digest
+
+
+fetch_image_digest_component = kfp.components.create_component_from_func(
+    fetch_image_digest, base_image='google/cloud-sdk:alpine'
+)
+
+kritis_check = kfp.components.load_component_from_text(
+    '''
+name: Kritis Check
+inputs:
+- {name: Image, type: String}
+outputs:
+- {name: Vulnerability Report, type: String}
+metadata:
+  annotations:
+    author: Yuan Gong <gongyuan94@gmail.com>
+implementation:
+  container:
+    image: gcr.io/gongyuan-pipeline-test/kritis-signer
+    command:
+    - /bin/bash
+    - -exc
+    - |
+      set -o pipefail
+
+      export PATH=/bin # this image does not have PATH defined
+      mkdir -p "$(dirname "$1")";
+      mkdir -p /workspace
+      cd /workspace
+      cat >policy.yaml <<EOF
+      apiVersion: kritis.grafeas.io/v1beta1
+      kind: VulnzSigningPolicy
+      metadata:
+        name: kfp-vsp
+      spec:
+        imageVulnerabilityRequirements:
+          maximumFixableSeverity: MEDIUM
+          maximumUnfixableSeverity: HIGH
+          allowlistCVEs: []
+      EOF
+      cat policy.yaml
+
+      /kritis/signer \
+        -v=10 \
+        -logtostderr \
+        -image="$0" \
+        -policy="policy.yaml" \
+        -mode=check-only \
+        |& tee "$1"
+
+    - inputValue: Image
+    - outputPath: Vulnerability Report
+'''
+)
+
+
+@func_to_container_op
+def kfp_images(version: str, registry_url: str) -> str:
+    images = [
+        'persistenceagent', 'scheduledworkflow', 'frontend',
+        'viewer-crd-controller', 'visualization-server', 'inverse-proxy-agent',
+        'metadata-writer', 'cache-server', 'cache-deployer', 'metadata-envoy'
+    ]
+    import json
+    return json.dumps([
+        '{}/{}:{}'.format(registry_url, image, version) for image in images
+    ])
+
+
+# Combining all pipelines together in a single pipeline
+def vulnz_check_pipeline(
+        version: str = '1.3.0',
+        registry_url: str = 'gcr.io/gongyuan-pipeline-test/dev'
+):
+    kfp_images_task = kfp_images(registry_url=registry_url, version=version)
+    with kfp.dsl.ParallelFor(kfp_images_task.output) as image:
+        fetch_image_digest_task = fetch_image_digest_component(image=image)
+        vulnz_check_task = kritis_check(image=fetch_image_digest_task.output)
+
+
+# %%
+if __name__ == '__main__':
+    # Submit the pipeline for execution:
+    client = kfp.Client(
+        host=
+        'https://1fee7064345e6fc9-dot-asia-east1.pipelines.googleusercontent.com'
+    )
+    run = client.create_run_from_pipeline_func(
+        vulnz_check_pipeline, arguments={'version': '1.3.0'}
+    )
+    timeout_in_seconds = 5 * 60
+    run_result = client.wait_for_run_completion(run.run_id, timeout_in_seconds)
+    print(run_result.run.status)  # Failed or ...
+
+    # Compiling the pipeline
+    # kfp.compiler.Compiler().compile(vulnz_check_pipeline, __file__ + '.yaml')
+
+# %%
