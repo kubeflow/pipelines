32defa9e5fe422521f0c493efae89649e61361f5
Yuan Gong <gongyuan94@gmail.com>
2021-02-08 17:06:25 +0800

    chore(backend): tidy go.mod and update tools.go

--- tools/tools.go
+++ tools/tools.go
@@ -1,5 +1,11 @@
+// +build tools
+
 package tools
 
+// This file tracks tools used in the project, but not directly imported by code.
+// https://github.com/go-modules-by-example/index/blob/master/010_tools/README.md
+
 import (
+	_ "github.com/google/addlicense"
 	_ "k8s.io/code-generator"
 )
