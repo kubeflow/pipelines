e3b33ee1ceb28604ac77421dadc51ebdd1c90d66
Yuan Gong <gongyuan94@gmail.com>
2021-02-09 10:45:26 +0000

    no longer use protobuffers repo

--- backend/api/generator.sh
+++ backend/api/generator.sh
@@ -39,18 +39,17 @@ ${PROTOCCOMPILER} -I. -Ibackend/api \
     -I/go/src/github.com/grpc-ecosystem/grpc-gateway/ \
     -I/go/src/github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger/options/ \
     -I/usr/include/ \
-     --plugin=protoc-gen-grpc-gateway=/go/bin/protoc-gen-grpc-gateway \
-     --grpc-gateway_out=logtostderr=true:${TMP_OUTPUT} \
-     backend/api/*.proto
+    --plugin=protoc-gen-grpc-gateway=/go/bin/protoc-gen-grpc-gateway \
+    --grpc-gateway_out=logtostderr=true:${TMP_OUTPUT} \
+    backend/api/*.proto
 # Move *.pb.go and *.gw.go to go_client folder.
 cp ${TMP_OUTPUT}/github.com/kubeflow/pipelines/backend/api/go_client/* ./backend/api/go_client
 # Generate *.swagger.json from *.proto into swagger folder.
 ${PROTOCCOMPILER} -I. -Ibackend/api \
-    -I/go/src/github.com/protocolbuffers/protobuf/src \
     -I/go/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
     -I/go/src/github.com/grpc-ecosystem/grpc-gateway/ \
     -I/go/src/github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger/options/ \
-    -I/go/src/github.com/protocolbuffers/protobuf/src/google/protobuf \
+    -I/usr/include/ \
     --plugin=protoc-gen-swagger=/go/bin/protoc-gen-swagger \
     --swagger_out=logtostderr=true:${TMP_OUTPUT} \
     backend/api/*.proto
