bc806b191481e43e2a5ac006b048308f10ad0bc4
Stefano Fioravanzo <stefano@arrikto.com>
2021-02-12 16:04:20 +0100

    Add AllJobsList unit tests
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

---
+++ frontend/src/pages/AllJobsList.test.tsx
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2021 Arrikto Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as React from 'react';
+import { AllJobsList } from './AllJobsList';
+import { PageProps } from './Page';
+import { RoutePage } from '../components/Router';
+import { shallow, ShallowWrapper } from 'enzyme';
+import { ButtonKeys } from '../lib/Buttons';
+import TestUtils from '../TestUtils';
+
+describe('AllJobsList', () => {
+  const updateBannerSpy = jest.fn();
+  let _toolbarProps: any = {};
+  const updateToolbarSpy = jest.fn(toolbarProps => (_toolbarProps = toolbarProps));
+  const historyPushSpy = jest.fn();
+  const props: PageProps = {
+    history: { push: historyPushSpy } as any,
+    location: '' as any,
+    match: '' as any,
+    toolbarProps: _toolbarProps,
+    updateBanner: updateBannerSpy,
+    updateDialog: jest.fn(),
+    updateSnackbar: jest.fn(),
+    updateToolbar: updateToolbarSpy,
+  };
+  let tree: ShallowWrapper;
+
+  function shallowMountComponent(
+    propsPatch: Partial<PageProps & { namespace?: string }> = {},
+  ): void {
+    tree = shallow(<AllJobsList {...props} {...propsPatch} />);
+    // Necessary since the component calls updateToolbar with the toolbar props,
+    // then expects to get them back in props
+    tree.setProps({ toolbarProps: _toolbarProps });
+    updateToolbarSpy.mockClear();
+  }
+
+  beforeEach(() => {
+    updateBannerSpy.mockClear();
+    updateToolbarSpy.mockClear();
+    historyPushSpy.mockClear();
+  });
+
+  afterEach(() => tree.unmount());
+
+  it('renders all jobs', () => {
+    shallowMountComponent();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('lists all jobs in namespace', () => {
+    shallowMountComponent({ namespace: 'test-ns' });
+    expect(tree.find('JobList').prop('namespaceMask')).toEqual('test-ns');
+  });
+
+  it('removes error banner on unmount', () => {
+    shallowMountComponent();
+    tree.unmount();
+    expect(updateBannerSpy).toHaveBeenCalledWith({});
+  });
+
+  // it('refreshes the job list when refresh button is clicked', async () => {
+  //   shallowMountComponent();
+  //   const spy = jest.spyOn(tree.find('RunList'), "refresh")
+  //   (tree.instance() as AllJobsList).setState((prevState, _) => ({
+  //     jobListRefreshCount: prevState.jobListRefreshCount + 1,
+  //   }));
+  //   expect(spy).toHaveBeenLastCalledWith();
+  // });
+
+  it('navigates to new run page when new run is clicked', () => {
+    shallowMountComponent();
+
+    _toolbarProps.actions[ButtonKeys.NEW_RUN].action();
+    expect(historyPushSpy).toHaveBeenLastCalledWith(RoutePage.NEW_RUN + "?experimentId=");
+  });
+});
