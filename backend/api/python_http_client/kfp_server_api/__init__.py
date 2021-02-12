2b95c002b741fadb6ddd2aa364c6a5b55ea78edd
Rui Fang <fang.ru@husky.neu.edu>
2021-02-12 09:31:46 +0000

    chore(release): bumped version to 1.4.0

--- backend/api/python_http_client/kfp_server_api/__init__.py
+++ backend/api/python_http_client/kfp_server_api/__init__.py
@@ -1,17 +1,3 @@
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # coding: utf-8
 
 # flake8: noqa
@@ -28,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "1.4.0-rc.1"
+__version__ = "1.4.0"
 
 # import apis into sdk package
 from kfp_server_api.api.experiment_service_api import ExperimentServiceApi
--- backend/api/python_http_client/setup.py
+++ backend/api/python_http_client/setup.py
@@ -1,17 +1,3 @@
-# Copyright 2021 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # coding: utf-8
 
 """
@@ -27,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "kfp-server-api"
-VERSION = "1.4.0-rc.1"
+VERSION = "1.4.0"
 # To install the library, run the following
 #
 # python setup.py install
