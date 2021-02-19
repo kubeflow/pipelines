95150137c3e3b39030cf477512d1d5dc09f70ecc
numerology <jxzheng@google.com>
2021-02-19 18:08:32 +0800

    fix tests

--- sdk/python/kfp/v2/google/aiplatform_test.py
+++ sdk/python/kfp/v2/google/aiplatform_test.py
@@ -50,7 +50,7 @@ _EXPECTED_TASK_SPEC = {
         'artifacts': {
             'examples': {
                 'taskOutputArtifact': {
-                    'producerTask': 'ingestor',
+                    'producerTask': 'task-ingestor',
                     'outputArtifactKey': 'output'}}}},
     'componentRef': {
         'name': 'comp-my-custom-job'}}
@@ -60,22 +60,25 @@ class AiplatformTest(unittest.TestCase):
   def testSingleNodeCustomContainer(self):
     self.maxDiff = None
     expected_custom_job_spec = {
-        'workerPoolSpecs': [
-            {
-                'machineSpec': {'machineType': 'n1-standard-4'},
-                'replicaCount': '1',
-                'containerSpec': {
-                    'imageUri': 'my_image:latest',
-                    'command': ['python', 'entrypoint.py'],
-                    'args': ['--input_path',
-                             "{{$.inputs.artifacts['examples'].uri}}",
-                             '--output_path',
-                             "{{$.outputs.artifacts['model'].uri}}",
-                             '--optimizer',
-                             "{{$.inputs.parameters['optimizer']}}",
-                             '--output_param_path',
-                             "{{$.outputs.parameters['out_param'].output_file}}"
-                             ]}}]}
+        'name': 'my-custom-job',
+        'jobSpec': {
+            'workerPoolSpecs': [
+                {
+                    'machineSpec': {'machineType': 'n1-standard-4'},
+                    'replicaCount': '1',
+                    'containerSpec': {
+                        'imageUri': 'my_image:latest',
+                        'command': ['python', 'entrypoint.py'],
+                        'args': ['--input_path',
+                                 "{{$.inputs.artifacts['examples'].uri}}",
+                                 '--output_path',
+                                 "{{$.outputs.artifacts['model'].uri}}",
+                                 '--optimizer',
+                                 "{{$.inputs.parameters['optimizer']}}",
+                                 '--output_param_path',
+                                 "{{$.outputs.parameters['out_param'].output_file}}"
+                                 ]}}]
+        }}
 
     task = aiplatform.custom_job(
         name='my-custom-job',
@@ -106,19 +109,23 @@ class AiplatformTest(unittest.TestCase):
 
   def testSingleNodeCustomPython(self):
     expected_custom_job_spec = {
-        'workerPoolSpecs': [{
-            'machineSpec': {'machineType': 'n1-standard-8'},
-            'replicaCount': '1',
-            'pythonPackageSpec': {
-                'executorImageUri': 'my_image:latest',
-                'packageUris': ['gs://my-bucket/my-training-prgram.tar.gz'],
-                'pythonModule': 'my_trainer',
-                'args': [
-                    '--input_path', "{{$.inputs.artifacts['examples'].path}}",
-                    '--output_path', "{{$.outputs.artifacts['model'].path}}",
-                    '--optimizer', "{{$.inputs.parameters['optimizer']}}",
-                    '--output_param_path',
-                    "{{$.outputs.parameters['out_param'].output_file}}"]}}]}
+        'name': 'my-custom-job',
+        'jobSpec': {
+            'workerPoolSpecs': [{
+                'machineSpec': {'machineType': 'n1-standard-8'},
+                'replicaCount': '1',
+                'pythonPackageSpec': {
+                    'executorImageUri': 'my_image:latest',
+                    'packageUris': ['gs://my-bucket/my-training-prgram.tar.gz'],
+                    'pythonModule': 'my_trainer',
+                    'args': [
+                        '--input_path',
+                        "{{$.inputs.artifacts['examples'].path}}",
+                        '--output_path',
+                        "{{$.outputs.artifacts['model'].path}}",
+                        '--optimizer', "{{$.inputs.parameters['optimizer']}}",
+                        '--output_param_path',
+                        "{{$.outputs.parameters['out_param'].output_file}}"]}}]}}
 
     task = aiplatform.custom_job(
         name='my-custom-job',
@@ -152,55 +159,57 @@ class AiplatformTest(unittest.TestCase):
 
   def testAdvancedTrainingSpec(self):
     expected_custom_job_spec = {
-        "workerPoolSpecs":[
-            {
-                "replicaCount":1,
-                "containerSpec":{
-                    "imageUri":"my-master-image:latest",
-                    "command":[
-                        "python3",
-                        "master_entrypoint.py"
-                    ],
-                    "args":[
-                        "--input_path",
-                        "{{$.inputs.artifacts['examples'].path}}",
-                        "--output_path",
-                        "{{$.outputs.artifacts['model'].path}}",
-                        "--optimizer",
-                        "{{$.inputs.parameters['optimizer']}}",
-                        "--output_param_path",
-                        "{{$.outputs.parameters['out_param'].output_file}}"
-                    ]
+        'name': 'my-custom-job',
+        'jobSpec': {
+            "workerPoolSpecs": [
+                {
+                    "replicaCount": 1,
+                    "containerSpec": {
+                        "imageUri": "my-master-image:latest",
+                        "command": [
+                            "python3",
+                            "master_entrypoint.py"
+                        ],
+                        "args": [
+                            "--input_path",
+                            "{{$.inputs.artifacts['examples'].path}}",
+                            "--output_path",
+                            "{{$.outputs.artifacts['model'].path}}",
+                            "--optimizer",
+                            "{{$.inputs.parameters['optimizer']}}",
+                            "--output_param_path",
+                            "{{$.outputs.parameters['out_param'].output_file}}"
+                        ]
+                    },
+                    "machineSpec": {
+                        "machineType": "n1-standard-4"
+                    }
                 },
-                "machineSpec":{
-                    "machineType":"n1-standard-4"
+                {
+                    "replicaCount": 4,
+                    "containerSpec": {
+                        "imageUri": "gcr.io/my-project/my-worker-image:latest",
+                        "command": [
+                            "python3",
+                            "worker_entrypoint.py"
+                        ],
+                        "args": [
+                            "--input_path",
+                            "{{$.inputs.artifacts['examples'].path}}",
+                            "--output_path",
+                            "{{$.outputs.artifacts['model'].path}}",
+                            "--optimizer",
+                            "{{$.inputs.parameters['optimizer']}}"
+                        ]
+                    },
+                    "machineSpec": {
+                        "machineType": "n1-standard-4",
+                        "acceleratorType": "NVIDIA_TESLA_K80",
+                        "acceleratorCount": 1
+                    }
                 }
-            },
-            {
-                "replicaCount":4,
-                "containerSpec":{
-                    "imageUri":"gcr.io/my-project/my-worker-image:latest",
-                    "command":[
-                        "python3",
-                        "worker_entrypoint.py"
-                    ],
-                    "args":[
-                        "--input_path",
-                        "{{$.inputs.artifacts['examples'].path}}",
-                        "--output_path",
-                        "{{$.outputs.artifacts['model'].path}}",
-                        "--optimizer",
-                        "{{$.inputs.parameters['optimizer']}}"
-                    ]
-                },
-                "machineSpec":{
-                    "machineType":"n1-standard-4",
-                    "acceleratorType":"NVIDIA_TESLA_K80",
-                    "acceleratorCount":1
-                }
-            }
-        ]
-    }
+            ]
+        }}
     task = aiplatform.custom_job(
         name='my-custom-job',
         input_artifacts={
@@ -261,51 +270,53 @@ class AiplatformTest(unittest.TestCase):
 
   def testScaffoldProgramToSpecs(self):
     expected_custom_job_spec = {
-        "workerPoolSpecs":[
-            {
-                "replicaCount":1,
-                "machineSpec":{
-                    "machineType":"n1-standard-4"
+        'name': 'my-custom-job',
+        'jobSpec': {
+            "workerPoolSpecs": [
+                {
+                    "replicaCount": 1,
+                    "machineSpec": {
+                        "machineType": "n1-standard-4"
+                    },
+                    "containerSpec": {
+                        "imageUri": "my_image:latest",
+                        "command": [
+                            "python",
+                            "entrypoint.py"
+                        ],
+                        "args": [
+                            "--input_path",
+                            "{{$.inputs.artifacts['examples'].uri}}",
+                            "--output_path",
+                            "{{$.outputs.artifacts['model'].uri}}",
+                            "--optimizer",
+                            "{{$.inputs.parameters['optimizer']}}",
+                            "--output_param_path",
+                            "{{$.outputs.parameters['out_param'].output_file}}"
+                        ]
+                    }
                 },
-                "containerSpec":{
-                    "imageUri":"my_image:latest",
-                    "command":[
-                        "python",
-                        "entrypoint.py"
-                    ],
-                    "args":[
-                        "--input_path",
-                        "{{$.inputs.artifacts['examples'].uri}}",
-                        "--output_path",
-                        "{{$.outputs.artifacts['model'].uri}}",
-                        "--optimizer",
-                        "{{$.inputs.parameters['optimizer']}}",
-                        "--output_param_path",
-                        "{{$.outputs.parameters['out_param'].output_file}}"
-                    ]
+                {
+                    "replicaCount": 4,
+                    "containerSpec": {
+                        "imageUri": "gcr.io/my-project/my-worker-image:latest",
+                        "command": [
+                            "python3",
+                            "override_entrypoint.py"
+                        ],
+                        "args": [
+                            "--arg1",
+                            "param1"
+                        ]
+                    },
+                    "machineSpec": {
+                        "machineType": "n1-standard-8",
+                        "acceleratorType": "NVIDIA_TESLA_K80",
+                        "acceleratorCount": 1
+                    }
                 }
-            },
-            {
-                "replicaCount":4,
-                "containerSpec":{
-                    "imageUri":"gcr.io/my-project/my-worker-image:latest",
-                    "command":[
-                        "python3",
-                        "override_entrypoint.py"
-                    ],
-                    "args":[
-                        "--arg1",
-                        "param1"
-                    ]
-                },
-                "machineSpec":{
-                    "machineType":"n1-standard-8",
-                    "acceleratorType":"NVIDIA_TESLA_K80",
-                    "acceleratorCount":1
-                }
-            }
-        ]
-    }
+            ]
+        }}
     task = aiplatform.custom_job(
         name='my-custom-job',
         input_artifacts={
