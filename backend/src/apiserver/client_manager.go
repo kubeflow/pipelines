dfe0b8f33e2548fa4f494aba7bce8ae6cddc5cea
uhhc <springshinehoo@gmail.com>
2021-02-20 19:12:16 +0800

    fix(backend): fixes sql error when deploy KFP in ANSI_QUOTES sql mode. Fixes #5121

--- backend/src/apiserver/client_manager.go
+++ backend/src/apiserver/client_manager.go
@@ -268,7 +268,7 @@ func initDBClient(initConnectionTimeout time.Duration) *storage.DB {
 	}
 
 	// If the old unique index idx_pipeline_version_uuid_name on pipeline_versions exists, remove it.
-	rows, err := db.Raw(`show index from pipeline_versions where Key_name="idx_pipeline_version_uuid_name"`).Rows()
+	rows, err := db.Raw(`show index from pipeline_versions where Key_name='idx_pipeline_version_uuid_name'`).Rows()
 	if err != nil {
 		glog.Fatalf("Failed to query pipeline_version table's indices. Error: %s", err)
 	}
