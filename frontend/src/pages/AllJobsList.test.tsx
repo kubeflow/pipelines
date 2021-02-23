da09a9596c8cc5d9938462e2ba727deeb55727bd
Stefano Fioravanzo <stefano@arrikto.com>
2021-02-23 09:18:59 +0100

    WIP Test refresh
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

--- frontend/src/pages/AllJobsList.test.tsx
+++ frontend/src/pages/AllJobsList.test.tsx
@@ -21,28 +21,36 @@ import { RoutePage } from '../components/Router';
 import { shallow, ShallowWrapper } from 'enzyme';
 import { ButtonKeys } from '../lib/Buttons';
 import TestUtils from '../TestUtils';
+import { fireEvent, render } from '@testing-library/react';
 
 describe('AllJobsList', () => {
   const updateBannerSpy = jest.fn();
-  let _toolbarProps: any = {};
+  let _toolbarProps: any = { actions: {}, breadcrumbs: [], pageTitle: '' };
   const updateToolbarSpy = jest.fn(toolbarProps => (_toolbarProps = toolbarProps));
   const historyPushSpy = jest.fn();
-  const props: PageProps = {
-    history: { push: historyPushSpy } as any,
-    location: '' as any,
-    match: '' as any,
-    toolbarProps: _toolbarProps,
-    updateBanner: updateBannerSpy,
-    updateDialog: jest.fn(),
-    updateSnackbar: jest.fn(),
-    updateToolbar: updateToolbarSpy,
-  };
+
   let tree: ShallowWrapper;
+
+  function generateProps(): PageProps {
+    const props: PageProps = {
+      history: { push: historyPushSpy } as any,
+      location: '' as any,
+      match: '' as any,
+      toolbarProps: _toolbarProps,
+      updateBanner: updateBannerSpy,
+      updateDialog: jest.fn(),
+      updateSnackbar: jest.fn(),
+      updateToolbar: updateToolbarSpy,
+    };
+    return Object.assign(props, {
+      toolbarProps: new AllJobsList(props).getInitialToolbarState(),
+    });
+  }
 
   function shallowMountComponent(
     propsPatch: Partial<PageProps & { namespace?: string }> = {},
   ): void {
-    tree = shallow(<AllJobsList {...props} {...propsPatch} />);
+    tree = shallow(<AllJobsList {...generateProps()} {...propsPatch} />);
     // Necessary since the component calls updateToolbar with the toolbar props,
     // then expects to get them back in props
     tree.setProps({ toolbarProps: _toolbarProps });
@@ -74,12 +82,9 @@ describe('AllJobsList', () => {
   });
 
   // it('refreshes the job list when refresh button is clicked', async () => {
-  //   shallowMountComponent();
-  //   const spy = jest.spyOn(tree.find('RunList'), "refresh")
-  //   (tree.instance() as AllJobsList).setState((prevState, _) => ({
-  //     jobListRefreshCount: prevState.jobListRefreshCount + 1,
-  //   }));
-  //   expect(spy).toHaveBeenLastCalledWith();
+  //   const tree = render(<AllJobsList {...generateProps()} />);
+  //   await TestUtils.flushPromises()
+  //   fireEvent.click(tree.getByText('Refresh'));
   // });
 
   it('navigates to new run page when new run is clicked', () => {
