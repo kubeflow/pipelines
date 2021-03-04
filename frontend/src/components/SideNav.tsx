4ff3ffb3b06e685b40a749340b150d808478c808
StefanoFioravanzo <stefano@arrikto.com>
2021-03-04 16:05:48 +0100

    feat(frontend): Add a new page to monitor existing Jobs (recurring runs) (#5131)
    * Introduce recurring runs page
    
    Introduce two new pages:
    
    - JobList: inspired by RunList, it lists all Jobs (Recurring Runs)
      with some useful info about each Job (status, trigger, related
      Experiment, creation date)
    - AllJobsList: inspired by AllRunsList, it renders JobList + adds
      the page title and contextual toolbar
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Remove ref usage
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Update Snapshots
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Add SideNav tests
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Add JobList unit tests
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Add AllJobsList unit tests
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Run prettier
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * WIP Test refresh
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Use inline snapshots
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Change timestamp label
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Change Job(s) to Recurring Run(s)
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Update tests
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Run prettier
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Fix import name
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
    
    * Add TODO above commented test
    
    Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

--- frontend/src/components/SideNav.tsx
+++ frontend/src/components/SideNav.tsx
@@ -38,6 +38,7 @@ import { Deployments, KFP_FLAGS } from '../lib/Flags';
 import { LocalStorage, LocalStorageKey } from '../lib/LocalStorage';
 import { logger } from '../lib/Utils';
 import { GkeMetadataContext, GkeMetadata } from 'src/lib/GkeMetadata';
+import { Alarm } from '@material-ui/icons';
 
 export const sideNavColors = {
   bg: '#f8fafb',
@@ -396,6 +397,39 @@ export class SideNav extends React.Component<SideNavInternalProps, SideNavState>
           <div
             className={classes(
               css.indicator,
+              !this._highlightRecurringRunsButton(page) && css.indicatorHidden,
+            )}
+          />
+          <Tooltip
+            title={'Recurring Runs List'}
+            enterDelay={300}
+            placement={'right-start'}
+            disableFocusListener={!collapsed}
+            disableHoverListener={!collapsed}
+            disableTouchListener={!collapsed}
+          >
+            <Link
+              id='recurringRunsBtn'
+              to={RoutePage.RECURRING_RUNS}
+              className={commonCss.unstyled}
+            >
+              <Button
+                className={classes(
+                  css.button,
+                  this._highlightRecurringRunsButton(page) && css.active,
+                  collapsed && css.collapsedButton,
+                )}
+              >
+                <Alarm />
+                <span className={classes(collapsed && css.collapsedLabel, css.label)}>
+                  Recurring Runs
+                </span>
+              </Button>
+            </Link>
+          </Tooltip>
+          <div
+            className={classes(
+              css.indicator,
               !this._highlightArtifactsButton(page) && css.indicatorHidden,
             )}
           />
@@ -563,10 +597,13 @@ export class SideNav extends React.Component<SideNavInternalProps, SideNavState>
   private _highlightRunsButton(page: string): boolean {
     return (
       page.startsWith(RoutePage.RUNS) ||
-      page.startsWith(RoutePrefix.RECURRING_RUN) ||
       page.startsWith(RoutePage.COMPARE) ||
       page === RoutePage.ARCHIVED_RUNS
     );
+  }
+
+  private _highlightRecurringRunsButton(page: string): boolean {
+    return page.startsWith(RoutePage.RECURRING_RUNS) || page.startsWith(RoutePrefix.RECURRING_RUN);
   }
 
   private _highlightArtifactsButton(page: string): boolean {
