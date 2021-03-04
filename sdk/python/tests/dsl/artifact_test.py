3e55bd36379bb7b4a3d7bb87d77cfcb4bc80bcde
dushyanthsc <43390008+dushyanthsc@users.noreply.github.com>
2021-03-03 16:14:48 -0800

    Artifact class update to support protobuf Struct (#5197)
    Change updates dsl Artifact class to support protobuff based metadata
    removing existing implementation that uses properties/custom_properties
    to handle metadata.
    
    This change modifies the ontology artifacts with enhanced schemas for
    model and datasets.

--- sdk/python/tests/dsl/artifact_test.py
+++ sdk/python/tests/dsl/artifact_test.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google LLC
+# Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,179 +11,107 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for kfp.dsl.artifact module."""
+"""Tests for kfp.dsl.artifact."""
+
+import os
 import unittest
-import textwrap
+import yaml
+import json
+import jsonschema
+
+from google.protobuf import json_format
+from google.protobuf.struct_pb2 import Struct
 
 from kfp.dsl import artifact
+from kfp.dsl import ontology_artifacts
 
+from google.protobuf import json_format
 
-class _MyArtifact(artifact.Artifact):
-  TYPE_NAME = 'MyTypeName'
-  PROPERTIES = {
-      'int1': artifact.Property(
-          type=artifact.PropertyType.INT,
-          description='An integer-typed property'),
-      'int2': artifact.Property(type=artifact.PropertyType.INT),
-      'float1': artifact.Property(
-          type=artifact.PropertyType.DOUBLE,
-          description='A float-typed property'),
-      'float2': artifact.Property(type=artifact.PropertyType.DOUBLE),
-      'string1': artifact.Property(
-          type=artifact.PropertyType.STRING,
-          description='A string-typed property'),
-      'string2': artifact.Property(type=artifact.PropertyType.STRING),
-  }
-
-
-_SERIALIZED_INSTANCE = """\
-{
-  "properties": {
-    "float1": {
-      "doubleValue": 1.11
-    },
-    "int1": {
-      "intValue": "1"
-    },
-    "string1": {
-      "stringValue": "111"
-    }
-  },
-  "type": {
-    "instanceSchema": "properties:\\n  float1:\\n    description: A float-typed property\\n    type: double\\n  float2:\\n    description:\\n    type: double\\n  int1:\\n    description: An integer-typed property\\n    type: int\\n  int2:\\n    description:\\n    type: int\\n  string1:\\n    description: A string-typed property\\n    type: string\\n  string2:\\n    description:\\n    type: string\\ntitle: kfp.MyTypeName\\ntype: object\\n"
-  }
-}"""
-
+_TEST_SIMPLE_INSTANCE_SCHEMA = """\
+title: TestArtifact
+type: object
+properties:
+  property1:
+    type: number
+    format: float
+    description: >
+      A simple number property.
+  property2:
+    type: string
+    description: >
+      A simple string property.
+"""
 
 class ArtifactTest(unittest.TestCase):
 
-  def testArtifact(self):
-    instance = _MyArtifact()
+  def test_setting_metadata(self):
+    test_artifact = artifact.Artifact(_TEST_SIMPLE_INSTANCE_SCHEMA)
+    test_artifact.property1 = 45.5
+    test_artifact.metadata['property2'] = 'test string'
+    test_artifact.metadata['custom_property'] = 4
+    test_artifact.metadata['custom_arr'] = [2, 4]
+    expected_json =  json.loads('{ "property1": 45.5, "property2": "test string", "custom_property": 4, "custom_arr": [2, 4]}')
+    self.assertEqual(expected_json, test_artifact.metadata)
 
-    # Test property getters.
-    self.assertEqual('', instance.uri)
-    self.assertEqual('', instance.name)
+  def test_property_type_checking(self):
+    test_artifact = artifact.Artifact(_TEST_SIMPLE_INSTANCE_SCHEMA)
+    with self.assertRaises(RuntimeError):
+      test_artifact.property1 = '45.5'
 
-    # Default property does not have span or split_names.
-    with self.assertRaisesRegex(AttributeError, "has no property 'span'"):
-      _ = instance.span
-    with self.assertRaisesRegex(AttributeError,
-                                 "has no property 'split_names'"):
-      _ = instance.split_names
+    test_artifact_2 = artifact.Artifact(_TEST_SIMPLE_INSTANCE_SCHEMA)
+    test_artifact_2.metadata['property1'] = '45.4'
+    with self.assertRaises(RuntimeError):
+      test_artifact_2.serialize()
 
-    # Test property setters.
-    instance.uri = '/tmp/uri2'
-    self.assertEqual('/tmp/uri2', instance.uri)
+  def test_complete_artifact(self):
+    test_artifact = artifact.Artifact(_TEST_SIMPLE_INSTANCE_SCHEMA)
+    test_artifact.property1 = 45.5
+    test_artifact.metadata['property2'] = 'test string'
+    test_artifact.metadata['custom_property'] = 4
+    test_artifact.metadata['custom_arr'] = [2, 4]
+    test_artifact.name = 'test_artifact'
+    test_artifact.uri = 'gcs://some_bucket/some_object'
+    with open(os.path.join(os.path.dirname(__file__), 'test_data', 'expected_complete_artifact.json')) as json_file:
+      expected_artifact_json = json.load(json_file)
+      self.assertEqual(expected_artifact_json, json.loads(test_artifact.serialize()))
 
-    instance.name = '1'
-    self.assertEqual('1', instance.name)
 
-    # Testing artifact does not have span.
-    with self.assertRaisesRegex(AttributeError, "unknown property 'span'"):
-      instance.span = 20190101
-    # Testing artifact does not have span.
-    with self.assertRaisesRegex(AttributeError,
-                                 "unknown property 'split_names'"):
-      instance.split_names = ''
+  def test_model_artifact(self):
+    model = ontology_artifacts.Model()
+    model.framework = 'TFX'
+    model.metadata["framework_version"] = '1.12'
+    model.metadata['custom_field'] = {'custom_field_1': 1, 'custom_field_2': [1, 3, 4]}
+    with open(os.path.join(os.path.dirname(__file__), 'test_data', 'expected_model_artifact.json')) as json_file:
+      expected_model_json = json.load(json_file)
+      self.assertEqual(expected_model_json, json.loads(model.serialize()))
 
-    instance.set_int_custom_property('int_key', 20)
-    self.assertEqual(
-        20, instance.runtime_artifact.custom_properties['int_key'].int_value)
+  def test_dataset_artifact(self):
+    dataset = ontology_artifacts.Dataset()
+    dataset.metadata['custom_field'] = 2
+    dataset.payload_format = 'JSON'
+    dataset.metadata['container_format'] = 'TFRecord'
+    with open(os.path.join(os.path.dirname(__file__), 'test_data', 'expected_dataset_artifact.json')) as json_file:
+      expected_dataset_json = json.load(json_file)
+      self.assertEqual(expected_dataset_json, json.loads(dataset.serialize()))
 
-    instance.set_string_custom_property('string_key', 'string_value')
-    self.assertEqual(
-        'string_value',
-        instance.runtime_artifact.custom_properties['string_key'].string_value)
+  # Test to ensure Artifact class can serialize and deserialize.
+  def test_artifact_serialize_deserialize(self):
+    with open(os.path.join(os.path.dirname(__file__), 'test_data', 'expected_complete_artifact.json')) as json_file:
+      read_artifact = json_file.read()
 
-    self.assertEqual(textwrap.dedent("""\
-        Artifact(artifact: name: "1"
-        type {
-          instance_schema: "properties:\\n  float1:\\n    description: A float-typed property\\n    type: double\\n  float2:\\n    description:\\n    type: double\\n  int1:\\n    description: An integer-typed property\\n    type: int\\n  int2:\\n    description:\\n    type: int\\n  string1:\\n    description: A string-typed property\\n    type: string\\n  string2:\\n    description:\\n    type: string\\ntitle: kfp.MyTypeName\\ntype: object\\n"
-        }
-        uri: "/tmp/uri2"
-        custom_properties {
-          key: "int_key"
-          value {
-            int_value: 20
-          }
-        }
-        custom_properties {
-          key: "string_key"
-          value {
-            string_value: "string_value"
-          }
-        }
-        , type_schema: properties:
-          float1:
-            description: A float-typed property
-            type: double
-          float2:
-            description:
-            type: double
-          int1:
-            description: An integer-typed property
-            type: int
-          int2:
-            description:
-            type: int
-          string1:
-            description: A string-typed property
-            type: string
-          string2:
-            description:
-            type: string
-        title: kfp.MyTypeName
-        type: object
-        )"""), str(instance))
+    deserialized_artifact = artifact.Artifact.deserialize(read_artifact)
+    expected_artifact_json = json.loads(read_artifact)
+    self.assertEqual(expected_artifact_json, json.loads(deserialized_artifact.serialize()))
 
-  def testArtifactProperties(self):
-    my_artifact = _MyArtifact()
+  # Test to ontology Artifact class can serialize and deserialize.
+  def test_model_serialize_deserialize(self):
+    with open(os.path.join(os.path.dirname(__file__), 'test_data', 'expected_model_artifact.json')) as json_file:
+      read_artifact = json_file.read()
 
-    self.assertEqual(0, my_artifact.int1)
-    self.assertEqual(0, my_artifact.int2)
-    my_artifact.int1 = 111
-    my_artifact.int2 = 222
-    self.assertEqual('', my_artifact.string1)
-    self.assertEqual('', my_artifact.string2)
-    my_artifact.string1 = '111'
-    my_artifact.string2 = '222'
-    self.assertEqual(0.0, my_artifact.float1)
-    self.assertEqual(0.0, my_artifact.float2)
-    my_artifact.float1 = 1.11
-    my_artifact.float2 = 2.22
-    self.assertEqual(my_artifact.int1, 111)
-    self.assertEqual(my_artifact.int2, 222)
-    self.assertEqual(my_artifact.string1, '111')
-    self.assertEqual(my_artifact.string2, '222')
-    self.assertEqual(1.11, my_artifact.float1)
-    self.assertEqual(2.22, my_artifact.float2)
-    self.assertEqual(my_artifact.get_string_custom_property('invalid'), '')
-    self.assertEqual(my_artifact.get_int_custom_property('invalid'), 0)
-    self.assertNotIn('invalid', my_artifact._artifact.custom_properties)
+    deserialized_artifact = artifact.Artifact.deserialize(read_artifact)
+    expected_artifact_json = json.loads(read_artifact)
+    self.assertEqual(expected_artifact_json, json.loads(deserialized_artifact.serialize()))
 
-    with self.assertRaisesRegex(
-        AttributeError, "Cannot set unknown property 'invalid' on artifact"):
-      my_artifact.invalid = 1
 
-    with self.assertRaisesRegex(
-        AttributeError, "Cannot set unknown property 'invalid' on artifact"):
-      my_artifact.invalid = 'x'
-
-    with self.assertRaisesRegex(AttributeError,
-                                "\D+ artifact has no property 'invalid'"):
-      _ = my_artifact.invalid
-
-  def testSerialize(self):
-    instance = _MyArtifact()
-    instance.int1 = 1
-    instance.string1 = '111'
-    instance.float1 = 1.11
-
-    self.assertEqual(_SERIALIZED_INSTANCE, instance.serialize())
-
-  def testDeserialize(self):
-    instance = artifact.Artifact.deserialize(_SERIALIZED_INSTANCE)
-    self.assertEqual(1, instance.int1)
-    self.assertEqual('111', instance.string1)
-    self.assertEqual(1.11, instance.float1)
-    self.assertEqual('kfp.MyTypeName', instance.type_name)
+if __name__ == '__main__':
+  unittest.main()
