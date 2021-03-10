907b0513b4f15c6a36779042858fff0cfb761070
Subreptivus <Subreptivus@gmail.com>
2021-03-10 14:59:43 +0200

    Ability to specify extra paramaters for DB connection

--- backend/src/cache/main.go
+++ backend/src/cache/main.go
@@ -15,6 +15,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
@@ -51,6 +52,7 @@ type WhSvrDBParameters struct {
 	dbUser              string
 	dbPwd               string
 	dbGroupConcatMaxLen string
+	dbExtraParams       map[string]string
 	namespaceToWatch    string
 }
 
@@ -63,9 +65,14 @@ func main() {
 	flag.StringVar(&params.dbUser, "db_user", "root", "Database user name.")
 	flag.StringVar(&params.dbPwd, "db_password", "", "Database password.")
 	flag.StringVar(&params.dbGroupConcatMaxLen, "db_group_concat_max_len", mysqlDBGroupConcatMaxLenDefault, "Database group concat max length.")
+	dbExtraParams := flag.String("db_extra_params", "{}", "Database extra parameters in JSON form.")
 	flag.StringVar(&params.namespaceToWatch, "namespace_to_watch", "kubeflow", "Namespace to watch.")
-
 	flag.Parse()
+
+	err := json.Unmarshal([]byte(*dbExtraParams), &params.dbExtraParams)
+	if err != nil {
+		log.Fatalf("Failed to get db_extra_params. Error: ", err)
+	}
 
 	log.Println("Initing client manager....")
 	clientManager := NewClientManager(params)
