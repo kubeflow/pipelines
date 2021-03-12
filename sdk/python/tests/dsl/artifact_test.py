63117a6b1443fcb3f535707393940b1814a5d5d4
dushyanthsc <43390008+dushyanthsc@users.noreply.github.com>
2021-03-12 03:19:24 -0800

    feat(sdk): Adds Metrics & ClassificationMetrics ontology types (#5259)
    Adds Metrics, ClassificationMetrics SlicedClassificationMetrics to ontology types.
    Common methods used by helper metric classes and Artifact class are moved to
    _artifact_utils.py

--- sdk/python/tests/dsl/artifact_test.py
+++ sdk/python/tests/dsl/artifact_test.py
@@ -50,13 +50,15 @@ class ArtifactTest(unittest.TestCase):
     test_artifact.metadata['property2'] = 'test string'
     test_artifact.metadata['custom_property'] = 4
     test_artifact.metadata['custom_arr'] = [2, 4]
-    expected_json =  json.loads('{ "property1": 45.5, "property2": "test string", "custom_property": 4, "custom_arr": [2, 4]}')
+    expected_json =  json.loads(
+      '{ "property1": 45.5, "property2": "test string", "custom_property": 4, "custom_arr": [2, 4]}')
     self.assertEqual(expected_json, test_artifact.metadata)
 
   def test_property_type_checking(self):
     test_artifact = artifact.Artifact(_TEST_SIMPLE_INSTANCE_SCHEMA)
+    test_artifact.property1 = '45.5'
     with self.assertRaises(RuntimeError):
-      test_artifact.property1 = '45.5'
+      test_artifact.serialize()
 
     test_artifact_2 = artifact.Artifact(_TEST_SIMPLE_INSTANCE_SCHEMA)
     test_artifact_2.metadata['property1'] = '45.4'
@@ -71,7 +73,8 @@ class ArtifactTest(unittest.TestCase):
     test_artifact.metadata['custom_arr'] = [2, 4]
     test_artifact.name = 'test_artifact'
     test_artifact.uri = 'gcs://some_bucket/some_object'
-    with open(os.path.join(os.path.dirname(__file__), 'test_data', 'expected_complete_artifact.json')) as json_file:
+    with open(os.path.join(os.path.dirname(__file__),
+        'test_data', 'expected_complete_artifact.json')) as json_file:
       expected_artifact_json = json.load(json_file)
       self.assertEqual(expected_artifact_json, json.loads(test_artifact.serialize()))
 
@@ -80,8 +83,10 @@ class ArtifactTest(unittest.TestCase):
     model = ontology_artifacts.Model()
     model.framework = 'TFX'
     model.metadata["framework_version"] = '1.12'
-    model.metadata['custom_field'] = {'custom_field_1': 1, 'custom_field_2': [1, 3, 4]}
-    with open(os.path.join(os.path.dirname(__file__), 'test_data', 'expected_model_artifact.json')) as json_file:
+    model.metadata['custom_field'] = {
+      'custom_field_1': 1, 'custom_field_2': [1, 3, 4]}
+    with open(os.path.join(os.path.dirname(__file__),
+        'test_data', 'expected_model_artifact.json')) as json_file:
       expected_model_json = json.load(json_file)
       self.assertEqual(expected_model_json, json.loads(model.serialize()))
 
@@ -90,13 +95,15 @@ class ArtifactTest(unittest.TestCase):
     dataset.metadata['custom_field'] = 2
     dataset.payload_format = 'JSON'
     dataset.metadata['container_format'] = 'TFRecord'
-    with open(os.path.join(os.path.dirname(__file__), 'test_data', 'expected_dataset_artifact.json')) as json_file:
+    with open(os.path.join(os.path.dirname(__file__),
+        'test_data', 'expected_dataset_artifact.json')) as json_file:
       expected_dataset_json = json.load(json_file)
       self.assertEqual(expected_dataset_json, json.loads(dataset.serialize()))
 
   # Test to ensure Artifact class can serialize and deserialize.
   def test_artifact_serialize_deserialize(self):
-    with open(os.path.join(os.path.dirname(__file__), 'test_data', 'expected_complete_artifact.json')) as json_file:
+    with open(os.path.join(os.path.dirname(__file__),
+        'test_data', 'expected_complete_artifact.json')) as json_file:
       read_artifact = json_file.read()
 
     deserialized_artifact = artifact.Artifact.deserialize(read_artifact)
@@ -105,13 +112,64 @@ class ArtifactTest(unittest.TestCase):
 
   # Test to ontology Artifact class can serialize and deserialize.
   def test_model_serialize_deserialize(self):
-    with open(os.path.join(os.path.dirname(__file__), 'test_data', 'expected_model_artifact.json')) as json_file:
+    with open(os.path.join(os.path.dirname(__file__),
+        'test_data', 'expected_model_artifact.json')) as json_file:
       read_artifact = json_file.read()
 
     deserialized_artifact = artifact.Artifact.deserialize(read_artifact)
     expected_artifact_json = json.loads(read_artifact)
     self.assertEqual(expected_artifact_json, json.loads(deserialized_artifact.serialize()))
 
+  def test_metrics(self):
+    metrics = ontology_artifacts.Metrics()
+    metrics.accuracy = 98.4
+    metrics.mean_squared_error = 32.1
+    metrics.log_metric('custom_metric1', 24)
+    metrics.metadata['custom_metric2'] = 24
+    metrics.name = 'test_artifact'
+
+    with open(os.path.join(os.path.dirname(__file__),
+        'test_data', 'expected_metrics.json')) as json_file:
+      expected_json = json.load(json_file)
+      self.assertEqual(expected_json, json.loads(metrics.serialize()))
+
+  def test_classification_metrics(self):
+    cls_metrics = ontology_artifacts.ClassificationMetrics()
+    cls_metrics.name = "test_me"
+    cls_metrics.log_roc_reading(24.3, 24.5, 98.4)
+    cls_metrics.set_confusion_matrix_categories(['dog', 'cat', 'horses'])
+    cls_metrics.log_confusion_matrix_row('dog', [2, 6, 0])
+    cls_metrics.log_confusion_matrix_cell('cat', 'dog', 3)
+    cls_metrics.metadata['test'] = 1
+    with open(os.path.join(os.path.dirname(__file__),
+        'test_data', 'expected_classification_metrics.json')) as json_file:
+      expected_json = json.load(json_file)
+      self.assertEqual(expected_json, json.loads(cls_metrics.serialize()))
+
+  def test_bulk_load_classification_metrics(self):
+    cls_metrics = ontology_artifacts.ClassificationMetrics()
+    cls_metrics.load_roc_readings([
+      [53.6, 52.6, 85.1],
+      [53.6, 52.6, 85.1],
+      [53.6, 52.6, 85.1]])
+    cls_metrics.load_confusion_matrix(['dog', 'cat', 'horses'], [[2, 6, 0], [3, 5,6], [5,7,8]])
+    with open(os.path.join(os.path.dirname(__file__),
+        'test_data', 'expected_bulk_loaded_classification_metrics.json')) as json_file:
+      expected_json = json.load(json_file)
+      self.assertEqual(expected_json, json.loads(cls_metrics.serialize()))
+
+  def test_sliced_classification_metrics(self):
+    sliced_metrics = ontology_artifacts.SlicedClassificationMetrics()
+    sliced_metrics.name = "test_me"
+    sliced_metrics.log_roc_reading('dog', 24.3, 24.5, 98.4)
+    sliced_metrics.set_confusion_matrix_categories('overall', ['dog', 'cat', 'horses'])
+    sliced_metrics.log_confusion_matrix_row('overall', 'dog', [2, 6, 0])
+    sliced_metrics.log_confusion_matrix_cell('overall', 'cat', 'dog', 3)
+    sliced_metrics.metadata['test'] = 1
+    with open(os.path.join(os.path.dirname(__file__),
+        'test_data', 'expected_sliced_classification_metrics.json')) as json_file:
+      expected_json = json.load(json_file)
+      self.assertEqual(expected_json, json.loads(sliced_metrics.serialize()))
 
 if __name__ == '__main__':
   unittest.main()
