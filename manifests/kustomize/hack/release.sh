86ffb2983633d2735fc8a1fbc5671a6a1ea23a9d
Yuan Gong <gongyuan94@gmail.com>
2021-04-01 10:39:38 +0800

    chore: fix metadata release script

--- manifests/kustomize/hack/release.sh
+++ manifests/kustomize/hack/release.sh
@@ -29,7 +29,7 @@ echo "This release script uses yq, it can be downloaded at https://github.com/mi
 kustomization_yamls_with_images=(
   "base/cache-deployer/kustomization.yaml"
   "base/cache/kustomization.yaml"
-  "base/metadata/kustomization.yaml"
+  "base/metadata/base/kustomization.yaml"
   "base/pipeline/metadata-writer/kustomization.yaml"
   "base/pipeline/kustomization.yaml"
   "env/gcp/inverse-proxy/kustomization.yaml"
