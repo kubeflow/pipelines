87b3f4d6e5a9ac204b67b8f2757f0ab573dd97f5
sina chavoshi <sina.chavoshi@gmail.com>
2021-04-05 02:06:29 -0700

    components: Add package structure and base files for Google Cloud Components and Model builder SDK components  (#5391)
    * Add repo structure and base files
    
    * Correcting source path.
    
    * Remove old src path
    
    * Add KFP and MB SDK as direct dependancies
    
    * Update with pylint formatting.
    
    * Install sdk in docker instead of copying remote module.
    
    * Updated based on code review comments.
    
    * Move cloud_build to uCAIP_MB_SDK folder.
    
    * Add pip3 upgrade to docker file
    
    * update lint and license text per review comments
    
    * Update description and address minor issues raised in review.
    
    * correcting owner file format per google-oss guidlines
    
    * Addresss owner file parsing issue [attempt 2]
    
    * added TODO to track example, typing,  and validation for kwargs
    
    * lint/formatting updates
    
    * Add note to README on SDK is in  prototyping state
    
    * Change container path to uCAIP instead of MB_SDK
    
    * making the dependencies version-bound
    
    * change mb_sdk_component to aiplatfrom_component
    
    * minor code clean on remote_runner.py
    
    * use [len('gs://'):] instead of len[5:]
    
    * add descriptive name and docs string for component_yaml_generator
    
    * add chensun to owners / reviewers
    
    * remove internal bug ids
    
    * add a Todo for type annoation and doc strings
    
    * Use dsl.PipelineParam instead of _pipeline_param
    
    * add install dependancies to test by default
    
    * correcting path containers/aiplatfrom
    
    * move package source code under google-cloud/src
    
    * restructuring repo under aiplatfrom/sdk

---
+++ components/google-cloud/aiplatform/sdk/README.md
@@ -0,0 +1,3 @@
+# Google Cloud Components
+
+Note, this SDK is in early prototyping state and it is not ready for consumption. 
---
+++ components/google-cloud/aiplatform/sdk/google_cloud_components/aiplatform/utils.py
@@ -0,0 +1,96 @@
+# Copyright 2021 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module for creating pipeline components based on AI Platform SDK."""
+
+import inspect
+import kfp
+from kfp import components
+
+INIT_KEY = "init"
+METHOD_KEY = "method"
+
+
+def convert_method_to_component(method, should_serialize_init=False):
+  """Creates a corresponding KFP Component for a given MBD SDK method."""
+  method_name = method.__name__
+
+  if inspect.ismethod(method):
+    cls_name = method.__self__.__name__
+    init_signature = inspect.signature(method.__self__.__init__)
+  else:
+    cls = getattr(
+        inspect.getmodule(method),
+        method.__qualname__.split(".<locals>", 1)[0].rsplit(".", 1)[0], None)
+    cls_name = cls.__name__
+    init_signature = inspect.signature(cls.__init__)
+
+  init_arg_names = set(
+      init_signature.parameters.keys()) if should_serialize_init else set([])
+
+  def make_args(sa):
+    additional_args = []
+    for key, args in sa.items():
+      for arg_key, value in args.items():
+        additional_args.append(f"    - --{key}.{arg_key}={value}")
+    return "\n".join(additional_args)
+
+  def component_yaml_generator(**kwargs):
+    """Funciton to create the actual component yaml for the input kwargs.""" 
+    inputs = ["inputs:"]
+    input_args = []
+    input_kwargs = {}
+
+    serialized_args = {"init": {}, "method": {}}
+
+    for key, value in kwargs.items():
+      prefix_key = "init" if key in init_arg_names else "method"
+      if isinstance(value, kfp.dsl.PipelineParam):
+        name = key
+        inputs.append("- {name: %s, type: Artifact}" % (name))
+        input_args.append("""
+    - --%s
+    - {inputUri: %s}
+""" % (f"{prefix_key}.{key}", key))
+        input_kwargs[key] = value
+      else:
+        serialized_args[prefix_key][key] = value
+
+    inputs = "\n".join(inputs) if len(inputs) > 1 else ""
+    input_args = "\n".join(input_args) if input_args else ""
+    component_text = """
+name: %s-%s
+%s
+outputs:
+- {name: resource_name_output, type: Artifact}
+implementation:
+  container:
+    image: gcr.io/sashaproject-1/AIPlatform_component:latest
+    command:
+    - python3
+    - remote_runner.py
+    - --cls_name=%s
+    - --method_name=%s
+%s
+    args:
+    - --resource_name_output_uri
+    - {outputUri: resource_name_output}
+%s
+""" % (cls_name, method_name, inputs, cls_name, method_name,
+       make_args(serialized_args), input_args)
+
+    print(component_text)
+
+    return components.load_component_from_text(component_text)(**input_kwargs)
+
+  return component_yaml_generator
