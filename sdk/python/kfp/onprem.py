925663becef9efe3b3dc8635df37f53481c776a4
ConverJens <61828156+ConverJens@users.noreply.github.com>
2021-04-05 11:06:23 +0200

    feat(sdk/extensions): Add kfp.onprem.use_k8s_secret method that puts k8s secrets in env vars (#5365)
    * Add task in kfp.onprem to load k8s secrets as env vars
    
    * Fix nits

--- sdk/python/kfp/onprem.py
+++ sdk/python/kfp/onprem.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 
 def mount_pvc(pvc_name='pipeline-claim', volume_name='pipeline', volume_mount_path='/mnt/pipeline'):
     """Modifier function to apply to a Container Op to simplify volume, volume mount addition and
@@ -26,3 +28,48 @@ def mount_pvc(pvc_name='pipeline-claim', volume_name='pipeline', volume_mount_pa
                 )
         )
     return _mount_pvc
+
+
+def use_k8s_secret(secret_name: str = 'k8s-secret', k8s_secret_key_to_env: Dict = {}):
+    """An operator that configures the container to use k8s credentials.
+
+    k8s_secret_key_to_env specifies a mapping from the name of the keys in the k8s secret to the name of the
+    environment variables where the values will be added.
+
+    The secret needs to be deployed manually a priori.
+
+    Example:
+        ::
+
+            train = train_op(...)
+            train.apply(use_k8s_secret(secret_name='s3-secret',
+            k8s_secret_key_to_env={'secret_key': 'AWS_SECRET_ACCESS_KEY'}))
+
+        This will load the value in secret 's3-secret' at key 'secret_key' and source it as the environment variable
+        'AWS_SECRET_ACCESS_KEY'. I.e. it will produce the following section on the pod:
+        env:
+        - name: AWS_SECRET_ACCESS_KEY
+          valueFrom:
+            secretKeyRef:
+              name: s3-secret
+              key: secret_key
+    """
+
+    def _use_k8s_secret(task):
+        from kubernetes import client as k8s_client
+        for secret_key, env_var in k8s_secret_key_to_env.items():
+            task.container \
+                .add_env_variable(
+                k8s_client.V1EnvVar(
+                    name=env_var,
+                    value_from=k8s_client.V1EnvVarSource(
+                        secret_key_ref=k8s_client.V1SecretKeySelector(
+                            name=secret_name,
+                            key=secret_key
+                        )
+                    )
+                )
+            )
+        return task
+
+    return _use_k8s_secret
