ecd38b26e1e519570444d1b8b5501948db2ed753
sina chavoshi <sina.chavoshi@gmail.com>
2021-04-05 11:45:00 -0700

    components: flattening the components/google-cloud/ sdk structure  (#5428)
    * flattening the repo strucuture ( step 1 of 2 )
    
    * bringing package root to google-cloud/

--- components/google-cloud/google_cloud_components/__init__.py
+++ components/google-cloud/google_cloud_components/__init__.py
@@ -12,3 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Google Cloud Components root."""
+from google_cloud_components import aiplatform
