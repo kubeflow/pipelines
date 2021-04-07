2953964f16d4e9e2415905f1d12801ce4bc7e8c5
Sasha Sobran <asobran@google.com>
2021-04-07 18:39:41 -0400

    Enabled remainder of components. Added support for json serialized inputs/outputs, filtering signature and adding signature union, validating input signature, annotation inspection, mapping resource nouns to metadata types, components without output.

--- components/google-cloud/google_cloud_components/aiplatform/__init__.py
+++ components/google-cloud/google_cloud_components/aiplatform/__init__.py
@@ -13,18 +13,74 @@
 # limitations under the License.
 """Core modules for AI Platform Components."""
 
-from google.cloud import aiplatform as aip
+from google.cloud import aiplatform
 from google_cloud_components.aiplatform import utils
 
-DatasetCreateOp = utils.convert_method_to_component(aip.Dataset.create)
+DatasetCreateOp = utils.convert_method_to_component(
+	aiplatform.Dataset.create)
 
 DatasetExportDataOp = utils.convert_method_to_component(
-    aip.Dataset.export_data, should_serialize_init=True)
+	aiplatform.Dataset.export_data,
+	should_serialize_init=True)
+
+ImageDatasetCreateOp = utils.convert_method_to_component(
+	aiplatform.ImageDataset.create)
+
+TabularDatasetCreateOp = utils.convert_method_to_component(
+	aiplatform.TabularDataset.create)
+
+TextDatasetCreateOp = utils.convert_method_to_component(
+	aiplatform.TextDataset.create)
+
+VideoDatasetCreateOp = utils.convert_method_to_component(
+	aiplatform.VideoDataset.create)
+
+ImageDatasetImportDataOp = utils.convert_method_to_component(
+	aiplatform.ImageDataset.import_data, should_serialize_init=True)
+
+TabularDatasetImportDataOp = utils.convert_method_to_component(
+	aiplatform.TabularDataset.import_data, should_serialize_init=True)
+
+TextDatasetImportDataOp = utils.convert_method_to_component(
+	aiplatform.TextDataset.import_data, should_serialize_init=True)
+
+VideoDatasetImportDataOp = utils.convert_method_to_component(
+	aiplatform.VideoDataset.import_data, should_serialize_init=True)
 
 CustomContainerTrainingJobRunOp = utils.convert_method_to_component(
-    aip.CustomContainerTrainingJob.run, should_serialize_init=True)
+	aiplatform.CustomContainerTrainingJob.run,
+    should_serialize_init=True)
+
+CustomPythonPackageTrainingJobRunOp = utils.convert_method_to_component(
+	aiplatform.CustomPythonPackageTrainingJob.run,
+    should_serialize_init=True)
+
+AutoMLImageTrainingJobRunOp = utils.convert_method_to_component(
+	aiplatform.AutoMLImageTrainingJob.run,
+	should_serialize_init=True)
+
+AutoMLTextTrainingJobRunOp = utils.convert_method_to_component(
+	aiplatform.AutoMLTextTrainingJob.run,
+	should_serialize_init=True)
+
+AutoMLTabularTrainingJobRunOp = utils.convert_method_to_component(
+	aiplatform.AutoMLTabularTrainingJob.run,
+	should_serialize_init=True)
+
+AutoMLVideoTrainingJobRunOp = utils.convert_method_to_component(
+	aiplatform.AutoMLVideoTrainingJob.run,
+	should_serialize_init=True)
 
 ModelDeployOp = utils.convert_method_to_component(
-    aip.Model.deploy, should_serialize_init=True)
+	aiplatform.Model.deploy,
+	should_serialize_init=True)
 
-ModelUploadOp = utils.convert_method_to_component(aip.Model.upload)
+ModelBatchPredictOp = utils.convert_method_to_component(
+	aiplatform.Model.batch_predict,
+	should_serialize_init=True)
+
+ModelUploadOp = utils.convert_method_to_component(
+	aiplatform.Model.upload)
+
+EndpointCreateOp = utils.convert_method_to_component(
+	aiplatform.Endpoint.create)
