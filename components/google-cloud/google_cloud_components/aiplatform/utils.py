d8b2b24ed2f0aa657a672489d50aafb3fb8a382b
sasha-gitg <44654632+sasha-gitg@users.noreply.github.com>
2021-04-09 17:54:03 -0400

    feat(google_cloud_components sdk): Enable support for all components (#5442)
    * Enabled remainder of components. Added support for json serialized inputs/outputs, filtering signature and adding signature union, validating input signature, annotation inspection, mapping resource nouns to metadata types, components without output.
    
    * chore: clean up
    
    * change python-aiplatform depedency to point to dev
    
    * fix: aiplatform sdk reference
    
    * fix: lower case container name, rename cloudbuild, change commit pointer
    
    * fix: aiplatform name
    
    * fix: yaml command and type signature
    
    * chore: update to latest commit id
    
    * chore:lint
    
    * chore: update commit id
    
    * chore: doc formatting
    
    * fix: change _type to type_to_resolve

--- components/google-cloud/google_cloud_components/aiplatform/utils.py
+++ components/google-cloud/google_cloud_components/aiplatform/utils.py
@@ -13,84 +13,471 @@
 # limitations under the License.
 """Module for creating pipeline components based on AI Platform SDK."""
 
+import collections
 import inspect
+import json
+from typing import Any, Callable, Dict, Optional, Tuple, Union
+
+from google.cloud import aiplatform
 import kfp
 from kfp import components
 
-INIT_KEY = "init"
-METHOD_KEY = "method"
+# prefix for keyword arguments to separate constructor and method args
+INIT_KEY = 'init'
+METHOD_KEY = 'method'
+
+# map of MB SDK type to Metadata type
+RESOURCE_TO_METADATA_TYPE = {
+    aiplatform.Dataset: "Dataset",
+    aiplatform.Model: "Model",
+    aiplatform.Endpoint: "Artifact",
+    aiplatform.BatchPredictionJob: "Artifact"
+}
 
 
-def convert_method_to_component(method, should_serialize_init=False):
-  """Creates a corresponding KFP Component for a given MBD SDK method."""
-  method_name = method.__name__
+def get_forward_reference(
+    annotation: Any
+) -> Optional[aiplatform.base.AiPlatformResourceNoun]:
+    """Resolves forward references to AiPlatform Class."""
 
-  if inspect.ismethod(method):
-    cls_name = method.__self__.__name__
-    init_signature = inspect.signature(method.__self__.__init__)
-  else:
-    cls = getattr(
-        inspect.getmodule(method),
-        method.__qualname__.split(".<locals>", 1)[0].rsplit(".", 1)[0], None)
-    cls_name = cls.__name__
-    init_signature = inspect.signature(cls.__init__)
+    def get_aiplatform_class_by_name(_annotation):
+        """Resolves str annotation to AiPlatfrom Class."""
+        if isinstance(_annotation, str):
+            return getattr(aiplatform, _annotation, None)
 
-  init_arg_names = set(
-      init_signature.parameters.keys()) if should_serialize_init else set([])
+    ai_platform_class = get_aiplatform_class_by_name(annotation)
+    if ai_platform_class:
+        return ai_platform_class
 
-  def make_args(sa):
-    additional_args = []
-    for key, args in sa.items():
-      for arg_key, value in args.items():
-        additional_args.append(f"    - --{key}.{arg_key}={value}")
-    return "\n".join(additional_args)
+    try:
+        # Python 3.7+
+        from typing import ForwardRef
+        if isinstance(annotation, ForwardRef):
+            annotation = annotation.__forward_arg__
+            ai_platform_class = get_aiplatform_class_by_name(annotation)
+            if ai_platform_class:
+                return ai_platform_class
 
-  def component_yaml_generator(**kwargs):
-    """Function to create the actual component yaml for the input kwargs."""
-    inputs = ["inputs:"]
-    input_args = []
-    input_kwargs = {}
+    except ImportError:
+        pass
 
-    serialized_args = {"init": {}, "method": {}}
 
-    for key, value in kwargs.items():
-      prefix_key = "init" if key in init_arg_names else "method"
-      if isinstance(value, kfp.dsl.PipelineParam):
-        name = key
-        inputs.append("- {name: %s, type: Artifact}" % (name))
-        input_args.append("""
-    - --%s
-    - {inputUri: %s}
-""" % (f"{prefix_key}.{key}", key))
-        input_kwargs[key] = value
-      else:
-        serialized_args[prefix_key][key] = value
+def resolve_annotation(annotation: Any) -> Any:
+    """Resolves annotation type against a MB SDK type.
 
-    inputs = "\n".join(inputs) if len(inputs) > 1 else ""
-    input_args = "\n".join(input_args) if input_args else ""
-    component_text = """
-name: %s-%s
-%s
-outputs:
-- {name: resource_name_output, type: Artifact}
-implementation:
-  container:
-    image: gcr.io/sashaproject-1/AIPlatform_component:latest
-    command:
-    - python3
-    - remote_runner.py
-    - --cls_name=%s
-    - --method_name=%s
-%s
-    args:
-    - --resource_name_output_uri
-    - {outputUri: resource_name_output}
-%s
-""" % (cls_name, method_name, inputs, cls_name, method_name,
-       make_args(serialized_args), input_args)
+    Use this for Optional, Union, Forward References
 
-    print(component_text)
+    Args:
+        annotation: Annotation to resolve
+    Returns:
+        Direct annotation
+    """
 
-    return components.load_component_from_text(component_text)(**input_kwargs)
+    # handle forward refernce string
 
-  return component_yaml_generator
+    # if this is an Ai Platform resource noun
+    if inspect.isclass(annotation):
+        if issubclass(annotation, aiplatform.base.AiPlatformResourceNoun):
+            return annotation
+
+    # handle forward references
+    resolved_annotation = get_forward_reference(annotation)
+    if resolved_annotation:
+        return resolved_annotation
+
+    # handle optional types
+    if getattr(annotation, '__origin__', None) is Union:
+        # assume optional type
+        # TODO check for optional type
+        resolved_annotation = get_forward_reference(annotation.__args__[0])
+        if resolved_annotation:
+            return resolved_annotation
+        else:
+            return annotation.__args__[0]
+
+    if annotation is inspect._empty:
+        return None
+
+    return annotation
+
+
+def is_serializable_to_json(annotation: Any) -> bool:
+    """Checks if type is serializable.
+
+    Args:
+        annotation: parameter annotation
+    Returns:
+        True if serializable to json.
+    """
+    serializable_types = (dict, list, collections.abc.Sequence)
+    return getattr(annotation, '__origin__', None) in serializable_types
+
+
+def is_mb_sdk_resource_noun_type(mb_sdk_type: Any) -> bool:
+    """Determines if type passed in should be a metadata type.
+
+    Args:
+        mb_sdk_type: Type to check
+    Returns:
+        True if this is a resource noun
+    """
+    if inspect.isclass(mb_sdk_type):
+        return issubclass(mb_sdk_type, aiplatform.base.AiPlatformResourceNoun)
+    return False
+
+
+def get_serializer(annotation: Any) -> Optional[Callable]:
+    """Get serailizer for objects to pass them as strings.
+
+    Remote runner will deserialize.
+    # TODO handle proto.Message
+
+    Args:
+        annotation: Parameter annotation
+    Returns:
+        serializer for that annotation type
+    """
+    if is_serializable_to_json(annotation):
+        return json.dumps
+
+
+def get_deserializer(annotation: Any) -> Optional[Callable[..., str]]:
+    """Get deserailizer for objects to pass them as strings.
+
+    Remote runner will deserialize.
+    # TODO handle proto.Message
+    Args:
+        annotation: parameter annotatoin
+    Returns:
+        deserializer for annotation type
+    """
+    if is_serializable_to_json(annotation):
+        return json.loads
+
+
+def map_resource_to_metadata_type(
+    mb_sdk_type: aiplatform.base.AiPlatformResourceNoun
+) -> Tuple[str, str]:
+    """Maps an MB SDK type to Metadata type.
+
+    Returns:
+        Tuple of component parameter name and metadata type.
+        ie aiplatform.Model -> "model", "Model"
+    """
+    # type should always be in this map
+    if is_mb_sdk_resource_noun_type(mb_sdk_type):
+        for key in RESOURCE_TO_METADATA_TYPE.keys():
+            if issubclass(mb_sdk_type, key):
+                return key.__name__.split('.')[-1].lower(
+                ), RESOURCE_TO_METADATA_TYPE[key]
+
+    # handles case of exported_dataset
+    # TODO generalize to all serializable outputs
+    if is_serializable_to_json(mb_sdk_type):
+        return "exported_dataset", "JsonArray"
+
+
+def should_be_metadata_type(mb_sdk_type: Any) -> bool:
+    """Determines if type passed in should be a metadata type."""
+    if inspect.isclass(mb_sdk_type):
+        return issubclass(mb_sdk_type, aiplatform.base.AiPlatformResourceNoun)
+    return False
+
+
+def is_resource_name_parameter_name(param_name: str) -> bool:
+    """Determines if the mb_sdk parameter is a resource name."""
+    return param_name != 'display_name' and param_name.endswith('_name')
+
+
+# These parameters are filtered from MB SDK methods
+PARAMS_TO_REMOVE = {"self", "credentials", "sync"}
+
+
+def filter_signature(
+    signature: inspect.Signature,
+    is_init_signature: bool = False,
+    self_type: Optional[aiplatform.base.AiPlatformResourceNoun] = None,
+    component_param_name_to_mb_sdk_param_name: Dict[str, str] = None
+) -> inspect.Signature:
+    """Removes unused params from signature.
+
+    Args:
+        signature (inspect.Signature): Model Builder SDK Method Signature.
+        is_init_signature (bool): is this constructor signature
+        self_type (aiplatform.base.AiPlatformResourceNoun): This is used to replace *_name str fields with resource
+            name type.
+        component_param_name_to_mb_sdk_param_name dict[str, str]: Mapping to keep track of param names changed
+            to make them component friendly( ie: model_name -> model)
+
+    Returns:
+        Signature appropriate for component creation.
+    """
+    new_params = []
+    for param in signature.parameters.values():
+        if param.name not in PARAMS_TO_REMOVE:
+            # change resource name signatures to resource types
+            # to enforce metadata entry
+            # ie: model_name -> model
+            if is_init_signature and is_resource_name_parameter_name(param.name
+                                                                    ):
+                new_name = param.name[:-len('_name')]
+                new_params.append(
+                    inspect.Parameter(
+                        name=new_name,
+                        kind=param.kind,
+                        default=param.default,
+                        annotation=self_type
+                    )
+                )
+                component_param_name_to_mb_sdk_param_name[new_name] = param.name
+            else:
+                new_params.append(param)
+
+    return inspect.Signature(
+        parameters=new_params, return_annotation=signature.return_annotation
+    )
+
+
+def signatures_union(
+    init_sig: inspect.Signature, method_sig: inspect.Signature
+) -> inspect.Signature:
+    """Returns a Union of the constructor and method signature.
+
+    Args:
+        init_sig (inspect.Signature): Constructor signature
+        method_sig (inspect.Signature): Method siganture
+
+    Returns:
+        A Union of the the two Signatures as a single Signature
+    """
+
+    def key(param):
+        # all params are keyword or positional
+        # move the params without defaults to the front
+        if param.default is inspect._empty:
+            return -1
+        return 1
+
+    params = list(init_sig.parameters.values()
+                 ) + list(method_sig.parameters.values())
+    params.sort(key=key)
+    return inspect.Signature(
+        parameters=params, return_annotation=method_sig.return_annotation
+    )
+
+
+def convert_method_to_component(
+    method: Callable, should_serialize_init: bool = False
+) -> Callable:
+    """Converts a MB SDK Method to a Component wrapper.
+
+    The wrapper enforces the correct signature w.r.t the MB SDK. The signature
+    is also available to inspect.
+
+    For example:
+
+    aiplatform.Model.deploy is converted to ModelDeployOp
+
+    Which can be called:
+        model_deploy_step = ModelDeployOp(
+            project=project,  # Pipeline paramter
+            endpoint=endpoint_create_step.outputs['endpoint'],
+            model=model_upload_step.outputs['model'],
+            deployed_model_display_name='my-deployed-model',
+            machine_type='n1-standard-4',
+        )
+
+    Generates and invokes the following Component:
+
+    name: Model-deploy
+    inputs:
+    - {name: project, type: String}
+    - {name: endpoint, type: Artifact}
+    - {name: model, type: Model}
+    outputs:
+    - {name: endpoint, type: Artifact}
+    implementation:
+      container:
+        image: gcr.io/sashaproject-1/mb_sdk_component:latest
+        command:
+        - python3
+        - remote_runner.py
+        - --cls_name=Model
+        - --method_name=deploy
+        - --method.deployed_model_display_name=my-deployed-model
+        - --method.machine_type=n1-standard-4
+        args:
+        - --resource_name_output_uri
+        - {outputUri: endpoint}
+        - --init.project
+        - {inputValue: project}
+        - --method.endpoint
+        - {inputUri: endpoint}
+        - --init.model_name
+        - {inputUri: model}
+
+
+    Args:
+        method (Callable): A MB SDK Method
+        should_serialize_init (bool): Whether to also include the constructor params
+            in the compoennt
+    Retruns:
+        A Component wrapper that accepts the MB SDK params and returns a Task.
+    """
+    method_name = method.__name__
+    method_signature = inspect.signature(method)
+
+    # get class name and constructor signature
+    if inspect.ismethod(method):
+        cls = method.__self__
+        cls_name = cls.__name__
+        init_signature = inspect.signature(method.__self__.__init__)
+    else:
+        cls = getattr(
+            inspect.getmodule(method),
+            method.__qualname__.split('.<locals>', 1)[0].rsplit('.', 1)[0]
+        )
+        cls_name = cls.__name__
+        init_signature = inspect.signature(cls.__init__)
+
+    # map to store parameter names that are changed in components
+    # this is generally used for constructor where the mb sdk takes
+    # a resource name but the component takes a metadata entry
+    # ie: model: system.Model -> model_name: str
+    component_param_name_to_mb_sdk_param_name = {}
+    # remove unused parameters
+    method_signature = filter_signature(method_signature)
+    init_signature = filter_signature(
+        init_signature,
+        is_init_signature=True,
+        self_type=cls,
+        component_param_name_to_mb_sdk_param_name=
+        component_param_name_to_mb_sdk_param_name
+    )
+
+    # use this to partition args to method or constructor
+    init_arg_names = set(init_signature.parameters.keys()
+                        ) if should_serialize_init else set([])
+
+    # determines outputs for this component
+    output_type = resolve_annotation(method_signature.return_annotation)
+    outputs = ''
+    output_args = ''
+    if output_type:
+        output_metadata_name, output_metadata_type = map_resource_to_metadata_type(
+            output_type
+        )
+        outputs = '\n'.join([
+            'outputs:',
+            f'- {{name: {output_metadata_name}, type: {output_metadata_type}}}'
+        ])
+        output_args = '\n'.join([
+            '    - --resource_name_output_uri',
+            f'    - {{outputUri: {output_metadata_name}}}',
+        ])
+
+    def make_args(args_to_serialize: Dict[str, Dict[str, Any]]) -> str:
+        """Takes the args dicitionary and return serailized Component string
+        for args.
+
+        Args:
+            args_to_serialize: Dictionary of format
+                {'init': {'param_name_1': param_1}, {'method'}: {'param_name_2': param_name_2}}
+        Returns:
+            Serialized args compatible with Component YAML
+        """
+        additional_args = []
+        for key, args in args_to_serialize.items():
+            for arg_key, value in args.items():
+                additional_args.append(f"    - --{key}.{arg_key}={value}")
+        return '\n'.join(additional_args)
+
+    def component_yaml_generator(**kwargs):
+        inputs = ["inputs:"]
+        input_args = []
+        input_kwargs = {}
+
+        serialized_args = {"init": {}, "method": {}}
+
+        init_kwargs = {}
+        method_kwargs = {}
+
+        for key, value in kwargs.items():
+            if key in init_arg_names:
+                prefix_key = INIT_KEY
+                init_kwargs[key] = value
+                signature = init_signature
+            else:
+                prefix_key = METHOD_KEY
+                method_kwargs[key] = value
+                signature = method_signature
+
+            param_type = signature.parameters[key].annotation
+            param_type = resolve_annotation(param_type)
+            serializer = get_serializer(param_type)
+            if serializer:
+                param_type = str
+                value = serializer(value)
+
+            # TODO: remove PipelineParam check when Metadata Importer component available
+            # if we serialize we need to include the argument as input
+            # perhaps, another option is to embed in yaml as json seralized list
+            component_param_name = component_param_name_to_mb_sdk_param_name.get(
+                key, key
+            )
+            if isinstance(value,
+                          kfp.dsl._pipeline_param.PipelineParam) or serializer:
+                if is_mb_sdk_resource_noun_type(param_type):
+                    metadata_type = map_resource_to_metadata_type(param_type)[1]
+                    component_param_type, component_type = metadata_type, 'inputUri'
+                else:
+                    component_param_type, component_type = 'String', 'inputValue'
+
+                inputs.append(
+                    f"- {{name: {key}, type: {component_param_type}}}"
+                )
+                input_args.append(
+                    '\n'.join([
+                        f'    - --{prefix_key}.{component_param_name}',
+                        f'    - {{{component_type}: {key}}}'
+                    ])
+                )
+                input_kwargs[key] = value
+            else:
+                serialized_args[prefix_key][component_param_name] = value
+
+        # validate parameters
+        if should_serialize_init:
+            init_signature.bind(**init_kwargs)
+        method_signature.bind(**method_kwargs)
+
+        inputs = "\n".join(inputs) if len(inputs) > 1 else ''
+        input_args = "\n".join(input_args) if input_args else ''
+        component_text = "\n".join([
+            f'name: {cls_name}-{method_name}', f'{inputs}', outputs,
+            'implementation:', '  container:',
+            '    image: gcr.io/sashaproject-1/aiplatform_component:latest',
+            '    command:', '    - python3', '    - -m',
+            '    - google_cloud_components.aiplatform.remote_runner',
+            f'    - --cls_name={cls_name}',
+            f'    - --method_name={method_name}',
+            f'{make_args(serialized_args)}', '    args:', output_args,
+            f'{input_args}'
+        ])
+
+        print(component_text)
+
+        return components.load_component_from_text(component_text)(
+            **input_kwargs
+        )
+
+    component_yaml_generator.__signature__ = signatures_union(
+        init_signature, method_signature
+    ) if should_serialize_init else method_signature
+
+    # TODO:union docs based on signatures union
+    component_yaml_generator.__doc__ = method.__doc__
+
+    # TODO: Possibly rename method
+
+    return component_yaml_generator
