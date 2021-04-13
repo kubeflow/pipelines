cc4652ed68b36760e3c41439b914e6f1f77dd1f4
NikeNano <niklas.sven.hansson@gmail.com>
2021-04-13 18:31:18 +0200

    fixed test

--- backend/src/cache/server/mutation_test.go
+++ backend/src/cache/server/mutation_test.go
@@ -213,12 +213,11 @@ func TestSetImage(t *testing.T) {
 
 func TestCacheNodeRestriction(t *testing.T) {
 	os.Setenv("CACHE_NODE_RESTRICTIONS", "false")
-	defer os.Unsetenv("CACHE_NODE_RESTRICTIONS")
 
 	executionCache := &model.ExecutionCache{
 		ExecutionCacheKey: "f5fe913be7a4516ebfe1b5de29bcb35edd12ecc776b2f33f10ca19709ea3b2f0",
 		ExecutionOutput:   "testOutput",
-		ExecutionTemplate: `{"container": {"command": ["echo", "Hello"],"image": "python:3.7"}, "nodeSelector": {"disktype": "ssd"}}`,
+		ExecutionTemplate: `{"container":{"command":["echo", "Hello"],"image":"python:3.7"},"nodeSelector":{"disktype":"ssd"}}`,
 		MaxCacheStaleness: -1,
 	}
 	fakeClientManager.CacheStore().CreateExecutionCache(executionCache)
@@ -226,7 +225,7 @@ func TestCacheNodeRestriction(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, OperationTypeRemove, patchOperation[1].Op)
 	assert.Nil(t, patchOperation[1].Value)
-
+	os.Setenv("CACHE_NODE_RESTRICTIONS", "true")
 }
 
 func TestMutatePodIfCachedWithTeamplateCleanup(t *testing.T) {
