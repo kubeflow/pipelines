cd2ca83fa8ab8179bfa90c22747272965399da14
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-05-04 22:53:22 -0700

    Assigned copyright to the project authors (#5587)

--- backend/src/apiserver/visualization/exporter.py
+++ backend/src/apiserver/visualization/exporter.py
@@ -3,7 +3,7 @@ exporter.py provides utility functions for generating NotebookNode objects and
 converting those objects to HTML.
 """
 
-# Copyright 2019 Google LLC
+# Copyright 2019 The Kubeflow Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
