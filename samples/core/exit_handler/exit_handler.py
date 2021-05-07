31429dc29616c87bd6c7aeeeac93ca9b779dd896
Yuan (Bob) Gong <4957653+Bobgy@users.noreply.github.com>
2021-05-07 21:48:06 +0800

    chore(samples): v2 compatible exit handler sample (#5541)
    * chore(samples): v2 compatible exit handler sample
    
    * legacy-exit-handler only run on v1 mode
    
    * fix legacy_exit_handler_test
    
    * Update exit_handler_test.py

--- samples/core/exit_handler/exit_handler.py
+++ samples/core/exit_handler/exit_handler.py
@@ -15,15 +15,22 @@
 
 import kfp
 from kfp import dsl, components
+from kfp.components import InputPath
 
 gcs_download_op = kfp.components.load_component_from_url(
-    'https://raw.githubusercontent.com/kubeflow/pipelines/1.5.0-rc.3/components/google-cloud/storage/download_blob/component.yaml'
+    'https://raw.githubusercontent.com/kubeflow/pipelines/961b17fa6844e1d79e5d3686bb557d830d7b5a95/components/google-cloud/storage/download_blob/component.yaml'
 )
 
 
 @components.create_component_from_func
-def echo_op(msg: str):
-    """Print a message."""
+def print_file(file_path: InputPath('Any')):
+    """Print a file."""
+    with open(file_path) as f:
+        print(f.read())
+
+@components.create_component_from_func
+def echo_msg(msg: str):
+    """Echo a message by parameter."""
     print(msg)
 
 
@@ -35,11 +42,11 @@ def echo_op(msg: str):
 def pipeline_exit_handler(url='gs://ml-pipeline/shakespeare1.txt'):
     """A sample pipeline showing exit handler."""
 
-    exit_task = echo_op('exit!')
+    exit_task = echo_msg('exit!')
 
     with dsl.ExitHandler(exit_task):
         download_task = gcs_download_op(url)
-        echo_task = echo_op(download_task.output)
+        echo_task = print_file(download_task.output)
 
 
 if __name__ == '__main__':
