df97a32f7c69854284e320324368372510d6d14a
Yuan (Bob) Gong <4957653+Bobgy@users.noreply.github.com>
2021-05-11 16:02:33 +0800

    feat(frontend): ResourceInfo supports MLMD struct value (#5618)
    * upgrade typescript to 3.8, fix minor issues
    
    * add comments

--- frontend/src/lib/Buttons.ts
+++ frontend/src/lib/Buttons.ts
@@ -658,12 +658,10 @@ export default class Buttons {
   }
 
   private _createNewRun(isRecurring: boolean, experimentId?: string): void {
-    const searchString = this._urlParser.build(
-      Object.assign(
-        { [QUERY_PARAMS.experimentId]: experimentId || '' },
-        isRecurring ? { [QUERY_PARAMS.isRecurring]: '1' } : {},
-      ),
-    );
+    const searchString = this._urlParser.build({
+      [QUERY_PARAMS.experimentId]: experimentId || '',
+      ...(isRecurring ? { [QUERY_PARAMS.isRecurring]: '1' } : {}),
+    });
     this._props.history.push(RoutePage.NEW_RUN + searchString);
   }
 
