73694e5f12cf4e300ec8e4766937132df65c3ce2
Alexey Volkov <alexey.volkov@ark-kun.com>
2021-05-10 18:23:47 -0700

    Added component to the sample pipeline

--- components/PyTorch/_samples/Train_fully-connected_network.pipeline.py
+++ components/PyTorch/_samples/Train_fully-connected_network.pipeline.py
@@ -3,10 +3,12 @@ from kfp import components
 
 chicago_taxi_dataset_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/e3337b8bdcd63636934954e592d4b32c95b49129/components/datasets/Chicago%20Taxi/component.yaml')
 pandas_transform_csv_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/6162d55998b176b50267d351241100bb0ee715bc/components/pandas/Transform_DataFrame/in_CSV_format/component.yaml')
+download_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/240543e483076ae718f82c6f280441daa2f041fd/components/web/Download/component.yaml')
 
 create_fully_connected_pytorch_network_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/4e1facea1a270535b515a9e8cc59422d1ad76a9e/components/PyTorch/Create_fully_connected_network/component.yaml')
 train_pytorch_model_from_csv_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/603342c4b88fe2d69ff07682f702cd3601e883bb/components/PyTorch/Train_PyTorch_model/from_CSV/component.yaml')
 convert_to_onnx_from_pytorch_script_module_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/e011e4affa85542ef2b24d63fdac27f8d939bbee/components/PyTorch/Convert_to_OnnxModel_from_PyTorchScriptModule/component.yaml')
+create_pytorch_model_archive_op = components.load_component_from_url('https://raw.githubusercontent.com/kubeflow/pipelines/abc180be2b2b5538d19eb87124684629ec45e620/components/PyTorch/Create_PyTorch_Model_Archive/component.yaml')
 
 
 def pytorch_pipeline():
@@ -46,6 +48,16 @@ def pytorch_pipeline():
         model=trained_model,
         list_of_input_shapes=[[len(feature_columns)]],
     )
+
+    # TODO: Use a real working regression handler here. See https://github.com/pytorch/serve/issues/987
+    serving_handler = download_op('https://raw.githubusercontent.com/pytorch/serve/5c03e711a401387a1d42fc01072fcc38b4995b66/ts/torch_handler/base_handler.py').output
+    
+    model_archive = create_pytorch_model_archive_op(
+        model=trained_model,
+        handler=serving_handler,
+        # model_name="model",  # Optional
+        # model_version="1.0",  # Optional
+    ).output
 
 if __name__ == '__main__':
     import kfp
