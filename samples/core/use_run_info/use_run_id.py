e27b80cc513902eb2c476ab545626676c4cc70ea
Yuan Gong <gongyuan94@gmail.com>
2021-05-20 14:41:05 +0800

    fix, use latest test infra tools

--- samples/core/use_run_info/use_run_id.py
+++ samples/core/use_run_info/use_run_id.py
@@ -17,11 +17,14 @@ import kfp
 import kfp.dsl as dsl
 
 
-def get_run_info(run_id: str):
+def get_run_info(run_id: str) -> str:
     '''Example of getting run info for current pipeline run'''
     import kfp
-    import json
     print(f'Current run ID is {run_id}.')
+    # KFP API server is usually available as ml-pipeline service in the same
+    # namespace, but for full Kubeflow deployment, you need to edit this to
+    # http://ml-pipeline.kubeflow:8888, because your pipelines are running in
+    # user namespaces, but the API is at kubeflow namespace.
     client = kfp.Client(host='http://ml-pipeline:8888')
     run_info = client.get_run(run_id=run_id)
     # Hide verbose info
@@ -36,14 +39,17 @@ get_run_info_component = kfp.components.create_component_from_func(
 
 
 @dsl.pipeline(
-    name="pipeline_use_run_id",
+    name="use-run-id",
     description=
     "A pipeline that demonstrates how to use run information, including run ID etc."
 )
 def pipeline_use_run_id(run_id: str = kfp.dsl.RUN_ID_PLACEHOLDER):
-    '''kfp.dsl.RUN_ID_PLACEHOOLDER inside a parameter will be populated with KFP Run ID at runtime.'''
+    '''kfp.dsl.RUN_ID_PLACEHOLDER inside a pipeline parameter will be populated with KFP Run ID at runtime.'''
     run_info_op = get_run_info_component(run_id=run_id)
 
 
 if __name__ == '__main__':
-    kfp.compiler.Compiler().compile(pipeline_with_run_info, __file__ + '.yaml')
+    kfp.compiler.Compiler(
+        # uncomment this to compile as v2 compatible mode
+        # mode=kfp.dsl.PipelineExecutionMode.V2_COMPATIBLE,
+    ).compile(pipeline_use_run_id, __file__ + '.yaml')
