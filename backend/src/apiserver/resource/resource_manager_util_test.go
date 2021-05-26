435f6b1abe3c04e1aa37afb24a933d4442e29b7a
guanhuichen <guanhuichen@gmail.com>
2021-05-27 00:01:41 +0800

    fix(backend): Fixed workflow cannot be retried with node xxx in Omitted (#5738)
    Fixes #5537

--- backend/src/apiserver/resource/resource_manager_util_test.go
+++ backend/src/apiserver/resource/resource_manager_util_test.go
@@ -152,7 +152,7 @@ func TestRetryWorkflowWith(t *testing.T) {
 apiVersion: argoproj.io/v1alpha1
 kind: Workflow
 metadata:
-  creationTimestamp: "2019-08-02T07:15:14Z"
+  creationTimestamp: "2021-05-26T09:14:07Z"
   generateName: resubmit-
   generation: 1
   labels:
@@ -177,6 +177,11 @@ spec:
         - A
         name: B
         template: random-fail
+      - arguments: {}
+        dependencies:
+        - B
+        name: C
+        template: random-fail
     inputs: {}
     metadata: {}
     name: rand-fail-dag
@@ -196,44 +201,57 @@ spec:
     name: random-fail
     outputs: {}
 status:
-  finishedAt: "2019-08-02T07:15:19Z"
+  finishedAt: "2021-05-26T09:14:29Z"
   nodes:
     resubmit-hl9ft:
       children:
       - resubmit-hl9ft-3929423573
       displayName: resubmit-hl9ft
-      finishedAt: "2019-08-02T07:15:19Z"
+      finishedAt: "2021-05-26T09:14:29Z"
       id: resubmit-hl9ft
       name: resubmit-hl9ft
       phase: Failed
-      startedAt: "2019-08-02T07:15:14Z"
+      startedAt: "2021-05-26T09:14:07Z"
       templateName: rand-fail-dag
       type: DAG
     resubmit-hl9ft-3879090716:
       boundaryID: resubmit-hl9ft
+      children:
+      - resubmit-hl9ft-3895868335
       displayName: B
-      finishedAt: "2019-08-02T07:15:18Z"
+      finishedAt: "2021-05-26T09:14:23Z"
       id: resubmit-hl9ft-3879090716
       message: failed with exit code 1
       name: resubmit-hl9ft.B
       phase: Failed
-      startedAt: "2019-08-02T07:15:17Z"
+      startedAt: "2021-05-26T09:14:19Z"
       templateName: random-fail
       type: Pod
+    resubmit-hl9ft-3895868335:
+      boundaryID: resubmit-hl9ft
+      displayName: C
+      finishedAt: "2021-05-26T09:14:29Z"
+      id: resubmit-hl9ft-3895868335
+      message: 'omitted: depends condition not met'
+      name: resubmit-hl9ft.C
+      phase: Omitted
+      startedAt: "2021-05-26T09:14:29Z"
+      templateName: random-fail
+      type: Skipped
     resubmit-hl9ft-3929423573:
       boundaryID: resubmit-hl9ft
       children:
       - resubmit-hl9ft-3879090716
       displayName: A
-      finishedAt: "2019-08-02T07:15:16Z"
+      finishedAt: "2021-05-26T09:14:11Z"
       id: resubmit-hl9ft-3929423573
       name: resubmit-hl9ft.A
       phase: Succeeded
-      startedAt: "2019-08-02T07:15:15Z"
+      startedAt: "2021-05-26T09:14:07Z"
       templateName: random-fail
       type: Pod
   phase: Failed
-  startedAt: "2019-08-02T07:15:14Z"
+  startedAt: "2021-05-26T09:14:07Z"
 `
 
 	var workflow util.Workflow
@@ -249,7 +267,7 @@ status:
 		`apiVersion: argoproj.io/v1alpha1
 kind: Workflow
 metadata:
-  creationTimestamp: "2019-08-02T07:15:14Z"
+  creationTimestamp: "2021-05-26T09:14:07Z"
   generateName: resubmit-
   generation: 1
   labels:
@@ -273,6 +291,11 @@ spec:
         dependencies:
         - A
         name: B
+        template: random-fail
+      - arguments: {}
+        dependencies:
+        - B
+        name: C
         template: random-fail
     inputs: {}
     metadata: {}
@@ -303,7 +326,7 @@ status:
       id: resubmit-hl9ft
       name: resubmit-hl9ft
       phase: Running
-      startedAt: "2019-08-02T07:15:14Z"
+      startedAt: "2021-05-26T09:14:07Z"
       templateName: rand-fail-dag
       type: DAG
     resubmit-hl9ft-3929423573:
@@ -311,15 +334,15 @@ status:
       children:
       - resubmit-hl9ft-3879090716
       displayName: A
-      finishedAt: "2019-08-02T07:15:16Z"
+      finishedAt: "2021-05-26T09:14:11Z"
       id: resubmit-hl9ft-3929423573
       name: resubmit-hl9ft.A
       phase: Succeeded
-      startedAt: "2019-08-02T07:15:15Z"
+      startedAt: "2021-05-26T09:14:07Z"
       templateName: random-fail
       type: Pod
   phase: Running
-  startedAt: "2019-08-02T07:15:14Z"
+  startedAt: "2021-05-26T09:14:07Z"
 `
 
 	assert.Equal(t, expectedNewWfString, string(newWfString))
