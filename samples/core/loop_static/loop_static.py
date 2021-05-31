66a9f0f8853c95a4b867e086588af0ac0098c265
Chen Sun <chensun@users.noreply.github.com>
2021-05-31 07:31:42 +0000

    update samples

--- samples/core/loop_static/loop_static.py
+++ samples/core/loop_static/loop_static.py
@@ -10,22 +10,22 @@ def print_op(text: str) -> str:
 
 
 @components.create_component_from_func
-def sum_op(a: float, b: float) -> float:
+def concat_op(a: str, b: str) -> str:
     print(a + b)
     return a + b
 
 
-_DEFAULT_LOOP_ARGUMENTS = [{'a': 1, 'b': 2}, {'a': 10, 'b': 20}]
+_DEFAULT_LOOP_ARGUMENTS = [{'a': '1', 'b': '2'}, {'a': '10', 'b': '20'}]
 
 
 @dsl.pipeline(name='pipeline-with-loop-static')
 def my_pipeline(
     static_loop_arguments: List[dict] = _DEFAULT_LOOP_ARGUMENTS,
-    greeting='this is a test for looping through parameters'
+    greeting: str = 'this is a test for looping through parameters',
 ):
     print_task = print_op(text=greeting)
 
     with dsl.ParallelFor(static_loop_arguments) as item:
-        sum_task = sum_op(a=item.a, b=item.b)
-        sum_task.after(print_task)
-        print_task_2 = print_op(sum_task.output.ignore_type())
+        concat_task = concat_op(a=item.a, b=item.b)
+        concat_task.after(print_task)
+        print_task_2 = print_op(concat_task.output)
