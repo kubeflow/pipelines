eac712c993c8839235c4a8afbfaf3cac984ac231
Chen Sun <chensun@users.noreply.github.com>
2021-06-02 11:44:35 -0700

    release(api): Bump kfp-pipeline-spec version to 0.1.8 (#5778)

--- api/v2alpha1/python/setup.py
+++ api/v2alpha1/python/setup.py
@@ -22,7 +22,7 @@ except ImportError:
   from shutil import which as find_executable
 
 NAME = "kfp-pipeline-spec"
-VERSION = "0.1.7"
+VERSION = "0.1.8"
 
 PROTO_DIR = os.path.realpath(
     os.path.join(os.path.dirname(__file__), os.pardir))
