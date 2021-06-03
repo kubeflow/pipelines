28431fc3fdf6b117eaad0a2986e7fa270a50596d
Yuan (Bob) Gong <4957653+Bobgy@users.noreply.github.com>
2021-06-04 01:34:36 +0800

    feat: update MLMD to 1.0.0 (#5786)
    * feat: update MLMD to 1.0.0
    
    * update MLMD frontend client

--- test/tag_for_hosted.sh
+++ test/tag_for_hosted.sh
@@ -100,8 +100,8 @@ docker push gcr.io/$PROJECT_ID/hosted/$COMMIT_SHA:$MM_VER
 # * .cloudbuild.yaml and .release.cloudbuild.yaml
 # * manifests/kustomize/base/metadata/base/metadata-grpc-deployment.yaml
 # * test/tag_for_hosted.sh
-docker tag gcr.io/tfx-oss-public/ml_metadata_store_server:0.30.0 gcr.io/$PROJECT_ID/hosted/$COMMIT_SHA/metadataserver:$SEM_VER
-docker tag gcr.io/tfx-oss-public/ml_metadata_store_server:0.30.0 gcr.io/$PROJECT_ID/hosted/$COMMIT_SHA/metadataserver:$MM_VER
+docker tag gcr.io/tfx-oss-public/ml_metadata_store_server:1.0.0 gcr.io/$PROJECT_ID/hosted/$COMMIT_SHA/metadataserver:$SEM_VER
+docker tag gcr.io/tfx-oss-public/ml_metadata_store_server:1.0.0 gcr.io/$PROJECT_ID/hosted/$COMMIT_SHA/metadataserver:$MM_VER
 docker push gcr.io/$PROJECT_ID/hosted/$COMMIT_SHA/metadataserver:$SEM_VER
 docker push gcr.io/$PROJECT_ID/hosted/$COMMIT_SHA/metadataserver:$MM_VER
 
