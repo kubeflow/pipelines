44d11858fd0252c0b02f58b2e2f0d5bd5763e70d
ankan94 <51693147+ankan94@users.noreply.github.com>
2021-06-08 10:27:53 +0530

    feat(components): Adding Visualization component for PyTorch - KFP (#5810)
    * Adding visualization component
    
    Signed-off-by: ankan94 <ankan@ideas2it.com>
    
    * Switch to Apache2 License
    
    Signed-off-by: Arvind-Ideas2IT <arvindkumarsingh.gautam@ideas2it.com>
    
    Co-authored-by: Arvind-Ideas2IT <arvindkumarsingh.gautam@ideas2it.com>
    Co-authored-by: Shrinath Suresh <shrinath@ideas2it.com>

--- components/PyTorch/pytorch-kfp-components/pytorch_kfp_components/types/standard_component_specs.py
+++ components/PyTorch/pytorch-kfp-components/pytorch_kfp_components/types/standard_component_specs.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Module for defining standard specifications and validation of parameter
 type."""
 
