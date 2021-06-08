73780697c19c370f8e50da2733e3be2e2354d62f
capri-xiyue <52932582+capri-xiyue@users.noreply.github.com>
2021-06-07 22:37:52 -0700

    test(v2): presubmit unit tests of v2 (#5802)
    * presubmit unit tests of v2
    
    * added mlmd port forward
    
    * deleted mlmd port forward
    
    * rename
    
    * added kubectl port forward
    
    * renamed test

---
+++ v2/test/presubmit-v2-go-test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Copyright 2021 The Kubeflow Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Fail the entire script when any command fails.
+set -ex
+NAMESPACE=${NAMESPACE:-kubeflow}
+# The current directory is /home/prow/go/src/github.com/kubeflow/pipelines
+# 1. install go in /home/prow/go1.13.3
+cd /home/prow
+mkdir go1.13.3
+cd go1.13.3
+wget --quiet https://dl.google.com/go/go1.13.3.linux-amd64.tar.gz
+tar -xf go1.13.3.linux-amd64.tar.gz
+GO_CMD=/home/prow/go1.13.3/go/bin/go
+cd /home/prow/go/src/github.com/kubeflow/pipelines/v2
+# 2. Check go modules are tidy
+# Reference: https://github.com/golang/go/issues/27005#issuecomment-564892876
+${GO_CMD} mod download
+${GO_CMD} mod tidy
+git diff --exit-code -- go.mod go.sum || (echo "go modules are not tidy, run 'go mod tidy'." && exit 1)
+# Note, for tests that use metadata grpc api, port-forward it locally in a separate terminal by:
+kubectl port-forward svc/metadata-grpc-service 8080:8080 -n $NAMESPACE
+# 3. run test in project directory
+${GO_CMD} test -v -cover ./...
