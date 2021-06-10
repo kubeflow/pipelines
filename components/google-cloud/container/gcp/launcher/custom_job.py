4fef746b5e74eaae0acc8b24ae32d6d6d95bf126
IronPan <panyang06231989@gmail.com>
2021-06-10 15:44:10 -0700

    add gcp launcher container following the first party component interface, and add custom job implementation

--- components/google-cloud/container/gcp/launcher/custom_job.py
+++ components/google-cloud/container/gcp/launcher/custom_job.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 def create_custom_job(
   type,
   gcp_project,
@@ -19,13 +20,27 @@ def create_custom_job(
   payload,
   gcp_resources,
   ):
-  import logging
-  import subprocess
-  import time
+  """
+  Create and poll custom job status till it reaches a final state.
+  This follows the typical launching logic
+  1. Read if the custom job already exists in gcp_resources
+     - If already exists, jump to step 3 and poll the job status. This happens if the 
+       launcher container experienced unexpected termination, such as preemption
+  2. Deserialize the payload into the job spec and create the custom job.
+  3. Poll the custom job status every _POLLING_INTERVAL_IN_SECONDS seconds
+     - If the custom job is succeeded, return succeeded
+     - If the custom job is cancelled/paused, it's an unexpected scenario so return failed
+     - If the custom job is running, continue polling the status
+
+  Also retry on ConnectionError up to _CONNECTION_ERROR_RETRY_LIMIT times during the poll.
+  """
+
   import json
+  import logging
+  import os
+  import time
   from google.cloud import aiplatform
   from google.protobuf import json_format
-  from google.protobuf.struct_pb2 import Value
   from google.cloud.aiplatform.compat.types import job_state as gca_job_state
 
   _POLLING_INTERVAL_IN_SECONDS = 20
@@ -41,17 +56,23 @@ def create_custom_job(
   _JOB_ERROR_STATES = (
       gca_job_state.JobState.JOB_STATE_FAILED,
       gca_job_state.JobState.JOB_STATE_CANCELLED,
+      gca_job_state.JobState.JOB_STATE_PAUSED,
   )
 
   client_options = {"api_endpoint": gcp_region+'-aiplatform.googleapis.com'}
   # Initialize client that will be used to create and send requests.
   job_client = aiplatform.gapic.JobServiceClient(client_options=client_options)
 
-  parent = f"projects/{gcp_project}/locations/{gcp_region}"
-  job_spec = json.loads(payload)
-
-  create_custom_job_response = job_client.create_custom_job(parent=parent, custom_job=job_spec)
-  custom_job_name = create_custom_job_response.name
+  # Check if the Custom job already exists
+  if os.stat(gcp_resources).st_size != 0:
+    with open(gcp_resources) as f:
+      custom_job_name = f.read()
+      logging.info('CustomJob name already exists: %s. Continue polling the status', custom_job_name)
+  else:
+    parent = f"projects/{gcp_project}/locations/{gcp_region}"
+    job_spec = json.loads(payload)
+    create_custom_job_response = job_client.create_custom_job(parent=parent, custom_job=job_spec)
+    custom_job_name = create_custom_job_response.name
 
   # Write the job id to output
   with open(gcp_resources, 'w') as f:
@@ -70,14 +91,13 @@ def create_custom_job(
       retry_count = 0
     # Handle transient connection error.
     except ConnectionError as err:
+      retry_count += 1
       if retry_count < _CONNECTION_ERROR_RETRY_LIMIT:
-        retry_count += 1
         logging.warning(
             'ConnectionError (%s) encountered when polling job: %s. Trying to '
             'recreate the API client.', err, custom_job_name)
         # Recreate the Python API client.
         job_client = aiplatform.gapic.JobServiceClient(client_options=client_options)
-        get_custom_job_response = job_client.get_custom_job(name=custom_job_name)
       else:
         logging.error('Request failed after %s retries.',
                       _CONNECTION_ERROR_RETRY_LIMIT)
