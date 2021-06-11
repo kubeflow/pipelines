77c1de8152d8a753db9de371797d85be8708dfed
Yuan (Bob) Gong <4957653+Bobgy@users.noreply.github.com>
2021-06-11 21:39:52 +0800

    fix(launcher): duplicate input artifacts logged (#5836)
    * fix(launcher: input artifact logging when more than one
    
    * fix
    
    * fix
    
    * disable presubmit v2 go test

--- samples/test/lightweight_python_functions_v2_pipeline.py
+++ samples/test/lightweight_python_functions_v2_pipeline.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Sample pipeline for passing data in KFP v2."""
+from typing import Dict, List
+
 from kfp import dsl
 from kfp import components
 from kfp.components import InputPath, OutputPath
@@ -30,22 +32,38 @@ def preprocess(
     # `Dataset`.
     output_dataset_two_path: OutputPath('Dataset'),
     # A locally accessible filepath for an output parameter of type string.
-    output_parameter_path: OutputPath(str)
+    output_parameter_path: OutputPath(str),
+    # A locally accessible filepath for an output parameter of type bool.
+    output_bool_parameter_path: OutputPath(bool),
+    # A locally accessible filepath for an output parameter of type dict.
+    output_dict_parameter_path: OutputPath(Dict[str, int]),
+    # A locally accessible filepath for an output parameter of type list.
+    output_list_parameter_path: OutputPath(List[str]),
 ):
-    '''Dummy preprocessing step'''
+  """Dummy preprocessing step"""
 
-    # Use Dataset.path to access a local file path for writing.
-    # One can also use Dataset.uri to access the actual URI file path.
-    with open(output_dataset_one.path, 'w') as f:
-        f.write(message)
+  # Use Dataset.path to access a local file path for writing.
+  # One can also use Dataset.uri to access the actual URI file path.
+  with open(output_dataset_one.path, 'w') as f:
+    f.write(message)
 
-    # OutputPath is used to just pass the local file path of the output artifact
-    # to the function.
-    with open(output_dataset_two_path, 'w') as f:
-        f.write(message)
+  # OutputPath is used to just pass the local file path of the output artifact
+  # to the function.
+  with open(output_dataset_two_path, 'w') as f:
+    f.write(message)
 
-    with open(output_parameter_path, 'w') as f:
-        f.write(message)
+  with open(output_parameter_path, 'w') as f:
+    f.write(message)
+
+  with open(output_bool_parameter_path, 'w') as f:
+    f.write(str(True))  # use either `str()` or `json.dumps()` for bool values.
+
+  import json
+  with open(output_dict_parameter_path, 'w') as f:
+    f.write(json.dumps({'A': 1, 'B': 2}))
+
+  with open(output_list_parameter_path, 'w') as f:
+    f.write(json.dumps(['a', 'b', 'c']))
 
 
 @component
@@ -61,44 +79,51 @@ def train(
     # Use Output[T] to get a metadata-rich handle to the output artifact
     # of type `Dataset`.
     model: Output[Model],
+    # An input parameter of type bool.
+    input_bool: bool,
+    # An input parameter of type dict.
+    input_dict: Dict[str, int],
+    # An input parameter of type List[str].
+    input_list: List[str],
     # An input parameter of type int with a default value.
-    num_steps: int = 100
+    num_steps: int = 100,
 ):
-    '''Dummy Training step'''
-    with open(dataset_one_path, 'r') as input_file:
-        dataset_one_contents = input_file.read()
+  """Dummy Training step"""
+  with open(dataset_one_path, 'r') as input_file:
+    dataset_one_contents = input_file.read()
 
-    with open(dataset_two.path, 'r') as input_file:
-        dataset_two_contents = input_file.read()
+  with open(dataset_two.path, 'r') as input_file:
+    dataset_two_contents = input_file.read()
 
-    line = "dataset_one_contents: {} || dataset_two_contents: {} || message: {}\n".format(
-        dataset_one_contents, dataset_two_contents, message
-    )
+  line = (f'dataset_one_contents: {dataset_one_contents} || '
+          f'dataset_two_contents: {dataset_two_contents} || '
+          f'message: {message} || '
+          f'input_bool: {input_bool}, type {type(input_bool)} || '
+          f'input_dict: {input_dict}, type {type(input_dict)} || '
+          f'input_list: {input_list}, type {type(input_list)} \n')
 
-    with open(model.path, 'w') as output_file:
-        for i in range(num_steps):
-            output_file.write("Step {}\n{}\n=====\n".format(i, line))
+  with open(model.path, 'w') as output_file:
+    for i in range(num_steps):
+      output_file.write('Step {}\n{}\n=====\n'.format(i, line))
 
-    # Use `model` to get a Model artifact, which has a .metadata dictionary
-    # to store arbitrary metadata for the output artifact.
-    model.metadata['accuracy'] = 0.9
-
+  # Use model.get() to get a Model artifact, which has a .metadata dictionary
+  # to store arbitrary metadata for the output artifact.
+  model.metadata['accuracy'] = 0.9
 
 
 @dsl.pipeline(pipeline_root='dummy_root', name='my-test-pipeline-beta')
 def pipeline(message: str = 'message'):
-    preprocess_task = preprocess(message=message)
-    train_task = train(
-        dataset_one=preprocess_task.outputs['output_dataset_one'],
-        dataset_two=preprocess_task.outputs['output_dataset_two'],
-        message=preprocess_task.outputs['output_parameter'],
-        num_steps=5
-    )
+  preprocess_task = preprocess(message=message)
+  train_task = train(
+      dataset_one=preprocess_task.outputs['output_dataset_one'],
+      dataset_two=preprocess_task.outputs['output_dataset_two'],
+      message=preprocess_task.outputs['output_parameter'],
+      input_bool=preprocess_task.outputs['output_bool_parameter'],
+      input_dict=preprocess_task.outputs['output_dict_parameter'],
+      input_list=preprocess_task.outputs['output_list_parameter'],
+      num_steps=5)
 
 
 if __name__ == '__main__':
-    compiler.Compiler().compile(
-        pipeline_func=pipeline,
-        pipeline_root='dummy_root',
-        output_path=__file__ + '.json'
-    )
+  compiler.Compiler().compile(
+      pipeline_func=pipeline, package_path=__file__.replace('.py', '.json'))
--- samples/test/lightweight_python_functions_v2_pipeline_test.py
+++ samples/test/lightweight_python_functions_v2_pipeline_test.py
@@ -12,13 +12,103 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import unittest
+from pprint import pprint
+import kfp_server_api
+import kfp.dsl as dsl
+
 from .lightweight_python_functions_v2_pipeline import pipeline
-from .util import run_pipeline_func, TestCase
+from .util import run_pipeline_func, TestCase, KfpMlmdClient
 
 
-def verify(run, run_id: str, **kwargs):
-    assert run.status == 'Succeeded'
-    # TODO: verify output and MLMD artifacts.
+def verify(
+    run: kfp_server_api.ApiRun, mlmd_connection_config, argo_workflow_name: str,
+    **kwargs
+):
+    t = unittest.TestCase()
+    t.maxDiff = None  # we always want to see full diff
+    t.assertEqual(run.status, 'Succeeded')
+    client = KfpMlmdClient(mlmd_connection_config=mlmd_connection_config)
+    tasks = client.get_tasks(argo_workflow_name=argo_workflow_name)
+
+    task_names = [*tasks.keys()]
+    t.assertCountEqual(task_names, ['preprocess', 'train'], 'task names')
+    pprint(tasks)
+
+    preprocess = tasks['preprocess']
+    train = tasks['train']
+    pprint(preprocess.get_dict())
+    t.assertEqual(
+        {
+            'inputs': {
+                'artifacts': [],
+                'parameters': {
+                    'message': 'message'
+                }
+            },
+            'name': 'preprocess',
+            'outputs': {
+                'artifacts': [{
+                    'metadata': {},
+                    'name': 'output_dataset_one',
+                    'type': 'system.Dataset'
+                }, {
+                    'metadata': {},
+                    'name': 'output_dataset_two',
+                    'type': 'system.Dataset'
+                }],
+                'parameters': {
+                    'output_bool_parameter': 'True',
+                    'output_dict_parameter': '{"A": 1, "B": 2}',
+                    'output_list_parameter': '["a", "b", "c"]',
+                    'output_parameter': 'message'
+                }
+            },
+            'type': 'kfp.ContainerExecution'
+        },
+        preprocess.get_dict(),
+    )
+    t.assertEqual(
+        {
+            'inputs': {
+                'artifacts': [{
+                    'metadata': {},
+                    'name': 'output_dataset_one',
+                    'type': 'system.Dataset'
+                }, {
+                    'metadata': {},
+                    'name': 'output_dataset_two',
+                    'type': 'system.Dataset'
+                }],
+                'parameters': {
+                    'input_bool': 'True',
+                    'input_dict': '{"A": 1, "B": 2}',
+                    'input_list': '["a", "b", "c"]',
+                    'message': 'message',
+                    'num_steps': 5
+                }
+            },
+            'name': 'train',
+            'outputs': {
+                'artifacts': [{
+                    'metadata': {
+                        'accuracy': 0.9
+                    },
+                    'name': 'model',
+                    'type': 'system.Model'
+                }],
+                'parameters': {}
+            },
+            'type': 'kfp.ContainerExecution'
+        },
+        train.get_dict(),
+    )
 
 
-run_pipeline_func([TestCase(pipeline_func=pipeline, verify_func=verify)])
+run_pipeline_func([
+    TestCase(
+        pipeline_func=pipeline,
+        verify_func=verify,
+        mode=dsl.PipelineExecutionMode.V2_COMPATIBLE
+    ),
+])
