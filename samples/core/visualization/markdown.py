1b7f67eba7ed5c7e126b718d5ba226ee5a8eb2ab
James Liu <jamxl@google.com>
2021-06-28 15:32:36 -0700

    feat(sample) Add markdown visualization example for v1 and v2 compatible

---
+++ samples/core/visualization/markdown.py
@@ -0,0 +1,61 @@
+# Copyright 2021 The Kubeflow Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import kfp.dsl as dsl
+import kfp.components as comp
+
+# Advanced function
+# Demonstrates imports, helper functions and multiple outputs
+from typing import NamedTuple
+
+
+def markdown_visualization() -> NamedTuple('VisualizationOutput', [('mlpipeline_ui_metadata', 'UI_metadata')]):
+    import json
+
+    # Exports a sample tensorboard:
+    metadata = {
+        'outputs': [
+            {
+                # Markdown that is hardcoded inline
+                'storage': 'inline',
+                'source': '# Inline Markdown\n[A link](https://www.kubeflow.org/)',
+                'type': 'markdown',
+            },
+            {
+                # Markdown that is read from a file
+                'source': 'https://raw.githubusercontent.com/kubeflow/pipelines/master/README.md',
+                # Alternatively, use Google Cloud Storage for sample.
+                # 'source': 'gs://jamxl-kfp-bucket/v2-compatible/markdown/markdown_example.md',
+                'type': 'markdown',
+            }]
+    }
+
+    from collections import namedtuple
+    divmod_output = namedtuple('VisualizationOutput', [
+                               'mlpipeline_ui_metadata'])
+    return divmod_output(json.dumps(metadata))
+
+
+markdown_visualization_op = comp.func_to_container_op(
+    markdown_visualization, base_image='tensorflow/tensorflow:1.11.0-py3')
+
+
+@dsl.pipeline(
+    name='calculation-pipeline',
+    description='A toy pipeline that performs arithmetic calculations.'
+)
+def markdown_pipeline():
+    # Passing a task output reference as operation arguments
+    # For an operation with a single return value, the output reference can be accessed using `task.output` or `task.outputs['output_name']` syntax
+    markdown_visualization_task = markdown_visualization_op()
