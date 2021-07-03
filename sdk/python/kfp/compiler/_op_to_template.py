f44ce55a9616c41cf5a003ee147cfd3348daa7fc
NikeNano <niklas.sven.hansson@gmail.com>
2021-07-03 15:44:41 +0200

    clean up

--- sdk/python/kfp/compiler/_op_to_template.py
+++ sdk/python/kfp/compiler/_op_to_template.py
@@ -304,7 +304,8 @@ def _op_to_template(op: BaseOp):
     if isinstance(op, dsl.ContainerOp) and ('resources' in op.container.keys()):
         for setting, val in op.container['resources'].items():
             for resource, param in val.items():
-                if (resource in ['cpu', 'memory', 'amd.com/gpu', 'nvidia.com/gpu'] or re.match('^{{inputs.parameters.*}}$', resource)) and re.match('^{{inputs.parameters.*}}$', str(param)psamples/core/resource_spec/runtime_resource_request.py):
+                if (resource in ['cpu', 'memory', 'amd.com/gpu', 'nvidia.com/gpu'] or re.match('^{{inputs.parameters.*}}$', resource))\
+                    and re.match('^{{inputs.parameters.*}}$', str(param)):
                     if not 'containers' in podSpecPatch:
                         podSpecPatch = {'containers':[{'name':'main', 'resources':{}}]}
                     if setting not in podSpecPatch['containers'][0]['resources']:
