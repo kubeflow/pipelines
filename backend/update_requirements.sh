8f5421cd57fd58150e8633129c7a7001aa0264da
James Liu <37026441+zijianjoy@users.noreply.github.com>
2021-07-13 12:47:58 -0700

    fix(frontend): TFX artifact visualization update. Fix tensorflow/tfx#3933 (#5999)
    * fix(frontend): TFX artifact visualization update
    
    * revert requirements change
    
    * docker image change
    
    * outputartifact adjust
    
    * comment unused
    
    * statisticGen fix
    
    * sync config
    
    * fix teest

--- backend/update_requirements.sh
+++ backend/update_requirements.sh
@@ -4,7 +4,7 @@
 
 set -euo pipefail
 IMAGE=${1:-"python:3.7"}
-docker run --interactive --rm "$IMAGE" sh -c '
+docker run --interactive --rm  --entrypoint "" "$IMAGE" sh -c '
   python3 -m pip install pip setuptools --upgrade --quiet
   python3 -m pip install pip-tools==5.4.0 --quiet
   pip-compile --verbose --output-file - -
