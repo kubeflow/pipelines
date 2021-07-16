765919d4076b7b7e5e6704683bc30b5c181723c1
sina chavoshi <sina.chavoshi@gmail.com>
2021-07-16 17:21:10 +0000

    Make the package_data path relative

--- components/google-cloud/setup.py
+++ components/google-cloud/setup.py
@@ -23,9 +23,14 @@ from setuptools import find_packages
 from setuptools import setup
 
 relative_directory = os.path.relpath(os.path.dirname(os.path.abspath(__file__)))
+GCPC_DIR_NAME = "google_cloud_pipeline_components"
+relative_data_path = os.path.join(relative_directory, GCPC_DIR_NAME)
+
 loader = importlib.machinery.SourceFileLoader(
     fullname="version",
-    path=os.path.join(relative_directory, "google_cloud_pipeline_components/version.py"),
+    path=os.path.join(
+        relative_directory, "google_cloud_pipeline_components/version.py"
+    ),
 )
 version = types.ModuleType(loader.name)
 loader.exec_module(version)
@@ -37,7 +42,8 @@ setup(
     " components that allow users to take their experience from AI Platform"
     " SDK and other Google Cloud services and create a corresponding pipeline"
     " using KFP or Managed Pipelines.",
-    url="https://github.com/kubeflow/pipelines/tree/master/components/google-cloud",
+    url=
+    "https://github.com/kubeflow/pipelines/tree/master/components/google-cloud",
     author="The Google Cloud Pipeline Components authors",
     author_email="google-cloud-pipeline-components@google.com",
     license="Apache License 2.0",
@@ -65,12 +71,15 @@ setup(
     ],
     package_dir={
         "google_cloud_pipeline_components":
-            os.path.join(relative_directory, "google_cloud_pipeline_components")
+            os.path.join(
+                relative_directory, "google_cloud_pipeline_components"
+            )
     },
     packages=find_packages(where=relative_directory),
-    package_data = {
-        'google_cloud_pipeline_components': [
-            x.replace("google_cloud_pipeline_components/","") for x in glob(
-                '**/*.yaml',recursive=True)
-        ]},
+    package_data={
+        GCPC_DIR_NAME: [
+            x.replace(relative_data_path + "/", "")
+            for x in glob(relative_directory + '/**/*.yaml', recursive=True)
+        ]
+    },
 )
