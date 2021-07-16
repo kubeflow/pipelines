32c7cd7f8d26e4d05aa7714383518c45451d24d0
sina chavoshi <sina.chavoshi@gmail.com>
2021-07-16 07:44:50 +0000

    use a genrilized method to find yaml files.

--- components/google-cloud/setup.py
+++ components/google-cloud/setup.py
@@ -17,6 +17,7 @@ import importlib
 import os
 import types
 import dependencies
+from glob import glob
 
 from setuptools import find_packages
 from setuptools import setup
@@ -67,5 +68,9 @@ setup(
             os.path.join(relative_directory, "google_cloud_pipeline_components")
     },
     packages=find_packages(where=relative_directory),
-    package_data = {'google_cloud_pipeline_components': ['*/*.yaml','**/**/*.yaml','**/**/**/*.yaml']},
+    package_data = {
+        'google_cloud_pipeline_components': [
+            x.replace("google_cloud_pipeline_components/","") for x in glob(
+                '**/*.yaml',recursive=True)
+        ]},
 )
