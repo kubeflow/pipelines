721b6fb6f8641a9978049df4e0fcde2e14611ad0
Kelvin S. do Prado <kelvin.prado@luizalabs.com>
2021-07-17 21:07:04 -0300

    Remove unnecessary whitespace

--- frontend/src/components/Trigger.tsx
+++ frontend/src/components/Trigger.tsx
@@ -373,7 +373,7 @@ export default class Trigger extends React.Component<TriggerProps, TriggerState>
                 }
                 label={
                   <span>
-                    Allow editing cron expression. ( format is specified{' '}
+                    Allow editing cron expression. (format is specified{' '}
                     <ExternalLink href='https://pkg.go.dev/github.com/robfig/cron#hdr-CRON_Expression_Format'>
                       here
                     </ExternalLink>
