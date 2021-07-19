9bb80efedf5d339b2daed12d0a547b4361536c84
Chen Sun <chensun@users.noreply.github.com>
2021-07-19 13:21:14 -0700

    chore: release KFP SDK and v2 launcher 1.6.5 (#6084)

--- sdk/python/kfp/compiler/v2_compat.py
+++ sdk/python/kfp/compiler/v2_compat.py
@@ -23,7 +23,7 @@ from kfp.v2 import compiler
 
 from kubernetes import client as k8s_client
 
-_DEFAULT_LAUNCHER_IMAGE = "gcr.io/ml-pipeline/kfp-launcher:1.6.4"
+_DEFAULT_LAUNCHER_IMAGE = "gcr.io/ml-pipeline/kfp-launcher:1.6.5"
 
 
 def update_op(op: dsl.ContainerOp,
