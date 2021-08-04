45ff47e978353b0c7470dd2b6f10a07f46143110
Ajay Gopinathan <ajaygopinathan@google.com>
2021-08-03 19:53:44 -0700

    undo signed url change.

--- v2/test/sample_test.py
+++ v2/test/sample_test.py
@@ -13,12 +13,9 @@
 # limitations under the License.
 
 # %%
-import datetime
-import json
 import os
 from typing import Dict, List, Optional
 
-from google.cloud import storage
 from kubernetes import client as k8s_client
 import yaml
 
@@ -31,29 +28,12 @@ with open(SAMPLES_CONFIG_PATH, 'r') as stream:
     SAMPLES_CONFIG = yaml.safe_load(stream)
 
 download_gcs_tgz = kfp.components.load_component_from_file(
-    'components/download_gcs_tgz.yaml'
-)
+    'components/download_gcs_tgz.yaml')
 run_sample = kfp.components.load_component_from_file(
-    'components/run_sample.yaml'
-)
+    'components/run_sample.yaml')
 kaniko = kfp.components.load_component_from_file('components/kaniko.yaml')
 
 PIPELINE_TIME_OUT = 40 * 60  # 40 minutes
-
-
-def _get_signed_url(gcs_location: str):
-    print('Getting signed URL from: {}'.format(gcs_location))
-    storage_client = storage.Client()
-    bucket = storage.Bucket.from_string(gcs_location, storage_client)
-    blob = storage.Blob.from_string(gcs_location, storage_client)
-
-    result = blob.generate_signed_url(
-        version="v4",
-        # This URL is valid for 60 minutes
-        expiration=datetime.timedelta(minutes=60),
-        method="GET")
-    print('Generated signed URL: {}'.format(gcs_location))
-    return result
 
 
 @kfp.dsl.pipeline(name='v2 sample test')
@@ -63,7 +43,8 @@ def v2_sample_test(
     image_registry: 'URI' = 'gcr.io/ml-pipeline-test',
     kfp_host: 'URI' = 'http://ml-pipeline:8888',
     samples_config: List[Dict] = SAMPLES_CONFIG,
-    kfp_package_path: 'URI' = 'git+https://github.com/kubeflow/pipelines#egg=kfp&subdirectory=sdk/python'
+    kfp_package_path:
+    'URI' = 'git+https://github.com/kubeflow/pipelines#egg=kfp&subdirectory=sdk/python'
 ):
     # pipeline configs
     conf = kfp.dsl.get_pipeline_conf()
@@ -71,10 +52,8 @@ def v2_sample_test(
         PIPELINE_TIME_OUT
     )  # add timeout to avoid pipelines stuck in running leak indefinetely
 
-    download_src_op = download_gcs_tgz(
-        gcs_path=context
-    ).set_cpu_limit('0.5').set_memory_limit('500Mi'
-                                           ).set_display_name('download_src')
+    download_src_op = download_gcs_tgz(gcs_path=context).set_cpu_limit(
+        '0.5').set_memory_limit('500Mi').set_display_name('download_src')
     download_src_op.execution_options.caching_strategy.max_cache_staleness = "P0D"
 
     def build_image(name: str, dockerfile: str) -> kfp.dsl.ContainerOp:
@@ -108,11 +87,9 @@ def v2_sample_test(
     # build v2 engine images
     build_kfp_launcher_v2_op = build_image(
         name='kfp-launcher-v2',
-        dockerfile='v2/container/launcher-v2/Dockerfile'
-    )
+        dockerfile='v2/container/launcher-v2/Dockerfile')
     build_kfp_driver_op = build_image(
-        name='kfp-driver', dockerfile='v2/container/driver/Dockerfile'
-    )
+        name='kfp-driver', dockerfile='v2/container/driver/Dockerfile')
 
     # run test samples in parallel
     with kfp.dsl.ParallelFor(samples_config) as sample:
@@ -129,8 +106,10 @@ def v2_sample_test(
         run_sample_op.set_display_name(f'sample_{sample.name}')
         run_sample_op.set_retry(1, policy='Always')
 
-        run_sample_op.container.add_env_variable(k8s_client.V1EnvVar(
-            name='KFP_PACKAGE_PATH', value=kfp_package_path))
+        run_sample_op.container.add_env_variable(
+            k8s_client.V1EnvVar(name='KFP_PACKAGE_PATH',
+                                value=kfp_package_path))
+
 
 def main(
     context: str,
@@ -138,15 +117,14 @@ def main(
     gcr_root: str,
     gcs_root: str,
     experiment: str = 'v2_sample_test',
-    kfp_package_path: str = 'git+https://github.com/kubeflow/pipelines#egg=kfp&subdirectory=sdk/python'
+    kfp_package_path:
+    str = 'git+https://github.com/kubeflow/pipelines#egg=kfp&subdirectory=sdk/python'
 ):
     client = kfp.Client(host=host)
     client.create_experiment(
         name=experiment,
         description='An experiment with Kubeflow Pipelines v2 sample test runs.'
     )
-    if kfp_package_path.startswith('gs://'):
-        kfp_package_path = _get_signed_url(kfp_package_path)
 
     print('Using KFP package path: {}'.format(kfp_package_path))
     run_result = client.create_run_from_pipeline_func(
