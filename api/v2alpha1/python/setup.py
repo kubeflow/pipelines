f211d456482b00eb77408aa5ebc9998566ef95cf
Yaqi Ji <jiyaqi0712@gmail.com>
2021-08-05 10:04:24 -0700

    Update setup.py

--- api/v2alpha1/python/setup.py
+++ api/v2alpha1/python/setup.py
@@ -22,7 +22,7 @@ except ImportError:
   from shutil import which as find_executable
 
 NAME = "kfp-pipeline-spec"
-VERSION = "0.1.8"
+VERSION = "0.1.9"
 
 PROTO_DIR = os.path.realpath(
     os.path.join(os.path.dirname(__file__), os.pardir))
