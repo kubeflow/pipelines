49ae9e6f32c5088b5d51d11fcc242a424a37cbd8
Ajay Gopinathan <ajaygopinathan@google.com>
2021-08-09 17:27:01 -0700

    merge and update.

--- sdk/python/tests/compiler/compiler_tests.py
+++ sdk/python/tests/compiler/compiler_tests.py
@@ -318,6 +318,8 @@ class TestCompiler(parameterized.TestCase):
     tmpdir = tempfile.mkdtemp()
     try:
       target_yaml = os.path.join(tmpdir, file_base_name + '-pipeline.yaml')
+      if file_base_name == 'uri_artifacts':
+        target_yaml = os.path.join(test_data_dir, file_base_name + '.yaml')
       cmds = ['dsl-compile', '--py', py_file, '--output', target_yaml]
       if mode:
         cmds.extend(['--mode', mode])
