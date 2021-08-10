9cd9c375e8846912f3c3441f657751812ad55128
James Liu <jamxl@google.com>
2021-08-10 11:33:18 -0700

    npm run format

--- frontend/src/stories/Button.stories.tsx
+++ frontend/src/stories/Button.stories.tsx
@@ -11,7 +11,7 @@ export default {
   },
 } as ComponentMeta<typeof Button>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
--- frontend/src/stories/Button.tsx
+++ frontend/src/stories/Button.tsx
@@ -37,7 +37,7 @@ export const Button = ({
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
-      type="button"
+      type='button'
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
       style={{ backgroundColor }}
       {...props}
