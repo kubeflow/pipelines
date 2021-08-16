1f06ce6f99d5c4b529730b7d8ac122991fc0d338
sina chavoshi <sina.chavoshi@gmail.com>
2021-08-16 21:45:29 +0000

    setting a tighter lower bound on both dependancies

--- components/google-cloud/dependencies.py
+++ components/google-cloud/dependencies.py
@@ -16,8 +16,8 @@
 
 def make_required_install_packages():
     return [
-        "google-api-core<2dev,>=1.21.0",
-        "google-cloud-aiplatform>=1.1.1",
+        "google-api-core<2dev,>=1.26.0",
+        "google-cloud-aiplatform>=1.3",
         "kfp>=1.4.0,<2.0.0",
     ]
 
