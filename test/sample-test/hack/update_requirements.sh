1204cf2740f75b41b643cd13c9846d6834e1a69d
Yuan Gong <gongyuan94@gmail.com>
2021-08-16 07:29:06 +0000

    update cloud image

--- test/sample-test/hack/update_requirements.sh
+++ test/sample-test/hack/update_requirements.sh
@@ -3,4 +3,4 @@
 set -ex
 
 cat ../../sdk/python/requirements.in ../../backend/api/python_http_client/requirements.txt ../../backend/requirements.in requirements.in | \
-    ../../backend/update_requirements.sh google/cloud-sdk:315.0.0 >requirements.txt
+    ../../backend/update_requirements.sh google/cloud-sdk:352.0.0 >requirements.txt
