2de13a3650c2719150c01d8363748356193e9051
sina chavoshi <sina.chavoshi@gmail.com>
2021-08-16 15:18:42 -0700

    fix(components/google-cloud): Google cloud pipeline components 0.1.4 (#6344)
    * force google-api-core version for read the docs.
    
    * force version for read the docs
    
    * swap order of deployment for kfp and MB
    
    * explicity add dependancies
    
    * setting a tighter lower bound on both dependancies
    
    * google google-cloud-aiplatform version number

--- components/google-cloud/dependencies.py
+++ components/google-cloud/dependencies.py
@@ -16,8 +16,9 @@
 
 def make_required_install_packages():
     return [
+        "google-api-core<2dev,>=1.26.0",
+        "google-cloud-aiplatform>=1.3.0",
         "kfp>=1.4.0,<2.0.0",
-        "google-cloud-aiplatform>=1.1.1",
     ]
 
 
