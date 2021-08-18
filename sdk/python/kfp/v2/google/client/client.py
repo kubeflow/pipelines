d9ce484e7e92af158999d26cf37db7d50eedd159
Toshiya Kawasaki <kawasakitoshiya@gmail.com>
2021-08-18 15:26:37 +0900

    feat(google client): Enable to specify the service account to proxy function of scheduler in google client sdk (#6013)
    * google sdk: Enable to specify the service account of proxy function and cloud scheduler job
    
    * rename service_account_for_schedule to cloud_scheduler_service_account

--- sdk/python/kfp/v2/google/client/client.py
+++ sdk/python/kfp/v2/google/client/client.py
@@ -368,6 +368,7 @@ class AIPlatformClient(object):
       service_account: Optional[str] = None,
       enable_caching: Optional[bool] = None,
       app_engine_region: Optional[str] = None,
+      cloud_scheduler_service_account: Optional[str] = None,
     ) -> dict:
     """Creates schedule for compiled pipeline file.
 
@@ -399,6 +400,9 @@ class AIPlatformClient(object):
         If set, the setting applies to all tasks in the pipeline -- overrides
         the compile time settings.
       app_engine_region: The region that cloud scheduler job is created in.
+      cloud_scheduler_service_account: The service account that Cloud Scheduler job and the proxy cloud function use.
+        this should have permission to call AI Platform API and the proxy function.
+        If not specified, the functions uses the App Engine default service account.
 
     Returns:
       Created Google Cloud Scheduler Job object dictionary.
@@ -417,4 +421,5 @@ class AIPlatformClient(object):
         parameter_values=parameter_values,
         pipeline_root=pipeline_root,
         service_account=service_account,
-        app_engine_region=app_engine_region)
+        app_engine_region=app_engine_region,
+        cloud_scheduler_service_account=cloud_scheduler_service_account)
