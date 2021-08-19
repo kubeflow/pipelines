e59a1311e77a4eb345fcb2774b458568e3dee71e
Yuan (Bob) Gong <4957653+Bobgy@users.noreply.github.com>
2021-08-19 09:52:43 +0800

    Update parameterized_tfx_oss.py

--- samples/core/parameterized_tfx_oss/parameterized_tfx_oss.py
+++ samples/core/parameterized_tfx_oss/parameterized_tfx_oss.py
@@ -92,7 +92,7 @@ def _create_pipeline(
       model_specs=[
           tfma.ModelSpec(
               signature_name='serving_default', label_key='tips_xf',
-              preprocessing_function_names=['tft_layer'])
+              preprocessing_function_names=['preprocessing_function_names'])
       ],
       metrics_specs=[
           tfma.MetricsSpec(
