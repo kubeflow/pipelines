702e48b9ec17ddd39356c93ffbeaeeaee1270cc5
Matts966 <28551465+Matts966@users.noreply.github.com>
2021-08-19 22:23:53 +0900

    fix(backend): recognize "sidecar.istio.io/inject" annotation to use Istio service mesh optionally. Fixes #6200 (#6391)
    * Recognize "sidecar.istio.io/inject" annotation to use istio service mesh optionally
    
    * Fix return to continue
    
    Co-authored-by: Matts966 <Matts966@users.noreply.github.com>

--- backend/src/apiserver/resource/resource_manager.go
+++ backend/src/apiserver/resource/resource_manager.go
@@ -381,8 +381,8 @@ func (r *ResourceManager) CreateRun(ctx context.Context, apiRun *api.Run) (*mode
 
 	r.setDefaultServiceAccount(&workflow, apiRun.GetServiceAccount())
 
-	// Disable istio sidecar injection
-	workflow.SetAnnotationsToAllTemplates(util.AnnotationKeyIstioSidecarInject, util.AnnotationValueIstioSidecarInjectDisabled)
+	// Disable istio sidecar injection if not specified
+	workflow.SetAnnotationsToAllTemplatesIfKeyNotExist(util.AnnotationKeyIstioSidecarInject, util.AnnotationValueIstioSidecarInjectDisabled)
 	// Add a KFP specific label for cache service filtering. The cache_enabled flag here is a global control for whether cache server will
 	// receive targeting pods. Since cache server only receives pods in step level, the resource manager here will set this global label flag
 	// on every single step/pod so the cache server can understand.
@@ -729,8 +729,8 @@ func (r *ResourceManager) CreateJob(ctx context.Context, apiJob *api.Job) (*mode
 
 	r.setDefaultServiceAccount(&workflow, apiJob.GetServiceAccount())
 
-	// Disable istio sidecar injection
-	workflow.SetAnnotationsToAllTemplates(util.AnnotationKeyIstioSidecarInject, util.AnnotationValueIstioSidecarInjectDisabled)
+	// Disable istio sidecar injection if not specified
+	workflow.SetAnnotationsToAllTemplatesIfKeyNotExist(util.AnnotationKeyIstioSidecarInject, util.AnnotationValueIstioSidecarInjectDisabled)
 
 	swfGeneratedName, err := toSWFCRDResourceGeneratedName(apiJob.Name)
 	if err != nil {
