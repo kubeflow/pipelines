d03277fe4ed79f2bc4780ca1cdb6bab9469eeb61
James Liu <37026441+zijianjoy@users.noreply.github.com>
2021-08-31 10:04:03 -0700

    feat(frontend): SubDag Node and mock samples. Partial #6419. Fix #6302. Fix #6398 (#6420)
    * feat(frontend): SubDag Node and mock samples
    
    * fix import
    
    * comment address
    
    * from aside to div
    
    * address comments

---
+++ frontend/src/components/graph/SubDagNode.tsx
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2021 The Kubeflow Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import CropFreeIcon from '@material-ui/icons/CropFree';
+import React from 'react';
+import { Handle, Position } from 'react-flow-renderer';
+import { FlowElementDataBase } from './Constants';
+
+interface SubDagNodeProps {
+  id: string;
+  data: FlowElementDataBase;
+  // status: ExecutionNodeStatus;
+  // tooltip: string;
+  // isSelected: boolean;
+}
+
+function SubDagNode({ id, data }: SubDagNodeProps) {
+  return (
+    <>
+      <button
+        title={data.label}
+        className='focus:border-blue-500 rounded-xl border-gray-300 border-2 border-dashed'
+      >
+        <div className='container items-stretch h-24 w-80 relative grid '>
+          <div className='flex justify-items-center place-self-center self-center relative h-14 w-72 '>
+            <div className=' flex justify-between flex-row transform h-14 transition relative overflow:hidden hover:scale-105 bg-white shadow-lg rounded-xl  w-72 z-10'>
+              <div className='sm:px-4 sm:py-4 rounded-l-xl justify-items-center hover:shadow-inner '>
+                <CropFreeIcon style={{ color: '#63B3ED' }} />
+              </div>
+              <div className='sm:px-6 sm:py-4 w-60 flex flex-col justify-center items-center '>
+                <span className='w-full truncate' id={id}>
+                  {data.label}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </button>
+      <Handle
+        type='target'
+        position={Position.Top}
+        isValidConnection={connection => connection.source === 'some-id'}
+        onConnect={params => console.log('handle onConnect', params)}
+        style={{ background: '#000', width: '4px', height: '6px' }}
+      />
+      <Handle
+        type='source'
+        position={Position.Bottom}
+        isValidConnection={connection => connection.source === 'some-id'}
+        onConnect={params => console.log('handle onConnect', params)}
+        style={{ background: '#000', width: '4px', height: '6px' }}
+      />
+    </>
+  );
+}
+
+export default SubDagNode;
