4d038e04b9cd8357356bbb632440bf4c385df78b
Chen Sun <chensun@users.noreply.github.com>
2021-09-03 16:29:10 -0700

    chore: Release KFP SDK and v2 launcher 1.8.0 (#6505)

--- sdk/python/kfp/__init__.py
+++ sdk/python/kfp/__init__.py
@@ -16,7 +16,7 @@
 # https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
-__version__ = '1.7.2'
+__version__ = '1.8.0'
 
 from . import components
 from . import containers
