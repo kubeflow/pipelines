9326d1d981cf286d71db57a555d8baee35112c48
sina chavoshi <sina.chavoshi@gmail.com>
2021-09-24 22:09:21 +0000

    change custom job name to custom_training_job_op

--- components/google-cloud/tests/experimental/custom_job/test_custom_job.py
+++ components/google-cloud/tests/experimental/custom_job/test_custom_job.py
@@ -122,7 +122,7 @@ implementation:
             }
         }
         component_factory_function = self._create_a_container_based_component()
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function)
         self.assertDictEqual(custom_job_spec.component_spec.to_dict(),
                              expected_results)
@@ -174,7 +174,7 @@ implementation:
             }
         }
         component_factory_function = self._create_a_pytnon_based_component()
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function)
 
         self.assertDictContainsSubset(custom_job_spec.component_spec.to_dict(),
@@ -218,7 +218,7 @@ implementation:
                 }
             }
         }
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function, worker_pool_specs=worker_pool_spec)
 
         self.assertDictContainsSubset(
@@ -253,7 +253,7 @@ implementation:
                 }
             }
         }
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function, worker_pool_specs=python_package_spec)
 
         self.assertDictContainsSubset(
@@ -287,7 +287,7 @@ implementation:
                 }
             }
         }
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function,
             accelerator_type="test_accelerator_type",
             accelerator_count=2)
@@ -323,7 +323,7 @@ implementation:
                 }
             }
         }
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function, replica_count=2)
 
         self.assertDictContainsSubset(
@@ -357,7 +357,7 @@ implementation:
                 }
             }
         }
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function, replica_count=2)
 
         self.assertDictContainsSubset(
@@ -390,7 +390,7 @@ implementation:
                 }
             }
         }
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function, timeout=2)
 
         self.assertDictContainsSubset(
@@ -424,7 +424,7 @@ implementation:
                 }
             }
         }
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function, restart_job_on_worker_restart=True)
 
         self.assertDictContainsSubset(
@@ -458,7 +458,7 @@ implementation:
                 }
             }
         }
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function, service_account='test_service_account')
 
         self.assertDictContainsSubset(
@@ -491,7 +491,7 @@ implementation:
                 }
             }
         }
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function, display_name='test_display_name')
 
         self.assertDictContainsSubset(
@@ -509,7 +509,7 @@ implementation:
             'replica_count': 2
         }]
         with self.assertRaises(ValueError):
-            custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+            custom_job_spec = custom_job.custom_training_job_op(
                 component_factory_function, worker_pool_specs=worker_pool_spec)
 
     def test_run_as_vertex_ai_custom_with_network_converts_correctly(self):
@@ -538,7 +538,7 @@ implementation:
                 }
             }
         }
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function, network='test_network')
 
         self.assertDictContainsSubset(
@@ -571,7 +571,7 @@ implementation:
                 }
             }
         }
-        custom_job_spec = custom_job.run_as_vertex_ai_custom_job(
+        custom_job_spec = custom_job.custom_training_job_op(
             component_factory_function, labels={"test_key": "test_value"})
 
         self.assertDictContainsSubset(
