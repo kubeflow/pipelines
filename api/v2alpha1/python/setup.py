3dd9e2a9b5d731b90e6003a2d1e78877343ed0bf
Chen Sun <chensun@users.noreply.github.com>
2021-10-05 00:53:18 -0700

    chore(api): release kfp-pipeline spec 0.1.12 and update golang generated code (#6684)

--- api/v2alpha1/python/setup.py
+++ api/v2alpha1/python/setup.py
@@ -24,7 +24,7 @@ except ImportError:
     from shutil import which as find_executable
 
 NAME = "kfp-pipeline-spec"
-VERSION = "0.1.11"
+VERSION = "0.1.12"
 
 PROTO_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), os.pardir))
 
