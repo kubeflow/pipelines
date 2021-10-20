f621a147777191c038c11a25820b59c2f3697872
sina chavoshi <sina.chavoshi@gmail.com>
2021-10-19 18:47:22 -0700

    fix(components/google-cloud): Adding  protobuf<3.18.0,>=3.12.0  as an explict requirement to fix readthedocs build issue.

--- components/google-cloud/dependencies.py
+++ components/google-cloud/dependencies.py
@@ -17,7 +17,10 @@
 
 def make_required_install_packages():
     return [
-        "kfp==1.8.4",
+        # Explicity add google-api-core as a dependancy to avoid conflict
+        # between kfp & aiplatform.
+        "protobuf<3.18.0,>=3.12.0",
+        "google-api-core<2dev,>=1.26.0",
         "google-cloud-aiplatform>=1.4.3",
         "google-cloud-notebooks>=0.4.0",
     ]
