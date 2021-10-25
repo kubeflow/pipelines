9b76cd558a75c28ac0e86cff6cde6e0ec90cd285
James Liu <37026441+zijianjoy@users.noreply.github.com>
2021-10-25 09:35:09 -0700

    feat(frontend): SubDAG styling for KFPv2. Fix #6419 (#6744)
    * feat(frontend): SubDAG styling for KFPv2
    
    * update button

--- frontend/src/components/graph/SubDagNode.tsx
+++ frontend/src/components/graph/SubDagNode.tsx
@@ -18,6 +18,8 @@ import CropFreeIcon from '@material-ui/icons/CropFree';
 import React from 'react';
 import { Handle, Position } from 'react-flow-renderer';
 import { SubDagFlowElementData } from './Constants';
+// import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 interface SubDagNodeProps {
   id: string;
@@ -38,26 +40,32 @@ function SubDagNode({ id, data }: SubDagNodeProps) {
     <>
       <button
         title={data.label}
-        className='focus:border-blue-500 rounded-xl border-gray-300 border-2 border-dashed'
+        className='group focus:border-blue-500 rounded-xl border-gray-300 border-2 border-dashed'
       >
-        <div className='container items-stretch h-24 w-80 relative grid '>
-          <div className='flex justify-items-center place-self-center self-center relative h-14 w-72 '>
+        <div className='container items-stretch h-24 w-72 relative grid '>
+          <div className='flex justify-center place-self-center self-center relative h-14 w-72 '>
             <div className='transition transform hover:scale-105'>
-              <div className=' flex justify-between flex-row h-14 relative overflow:hidden  bg-white shadow-lg rounded-xl  w-72 z-20'>
-                <div
-                  onClick={handleClick}
-                  className='px-4 py-4 rounded-l-xl justify-items-center hover:shadow-inner'
-                >
-                  <CropFreeIcon style={{ color: '#63B3ED' }} />
-                </div>
+              <div className=' flex justify-between flex-row h-14 relative overflow:hidden bg-white shadow-lg rounded-xl w-60 z-20'>
                 <div className='px-6 py-4 w-60 flex flex-col justify-center items-center '>
                   <span className='w-full truncate' id={id}>
                     {data.label}
                   </span>
                 </div>
               </div>
-              <div className='flex absolute top-0 overflow:hidden bg-white shadow-lg rounded-xl h-14 w-72 ml-1 mt-1 z-10'></div>
+              <div className='flex absolute top-0 overflow:hidden bg-white shadow-lg rounded-xl h-14 w-60 ml-1 mt-1 z-10'></div>
             </div>
+          </div>
+
+          <div
+            onClick={handleClick}
+            className='transition transform hover:shadow-inner hover:scale-110 flex flex-col absolute rounded-full h-9 w-9 z-30 group-focus:border-blue-500 hover:border-blue-500  border-2 bg-white -right-5 top-8 items-center justify-center justify-items-center'
+          >
+            <div className='group-focus:text-blue-500 hover:text-blue-500 text-gray-300'>
+              <CropFreeIcon style={{ fontSize: 15 }} />
+            </div>
+            {/* The following is alternative to the expand icon  */}
+            {/* <ExpandLessIcon style={{ fontSize: 14, color: '#3B82F6', opacity: 1 }}></ExpandLessIcon>
+            <ExpandMoreIcon style={{ fontSize: 14, color: '#3B82F6', opacity: 1 }}></ExpandMoreIcon> */}
           </div>
         </div>
       </button>
