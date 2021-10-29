f1bb852852b16ccf0e316037834274ab5b60aab3
Yaqi Ji <jiyaqi0712@gmail.com>
2021-10-28 22:30:34 -0700

    feat(sdk): add load_component_from_* (#6822)
    * feat(sdk): add load_component_from_*
    
    * address comments'
    :
    
    * update release notes

--- sdk/RELEASE.md
+++ sdk/RELEASE.md
@@ -4,6 +4,7 @@
 
 * Support passing parameters in v2 using google.protobuf.Value [\#6804](https://github.com/kubeflow/pipelines/pull/6804).
 * Implement experimental v2 `@component` component [\#6825](https://github.com/kubeflow/pipelines/pull/6825)
+* Add load_component_from_* for v2 [\#6822](https://github.com/kubeflow/pipelines/pull/6822)
 
 ## Breaking Changes
 
--- sdk/python/kfp/v2/components/experimental/yaml_component.py
+++ sdk/python/kfp/v2/components/experimental/yaml_component.py
@@ -13,6 +13,12 @@
 # limitations under the License.
 """Functions for loading component from yaml."""
 
+__all__ = [
+    'load_component_from_text',
+    'load_component_from_url',
+    'load_component_from_file',
+]
+
 from kfp.v2.components.experimental import base_component
 from kfp.v2.components.experimental import structures
 
@@ -27,4 +33,34 @@ class YamlComponent(base_component.BaseComponent):
 def load_component_from_text(text: str) -> base_component.BaseComponent:
     """Loads component from text."""
     return YamlComponent(
-        component_spec=structures.ComponentSpec.load_from_component_yaml(text))
+        structures.ComponentSpec.load_from_component_yaml(text))
+
+def load_component_from_file(file_path: str) -> base_component.BaseComponent:
+    """Loads component from file.
+
+    Args:
+        file_path: A string containing path to the YAML file.
+    """
+    with open(file_path, 'rb') as component_stream:
+        return load_component_from_text(component_stream)
+
+def load_component_from_url(url: str, auth=None) -> base_component.BaseComponent:
+    """Loads component from url.
+
+    Args:
+        url: A string containing path to the url containing YAML file.
+        auth: The authentication credentials necessary for url access.
+    """
+
+    if url is None:
+        raise TypeError
+
+    if url.startswith('gs://'):
+        #Replacing the gs:// URI with https:// URI (works for public objects)
+        url = 'https://storage.googleapis.com/' + url[len('gs://'):]
+
+    import requests
+    resp = requests.get(url, auth=auth)
+    resp.raise_for_status()
+
+    return load_component_from_text(resp.content)
