cfbac47825ab864e5b4801f0a579ac234ca8564a
James Liu <jamxl@google.com>
2021-11-17 15:45:26 -0800

    container install rsync

--- components/gcp/container/build_image.sh
+++ components/gcp/container/build_image.sh
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 mkdir -p ./build
+apt install rsync
 rsync -arvp --exclude=.tox "component_sdk/python"/ ./build/
 
 ../../build_image.sh -l ml-pipeline-gcp "$@"
