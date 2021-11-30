2496509317eb7716f0d3803823b556d4b5cf5d11
Yuan (Bob) Gong <4957653+Bobgy@users.noreply.github.com>
2021-11-30 21:55:30 +0800

    chore(mlmd): fix MLMD update scripts (#6984)

--- third_party/ml-metadata/update_version.sh
+++ third_party/ml-metadata/update_version.sh
@@ -9,28 +9,30 @@ echo "line endings."
 echo "MLMD version page: https://github.com/google/ml-metadata/releases"
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
-TAG_NAME="$(cat $DIR/VERSION)"
-REPO_ROOT="$DIR/../.."
+TAG_NAME="$(cat ${DIR}/VERSION)"
+REPO_ROOT="${DIR}/../.."
 
-cd $REPO_ROOT
-echo $PWD
-echo $TAG_NAME
+cd "${REPO_ROOT}"
+echo "${PWD}"
+echo "${TAG_NAME}"
 
-image_files=( "$REPO_ROOT/.cloudbuild.yaml" \
-              "$REPO_ROOT/.release.cloudbuild.yaml" \
-              "$REPO_ROOT/manifests/kustomize/base/metadata/base/metadata-grpc-deployment.yaml" \
-              "$REPO_ROOT/test/tag_for_hosted.sh" \
-              "$REPO_ROOT/v2/Makefile" \
+image_files=( "${REPO_ROOT}/.cloudbuild.yaml" \
+              "${REPO_ROOT}/.release.cloudbuild.yaml" \
+              "${REPO_ROOT}/manifests/kustomize/base/metadata/base/metadata-grpc-deployment.yaml" \
+              "${REPO_ROOT}/test/tag_for_hosted.sh" \
+              "${REPO_ROOT}/v2/Makefile" \
               )
 for i in "${image_files[@]}"
 do
-    echo "Replacing $i"
-    sed -i.bak -r "s/gcr.io\/tfx-oss-public\/ml_metadata_store_server\:[0-9.a-z-]+/gcr.io\/tfx-oss-public\/ml_metadata_store_server\:$TAG_NAME/g" "$i"
+    echo "Replacing ${i}"
+    sed -i.bak -r "s/gcr.io\/tfx-oss-public\/ml_metadata_store_server\:[0-9.a-z-]+/gcr.io\/tfx-oss-public\/ml_metadata_store_server\:${TAG_NAME}/g" "${i}"
 done
 
-requirement_files=( "$REPO_ROOT/backend/metadata_writer/requirements.in" ) 
+requirement_files=( "${REPO_ROOT}/backend/metadata_writer/requirements.in" \
+                    "${REPO_ROOT}/v2/test/requirements.txt"
+                    )
 for i in "${requirement_files[@]}"
 do
-    echo "Replacing $i"
-    sed -i.bak -r "s/ml-metadata==[0-9.a-z-]+/ml-metadata==$TAG_NAME/g" "$i"
+    echo "Replacing ${i}"
+    sed -i.bak -r "s/ml-metadata==[0-9.a-z-]+/ml-metadata==${TAG_NAME}/g" "${i}"
 done
