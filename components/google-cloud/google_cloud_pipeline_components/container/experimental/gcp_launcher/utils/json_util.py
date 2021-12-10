96eb3aa0300adb45b3084d398595e7265e7bf8bf
Xiaowen Xie <xwx@google.com>
2021-12-10 10:02:30 -0800

    feat(components/google-cloud): Accept UnmanagedContainerModel artifact in Batch Prediction component
    PiperOrigin-RevId: 415546897

--- components/google-cloud/google_cloud_pipeline_components/container/experimental/gcp_launcher/utils/json_util.py
+++ components/google-cloud/google_cloud_pipeline_components/container/experimental/gcp_launcher/utils/json_util.py
@@ -13,30 +13,47 @@
 # limitations under the License.
 
 import json
+import re
+
+
+def _camel_case_to_snake_case(s):
+  return re.sub(r'(?<!^)(?=[A-Z])', '_', s).lower()
+
+
+def camel_case_to_snake_case_recursive(j):
+  if isinstance(j, list):
+    return [camel_case_to_snake_case_recursive(i) for i in j]
+
+  if isinstance(j, dict):
+    return {
+        _camel_case_to_snake_case(k): camel_case_to_snake_case_recursive(v)
+        for (k, v) in j.items()
+    }
+  return j
 
 
 # TODO(IronPan) This library can be removed once ifPresent is supported within concat[] in component YAML V2.
 # Currently the component YAML will generate the payload with all API fields presented,
 # and those fields will be left empty if user doesn't specify them in the Python.
 def __remove_empty(j):
-    """Remove the empty fields in the Json."""
-    if isinstance(j, list):
-        return list(filter(None, [__remove_empty(i) for i in j]))
+  """Remove the empty fields in the Json."""
+  if isinstance(j, list):
+    return list(filter(None, [__remove_empty(i) for i in j]))
 
-    if isinstance(j, dict):
-        final_dict = {}
-        for k, v in j.items():
-            if v:
-                final_dict[k] = __remove_empty(v)
-        return final_dict
-    return j
+  if isinstance(j, dict):
+    final_dict = {}
+    for k, v in j.items():
+      if v:
+        final_dict[k] = __remove_empty(v)
+    return final_dict
+  return j
 
 
 def recursive_remove_empty(j):
-    """Recursively remove the empty fields in the Json until there is no empty fields and sub-fields."""
-    needs_update = True
-    while needs_update:
-        new_j = __remove_empty(j)
-        needs_update = json.dumps(new_j) != json.dumps(j)
-        j = new_j
-    return j
+  """Recursively remove the empty fields in the Json until there is no empty fields and sub-fields."""
+  needs_update = True
+  while needs_update:
+    new_j = __remove_empty(j)
+    needs_update = json.dumps(new_j) != json.dumps(j)
+    j = new_j
+  return j
