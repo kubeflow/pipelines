f31d410daa666090c38beca42e1dd63803534593
Gerard Casas Saez <gcasassaez@twitter.com>
2021-12-16 08:22:49 -0700

    overwrite directly context_setting

--- sdk/python/kfp/_client.py
+++ sdk/python/kfp/_client.py
@@ -163,14 +163,16 @@ class Client(object):
                                    other_client_secret, existing_token, proxy,
                                    ssl_ca_cert, kube_context, credentials)
         
-        is_multi_user = self.get_kfp_healthz().multi_user is True
-        # Respect namespace if not default
-        if is_multi_user and namespace != 'kubeflow':
-            self.set_user_namespace(namespace)
         
         # Save the loaded API client configuration, as a reference if update is
         # needed.
-        self._load_context_setting_or_default()
+        self._load_context_setting_or_default(overwrite_namespace=(None if namespace=='kubeflow' else namespace))
+        
+        # If custom namespace provided, overwrite the loaded or default one in 
+        # context settings for current instance
+        if namespace != 'kubeflow':
+            self._context_setting['namespace'] = namespace
+
         self._existing_config = config
         if cookies is None:
             cookies = self._context_setting.get('client_authentication_cookie')
@@ -194,7 +196,7 @@ class Client(object):
             api_client)
         self._healthz_api = kfp_server_api.api.healthz_service_api.HealthzServiceApi(
             api_client)
-        if not self._context_setting['namespace'] and is_multi_user:
+        if not self._context_setting['namespace'] and self.get_kfp_healthz().multi_user:
             try:
                 with open(Client.NAMESPACE_PATH, 'r') as f:
                     current_namespace = f.read()
