c266365553741b51725b0f2ad289ff5fd0979d8c
Bo Yang <hiyangbo@google.com>
2021-12-16 19:42:06 -0800

    1. Refactor the long if - action list in launcher to an action dictionary for error proof purpose.  2. Raise error if job type unsupported This is important to debug wrong job types as container logs show nothings in this case. 3. clear all lint warnings in existing code.
    PiperOrigin-RevId: 416944442

--- components/google-cloud/google_cloud_pipeline_components/container/experimental/gcp_launcher/launcher.py
+++ components/google-cloud/google_cloud_pipeline_components/container/experimental/gcp_launcher/launcher.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Launcher client to launch jobs for various job types."""
 
 import argparse
 import os
@@ -26,6 +27,36 @@ from . import hyperparameter_tuning_job_remote_runner
 from . import upload_model_remote_runner
 from . import wait_gcp_resources
 
+_JOB_TYPE_TO_ACTION_MAP = {
+    'CustomJob':
+        custom_job_remote_runner.create_custom_job,
+    'BatchPredictionJob':
+        batch_prediction_job_remote_runner.create_batch_prediction_job,
+    'HyperparameterTuningJob':
+        hyperparameter_tuning_job_remote_runner
+        .create_hyperparameter_tuning_job,
+    'UploadModel':
+        upload_model_remote_runner.upload_model,
+    'CreateEndpoint':
+        create_endpoint_remote_runner.create_endpoint,
+    'ExportModel':
+        export_model_remote_runner.export_model,
+    'DeployModel':
+        deploy_model_remote_runner.deploy_model,
+    'BigqueryQueryJob':
+        bigquery_job_remote_runner.bigquery_query_job,
+    'BigqueryCreateModelJob':
+        bigquery_job_remote_runner.bigquery_create_model_job,
+    'BigqueryPredictModelJob':
+        bigquery_job_remote_runner.bigquery_predict_model_job,
+    'BigqueryExportModelJob':
+        bigquery_job_remote_runner.bigquery_export_model_job,
+    'BigqueryEvaluateModelJob':
+        bigquery_job_remote_runner.bigquery_evaluate_model_job,
+    'Wait':
+        wait_gcp_resources.wait_gcp_resources
+}
+
 
 def _make_parent_dirs_and_return_path(file_path: str):
   os.makedirs(os.path.dirname(file_path), exist_ok=True)
@@ -33,14 +64,7 @@ def _make_parent_dirs_and_return_path(file_path: str):
 
 
 def _parse_args(args):
-  """Parse command line arguments.
-
-    Args:
-        args: A list of arguments.
-
-    Returns:
-        An argparse.Namespace class instance holding parsed args.
-    """
+  """Parse command line arguments."""
   parser = argparse.ArgumentParser(
       prog='Vertex Pipelines service launcher', description='')
   parser.add_argument(
@@ -79,7 +103,8 @@ def _parse_args(args):
       required=(parsed_args.type in {
           'UploadModel', 'CreateEndpoint', 'BatchPredictionJob',
           'BigqueryQueryJob', 'BigqueryCreateModelJob',
-          'BigqueryPredictModelJob', 'BigqueryExportModelJob', 'BigQueryEvaluateModelJob'
+          'BigqueryPredictModelJob', 'BigqueryExportModelJob',
+          'BigQueryEvaluateModelJob'
       }),
       default=argparse.SUPPRESS)
   parser.add_argument(
@@ -93,7 +118,6 @@ def _parse_args(args):
       '--job_configuration_query_override',
       dest='job_configuration_query_override',
       type=str,
-      # payload_override is only needed for BigQuery job component.
       required=(parsed_args.type in {
           'BigqueryQueryJob', 'BigqueryCreateModelJob',
           'BigqueryPredictModelJob', 'BigQueryEvaluateModelJob'
@@ -103,8 +127,10 @@ def _parse_args(args):
       '--model_name',
       dest='model_name',
       type=str,
-      required=(parsed_args.type
-                in {'BigqueryPredictModelJob', 'BigqueryExportModelJob', 'BigQueryEvaluateModelJob'}),
+      required=(parsed_args.type in {
+          'BigqueryPredictModelJob', 'BigqueryExportModelJob',
+          'BigQueryEvaluateModelJob'
+      }),
       default=argparse.SUPPRESS)
   parser.add_argument(
       '--model_destination_path',
@@ -116,7 +142,7 @@ def _parse_args(args):
       '--table_name',
       dest='table_name',
       type=str,
-      # table_name is only needed for BigQuery model tvf job component.
+      # table_name is only needed for BigQuery tvf model job component.
       required=(parsed_args.type
                 in {'BigqueryPredictModelJob', 'BigQueryEvaluateModelJob'}),
       default=argparse.SUPPRESS)
@@ -124,7 +150,7 @@ def _parse_args(args):
       '--query_statement',
       dest='query_statement',
       type=str,
-      # query_statement is only needed for BigQuery model tvf job component.
+      # query_statement is only needed for BigQuery predict model job component.
       required=(parsed_args.type
                 in {'BigqueryPredictModelJob', 'BigQueryEvaluateModelJob'}),
       default=argparse.SUPPRESS)
@@ -132,7 +158,7 @@ def _parse_args(args):
       '--threshold',
       dest='threshold',
       type=float,
-      # threshold is only needed for BigQuery model tvf job component.
+      # threshold is only needed for BigQuery tvf model job component.
       required=(parsed_args.type
                 in {'BigqueryPredictModelJob', 'BigQueryEvaluateModelJob'}),
       default=argparse.SUPPRESS)
@@ -143,7 +169,7 @@ def _parse_args(args):
 def main(argv):
   """Main entry.
 
-    expected input args are as follows:
+  Eexpected input args are as follows:
     Project - Required. The project of which the resource will be launched.
     Region - Required. The region of which the resource will be launched.
     Type - Required. GCP launcher is a single container. This Enum will
@@ -152,40 +178,16 @@ def main(argv):
         Note this can contain the Pipeline Placeholders.
     gcp_resources placeholder output for returning job_id.
 
-    Args:
-        argv: A list of system arguments.
+  Args:
+    argv: A list of system arguments.
   """
-
   parsed_args = _parse_args(argv)
+  job_type = parsed_args['type']
 
-  if parsed_args['type'] == 'CustomJob':
-    custom_job_remote_runner.create_custom_job(**parsed_args)
-  if parsed_args['type'] == 'BatchPredictionJob':
-    batch_prediction_job_remote_runner.create_batch_prediction_job(
-        **parsed_args)
-  if parsed_args['type'] == 'HyperparameterTuningJob':
-    hyperparameter_tuning_job_remote_runner.create_hyperparameter_tuning_job(
-        **parsed_args)
-  if parsed_args['type'] == 'UploadModel':
-    upload_model_remote_runner.upload_model(**parsed_args)
-  if parsed_args['type'] == 'CreateEndpoint':
-    create_endpoint_remote_runner.create_endpoint(**parsed_args)
-  if parsed_args['type'] == 'ExportModel':
-    export_model_remote_runner.export_model(**parsed_args)
-  if parsed_args['type'] == 'DeployModel':
-    deploy_model_remote_runner.deploy_model(**parsed_args)
-  if parsed_args['type'] == 'BigqueryQueryJob':
-    bigquery_job_remote_runner.bigquery_query_job(**parsed_args)
-  if parsed_args['type'] == 'BigqueryCreateModelJob':
-    bigquery_job_remote_runner.bigquery_create_model_job(**parsed_args)
-  if parsed_args['type'] == 'BigqueryPredictModelJob':
-    bigquery_job_remote_runner.bigquery_predict_model_job(**parsed_args)
-  if parsed_args['type'] == 'BigqueryExportModelJob':
-    bigquery_job_remote_runner.bigquery_export_model_job(**parsed_args)
-  if parsed_args['type'] == 'BigqueryEvaluateModelJob':
-    bigquery_job_remote_runner.bigquery_evaluate_model_job(**parsed_args)
-  if parsed_args['type'] == 'Wait':
-    wait_gcp_resources.wait_gcp_resources(**parsed_args)
+  if job_type not in _JOB_TYPE_TO_ACTION_MAP:
+    raise ValueError('Unsupported job type: ' + job_type)
+
+  _JOB_TYPE_TO_ACTION_MAP[job_type](**parsed_args)
 
 
 if __name__ == '__main__':
