d0cc5e6b23dbccd6a41850039e2a7d0d116ab499
Andres De Castro <andres@adecastro.ca>
2021-12-17 13:00:36 -0500

    feat: adds secrets to ml-pipeline-ui-artifact deployments in user profiles (#5864)
    * added minio credentials to UI deploy
    
    * Update manifests/kustomize/base/installs/multi-user/pipelines-profile-controller/sync.py
    
    Co-authored-by: Jacob Malmberg <jacobmalmberg@gmail.com>
    
    Co-authored-by: Jacob Malmberg <jacobmalmberg@gmail.com>

--- manifests/kustomize/base/installs/multi-user/pipelines-profile-controller/sync.py
+++ manifests/kustomize/base/installs/multi-user/pipelines-profile-controller/sync.py
@@ -299,6 +299,35 @@ def server_factory(visualization_server_image,
                                     "ports": [{
                                         "containerPort": 3000
                                     }],
+                                    "env": [
+                                        {
+                                            "name": "MINIO_ACCESS_KEY",
+                                            "valueFrom": {
+                                                "secretKeyRef": {
+                                                    "key": "accesskey",
+                                                    "name": "mlpipeline-minio-artifact"
+                                                }
+                                            }
+                                        },
+                                        {
+                                            "name": "MINIO_SECRET_KEY",
+                                            "valueFrom": {
+                                                "secretKeyRef": {
+                                                    "key": "secretkey",
+                                                    "name": "mlpipeline-minio-artifact"
+                                                }
+                                            }
+                                        }
+                                    ],
+                                        { 
+                                            "name": "MINIO_ACCESS_KEY",
+                                            "value": mlpipeline_minio_access_key
+                                        },
+                                        {
+                                            "name": "MINIO_SECRET_KEY",
+                                            "value": mlpipeline_minio_secret_key
+                                        }
+                                    ],
                                     "resources": {
                                         "requests": {
                                             "cpu": "10m",
