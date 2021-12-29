f9db81e2c8734afac1f4446804e2f7e7cef9ded4
Yuan (Bob) Gong <4957653+Bobgy@users.noreply.github.com>
2021-12-30 03:03:48 +0800

    test: fix integration-v2 test (#7122)
    * Update parameterized_tfx_oss.py
    
    * test: fix integration-v2 test
    
    * reduce test log noise

--- v2/test/sample-test.sh
+++ v2/test/sample-test.sh
@@ -20,6 +20,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 cd "${DIR}"
 source "${DIR}/scripts/ci-env.sh"
 
-pip3 install -r requirements.txt
+pip3 install --quiet -r requirements.txt
 # Run sample test
 ENV_PATH=kfp-ci.env make
