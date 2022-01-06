693935fb7e6aa0572470a13f76a0b6e0311c6377
You Quan Chong <yqchong@google.com>
2022-01-06 12:58:38 -0800

    feat(components): Add IsMetricBeyondThresholdOp util function for HP tuning job; add tests for all util components
    PiperOrigin-RevId: 420125024

--- components/google-cloud/google_cloud_pipeline_components/experimental/hyperparameter_tuning_job/utils.py
+++ components/google-cloud/google_cloud_pipeline_components/experimental/hyperparameter_tuning_job/utils.py
@@ -23,18 +23,32 @@ from kfp.v2 import dsl
         'protobuf'
     ],
     base_image='python:3.7')
-def GetTrialsOp(gcp_resources: str, region: str) -> list:
+def GetTrialsOp(gcp_resources: str) -> list:
+  """Retrieves the best trial from the trials.
+
+  Args:
+      gcp_resources (str): Proto tracking the hyperparameter tuning job.
+
+  Returns:
+      List of strings representing the intermediate JSON representation of the
+      trials from the hyperparameter tuning job.
+  """
   from google.cloud import aiplatform
   from google_cloud_pipeline_components.proto.gcp_resources_pb2 import GcpResources
   from google.protobuf.json_format import Parse
   from google.cloud.aiplatform_v1.types import study
 
-  client_options = {'api_endpoint': region + '-aiplatform.googleapis.com'}
-  job_client = aiplatform.gapic.JobServiceClient(client_options=client_options)
+  api_endpoint_suffix = '-aiplatform.googleapis.com'
   gcp_resources_proto = Parse(gcp_resources, GcpResources())
   gcp_resources_split = gcp_resources_proto.resources[0].resource_uri.partition(
       'projects')
   resource_name = gcp_resources_split[1] + gcp_resources_split[2]
+  prefix_str = gcp_resources_split[0]
+  prefix_str = prefix_str[:prefix_str.find(api_endpoint_suffix)]
+  api_endpoint = prefix_str[(prefix_str.rfind('//') + 2):] + api_endpoint_suffix
+
+  client_options = {'api_endpoint': api_endpoint}
+  job_client = aiplatform.gapic.JobServiceClient(client_options=client_options)
   response = job_client.get_hyperparameter_tuning_job(name=resource_name)
 
   return [study.Trial.to_json(trial) for trial in response.trials]
@@ -43,6 +57,26 @@ def GetTrialsOp(gcp_resources: str, region: str) -> list:
 @dsl.component(
     packages_to_install=['google-cloud-aiplatform'], base_image='python:3.7')
 def GetBestTrialOp(trials: list, study_spec_metrics: list) -> str:
+  """Retrieves the best trial from the trials.
+
+  Args:
+      trials (list): Required. List representing the intermediate
+        JSON representation of the trials from the hyperparameter tuning job.
+      study_spec_metrics (list): Required. List serialized from dictionary
+        representing the metrics to optimize.
+        The dictionary key is the metric_id, which is reported by your training
+        job, and the dictionary value is the optimization goal of the metric
+        ('minimize' or 'maximize'). example:
+        metrics = hyperparameter_tuning_job.serialize_metrics(
+            {'loss': 'minimize', 'accuracy': 'maximize'})
+
+  Returns:
+      String representing the intermediate JSON representation of the best
+      trial from the list of trials.
+
+  Raises:
+      RuntimeError: If there are multiple metrics.
+  """
   from google.cloud.aiplatform_v1.types import study
 
   if len(study_spec_metrics) > 1:
@@ -65,6 +99,26 @@ def GetBestTrialOp(trials: list, study_spec_metrics: list) -> str:
 @dsl.component(
     packages_to_install=['google-cloud-aiplatform'], base_image='python:3.7')
 def GetBestHyperparametersOp(trials: list, study_spec_metrics: list) -> list:
+  """Retrieves the best hyperparameters based on the trials.
+
+  Args:
+      trials (list): Required. List representing the intermediate
+        JSON representation of the trials from the hyperparameter tuning job.
+      study_spec_metrics (list): Required. List serialized from dictionary
+        representing the metrics to optimize.
+        The dictionary key is the metric_id, which is reported by your training
+        job, and the dictionary value is the optimization goal of the metric
+        ('minimize' or 'maximize'). example:
+        metrics = hyperparameter_tuning_job.serialize_metrics(
+            {'loss': 'minimize', 'accuracy': 'maximize'})
+
+  Returns:
+      List representing the intermediate JSON representation of the best
+      hyperparameters from the best trial in the list of trials.
+
+  Raises:
+      RuntimeError: If there are multiple metrics.
+  """
   from google.cloud.aiplatform_v1.types import study
 
   if len(study_spec_metrics) > 1:
@@ -82,14 +136,94 @@ def GetBestHyperparametersOp(trials: list, study_spec_metrics: list) -> list:
       trials_list, key=lambda trial: trial.final_measurement.metrics[0].value)
 
   return [
-      study.Trial.Parameter.to_json(param) for param in best_trial.parameters  # pytype: disable=bad-return-type
+      study.Trial.Parameter.to_json(param) for param in best_trial.parameters
   ]
+
+
+@dsl.component(
+    packages_to_install=['google-cloud-aiplatform'], base_image='python:3.7')
+def GetHyperparametersOp(trial: str) -> list:
+  """Retrieves the hyperparameters from the given trial.
+
+  Args:
+      trial (str): Required. The intermediate JSON representation of a
+        hyperparameter tuning job trial.
+
+  Returns:
+      List representing the intermediate JSON representation of the
+      hyperparameters from the trial.
+  """
+  from google.cloud.aiplatform_v1.types import study
+
+  trial_proto = study.Trial.from_json(trial)
+
+  return [
+      study.Trial.Parameter.to_json(param) for param in trial_proto.parameters
+  ]
+
+
+@dsl.component(
+    packages_to_install=['google-cloud-aiplatform'], base_image='python:3.7')
+def IsMetricBeyondThresholdOp(trial: str, study_spec_metrics: list,
+                              threshold: float) -> str:
+  """Determines if the metric of the best trial beyond the threshold given.
+
+  Args:
+      trial (str): Required. The intermediate JSON representation of a
+        hyperparameter tuning job trial.
+      study_spec_metrics (list): Required. List serialized from dictionary
+        representing the metrics to optimize.
+        The dictionary key is the metric_id, which is reported by your training
+        job, and the dictionary value is the optimization goal of the metric
+        ('minimize' or 'maximize'). example:
+        metrics = hyperparameter_tuning_job.serialize_metrics(
+            {'loss': 'minimize', 'accuracy': 'maximize'})
+      threshold (float): Required. Threshold to compare metric against.
+
+  Returns:
+      "true" if metric is beyond the threshold, otherwise "false"
+
+  Raises:
+      RuntimeError: If there are multiple metrics.
+  """
+  from google.cloud.aiplatform_v1.types import study
+
+  if len(study_spec_metrics) > 1:
+    raise RuntimeError('Unable to determine best parameters for multi-objective'
+                       ' hyperparameter tuning.')
+  trial_proto = study.Trial.from_json(trial)
+  val = trial_proto.final_measurement.metrics[0].value
+  goal = study_spec_metrics[0]['goal']
+
+  is_beyond_threshold = False
+  if goal == study.StudySpec.MetricSpec.GoalType.MAXIMIZE:
+    is_beyond_threshold = val > threshold
+  elif goal == study.StudySpec.MetricSpec.GoalType.MINIMIZE:
+    is_beyond_threshold = val < threshold
+
+  return 'true' if is_beyond_threshold else 'false'
 
 
 @dsl.component(
     packages_to_install=['google-cloud-aiplatform'], base_image='python:3.7')
 def GetWorkerPoolSpecsOp(best_hyperparameters: list,
                          worker_pool_specs: list) -> list:
+  """Constructs worker_pool_specs based on the best hyperparameters.
+
+  Args:
+      best_hyperparameters (list): Required. List representing the intermediate
+        JSON representation of the best hyperparameters from the
+        hyperparameter tuning job.
+      worker_pool_specs (list): Required. The spec of the worker pools
+        including machine type and Docker image. All worker pools except the
+        first one are optional and can be skipped by providing an empty value.
+
+  Returns:
+      List containing an intermediate JSON representation of the
+      worker_pool_specs updated with the best hyperparameters as arguments
+      in the container_spec.
+
+  """
   from google.cloud.aiplatform_v1.types import study
 
   for worker_pool_spec in worker_pool_specs:
@@ -141,7 +275,7 @@ def serialize_metrics(metric_spec: dict) -> list:
   """Serializes a metric spec to dictionary format.
 
   Args:
-      metric_spec: (Dict[str, str]): Required. Dictionary representing metrics
+      metric_spec (Dict[str, str]): Required. Dictionary representing metrics
         to optimize. The dictionary key is the metric_id, which is reported by
         your training job, and the dictionary value is the optimization goal of
         the metric ('minimize' or 'maximize'). Example:
---
+++ components/google-cloud/tests/experimental/hyperparameter_tuning_job/integration/test_util_components_compile.py
@@ -0,0 +1,148 @@
+# Copyright 2021 The Kubeflow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Test google-cloud-pipeline-Components to ensure the compile without error."""
+
+import json
+import os
+
+from google_cloud_pipeline_components.experimental.hyperparameter_tuning_job import HyperparameterTuningJobRunOp, serialize_metrics, GetTrialsOp, GetBestTrialOp, GetBestHyperparametersOp, GetHyperparametersOp, GetWorkerPoolSpecsOp, IsMetricBeyondThresholdOp
+import kfp
+from kfp.v2 import compiler
+
+import unittest
+
+
+class UtilComponentsCompileTest(unittest.TestCase):
+
+  def setUp(self):
+    super(UtilComponentsCompileTest, self).setUp()
+    self._package_path = os.path.join(
+        os.getenv("TEST_UNDECLARED_OUTPUTS_DIR"), "pipeline.json")
+    self._gcp_resources = ('{ "resources": [ { "resourceType": '
+                           '"HyperparameterTuningJob", "resourceUri": '
+                           '"https://us-central1-aiplatform.googleapis.com/'
+                           'v1/projects/186556260430/locations/us-central1/'
+                           'hyperparameterTuningJobs/1234567890123456789" '
+                           '} ] }')
+    self._trial = ('{\n \"id\": \"1\",\n \"state\": 4,\n \"parameters\": [\n '
+                   '{\n \"parameterId\": \"learning_rate\",\n \"value\": '
+                   '0.03162277660168379\n },\n {\n \"parameterId\": '
+                   '\"momentum\",\n \"value\": 0.5\n },\n {\n \"parameterId\":'
+                   ' \"num_neurons\",\n \"value\": 128.0\n }\n ],\n '
+                   '\"finalMeasurement\": {\n \"stepCount\": \"10\",\n '
+                   '\"metrics\": [\n {\n \"metricId\": '
+                   '\"accuracy\",\n \"value\": 0.734375\n }\n ]\n },\n '
+                   '\"startTime\": \"2021-12-10T00:41:57.675086142Z\",\n '
+                   '\"endTime\": \"2021-12-10T00:52:35Z\",\n \"name\": \"\",\n '
+                   '\"measurements\": [],\n \"clientId\": \"\",\n '
+                   '\"infeasibleReason\": \"\",\n \"customJob\": \"\"\n}')
+    self._best_hp = [
+        "{\n \"parameterId\": \"learning_rate\",\n \"value\": 0.028\n}",
+        "{\n \"parameterId\": \"momentum\",\n \"value\": 0.49\n}",
+        "{\n \"parameterId\": \"num_neurons\",\n \"value\": 512.0\n}"]
+    self._worker_pool_specs = [{
+        "machine_spec": {
+            "machine_type": "n1-standard-4",
+            "accelerator_type": "NVIDIA_TESLA_T4",
+            "accelerator_count": 1
+        },
+        "replica_count": 1,
+        "container_spec": {
+            "image_uri": "gcr.io/project_id/test"
+        }
+    }]
+    self._metrics_spec = serialize_metrics({"accuracy": "maximize"})
+
+  def tearDown(self):
+    super(UtilComponentsCompileTest, self).tearDown()
+    if os.path.exists(self._package_path):
+      os.remove(self._package_path)
+
+  def test_get_trials_op_compile(self):
+
+    @kfp.dsl.pipeline(name="get-trials-op-test")
+    def pipeline():
+      _ = GetTrialsOp(gcp_resources=self._gcp_resources)
+
+    self._test_compilation(pipeline, "../testdata/get_trials_op_pipeline.json")
+
+  def test_get_best_trial_op_compile(self):
+
+    @kfp.dsl.pipeline(name="get-best-trial-op-test")
+    def pipeline():
+      _ = GetBestTrialOp(
+          trials=[self._trial], study_spec_metrics=self._metrics_spec)
+
+    self._test_compilation(pipeline,
+                           "../testdata/get_best_trial_op_pipeline.json")
+
+  def test_get_best_hyperparameters_op_compile(self):
+
+    @kfp.dsl.pipeline(name="get-best-hyperparameters-op-test")
+    def pipeline():
+      _ = GetBestHyperparametersOp(
+          trials=[self._trial], study_spec_metrics=self._metrics_spec)
+
+    self._test_compilation(
+        pipeline, "../testdata/get_best_hyperparameters_op_pipeline.json")
+
+  def test_get_hyperparameters_op_compile(self):
+
+    @kfp.dsl.pipeline(name="get-hyperparameters-op-test")
+    def pipeline():
+      _ = GetHyperparametersOp(trial=self._trial)
+
+    self._test_compilation(pipeline,
+                           "../testdata/get_hyperparameters_op_pipeline.json")
+
+  def test_get_worker_pool_specs_op_compile(self):
+
+    @kfp.dsl.pipeline(name="get-worker-pool-specs-op-test")
+    def pipeline():
+      _ = GetWorkerPoolSpecsOp(
+          best_hyperparameters=self._best_hp,
+          worker_pool_specs=self._worker_pool_specs)
+
+    self._test_compilation(pipeline,
+                           "../testdata/get_worker_pool_specs_op_pipeline.json")
+
+  def test_is_metric_beyond_threshold_op_compile(self):
+
+    @kfp.dsl.pipeline(name="is-metric-beyond-threshold-op-test")
+    def pipeline():
+      _ = IsMetricBeyondThresholdOp(
+          trial=self._trial,
+          study_spec_metrics=self._metrics_spec,
+          threshold=0.5)
+
+    self._test_compilation(
+        pipeline, "../testdata/is_metric_beyond_threshold_pipeline.json")
+
+  def _test_compilation(self, pipeline_func: object, test_json_path: str):
+    compiler.Compiler().compile(
+        pipeline_func=pipeline_func, package_path=self._package_path)
+
+    with open(self._package_path) as f:
+      executor_output_json = json.load(f, strict=False)
+
+    with open(
+        os.path.join(
+            os.path.dirname(__file__),
+            test_json_path)) as ef:
+      expected_executor_output_json = json.load(ef, strict=False)
+
+    # Ignore the kfp SDK & schema version during comparison
+    del executor_output_json["sdkVersion"]
+    del executor_output_json["schemaVersion"]
+    self.assertEqual(executor_output_json, expected_executor_output_json)
