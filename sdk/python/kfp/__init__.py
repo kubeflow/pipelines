ad56371baa618315c0998ecc30267273cba6921e
Chen Sun <chensun@users.noreply.github.com>
2022-01-24 15:14:20 -0800

    chore(sdk1.8): cherry pick #7202 (#7203)

--- sdk/python/kfp/__init__.py
+++ sdk/python/kfp/__init__.py
@@ -16,7 +16,7 @@
 # https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
-__version__ = '1.8.10'
+__version__ = '1.8.11'
 
 from . import components
 from . import containers
