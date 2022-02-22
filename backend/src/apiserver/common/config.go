f1aa28d516b68e69b6384b163a97a45b4c778a60
James Liu <37026441+zijianjoy@users.noreply.github.com>
2022-02-21 23:34:23 -0800

    fix(backend): Read env var for default service account in multi-user mode. Fix #7336 (#7337)
    * fix(backend): Read env var for default service account in multi-user mode
    
    * fix test
    
    * update config.json

--- backend/src/apiserver/common/config.go
+++ backend/src/apiserver/common/config.go
@@ -23,14 +23,15 @@ import (
 )
 
 const (
-	MultiUserMode                       string = "MULTIUSER"
-	MultiUserModeSharedReadAccess       string = "MULTIUSER_SHARED_READ"
-	PodNamespace                        string = "POD_NAMESPACE"
-	CacheEnabled                        string = "CacheEnabled"
-	KubeflowUserIDHeader                string = "KUBEFLOW_USERID_HEADER"
-	KubeflowUserIDPrefix                string = "KUBEFLOW_USERID_PREFIX"
-	UpdatePipelineVersionByDefault      string = "AUTO_UPDATE_PIPELINE_DEFAULT_VERSION"
-	TokenReviewAudience                 string = "TOKEN_REVIEW_AUDIENCE"
+	MultiUserMode                           string = "MULTIUSER"
+	MultiUserModeSharedReadAccess           string = "MULTIUSER_SHARED_READ"
+	PodNamespace                            string = "POD_NAMESPACE"
+	CacheEnabled                            string = "CacheEnabled"
+	DefaultPipelineRunnerServiceAccountFlag string = "DEFAULTPIPELINERUNNERSERVICEACCOUNT"
+	KubeflowUserIDHeader                    string = "KUBEFLOW_USERID_HEADER"
+	KubeflowUserIDPrefix                    string = "KUBEFLOW_USERID_PREFIX"
+	UpdatePipelineVersionByDefault          string = "AUTO_UPDATE_PIPELINE_DEFAULT_VERSION"
+	TokenReviewAudience                     string = "TOKEN_REVIEW_AUDIENCE"
 )
 
 func IsPipelineVersionUpdatedByDefault() bool {
