264a0a02552c7e6b2b3f79b09f584971d2907073
Chen Sun <chensun@users.noreply.github.com>
2022-02-25 16:43:56 -0800

    chore(release): bump version to 1.8.1 on master branch

--- backend/api/python_http_client/kfp_server_api/__init__.py
+++ backend/api/python_http_client/kfp_server_api/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "1.8.0"
+__version__ = "1.8.1"
 
 # import apis into sdk package
 from kfp_server_api.api.experiment_service_api import ExperimentServiceApi
