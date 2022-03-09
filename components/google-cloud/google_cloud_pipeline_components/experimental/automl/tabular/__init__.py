c98115fe30e8a47545ce04408510d6521f22c334
Googler <nobody@google.com>
2022-03-09 15:24:53 -0800

    Add AutoML Tables Built-in Algorithm HyperparameterTuning Job components and Wide & Deep HyperparameterTuning Job pipeline to GCPC SDK.
    PiperOrigin-RevId: 433590637

--- components/google-cloud/google_cloud_pipeline_components/experimental/automl/tabular/__init__.py
+++ components/google-cloud/google_cloud_pipeline_components/experimental/automl/tabular/__init__.py
@@ -23,7 +23,7 @@ except ImportError:
 __all__ = [
     'CvTrainerOp', 'InfraValidatorOp', 'Stage1TunerOp', 'EnsembleOp',
     'StatsAndExampleGenOp', 'FeatureSelectionOp', 'TransformOp', 'FinalizerOp',
-    'WideAndDeepTrainerOp'
+    'WideAndDeepTrainerOp', 'BuiltinAlgorithmHyperparameterTuningJobOp'
 ]
 
 CvTrainerOp = load_component_from_file(
@@ -44,3 +44,7 @@ FinalizerOp = load_component_from_file(
     os.path.join(os.path.dirname(__file__), 'finalizer.yaml'))
 WideAndDeepTrainerOp = load_component_from_file(
     os.path.join(os.path.dirname(__file__), 'wide_and_deep_trainer.yaml'))
+BuiltinAlgorithmHyperparameterTuningJobOp = load_component_from_file(
+    os.path.join(
+        os.path.dirname(__file__),
+        'builtin_algorithm_hyperparameter_tuning_job.yaml'))
