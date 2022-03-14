896cac5bc74b4f5e4af0ae3b43339f7bd2dae682
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-03-14 13:40:19 -0600

    silence pylint

--- sdk/python/kfp/v2/__init__.py
+++ sdk/python/kfp/v2/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable=wrong-import-position
 import warnings
 
 warnings.warn(
