a43953844e2f8fdf492112ca571016b9c9f91ba7
Chen Sun <chensun@users.noreply.github.com>
2022-03-15 14:45:37 -0700

    feat(sdk): client to support KFP v2 API. (#7411)
    * copy _client.py and _auth.py from deprecated folder
    
    * Make kfp.Client support v2 API (IR).
    
    * fix test golden
    
    * address comments
    
    * copyright year
    
    * chore(backend): clean up pipelinespec.Value usage (#7407)
    
    * chore(sdk): clean, dedup, reconcile, organize requirements (#7400)
    
    * chore(sdk): clean, dedup, reconcile, organize requirements
    
    * apply yapf formatting
    
    * correct copyright
    
    * chore(sdk): fix test missing tempdir cleanup (#7403)
    
    * chore(sdk): fix test missing tempdir cleanup
    
    * fix test path
    
    * fix test path
    
    * clean up setUp method
    
    * change string placeholder from s to test_dir
    
    * release note
    
    Co-authored-by: Connor McCarthy <mccarthy.connor.james@gmail.com>

--- sdk/python/kfp/client/__init__.py
+++ sdk/python/kfp/client/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2022 The Kubeflow Authors
+# Copyright 2022 The Kubeflow Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,17 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# `kfp` is a namespace package.
-# https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages
-__path__ = __import__("pkgutil").extend_path(__path__, __name__)
-
-__version__ = '2.0.0-alpha.0'
-
-TYPE_CHECK = True
-
-from kfp.client import Client  # pylint: disable=wrong-import-position
-
-# TODO: clean up COMPILING_FOR_V2
-# COMPILING_FOR_V2 is True when using kfp.compiler or use (v1) kfp.compiler
-# with V2_COMPATIBLE or V2_ENGINE mode
-COMPILING_FOR_V2 = False
+from kfp.client.client import Client
