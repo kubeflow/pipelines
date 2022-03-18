7526829813e9a2b2fee782c7e72cb9c4d446bcc0
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-03-18 14:20:29 -0600

    clean comment

--- sdk/python/kfp/components/v1_modelbase.py
+++ sdk/python/kfp/components/v1_modelbase.py
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# mypy: ignore-errors
-# pylint: disable=all
+
 import inspect
 from collections import OrderedDict
 from collections import abc
