7dbe8d366f93f81c62763d0a4a342266083e4592
You Quan Chong <yqchong@google.com>
2022-03-25 14:30:51 -0700

    Add test skeleton

--- sdk/python/kfp/registry/client.py
+++ sdk/python/kfp/registry/client.py
@@ -18,13 +18,18 @@ import logging
 import google.auth
 import json
 import requests
+import re
 from kfp.components import base_component, load_component_from_text
 from typing import Any
 from google.protobuf import json_format
 
-_PKG_DEV_HOST_TEMPLATE = r'(https://(?P<location>.*)-kfp.pkg.dev/(?P<project_id>.*)/(?P<repo_id>.*))'
+_AR_HOST_TEMPLATE_GENERIC = r'(^https\:\/\/[\w\-]+\-kfp\.pkg\.dev\/.*$)'
+_AR_HOST_TEMPLATE_GROUPS = r'(https://(?P<location>.*)-kfp.pkg.dev/(?P<project_id>.*)/(?P<repo_id>.*))'
 AR_HOST_SUFFIX = 'pkg.dev'
 
+class _SafeDict(dict):
+    def __missing__(self, key):
+        return '{' + key + '}'
 
 class Client:
     def __init__(self,
@@ -37,12 +42,15 @@ class Client:
         self._config = self.load_config(host)
         if credentials:
             self._credentials = credentials
-        elif AR_HOST_SUFFIX in host:
+        elif self._is_ar_host(host):
             logger = logging.getLogger("google.auth._default")
             logging_warning_filter = utils.LoggingFilter(logging.WARNING)
             logger.addFilter(logging_warning_filter)
             self._creds, _ = google.auth.default()
             logger.removeFilter(logging_warning_filter)
+
+    def _is_ar_host(self, host: str):
+        return bool(re.match(_AR_HOST_TEMPLATE_GENERIC, host.rstrip('/')))
 
     def _request(self, request_url: str, request_body: str = '',
                 http_request: str = 'get', extra_headers: str = '') -> Any:
@@ -64,17 +72,14 @@ class Client:
     def load_config(self, host: str):
         config = {}
         host = host.rstrip('/')
-        if AR_HOST_SUFFIX in host:
-            host_pattern = re.compile(_PKG_DEV_HOST_TEMPLATE)
-            match = host_pattern.match(host)
+        if self._is_ar_host(host):
             try:
-                location = match.group('location')
-                project_id = match.group('project_id')
-                repo_id = match.group('repo_id')
+                matched = re.match(_AR_HOST_TEMPLATE_GROUPS, host)
                 registry_endpoint = 'https://artifactregistry.googleapis.com/v1/'
-                repo_resource_format = (f'projects/'
-                    f'{project_id}/locations/{location}/'
-                    f'repositories/{repo_id}')
+                repo_resource_format = ('projects/'
+                    '{project_id}/locations/{location}/'
+                    'repositories/{repo_id}'.format_map(
+                        _SafeDict(matched.groupdict())))
                 api_endpoint = f'{registry_endpoint}/{repo_resource_format}'
                 package_endpoint = f'{api_endpoint}/packages'
                 package_name_endpoint = f'{package_endpoint}/{{package_name}}'
@@ -96,9 +101,9 @@ class Client:
                     'get_version_url': f'{versions_endpoint}/{{version}}',
                     'list_versions_url': f'{versions_endpoint}/',
                     'delete_version_url': f'{versions_endpoint}/{{version}}',
-                    'package_resource_format': f'{repo_resource_format}/packages/{{package_name}}',
-                    'tag_resource_format': f'{repo_resource_format}/packages/{{package_name}}/tags/{{tag}}',
-                    'version_resource_format': f'{repo_resource_format}/packages/{{package_name}}/versions/{{version}}',
+                    'package_format': f'{repo_resource_format}/packages/{{package_name}}',
+                    'tag_format': f'{repo_resource_format}/packages/{{package_name}}/tags/{{tag}}',
+                    'version_format': f'{repo_resource_format}/packages/{{package_name}}/versions/{{version}}',
                 }
             except AttributeError as err:
                 raise ValueError('Invalid host URL')
@@ -108,6 +113,23 @@ class Client:
 
     def upload_component_template(self, file_name: str, tags: Optional[List[str]],    
                     extra_headers: Optional[dict]) -> Tuple[str, str]:
+        url = self._config['upload_url']
+        if not self._creds.valid:
+            self._creds.refresh(google.auth.transport.requests.Request())
+        headers = {
+            'Authorization': 'Bearer ' + self._creds.token,
+            **extra_headers,
+        }
+        request_body = {}
+        if tags:
+            request_body = {'tags': ','.join(tags)}
+
+        files = {'content': open(file_name, 'rb')}
+        response = requests.post(
+            url=url, data=request_body, headers=headers, files=files).json()
+        response.raise_for_status()
+
+        return response
 
     def _get_download_url(package_name: str,
                       version: Optional[str] = None,
