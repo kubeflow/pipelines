d01535eacd9d4a986bbfd238905afa1d88154371
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-04-08 15:52:58 -0600

    add ir_utils tests

--- sdk/python/kfp/utils/ir_utils_test.py
+++ sdk/python/kfp/utils/ir_utils_test.py
@@ -6,12 +6,12 @@ import unittest
 import yaml
 from kfp.utils import ir_utils
 
-json_string = json.dumps({"key": "val", "list": ["1", 2, 3.0]})
+json_dict = {"key": "val", "list": ["1", 2, 3.0]}
 
 
 def load_from_file(filepath: str) -> str:
     with open(filepath, "r") as f:
-        return json.dumps(yaml.safe_load(f))
+        return yaml.safe_load(f)
 
 
 class TestWriteIrToFile(unittest.TestCase):
@@ -19,17 +19,30 @@ class TestWriteIrToFile(unittest.TestCase):
     def test_yaml(self):
         with tempfile.TemporaryDirectory() as tempdir:
             temp_filepath = os.path.join(tempdir, 'output.yaml')
-            ir_utils._write_ir_to_file(json_string, temp_filepath)
+            ir_utils._write_ir_to_file(json_dict, temp_filepath)
             actual = load_from_file(temp_filepath)
-        self.assertEqual(actual, json_string)
+        self.assertEqual(actual, json_dict)
+
+    def test_yml(self):
+        with tempfile.TemporaryDirectory() as tempdir:
+            temp_filepath = os.path.join(tempdir, 'output.yml')
+            ir_utils._write_ir_to_file(json_dict, temp_filepath)
+            actual = load_from_file(temp_filepath)
+        self.assertEqual(actual, json_dict)
 
     def test_json(self):
         with tempfile.TemporaryDirectory() as tempdir, self.assertWarnsRegex(
                 DeprecationWarning, r"Compiling to JSON is deprecated"):
             temp_filepath = os.path.join(tempdir, 'output.json')
-            ir_utils._write_ir_to_file(json_string, temp_filepath)
+            ir_utils._write_ir_to_file(json_dict, temp_filepath)
             actual = load_from_file(temp_filepath)
-        self.assertEqual(actual, json_string)
+        self.assertEqual(actual, json_dict)
+
+    def test_incorrect_extension(self):
+        with tempfile.TemporaryDirectory() as tempdir, self.assertRaisesRegex(
+                ValueError, r'should end with "\.yaml"\.'):
+            temp_filepath = os.path.join(tempdir, 'output.txt')
+            ir_utils._write_ir_to_file(json_dict, temp_filepath)
 
 
 if __name__ == '__main__':
