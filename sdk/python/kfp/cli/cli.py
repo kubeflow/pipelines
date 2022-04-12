f91c9c6d28eff8f9c30b14c7686bf1c94335a6a7
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-04-12 10:27:38 -0600

    simplify diagnose_me command

--- sdk/python/kfp/cli/cli.py
+++ sdk/python/kfp/cli/cli.py
@@ -16,8 +16,6 @@ import click
 from kfp.cli.output import OutputFormat
 from kfp.client import Client
 
-_NO_CLIENT_COMMANDS = ['diagnose_me', 'components']
-
 
 @click.group()
 @click.option('--endpoint', help='Endpoint of the KFP API service to connect.')
@@ -48,7 +46,8 @@ def cli(ctx: click.Context, endpoint: str, iap_client_id: str, namespace: str,
     Feature stage:
     [Alpha](https://github.com/kubeflow/pipelines/blob/07328e5094ac2981d3059314cc848fbb71437a76/docs/release/feature-stages.md#alpha)
     """
-    if ctx.invoked_subcommand in _NO_CLIENT_COMMANDS:
+    NO_CLIENT_COMMANDS = ['diagnose_me', 'components']
+    if ctx.invoked_subcommand in NO_CLIENT_COMMANDS:
         # Do not create a client for these subcommands
         return
     ctx.obj['client'] = Client(endpoint, iap_client_id, namespace,
