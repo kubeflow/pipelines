e1e2058e8fc0186e941748b8545fe6a0f1dd1048
You Quan Chong <yqchong@google.com>
2022-04-25 13:48:56 -0700

    fix renaming in __init__.py

--- sdk/python/kfp/registry/__init__.py
+++ sdk/python/kfp/registry/__init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from kfp.registry.client import Client, ApiAuth
+from kfp.registry.client import RegistryClient, ApiAuth
 
