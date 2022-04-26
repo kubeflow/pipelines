80bfd957a4e8166a5041a2421ec8a589ec007e71
Connor McCarthy <mccarthy.connor.james@gmail.com>
2022-04-26 15:05:31 -0600

    feat(sdk): use dsl noun group for compile CLI commands (#7603)
    * use kfp dsl compile for compilation commands
    
    * create and move tests

--- sdk/python/kfp/cli/cli_test.py
+++ sdk/python/kfp/cli/cli_test.py
@@ -14,15 +14,19 @@
 
 import functools
 import itertools
+import json
 import os
 import re
 import tempfile
 import unittest
+from typing import Any, Dict, List, Optional
 from unittest import mock
 
+import yaml
 from absl.testing import parameterized
 from click import testing
 from kfp.cli import cli
+from kfp.cli import dsl_compile
 
 
 class TestCliNounAliases(unittest.TestCase):
@@ -31,6 +35,60 @@ class TestCliNounAliases(unittest.TestCase):
         runner = testing.CliRunner()
         self.invoke = functools.partial(
             runner.invoke, cli=cli.cli, catch_exceptions=False, obj={})
+
+    def test_aliases_singular(self):
+        result = self.invoke(args=['component'])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_aliases_plural(self):
+        result = self.invoke(args=['components'])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_aliases_fails(self):
+        result = self.invoke(args=['componentss'])
+        self.assertEqual(result.exit_code, 2)
+        self.assertEqual("Error: Unrecognized command 'componentss'\n",
+                         result.output)
+
+
+def _ignore_kfp_version_helper(spec: Dict[str, Any]) -> Dict[str, Any]:
+    """Ignores kfp sdk versioning in command.
+
+    Takes in a YAML input and ignores the kfp sdk versioning in command
+    for comparison between compiled file and goldens.
+    """
+    pipeline_spec = spec.get('pipelineSpec', spec)
+
+    if 'executors' in pipeline_spec['deploymentSpec']:
+        for executor in pipeline_spec['deploymentSpec']['executors']:
+            pipeline_spec['deploymentSpec']['executors'][
+                executor] = yaml.safe_load(
+                    re.sub(
+                        r"'kfp==(\d+).(\d+).(\d+)(-[a-z]+.\d+)?'", 'kfp',
+                        yaml.dump(
+                            pipeline_spec['deploymentSpec']['executors']
+                            [executor],
+                            sort_keys=True)))
+    return spec
+
+
+def load_compiled_file(filename: str) -> Dict[str, Any]:
+    with open(filename, 'r') as f:
+        contents = yaml.safe_load(f)
+        pipeline_spec = contents[
+            'pipelineSpec'] if 'pipelineSpec' in contents else contents
+        # ignore the sdkVersion
+        del pipeline_spec['sdkVersion']
+        return _ignore_kfp_version_helper(contents)
+
+
+class TestAliases(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        runner = testing.CliRunner()
+        cls.invoke = functools.partial(
+            runner.invoke, cli=cli.cli, catch_exceptions=True, obj={})
 
     def test_aliases_singular(self):
         result = self.invoke(args=['component'])
@@ -113,3 +171,90 @@ class TestCliVersion(unittest.TestCase):
         self.assertEqual(result.exit_code, 0)
         matches = re.match(r'^kfp \d\.\d\.\d.*', result.output)
         self.assertTrue(matches)
+
+
+COMPILER_CLI_TEST_DATA_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), 'compiler_cli_tests',
+    'test_data')
+
+SPECIAL_TEST_PY_FILES = {'two_step_pipeline.py'}
+
+TEST_PY_FILES = {
+    file.split('.')[0]
+    for file in os.listdir(COMPILER_CLI_TEST_DATA_DIR)
+    if ".py" in file and file not in SPECIAL_TEST_PY_FILES
+}
+
+
+class TestDslCompile(parameterized.TestCase):
+
+    def invoke(self, args: List[str]) -> testing.Result:
+        starting_args = ['dsl', 'compile']
+        args = starting_args + args
+        runner = testing.CliRunner()
+        return runner.invoke(
+            cli=cli.cli, args=args, catch_exceptions=False, obj={})
+
+    def invoke_deprecated(self, args: List[str]) -> testing.Result:
+        runner = testing.CliRunner()
+        return runner.invoke(
+            cli=dsl_compile.dsl_compile,
+            args=args,
+            catch_exceptions=False,
+            obj={})
+
+    def _test_compile_py_to_yaml(
+            self,
+            file_base_name: str,
+            additional_arguments: Optional[List[str]] = None) -> None:
+        py_file = os.path.join(COMPILER_CLI_TEST_DATA_DIR,
+                               f'{file_base_name}.py')
+
+        golden_compiled_file = os.path.join(COMPILER_CLI_TEST_DATA_DIR,
+                                            f'{file_base_name}.yaml')
+
+        if additional_arguments is None:
+            additional_arguments = []
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            generated_compiled_file = os.path.join(
+                tmpdir, f'{file_base_name}-pipeline.yaml')
+
+            result = self.invoke(
+                ['--py', py_file, '--output', generated_compiled_file] +
+                additional_arguments)
+
+            self.assertEqual(result.exit_code, 0)
+
+            compiled = load_compiled_file(generated_compiled_file)
+
+        golden = load_compiled_file(golden_compiled_file)
+        self.assertEqual(golden, compiled)
+
+    def test_two_step_pipeline(self):
+        self._test_compile_py_to_yaml(
+            'two_step_pipeline',
+            ['--pipeline-parameters', '{"text":"Hello KFP!"}'])
+
+    def test_two_step_pipeline_failure_parameter_parse(self):
+        with self.assertRaisesRegex(json.decoder.JSONDecodeError,
+                                    r"Unterminated string starting at:"):
+            self._test_compile_py_to_yaml(
+                'two_step_pipeline',
+                ['--pipeline-parameters', '{"text":"Hello KFP!}'])
+
+    @parameterized.parameters(TEST_PY_FILES)
+    def test_compile_pipelines(self, file: str):
+
+        # To update all golden snapshots:
+        # for f in test_data/*.py ; do python3 "$f" ; done
+
+        self._test_compile_py_to_yaml(file)
+
+    def test_deprecated_command_is_found(self):
+        result = self.invoke_deprecated(['--help'])
+        self.assertEqual(result.exit_code, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
