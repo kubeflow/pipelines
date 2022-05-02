6d6da63d2741b9cf32de156828a0ff05dce04dac
Lingqing Gan <lingqing.gan@gmail.com>
2022-05-02 11:23:15 -0700

    Update loop_static_test.py

--- samples/core/loop_static/loop_static_test.py
+++ samples/core/loop_static/loop_static_test.py
@@ -19,12 +19,14 @@ import kfp_server_api
 from .loop_static import my_pipeline
 from .loop_static_v2 import my_pipeline as my_pipeline_v2
 from kfp.samples.test.utils import KfpTask, debug_verify, run_pipeline_func, TestCase
+from pprint import pprint
 
 
 def verify(t: unittest.TestCase, run: kfp_server_api.ApiRun,
            tasks: dict[str, KfpTask], **kwargs):
     t.assertEqual(run.status, 'Succeeded')
-    print(tasks.__dict__)
+    task_dir = dir(tasks)
+    pprint(task_dir)
     # assert DAG structure
     t.assertCountEqual(['print-op', 'for-loop-1'], tasks.keys())
     # assert all iteration parameters
