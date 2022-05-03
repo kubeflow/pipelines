f7d7a09f2e3de15a2d759c1cab30b107b2632f98
You Quan Chong <yqchong@google.com>
2022-05-03 12:28:58 -0700

    rename files

--- sdk/python/kfp/registry/__init__.py
+++ sdk/python/kfp/registry/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from kfp.registry.client import RegistryClient, ApiAuth
+from kfp.registry.registry_client import RegistryClient, ApiAuth
