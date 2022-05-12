c185da8d2cf42535a8e50e30e3ffbe5707deea6b
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-05-12 16:21:27 -0600

    clean files

--- sdk/python/kfp/utils/ir_utils_test.py
+++ sdk/python/kfp/utils/ir_utils_test.py
@@ -31,11 +31,11 @@ import unittest
 import yaml
 from kfp.utils import ir_utils
 
-json_dict = {"key": "val", "list": ["1", 2, 3.0]}
+json_dict = {'key': 'val', 'list': ['1', 2, 3.0]}
 
 
 def load_from_file(filepath: str) -> str:
-    with open(filepath, "r") as f:
+    with open(filepath) as f:
         return yaml.safe_load(f)
 
 
@@ -57,7 +57,7 @@ class TestWriteIrToFile(unittest.TestCase):
 
     def test_json(self):
         with tempfile.TemporaryDirectory() as tempdir, self.assertWarnsRegex(
-                DeprecationWarning, r"Compiling to JSON is deprecated"):
+                DeprecationWarning, r'Compiling to JSON is deprecated'):
             temp_filepath = os.path.join(tempdir, 'output.json')
             ir_utils._write_ir_to_file(json_dict, temp_filepath)
             actual = load_from_file(temp_filepath)
