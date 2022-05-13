2570922a7fd1cb71d5178a6f4898958a66e450ae
Connor McCarthy <mccarthy.connor.james@gmail.com>
2022-05-13 11:34:34 -0600

    feat(sdk): compile JSON with formatting (#7712)
    * clean files
    
    * implement formatted write
    
    * add tests

--- sdk/python/kfp/utils/ir_utils.py
+++ sdk/python/kfp/utils/ir_utils.py
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, Any
-import warnings
 import json
+import warnings
+from typing import Any, Dict
+
 import yaml
 
 
@@ -29,18 +30,17 @@ def _write_ir_to_file(ir_dict: Dict[str, Any], output_file: str) -> None:
         ValueError: If output file path is not JSON or YAML.
     """
 
-    if output_file.endswith(".json"):
+    if output_file.endswith('.json'):
         warnings.warn(
-            ("Compiling to JSON is deprecated and will be "
-             "removed in a future version. Please compile to a YAML file by "
-             "providing a file path with a .yaml extension instead."),
+            ('Compiling to JSON is deprecated and will be '
+             'removed in a future version. Please compile to a YAML file by '
+             'providing a file path with a .yaml extension instead.'),
             category=DeprecationWarning,
             stacklevel=2,
         )
-        ir_json = json.dumps(ir_dict, sort_keys=True)
         with open(output_file, 'w') as json_file:
-            json_file.write(ir_json)
-    elif output_file.endswith((".yaml", ".yml")):
+            json.dump(ir_dict, json_file, indent=2, sort_keys=True)
+    elif output_file.endswith(('.yaml', '.yml')):
         with open(output_file, 'w') as yaml_file:
             yaml.dump(ir_dict, yaml_file, sort_keys=True)
     else:
