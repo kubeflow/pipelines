db75d725e224f2595f30a5e125b9e019f3f25c38
James Wu <jamesjwu@google.com>
2022-05-17 06:35:27 +0000

    feat(conformance): containerize KFP conformance test.

---
+++ backend/conformance/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Run conformance test and generate test report.
+./api-test \
+    -namespace kubeflow \
+    -runIntegrationTests=true \
+    -isDevMode=true \
+    -isKubeflowMode=true \
+    -resourceNamespace=kf-conformance \
+    -test.v \
+    | tee /tmp/kfp-conformance.log
+
+# Create the done file.
+touch /tmp/kfp-conformance.done
+
+echo "Done..."
+# Keep the container running so the test logs can be downloaded.
+while true; do sleep 10000; done
