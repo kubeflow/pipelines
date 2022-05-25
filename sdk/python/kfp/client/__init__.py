e71030a00e0a024fe93e195ed1c88b9f2063d622
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-05-25 12:20:59 -0600

    initial auth migration (@ji-yaqi)

--- sdk/python/kfp/client/__init__.py
+++ sdk/python/kfp/client/__init__.py
@@ -18,3 +18,10 @@ __all__ = [
 ]
 
 from kfp.client.client import Client
+from kfp.client.set_volume_credentials import \
+    ServiceAccountTokenVolumeCredentials
+from kfp.client.token_credentials_base import read_token_from_file
+from kfp.client.token_credentials_base import TokenCredentialsBase
+
+KF_PIPELINES_SA_TOKEN_ENV = "KF_PIPELINES_SA_TOKEN_PATH"
+KF_PIPELINES_SA_TOKEN_PATH = "/var/run/secrets/kubeflow/pipelines/token"
