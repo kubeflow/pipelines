3e2f53ee6449860f13a61217de4e944ccf55a894
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-05-27 17:20:39 -0600

    wip

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ from kfp import client
 import kfp_server_api
 from sdk.python.kfp.client.client import RunPipelineResult
 
-HOST = 'https://1c0b05d19abcd1c1-dot-us-central1.pipelines.googleusercontent.com/'
+MP_ENDPOINT = 'https://1c0b05d19abcd1c1-dot-us-central1.pipelines.googleusercontent.com/'
+KFP_ENDPOINT = ''
 MINUTE = 60
 
 
@@ -30,7 +31,11 @@ def import_pipeline_func(module_name: str, function_name: str) -> Callable:
     return getattr(module, function_name)
 
 
-def run(module_name: str, function_name: str) -> RunPipelineResult:
+def run(
+    endpoint: str,
+    module_name: str,
+    function_name: str,
+) -> RunPipelineResult:
     """Run the pipeline
 
     Args:
@@ -41,7 +46,8 @@ def run(module_name: str, function_name: str) -> RunPipelineResult:
         RunPipelineResult: The run result.
     """
     pipeline_func = import_pipeline_func(module_name, function_name)
-    return client.Client(host=HOST).create_run_from_pipeline_func(pipeline_func)
+    return client.Client(host=endpoint).create_run_from_pipeline_func(
+        pipeline_func, arguments={"force_flip_result": "heads"})
 
 
 def wait(run_result: RunPipelineResult) -> kfp_server_api.ApiRun:
@@ -61,24 +67,37 @@ config[0]['path'] = '.'.join(
     config[0]['path'].split('.')[:-1]) + '.condition_v2'
 path = config[0]['path']
 
-keys = list(config[0].keys())
-config = [tuple(dictionary.values()) for dictionary in config]
+params = list(config[0].keys())
+test_cases = [tuple(dictionary.values()) for dictionary in config]
+
+if os.environ['CODE_CHANGE'] == 'backend':
+    enable_caching = False
+    endpoints = [(MP_ENDPOINT,)]
+elif os.environ['CODE_CHANGE'] == 'sdk':
+    enable_caching = True
+    endpoints = [(MP_ENDPOINT,), (KFP_ENDPOINT,)]
+else:
+    raise ValueError("CODE_CHANGE env var must be either 'backend' or 'sdk'.")
 
 
 @pytest.mark.asyncio_cooperative
-@pytest.mark.parametrize(keys, config)
-async def test(path: str, name: str):
-    """Runs all samples and test that they succeed.
+@pytest.mark.parametrize(params, test_cases)
+@pytest.mark.parametrize(('endpoint',), endpoints)
+async def test(path: str, name: str, endpoint: str) -> None:
+    """Asynchronously runs all samples and test that they succeed.
 
     Args:
-        module_name: The name of the module.
-        function_name: The name of the function.
+        module_name (str): The name of the module.
+        function_name (str): The name of the function.
+        endpoint (str): The endpoint on which to test.
     """
-    loop = asyncio.get_running_loop()
 
-    run_result = await loop.run_in_executor(None, run, path, name)
-    url = f"{HOST}/#/runs/details/{run_result.run_id}"
+    event_loop = asyncio.get_running_loop()
+
+    run_result = await event_loop.run_in_executor(None, run, endpoint, path,
+                                                  name)
+    url = f"{endpoint}/#/runs/details/{run_result.run_id}"
     print(f'Running pipeline {name} from {path}:  {url}.')
 
-    api_run = await loop.run_in_executor(None, wait, run_result)
+    api_run = await event_loop.run_in_executor(None, wait, run_result)
     assert api_run.status == 'Succeeded', f'Pipeline {path}.{name} failed. More info: {url}.'
