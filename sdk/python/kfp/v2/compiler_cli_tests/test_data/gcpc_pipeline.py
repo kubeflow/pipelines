2b5374f592908e30e1a00e033b23a451ff4c7ef1
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-06-02 15:21:53 -0600

    simplify sample

--- sdk/python/kfp/v2/compiler_cli_tests/test_data/gcpc_pipeline.py
+++ sdk/python/kfp/v2/compiler_cli_tests/test_data/gcpc_pipeline.py
@@ -1,29 +1,24 @@
-import kfp
-from google_cloud_pipeline_components import aiplatform as gcc_aip
-from typing import NamedTuple
-from kfp.v2 import dsl
-from kfp.v2.dsl import (ClassificationMetrics, Input, Metrics, Model, Output,
-                        component)
-from google_cloud_pipeline_components.types import artifact_types
+from tabnanny import verbose
+
 import google.cloud.aiplatform as aip
+from google_cloud_pipeline_components import aiplatform as gcc_aip
+import kfp
+from kfp.v2.dsl import component
+from kfp.v2.dsl import Input
 
-DISPLAY_NAME = "automl-image-training"
-PIPELINE_NAME = "automl-tabular-beans-training-v2"
-PROJECT_ID = 'chesu-dev'
+DISPLAY_NAME = "test-vertex-dataset"
+PIPELINE_NAME = "test-vertex-dataset"
+PROJECT_ID = 'cjmccarthy-kfp'
 
-from kfp.v2.dsl import Artifact, Input
-
-from google_cloud_pipeline_components.types.artifact_types import VertexModel
+from google_cloud_pipeline_components.types.artifact_types import VertexDataset
+from kfp.v2.dsl import Input
 
 
 @component(
     kfp_package_path='git+https://github.com/chensun/pipelines@custom-type#egg=kfp&subdirectory=sdk/python',
     packages_to_install=['google_cloud_pipeline_components'],
-    imports=[
-        'from google_cloud_pipeline_components.types.artifact_types import VertexModel'
-    ],
 )
-def dummy_op(artifact: Input[VertexModel]):
+def dummy_op(artifact: Input[VertexDataset]):
     print('artifact.type: ', type(artifact))
     print('artifact.name: ', artifact.name)
     print('artifact.uri: ', artifact.uri)
@@ -35,35 +30,15 @@ def pipeline():
     ds_op = gcc_aip.ImageDatasetCreateOp(
         project=PROJECT_ID,
         display_name=DISPLAY_NAME,
-        gcs_source="gs://cloud-samples-data/vision/automl_classification/flowers/all_data_v2.csv",
+        gcs_source='gs://cloud-samples-data/vision/automl_classification/flowers/all_data_v2.csv',
         import_schema_uri=aip.schema.dataset.ioformat.image
         .single_label_classification,
     )
 
-    training_job_run_op = gcc_aip.AutoMLImageTrainingJobRunOp(
-        project=PROJECT_ID,
-        display_name=DISPLAY_NAME,
-        prediction_type="classification",
-        model_type="CLOUD",
-        base_model=None,
-        dataset=ds_op.outputs["dataset"],
-        model_display_name="train-automl-flowers",
-        training_fraction_split=0.6,
-        validation_fraction_split=0.2,
-        test_fraction_split=0.2,
-        budget_milli_node_hours=8000,
-    )
-
-    create_endpoint_op = gcc_aip.EndpointCreateOp(
-        project=PROJECT_ID,
-        display_name=DISPLAY_NAME,
-    )
-
-    dummy_op(training_job_run_op.outputs["model"])
-    #dummy_op(create_endpoint_op.outputs['endpoint'])
+    dummy_op(ds_op.outputs['dataset'])
 
 
 if __name__ == '__main__':
-    import kfp.v2.compiler as compiler
+    from kfp.v2 import compiler
     compiler.Compiler().compile(
         pipeline_func=pipeline, package_path=__file__.replace('.py', '.json'))
