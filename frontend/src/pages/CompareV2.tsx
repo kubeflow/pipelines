152cc14f80a6a5cade1fdcb75399c14208048c96
zpChris <elliott77chris@gmail.com>
2022-06-15 23:04:50 +0000

    Add request for artifact types and new filter functionality.

--- frontend/src/pages/CompareV2.tsx
+++ frontend/src/pages/CompareV2.tsx
@@ -23,12 +23,14 @@ import { Apis } from 'src/lib/Apis';
 import { URLParser } from 'src/lib/URLParser';
 import { errorToMessage } from 'src/lib/Utils';
 import {
+  filterLinkedArtifactsByType,
+  getArtifactTypes,
   getExecutionsFromContext,
   getKfpV2RunContext,
   getOutputLinkedArtifactsInExecution,
   LinkedArtifact,
 } from 'src/mlmd/MlmdUtils';
-import { Execution } from 'src/third_party/mlmd';
+import { ArtifactType, Execution } from 'src/third_party/mlmd';
 import { PageProps } from './Page';
 
 interface ExecutionArtifacts {
@@ -39,6 +41,65 @@ interface ExecutionArtifacts {
 interface RunArtifacts {
   run: ApiRunDetail;
   executionArtifacts: ExecutionArtifacts[];
+}
+
+enum MetricsType {
+  SCALAR_METRICS,
+  CONFUSION_MATRIX,
+  ROC_CURVE,
+  HTML,
+  MARKDOWN,
+}
+
+// Include only the runs and executions which have artifacts of the specified type.
+function filterRunArtifactsByType(
+  runArtifacts: RunArtifacts[],
+  artifactTypes: ArtifactType[],
+  metricsType: MetricsType,
+): RunArtifacts[] {
+  const metricsFilter =
+    metricsType === MetricsType.SCALAR_METRICS
+      ? 'system.Metrics'
+      : metricsType === MetricsType.CONFUSION_MATRIX || metricsType === MetricsType.ROC_CURVE
+      ? 'system.ClassificationMetrics'
+      : metricsType === MetricsType.HTML
+      ? 'system.HTML'
+      : metricsType === MetricsType.MARKDOWN
+      ? 'system.Markdown'
+      : '';
+  const typeRuns: RunArtifacts[] = [];
+  for (const runArtifact of runArtifacts) {
+    const typeExecutions: ExecutionArtifacts[] = [];
+    for (const e of runArtifact.executionArtifacts) {
+      let typeArtifacts: LinkedArtifact[] = filterLinkedArtifactsByType(
+        metricsFilter,
+        artifactTypes,
+        e.linkedArtifacts,
+      );
+      if (metricsType === MetricsType.CONFUSION_MATRIX) {
+        typeArtifacts = typeArtifacts.filter(x =>
+          x.artifact.getCustomPropertiesMap().has('confusionMatrix'),
+        );
+      } else if (metricsType === MetricsType.ROC_CURVE) {
+        typeArtifacts = typeArtifacts.filter(x =>
+          x.artifact.getCustomPropertiesMap().has('confidenceMetrics'),
+        );
+      }
+      if (typeArtifacts.length > 0) {
+        typeExecutions.push({
+          execution: e.execution,
+          linkedArtifacts: typeArtifacts,
+        } as ExecutionArtifacts);
+      }
+    }
+    if (typeExecutions.length > 0) {
+      typeRuns.push({
+        run: runArtifact.run,
+        executionArtifacts: typeExecutions,
+      } as RunArtifacts);
+    }
+  }
+  return typeRuns;
 }
 
 function CompareV2(props: PageProps) {
@@ -103,6 +164,63 @@ function CompareV2(props: PageProps) {
       onSuccess: () => props.updateBanner({}),
     },
   );
+
+  // artifactTypes allows us to map from artifactIds to artifactTypeNames,
+  // so we can identify metrics artifact provided by system.
+  const { data: artifactTypes } = useQuery<ArtifactType[], Error>(
+    ['artifact_types', {}],
+    () => getArtifactTypes(),
+    {
+      staleTime: Infinity,
+      onError: error =>
+        props.updateBanner({
+          message: 'Cannot get Artifact Types for MLMD.',
+          additionalInfo: error.message,
+          mode: 'error',
+        }),
+    },
+  );
+
+  if (!runArtifacts || !artifactTypes) {
+    return (
+      <div className={commonCss.page}>
+        <p>This is the V2 Run Comparison page.</p>
+      </div>
+    );
+  }
+
+  const scalarMetricsArtifacts = filterRunArtifactsByType(
+    runArtifacts,
+    artifactTypes,
+    MetricsType.SCALAR_METRICS,
+  );
+  const confusionMatrixArtifacts = filterRunArtifactsByType(
+    runArtifacts,
+    artifactTypes,
+    MetricsType.CONFUSION_MATRIX,
+  );
+  const rocCurveArtifacts = filterRunArtifactsByType(
+    runArtifacts,
+    artifactTypes,
+    MetricsType.ROC_CURVE,
+  );
+  const htmlArtifacts = filterRunArtifactsByType(runArtifacts, artifactTypes, MetricsType.HTML);
+  const markdownArtifacts = filterRunArtifactsByType(
+    runArtifacts,
+    artifactTypes,
+    MetricsType.MARKDOWN,
+  );
+
+  console.log('Scalar Metrics');
+  console.log(scalarMetricsArtifacts);
+  console.log('Confusion Matrix');
+  console.log(confusionMatrixArtifacts);
+  console.log('ROC Curve');
+  console.log(rocCurveArtifacts);
+  console.log('HTML');
+  console.log(htmlArtifacts);
+  console.log('Markdown');
+  console.log(markdownArtifacts);
 
   return (
     <div className={commonCss.page}>
