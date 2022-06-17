d15440eee255648511d1899797e69bb915900c4d
Scott Xu <scottxu@google.com>
2022-06-17 23:26:37 +0000

    use absolute path in test for overriding caching

--- sdk/python/kfp/client/client_test.py
+++ sdk/python/kfp/client/client_test.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 import unittest
+import os
+import yaml
 
 from absl.testing import parameterized
 from kfp.client import client
@@ -41,16 +43,43 @@ class TestValidatePipelineName(parameterized.TestCase):
             client.validate_pipeline_resource_name(name)
 
 
+PIPELINES_TEST_DATA_DIR = os.path.join(
+    os.path.abspath(os.path.join(__file__, "../..")),
+    'compiler', 'test_data', 'pipelines')
+
+PIPELINE_TEST_CASES = [
+    'pipeline_with_importer',
+    'pipeline_with_ontology',
+    'pipeline_with_if_placeholder',
+    'pipeline_with_concat_placeholder',
+    'pipeline_with_resource_spec',
+    'pipeline_with_various_io_types',
+    'pipeline_with_reused_component',
+    'pipeline_with_after',
+    'pipeline_with_condition',
+    'pipeline_with_nested_conditions',
+    'pipeline_with_nested_conditions_yaml',
+    'pipeline_with_loops',
+    'pipeline_with_nested_loops',
+    'pipeline_with_loops_and_conditions',
+    'pipeline_with_params_containing_format',
+    'lightweight_python_functions_pipeline',
+    'lightweight_python_functions_with_outputs',
+    'xgboost_sample_pipeline',
+    'pipeline_with_metrics_outputs',
+    'pipeline_with_exit_handler',
+    'pipeline_with_env',
+    'component_with_optional_inputs',
+    'pipeline_with_gcpc_types',
+    'pipeline_with_placeholders',
+    'pipeline_with_task_final_status',
+    'pipeline_with_task_final_status_yaml',
+    'component_with_pip_index_urls',
+]
 class TestOverrideCachingOptions(parameterized.TestCase):
-    @parameterized.parameters([
-        '../compiler/test_data/pipeline_with_concat_placeholder.yaml',
-        '../compiler/test_data/pipeline_with_after.yaml',
-        '../compiler/test_data/pipeline_with_loops.yaml',
-        '../compiler/test_data/pipeline_with_importer.yaml',
-        '../compiler/test_data/pipeline_with_condition.yaml',
-    ])
-    def test_override_caching(self, pipeline_path: str):
-        import yaml
+    @parameterized.parameters(PIPELINE_TEST_CASES)
+    def test_override_caching(self, pipeline_base_name: str):
+        pipeline_path = os.path.join(PIPELINES_TEST_DATA_DIR, f'{pipeline_base_name}.yaml')
         with open(pipeline_path) as f:
             yaml_dict = yaml.safe_load(f)
             test_client = client.Client(namespace='dummy_namespace')
