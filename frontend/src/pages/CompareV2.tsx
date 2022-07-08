fa4efa8ccdc6bfa5017b809bca76a0b41b4aa9ee
Chris Elliott <elliott77chris@gmail.com>
2022-07-08 11:47:52 -0700

    feat(frontend): Display HTML and Markdown files (#7981)
    * Starting dropdown progress with non-working recursive implementation.
    
    * Create working version of recursive dropdown.
    
    * Create two-level dropdown instead of recursive solution.
    
    * Further updates on multi-level dropdown, including text ellipses on all components for max widths.
    
    * Further updates on dropdown, add selection.
    
    * Refactor and implement idea for dropdown sub-headers.
    
    * Add secondary item name to sub items.
    
    * Add tooltips to dropdown.
    
    * Fix formatting issues.
    
    * Fix overflow and background issues.
    
    * Simplification of dropdown button and the necessary keys.
    
    * Isolate distinct portions of the two-level dropdown menu and sub menu.
    
    * Create the artifacts dropdown from the filtered run lists.
    
    * Remove unnecessary components in the CompareV2 page.
    
    * Remove two-level dropdown imports.
    
    * Revert CompareV2 to master.
    
    * Create tests for the two-level dropdown, and hide dropdown if not triggered.
    
    * Simplify sub dropdown toggle logic.
    
    * Update imports and add additional test.
    
    * Update test with correct query check.
    
    * Fix formatting.
    
    * Update dropdown items format.
    
    * test the dropdown render conditions.
    
    * Add logger checks if names for execution or artifact are not present.
    
    * Change metrics dropdown to use for...of.
    
    * Update useEffect to only update artifacts when all components are loaded.
    
    * Add visualization placeholder below dropdown.
    
    * Combine setSelectedItem and setDropdownActive into single function deactivateDropdown.
    
    * Allow user to select and display a confusion matrix.
    
    * Log a warning when the run does not have a specified name.
    
    * Both confusion matrices displayhed side-by-side in table.
    
    * Display the selected artifacts and save them across tab switching.
    
    * Simplify getArtifact process and actually get specific artifact.
    
    * Add an event listener rather than set it so multiple dropdowns work together.
    
    * Add comment to remove exhaustive-deps requirement.
    
    * Update boolean to following naming conventions.
    
    * Rename variables and interfaces to clarify meaning.
    
    * Fix npm formatting.
    
    * Break apart and attempt to simplify process to get the dropdown items.
    
    * Add test to ensure that confusion matrix is displayed when selected.
    
    * Revise and add to existing test.
    
    * Update filter syntax to switch-case.
    
    * Update function to use existing implementation.
    
    * Address useEffect issue by checking for equality before update.
    
    * Initial implementation of HTML and Markdown loading.
    
    * Address the issue where execution or artifact names are not available.
    
    * Remove unnecessary console.log statements.
    
    * Fix runName check.
    
    * Update execution and artifact ID organization.
    
    * Add tests and update fetch process if execution or artifact names are not present.
    
    * Update getArtifact naming to better reflect function.
    
    * Update HTML and Markdown Viewers so they can update with new content.
    
    * Update location of LinkedArtifact import.
    
    * Simplify the two useEffect hooks by calling a separate useCallback hook.
    
    * Simplify VisualizationPanelItem by incorporating the placeholder.
    
    * Add tests to verify HTML and Markdown fetching behavior.
    
    * Remove viewerConfigs map because useQuery takes care of that need.
    
    * Remove unused useQuery attributes.
    
    * Update error and loading display for the HTML and Markdown files.
    
    * Revert "Update error and loading display for the HTML and Markdown files."
    
    This reverts commit 0497f1f00a323c125670aa61f409a3667bc1d151.
    
    * Remove unnecessary viewer configs.
    
    * Fix two testing comments.
    
    * Clarify naming on the SelectedArtifact interface.
    
    * Simplify logic to set the selected item and update selected artifacts.
    
    * Update props for the MetricsDropdown component.
    
    * Remove unnecessary  useCallback import.
    
    * Add TODO to move away from argo workflow.

--- frontend/src/pages/CompareV2.tsx
+++ frontend/src/pages/CompareV2.tsx
@@ -47,7 +47,13 @@ import TwoLevelDropdown, {
   SelectedItem,
 } from 'src/components/TwoLevelDropdown';
 import MD2Tabs from 'src/atoms/MD2Tabs';
-import { ConfusionMatrixSection } from 'src/components/viewers/MetricsVisualizations';
+import {
+  ConfusionMatrixSection,
+  getHtmlViewerConfig,
+  getMarkdownViewerConfig,
+} from 'src/components/viewers/MetricsVisualizations';
+import PlotCard from 'src/components/PlotCard';
+import { ViewerConfig } from 'src/components/viewers/Viewer';
 
 const css = stylesheet({
   leftCell: {
@@ -233,9 +239,65 @@ function VisualizationPlaceholder(props: VisualizationPlaceholderProps) {
   );
 }
 
+interface VisualizationPanelItemProps {
+  metricsTab: MetricsType;
+  metricsTabText: string;
+  linkedArtifact: LinkedArtifact | undefined;
+  namespace: string | undefined;
+}
+
+function VisualizationPanelItem(props: VisualizationPanelItemProps) {
+  const { metricsTab, metricsTabText, linkedArtifact, namespace } = props;
+
+  const { data: viewerConfigs } = useQuery<ViewerConfig[], Error>(
+    [
+      'viewerConfig',
+      {
+        artifact: linkedArtifact?.artifact.getId(),
+        namespace,
+      },
+    ],
+    async () => {
+      let viewerConfigs: ViewerConfig[] = [];
+      if (linkedArtifact) {
+        try {
+          if (metricsTab === MetricsType.HTML) {
+            viewerConfigs = await getHtmlViewerConfig([linkedArtifact], namespace);
+          } else if (metricsTab === MetricsType.MARKDOWN) {
+            viewerConfigs = await getMarkdownViewerConfig([linkedArtifact], namespace);
+          }
+        } catch (err) {
+          throw err;
+        }
+      }
+      return viewerConfigs;
+    },
+    { staleTime: Infinity },
+  );
+
+  if (!linkedArtifact) {
+    return <VisualizationPlaceholder metricsTabText={metricsTabText} />;
+  }
+
+  if (metricsTab === MetricsType.CONFUSION_MATRIX) {
+    return (
+      <React.Fragment key={linkedArtifact.artifact.getId()}>
+        <ConfusionMatrixSection artifact={linkedArtifact.artifact} />
+      </React.Fragment>
+    );
+  }
+
+  // TODO(zpChris): Show loading and error pages for HTML and Markdown.
+  if (viewerConfigs && (metricsTab === MetricsType.HTML || metricsTab === MetricsType.MARKDOWN)) {
+    return <PlotCard configs={viewerConfigs} title={`Static ${metricsTabText}`} />;
+  }
+
+  return <></>;
+}
+
 interface SelectedArtifact {
   selectedItem: SelectedItem;
-  artifact?: Artifact;
+  linkedArtifact?: LinkedArtifact;
 }
 
 interface MetricsDropdownProps {
@@ -313,10 +375,21 @@ function getDropdownItems(filteredRunArtifacts: RunArtifact[]) {
   return dropdownItems;
 }
 
-function getArtifactFromSelectedItem(
+const getNamespace = (selectedItem: SelectedItem, filteredRunArtifacts: RunArtifact[]) => {
+  const selectedRun = filteredRunArtifacts.find(
+    runArtifact => runArtifact.run.run?.name === selectedItem.itemName,
+  )?.run;
+  if (selectedRun) {
+    // TODO(zpChris): Move away from workflow_manifest as this is V1 specific.
+    const jsonWorkflow = JSON.parse(selectedRun.pipeline_runtime!.workflow_manifest || '{}');
+    return jsonWorkflow.metadata?.namespace;
+  }
+};
+
+function getLinkedArtifactFromSelectedItem(
   filteredRunArtifacts: RunArtifact[],
   selectedItem: SelectedItem,
-): Artifact | undefined {
+): LinkedArtifact | undefined {
   const filteredRunArtifact = filteredRunArtifacts.find(
     runArtifact => runArtifact.run.run?.name === selectedItem.itemName,
   );
@@ -328,13 +401,13 @@ function getArtifactFromSelectedItem(
     return executionText === selectedItem.subItemName;
   });
 
-  const artifact = executionArtifact?.linkedArtifacts.find(linkedArtifact => {
+  const linkedArtifact = executionArtifact?.linkedArtifacts.find(linkedArtifact => {
     const linkedArtifactText: string =
       getArtifactName(linkedArtifact) || linkedArtifact.artifact.getId().toString();
     return linkedArtifactText === selectedItem.subItemSecondaryName;
-  })?.artifact;
+  });
 
-  return artifact;
+  return linkedArtifact;
 }
 
 function MetricsDropdown(props: MetricsDropdownProps) {
@@ -351,18 +424,20 @@ function MetricsDropdown(props: MetricsDropdownProps) {
   const [secondSelectedItem, setSecondSelectedItem] = useState<SelectedItem>(
     selectedArtifacts[1].selectedItem,
   );
+  const [firstSelectedNamespace, setFirstSelectedNamespace] = useState<string | undefined>();
+  const [secondSelectedNamespace, setSecondSelectedNamespace] = useState<string | undefined>();
 
   const updateSelectedItemAndArtifact = (
     setSelectedItem: (selectedItem: SelectedItem) => void,
+    setSelectedNamespace: (selectedNamespace: string | undefined) => void,
     panelIndex: number,
     selectedItem: SelectedItem,
   ): void => {
     setSelectedItem(selectedItem);
     selectedArtifacts[panelIndex].selectedItem = selectedItem;
-    selectedArtifacts[panelIndex].artifact = getArtifactFromSelectedItem(
-      filteredRunArtifacts,
-      selectedItem,
-    );
+    const linkedArtifact = getLinkedArtifactFromSelectedItem(filteredRunArtifacts, selectedItem);
+    selectedArtifacts[panelIndex].linkedArtifact = linkedArtifact;
+    setSelectedNamespace(getNamespace(selectedItem, filteredRunArtifacts));
     updateSelectedArtifacts(selectedArtifacts);
   };
 
@@ -381,30 +456,38 @@ function MetricsDropdown(props: MetricsDropdownProps) {
               title={`Choose a first ${metricsTabText} artifact`}
               items={dropdownItems}
               selectedItem={firstSelectedItem}
-              setSelectedItem={updateSelectedItemAndArtifact.bind(null, setFirstSelectedItem, 0)}
+              setSelectedItem={updateSelectedItemAndArtifact.bind(
+                null,
+                setFirstSelectedItem,
+                setFirstSelectedNamespace,
+                0,
+              )}
             />
-            {metricsTab === MetricsType.CONFUSION_MATRIX && selectedArtifacts[0].artifact ? (
-              <React.Fragment key={selectedArtifacts[0].artifact.getId()}>
-                <ConfusionMatrixSection artifact={selectedArtifacts[0].artifact} />
-              </React.Fragment>
-            ) : (
-              <VisualizationPlaceholder metricsTabText={metricsTabText} />
-            )}
+            <VisualizationPanelItem
+              metricsTab={metricsTab}
+              metricsTabText={metricsTabText}
+              linkedArtifact={selectedArtifacts[0].linkedArtifact}
+              namespace={firstSelectedNamespace}
+            />
           </td>
           <td className={classes(css.cell, css.rightCell)}>
             <TwoLevelDropdown
               title={`Choose a second ${metricsTabText} artifact`}
               items={dropdownItems}
               selectedItem={secondSelectedItem}
-              setSelectedItem={updateSelectedItemAndArtifact.bind(null, setSecondSelectedItem, 1)}
+              setSelectedItem={updateSelectedItemAndArtifact.bind(
+                null,
+                setSecondSelectedItem,
+                setSecondSelectedNamespace,
+                1,
+              )}
             />
-            {metricsTab === MetricsType.CONFUSION_MATRIX && selectedArtifacts[1].artifact ? (
-              <React.Fragment key={selectedArtifacts[1].artifact.getId()}>
-                <ConfusionMatrixSection artifact={selectedArtifacts[1].artifact} />
-              </React.Fragment>
-            ) : (
-              <VisualizationPlaceholder metricsTabText={metricsTabText} />
-            )}
+            <VisualizationPanelItem
+              metricsTab={metricsTab}
+              metricsTabText={metricsTabText}
+              linkedArtifact={selectedArtifacts[1].linkedArtifact}
+              namespace={secondSelectedNamespace}
+            />
           </td>
         </tr>
       </tbody>
