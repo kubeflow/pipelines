f8eb2ee0b1f08927259d99df3ffc50c7400e4025
Linchin <lingqing.gan@gmail.com>
2022-07-12 23:57:58 +0000

    log info

--- backend/test/integration/upgrade_test.go
+++ backend/test/integration/upgrade_test.go
@@ -321,6 +321,7 @@ func (s *UpgradeTests) VerifyRuns() {
 		s.runClient,
 		&runParams.ListRunsParams{SortBy: util.StringPointer("created_at")},
 		s.resourceNamespace)
+	glog.Info(runs)
 	require.Nil(t, err)
 	require.True(t, len(runs) >= 1)
 	require.Equal(t, "hello world", runs[0].Name)
