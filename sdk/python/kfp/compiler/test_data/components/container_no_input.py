6180571579cc82414ac83ae5b6ef04b078a6a3b5
scottxu <scottxu@google.com>
2022-07-15 10:49:37 -0700

    resolve isort error for test data

--- sdk/python/kfp/compiler/test_data/components/container_no_input.py
+++ sdk/python/kfp/compiler/test_data/components/container_no_input.py
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from kfp.dsl import ContainerSpec
 from kfp.dsl import container_component
+from kfp.dsl import ContainerSpec
 
 
 @container_component
