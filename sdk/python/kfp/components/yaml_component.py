7a85f54f43b1e7065233495eec8028c6aecd07c0
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-07-18 17:43:14 -0600

    move docstring below copyright

--- sdk/python/kfp/components/yaml_component.py
+++ sdk/python/kfp/components/yaml_component.py
@@ -1,5 +1,3 @@
-"""Functions for loading components from compiled YAML."""
-
 # Copyright 2021-2022 The Kubeflow Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Functions for loading components from compiled YAML."""
 
 from typing import Optional, Tuple
 
