d8b9439ef92b88da3420df9e8c67db0f1e89d4ef
Connor McCarthy <mccarthy.connor.james@gmail.com>
2022-07-19 15:53:54 -0600

    add kfp v2 api reference docs (#8042)
    * use __all__ in top-level modules to record public api
    
    * add index and source files
    
    * add kubeflow assets to _static/
    
    * add and pin requirements
    
    * use block quote instead of header for readme notice
    
    * update conf.py
    
    * delete old files

--- docs/source/kfp.containers.rst
+++
@@ -1,8 +0,0 @@
-kfp.containers package
-======================
-
-.. automodule:: kfp.containers
-    :members:
-    :undoc-members:
-    :show-inheritance:
-    :imported-members:
--- sdk/python/kfp/compiler/__init__.py
+++ sdk/python/kfp/compiler/__init__.py
@@ -12,4 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+__all__ = [
+    'Compiler',
+]
 from kfp.compiler.compiler import Compiler
