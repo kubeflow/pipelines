4df9d7a132b949831e87a990c50ab240d3d1ebf7
Ablai Akhazhanov <gkcalat@ucla.edu>
2022-07-21 11:34:51 +0000

    feat: Upgrade argo-workflow to v3.3.8

--- test/install-argo-cli.sh
+++ test/install-argo-cli.sh
@@ -19,7 +19,7 @@ set -ex
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 REPO_ROOT="${DIR}/.."
 ARGO_VERSION="$(cat ${REPO_ROOT}/third_party/argo/VERSION)"
-# ARGO_VERSION=v3.2.3
+# ARGO_VERSION=v3.3.8
 OS=${OS:-"linux-amd64"}
 
 # if argo is not installed
