87a90847475ce128a0331d9cc4275aa1ed847979
You Quan Chong <yqchong@google.com>
2022-07-21 14:27:50 -0700

    update dependencies

--- components/google-cloud/dependencies.py
+++ components/google-cloud/dependencies.py
@@ -19,7 +19,7 @@ def make_required_install_packages():
     return [
         # Pin google-api-core version for the bug fixing in 1.31.5
         # https://github.com/googleapis/python-api-core/releases/tag/v1.31.5
-        "google-api-core>=1.31.5,<3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
+        "google-api-core>=1.31.5,<3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*",
         # To resolve RTD errors with "No module named 'google.cloud.location'"
         "googleapis-common-protos>=1.56.2,<2.0dev",
         "google-cloud-storage>=1.20.0,<2",
