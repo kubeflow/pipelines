2237b310545d16c6b304c393269ede8bc8b5a590
James Liu <jamxl@google.com>
2022-08-04 06:13:22 +0000

    fix(frontend): Replace license-checker with yarn licenses.

---
+++ frontend/scripts/yarn-licenses.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+# 1. Install yarn
+npm install yarn
+
+# 2. Set up yarn: It will convert from package.json to yarn.lock
+npx yarn install
+
+# 3. List all packages with their license
+# npx yarn licenses list | less
+
+# # 4. Summarize licenses
+# yarn licenses list | grep License | sort -u | cut -d: -f2
+
+# 5. Generate full license texts in one file
+npx yarn licenses generate-disclaimer > dependency-licenses.txt
+
+pushd server
+npx yarn install
+npx yarn licenses generate-disclaimer > dependency-licenses.txt
+popd
+ 
+cat dependency-licenses.txt >> server/dependency-licenses.txt
