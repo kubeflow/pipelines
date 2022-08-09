44b18b6ccf1a3fa09a8d4e2fe38b2dae22d7f485
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-08-09 17:36:58 -0600

    remove existing unused imports

--- sdk/python/kfp/v2/components.py
+++ sdk/python/kfp/v2/components.py
@@ -12,5 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# pylint: disable=wildcard-import,unused-wildcard-import
-from kfp.components import *
+from kfp.components import *  # noqa
