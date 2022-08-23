d89cd87f9ef65c278102a02b9edaf28f0e70b8a0
Alex Latchford <alexla@zillowgroup.com>
2022-08-23 08:22:57 -0700

    Bump KFP Python SDK to support Py v3.10

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -87,6 +87,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Software Development',
