54012a71a188992d29cafa34c150e26478f6ddb1
Joe Li <yyjoeli@google.com>
2022-08-25 20:57:24 +0000

    Added updating URL for changed experiment back.

--- frontend/src/pages/NewRunV2.tsx
+++ frontend/src/pages/NewRunV2.tsx
@@ -179,6 +179,26 @@ function NewRunV2(props: NewRunV2Props) {
     }
   }, [updatedPipelineVersion]);
 
+  useEffect(() => {
+    if (apiExperiment?.id && pipeline?.id && pipelineVersion?.id) {
+      const searchString = urlParser.build({
+        [QUERY_PARAMS.experimentId]: apiExperiment.id || '',
+        [QUERY_PARAMS.pipelineId]: pipeline.id || '',
+        [QUERY_PARAMS.pipelineVersionId]: pipelineVersion.id || '',
+      });
+      props.history.replace(searchString);
+      return;
+    }
+
+    if (apiExperiment?.id) {
+      const searchString = urlParser.build({
+        [QUERY_PARAMS.experimentId]: apiExperiment.id || '',
+      });
+      props.history.replace(searchString);
+      return;
+    }
+  }, [apiExperiment]);
+
   // Title and list of actions on the top of page.
   useEffect(() => {
     props.updateToolbar({
