efcf214e0fd1591dbbfbd8db28b076a14af3e40a
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-09-01 13:34:17 -0600

    add importer to compiler test

--- sdk/python/test_data/pipelines/pipeline_with_custom_artifact_type.py
+++ sdk/python/test_data/pipelines/pipeline_with_custom_artifact_type.py
@@ -34,7 +34,22 @@ def create_temporary_non_kfp_artifact_package(
         def path(self) -> str:
             return self.uri.replace('gs://', '/')
 
-    class_source = textwrap.dedent(inspect.getsource(ThirdPartyModel))
+    class ThirdPartyDataset:
+        schema_title = 'third_party.ThirdPartyDataset'
+        schema_version = '0.0.0'
+
+        def __init__(self, name: str, uri: str, metadata: dict) -> None:
+            self.name = name
+            self.uri = uri
+            self.metadata = metadata
+
+        @property
+        def path(self) -> str:
+            return self.uri.replace('gs://', '/')
+
+    class_source = textwrap.dedent(
+        inspect.getsource(ThirdPartyModel)) + '\n\n' + textwrap.dedent(
+            inspect.getsource(ThirdPartyDataset))
 
     with open(os.path.join(temp_dir.name, 'dummy_third_party_package.py'),
               'w') as f:
@@ -48,6 +63,7 @@ try:
     create_temporary_non_kfp_artifact_package(temp_dir)
 
     import dummy_third_party_package
+    from dummy_third_party_package import ThirdPartyDataset
     from dummy_third_party_package import ThirdPartyModel
     from kfp import compiler
     from kfp import dsl
@@ -66,16 +82,32 @@ try:
             f.write('my model')
 
     @dsl.component(packages_to_install=PACKAGES_TO_INSTALL)
-    def model_consumer(model: Input[ThirdPartyModel]):
+    def model_consumer(model: Input[ThirdPartyModel],
+                       dataset: Input[ThirdPartyDataset]):
+        print('Model')
         print('artifact.type: ', type(model))
         print('artifact.name: ', model.name)
         print('artifact.uri: ', model.uri)
         print('artifact.metadata: ', model.metadata)
 
+        print('Dataset')
+        print('artifact.type: ', type(dataset))
+        print('artifact.name: ', dataset.name)
+        print('artifact.uri: ', dataset.uri)
+        print('artifact.metadata: ', dataset.metadata)
+
     @dsl.pipeline(name='pipeline-with-vertex-types')
     def my_pipeline():
         producer_task = model_producer()
-        model_consumer(model=producer_task.outputs['model'])
+        importer = dsl.importer(
+            artifact_uri='gs://ml-pipeline-playground/shakespeare1.txt',
+            artifact_class=ThirdPartyDataset,
+            reimport=False,
+            metadata={'key': 'value'})
+        model_consumer(
+            model=producer_task.outputs['model'],
+            dataset=importer.output,
+        )
 
     if __name__ == '__main__':
         ir_file = __file__.replace('.py', '.yaml')
