c740774eb02c32f958a22b7751e1c80834bfa806
You Quan Chong <yqchong@google.com>
2022-09-01 19:22:50 -0700

    update dependencies

--- components/google-cloud/dependencies.py
+++ components/google-cloud/dependencies.py
@@ -19,7 +19,7 @@ def make_required_install_packages():
     return [
         # Pin google-api-core version for the bug fixing in 1.31.5
         # https://github.com/googleapis/python-api-core/releases/tag/v1.31.5
-        "google-api-core>=1.31.5,<3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*,<2.10.0",
+        "google-api-core>=1.31.5,<3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*,<2.9.0",
         # To resolve RTD errors with error: protobuf<4.0.0dev,>=3.19.0 is required by {'google-cloud-aiplatform'}
         "protobuf<4.0.0dev,>=3.19.0",
         "grpcio-status<=1.47.0",
