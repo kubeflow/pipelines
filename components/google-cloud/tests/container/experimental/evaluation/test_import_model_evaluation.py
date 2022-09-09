82bf1ec9b44df75dd6d509dd9026901e11c0b553
Xiaowen Xie <xwx@google.com>
2022-09-09 10:49:03 -0700

    feat(components): Add metadata field for pipeline resource name
    PiperOrigin-RevId: 473290571

--- components/google-cloud/tests/container/experimental/evaluation/test_import_model_evaluation.py
+++ components/google-cloud/tests/container/experimental/evaluation/test_import_model_evaluation.py
@@ -362,6 +362,28 @@ class ImportModelEvaluationTest(unittest.TestCase):
         })
 
   @mock_api_call
+  def test_import_model_evaluation_with_pipeline_resource_name(
+      self, mock_api, _):
+    main([
+        '--metrics', self.metrics_path, '--problem_type', 'classification',
+        '--model_name', self._model_name, '--pipeline_job_resource_name',
+        PIPELINE_JOB_ID, '--gcp_resources', self._gcp_resources
+    ])
+    mock_api.assert_called_with(
+        mock.ANY,
+        parent=self._model_name,
+        model_evaluation={
+            'metrics':
+                to_value(
+                    json.loads(METRICS)['slicedMetrics'][0]['metrics']
+                    ['regression']),
+            'metrics_schema_uri':
+                PROBLEM_TYPE_TO_SCHEMA_URI['classification'],
+            'metadata':
+                to_value({'pipeline_job_resource_name': PIPELINE_JOB_ID})
+        })
+
+  @mock_api_call
   def test_import_model_evaluation_slice(self, _, mock_api):
     main([
         '--metrics', self.metrics_path, '--problem_type', 'classification',
