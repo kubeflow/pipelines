ac02a0e1910ccf8985db7961ba79cf739a96bc92
JOCSTAA <65559367+JOCSTAA@users.noreply.github.com>
2022-10-17 18:40:27 -0500

    fix(sdk): Throw exception for component parameter named Output (#8367)
    * fix(sdk): Throw exception for component parameter named Output
    
    * Add unit test
    
    * lint fix
    
    * nit fixes
    
    * nit
    
    * nit
    
    * format fixes

--- sdk/python/kfp/components/component_factory_test.py
+++ sdk/python/kfp/components/component_factory_test.py
@@ -15,6 +15,8 @@
 import unittest
 
 from kfp.components import component_factory
+from kfp.components.component_decorator import component
+from kfp.components.types.type_annotations import OutputPath
 
 
 class TestGetPackagesToInstallCommand(unittest.TestCase):
@@ -44,6 +46,27 @@ class TestGetPackagesToInstallCommand(unittest.TestCase):
         concat_command = ' '.join(command)
         for package in packages_to_install + pip_index_urls:
             self.assertTrue(package in concat_command)
+
+
+class TestInvalidParameterName(unittest.TestCase):
+
+    def test_output_named_Output(self):
+
+        with self.assertRaisesRegex(ValueError,
+                                    r'"Output" is an invalid parameter name.'):
+
+            @component
+            def comp(Output: OutputPath(str)):
+                pass
+
+    def test_output_named_Output_with_string_output(self):
+
+        with self.assertRaisesRegex(ValueError,
+                                    r'"Output" is an invalid parameter name.'):
+
+            @component
+            def comp(Output: OutputPath(str), text: str) -> str:
+                pass
 
 
 if __name__ == '__main__':
