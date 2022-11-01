15a552054d99c33b4ab4da64c59a162eae9a8279
Connor McCarthy <mccarthy.connor.james@gmail.com>
2022-11-01 11:31:46 -0700

    chore(sdk): remove unused linter comments (#8405)

--- sdk/python/kfp/v2/__init__.py
+++ sdk/python/kfp/v2/__init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# pylint: disable=wrong-import-position
 import warnings
 
 warnings.warn(
