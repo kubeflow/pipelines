7dedd8f7d0e3266c719a412374912b31a4423598
connor-mccarthy <mccarthy.connor.james@gmail.com>
2022-12-08 11:06:36 -0800

    implement review feedback

--- sdk/python/kfp/v2/components/executor.py
+++ sdk/python/kfp/v2/components/executor.py
@@ -253,16 +253,18 @@ class Executor():
                     .format(self._return_annotation))
         import os
         executor_output_path = self._input['outputs']['outputFile']
-        
+
         # This check is to ensure only one worker (in a mirrored, distributed training/compute strategy) attempts to write to the same executor output file at the same time using gcsfuse, which enforces immutability of files.
         write_file = True
-        
-        cluster_spec_string = os.environ.get('CLUSTER_SPEC')
+
+        CLUSTER_SPEC_ENV_VAR_NAME = 'CLUSTER_SPEC'
+        CHEIF_NODE_LABEL = 'workerpool0'
+
+        cluster_spec_string = os.environ.get(CLUSTER_SPEC_ENV_VAR_NAME)
         if cluster_spec_string:
             cluster_spec = json.loads(cluster_spec_string)
-            chief_node_label = 'workerpool0'
-            write_file = cluster_spec['task']['type'] == chief_node_label
-            
+            write_file = cluster_spec['task']['type'] == CHEIF_NODE_LABEL
+
         if write_file:
             os.makedirs(os.path.dirname(executor_output_path), exist_ok=True)
             with open(executor_output_path, 'w') as f:
