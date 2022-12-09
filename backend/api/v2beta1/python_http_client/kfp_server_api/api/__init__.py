8e02cc5c5a6a29773a4a262385aab51bfb04ca83
gkcalat <35157096+gkcalat@users.noreply.github.com>
2022-12-08 17:46:11 -0800

    chore(backend): Add pipeline and runtime protos (#8533)
    * Adjust generator.sh
    
    * Format the existing protos
    
    * Add pipeline protos
    
    * Add runtime protos
    
    * Generate client libs
    
    * Fix typos, adjust readme
    
    * Add python client
    
    * Update package name
    
    * Replace error.proto with google.rps.Status

--- backend/api/v2beta1/python_http_client/kfp_server_api/api/__init__.py
+++ backend/api/v2beta1/python_http_client/kfp_server_api/api/__init__.py
@@ -4,3 +4,7 @@ from __future__ import absolute_import
 
 # import apis into api package
 from kfp_server_api.api.experiment_service_api import ExperimentServiceApi
+from kfp_server_api.api.pipeline_service_api import PipelineServiceApi
+from kfp_server_api.api.pipeline_upload_service_api import PipelineUploadServiceApi
+from kfp_server_api.api.report_service_api import ReportServiceApi
+from kfp_server_api.api.run_service_api import RunServiceApi
