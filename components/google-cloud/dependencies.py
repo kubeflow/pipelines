e5e3fa6b788cc4fdfd41b9871f07e624eb1dad77
You Quan Chong <yqchong@google.com>
2022-12-20 17:15:49 -0800

    update dependencies

--- components/google-cloud/dependencies.py
+++ components/google-cloud/dependencies.py
@@ -17,15 +17,19 @@
 
 def make_required_install_packages():
     return [
+        # To resolve RTD errors with "No module named 'google.cloud.location'"
+        "googleapis-common-protos>=1.56.2,<2.0dev",
         # Pin google-api-core version for the bug fixing in 1.31.5
         # https://github.com/googleapis/python-api-core/releases/tag/v1.31.5
-        "google-api-core>=1.31.5,<3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
-        "google-cloud-storage>=1.20.0,<3",
-        "kfp>=1.8.9",
-        "google-cloud-aiplatform>=1.11.0,<2",
+        "google-api-core>=1.31.5,<3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*",
+        # To resolve RTD errors with error: protobuf<4.0.0dev,>=3.19.0 is required by {'google-cloud-aiplatform'}
+        "protobuf<4.0.0dev,>=3.19.0",
+        "grpcio-status<=1.47.0",
+        "google-cloud-storage>=1.20.0,<2",
+        "kfp>=1.8.9,<2.0.0",
         "google-cloud-notebooks>=0.4.0",
+        "google-cloud-aiplatform==1.14.0,<2",
     ]
-
 
 def make_required_test_packages():
     return make_required_install_packages() + [
