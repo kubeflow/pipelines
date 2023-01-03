cf3175ef4ea80e227243cc9ba4e644f0b56ad1c8
Chen Sun <chensun@users.noreply.github.com>
2023-01-03 15:08:24 -0800

    chore: address kubectl dry-run deprecation (#8643)

--- backend/src/cache/deployer/webhook-create-signed-cert.sh
+++ backend/src/cache/deployer/webhook-create-signed-cert.sh
@@ -149,5 +149,5 @@ echo ${serverCert} > ${cert_output_path}
 kubectl create secret generic ${secret} \
         --from-file=key.pem=${tmpdir}/server-key.pem \
         --from-file=cert.pem=${tmpdir}/server-cert.pem \
-        --dry-run -o yaml |
+        --dry-run=client -o yaml |
     kubectl -n ${namespace} apply -f -
