bf22366c80ba10b5f1ff61048050eb633d0ff093
You Quan Chong <yqchong@google.com>
2023-01-18 11:47:03 -0800

    update conf.py

--- components/google-cloud/docs/source/conf.py
+++ components/google-cloud/docs/source/conf.py
@@ -66,4 +66,10 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
