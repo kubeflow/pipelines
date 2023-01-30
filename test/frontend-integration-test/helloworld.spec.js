941434a7466925e3c7db3c2eb7fb07edf83d1be6
Ablai Akhazhanov <gkcalat@ucla.edu>
2023-01-30 23:37:53 +0000

    Fix bug in the front-end test

--- test/frontend-integration-test/helloworld.spec.js
+++ test/frontend-integration-test/helloworld.spec.js
@@ -72,6 +72,10 @@ describe('deploy helloworld sample run', () => {
     $('#experimentDescription').setValue(experimentDescription);
 
     $('#createExperimentBtn').click();
+
+    browser.waitUntil(() => {
+      return new URL(browser.getUrl()).hash.startsWith('#/runs/new');
+    }, waitTimeout);
   });
 
   it('creates a new run in the experiment', () => {
@@ -94,6 +98,7 @@ describe('deploy helloworld sample run', () => {
     $('#usePipelineVersionBtn').click();
 
     $('#pipelineVersionSelectorDialog').waitForVisible(waitTimeout, true);
+    browser.pause(1000);
 
     browser.keys(runName);
 
@@ -135,31 +140,35 @@ describe('deploy helloworld sample run', () => {
     assert(attempts, 'waited for 30 seconds but run did not start.');
 
     assert.equal($$('.tableRow').length, 1, 'should only show one run');
-
-    // Navigate to details of the deployed run by clicking its anchor element
-    browser.execute('document.querySelector(".tableRow a").click()');
   });
 
-  it('switches to config tab', () => {
-    $('button=Config').waitForVisible(waitTimeout);
-    $('button=Config').click();
-  });
-
+  // Wait for a reasonable amount of time until the run is done
+  // and navigate to the run details page
   it('waits for run to finish', () => {
-    let status = getValueFromDetailsTable('Status');
-
     let attempts = 0;
-    const maxAttempts = 60;
+    const maxAttempts = 90;
 
-    // Wait for a reasonable amount of time until the run is done
-    while (attempts < maxAttempts && status.trim() !== 'Succeeded') {
+    while (attempts < maxAttempts) {
       browser.pause(1000);
-      status = getValueFromDetailsTable('Status');
       attempts++;
     }
 
-    assert(attempts < maxAttempts, `waited for ${maxAttempts} seconds but run did not succeed. ` +
-      'Current status is: ' + status);
+    browser.execute('document.querySelector(".tableRow a").click()');
+    browser.waitUntil(() => {
+      return new URL(browser.getUrl()).hash.startsWith('#/runs/details');
+    }, waitTimeout);
+  });
+
+  it('switches to config tab', () => {
+    $('button=Config').waitForVisible();
+    $('button=Config').click();
+    browser.pause(waitTimeout);
+  });
+
+  it('verifies run status', () => {
+    const status = getValueFromDetailsTable('Status');
+    assert.equal(status.trim(), 'Succeeded',
+      'run has not finished on time. Current status is: ' + status);
   });
 
   it('displays run created at date correctly', () => {
@@ -179,6 +188,7 @@ describe('deploy helloworld sample run', () => {
 
   it('has a 4-node graph', () => {
     const nodeSelector = '.graphNode';
+    $(nodeSelector).waitForVisible();
     const nodes = $$(nodeSelector).length;
     assert(nodes === 4, 'should have a 4-node graph, instead has: ' + nodes);
   });
@@ -218,6 +228,7 @@ describe('deploy helloworld sample run', () => {
     $('#usePipelineBtn').click();
 
     $('#pipelineSelectorDialog').waitForVisible(waitTimeout, true);
+    browser.pause(1000);
 
     browser.keys('Tab');
     browser.keys(runWithoutExperimentName);
