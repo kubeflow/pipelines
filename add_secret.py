7333b4ab0abcc35410eb6617db119de7183f58e0
connor-mccarthy <mccarthy.connor.james@gmail.com>
2023-01-31 19:56:30 -0800

    wip

--- add_secret.py
+++ add_secret.py
@@ -22,7 +22,6 @@ def pipeline():
     task1 = comp()
     pvc_name = kubernetes.use_pvc(
         task1,
-        kind='PersistentVolumeClaim',
         generate_name='my-pvc-prefix-',
         access_modes=['ReadWriteMany'],
         resources='1Gi',
@@ -135,7 +134,7 @@ def mount_pvc(
     storage_class: Optional[str] = None,
     volume_name: Optional[str] = None,
 ) -> None:
-    """If storage_class is provided, volume_name cannot be provided. If volume_name is provided, storage_class must equal '' (empty string) or it will be set to the default storage class by Kubernetes."""
+    """If volume_name is provided, a pre-existing PV will be used."""
     ...
 
 
@@ -160,5 +159,82 @@ def mount_pvc(
     volume=V1Volume,
     volume_mount=V1VolumeMount,
 ) -> None:
+    ...
     """Mounts a PVC onto task's container. Dynamically provisions PVC if it does not exist. Else, uses the existing PVC."""
     ...
+
+
+# -------- V1 -----
+# volume
+import kfp.dsl as dsl
+
+
+@dsl.pipeline(name='volume-pipeline')
+def volume_pipeline():
+    # create PVC and dynamically provision volume
+    create_pvc_task = dsl.VolumeOp(
+        name='create_volume',
+        generate_unique_name=True,
+        resource_name='pvc1',
+        size='1Gi',
+        storage_class='standard',
+        modes=dsl.VOLUME_MODE_RWM,
+    )
+
+    # use PVC; write to file
+    task_a = dsl.ContainerOp(
+        name='step1_ingest',
+        image='alpine',
+        command=['sh', '-c'],
+        arguments=[
+            'mkdir /data/step1 && '
+            'echo hello > /data/step1/file1.txt'
+        ],
+        pvolumes={'/data': create_pvc_task.volume},
+    )
+
+    # create a PVC from a pre-existing volume
+    create_pvc_from_existing_vol = dsl.VolumeOp(
+        name='pre_existing_volume',
+        generate_unique_name=True,
+        resource_name='pvc2',
+        size='5Gi',
+        storage_class='standard',
+        volume_name='my-pre-existing-volume',
+        modes=dsl.VOLUME_MODE_RWM,
+    )
+
+    # use the previous task's PVC and the newly created PVC
+    task_b = dsl.ContainerOp(
+        name='step2_gunzip',
+        image='library/bash:4.4.23',
+        command=['sh', '-c'],
+        arguments=['cat /data/step1/file.txt'],
+        pvolumes={
+            '/data': task_a.pvolume,
+            '/other_data': create_pvc_from_existing_vol.volume
+        },
+    )
+
+
+# if __name__ == '__main__':
+#     import datetime
+#     import warnings
+#     import webbrowser
+
+#     from kfp import compiler
+
+#     warnings.filterwarnings('ignore')
+#     ir_file = __file__.replace('.py', '.yaml')
+#     compiler.Compiler().compile(pipeline_func=volume_pipeline,
+#                                 package_path=ir_file)
+#     # pipeline_name = __file__.split('/')[-1].replace('_',
+#     #                                                 '-').replace('.py', '')
+#     # display_name = datetime.datetime.now().strftime('%m-%d-%Y-%H-%M-%S')
+#     # job_id = f'{pipeline_name}-{display_name}'
+#     # endpoint = 'https://75167a6cffcb723c-dot-us-central1.pipelines.googleusercontent.com'
+#     # kfp_client = Client(host=endpoint)
+#     # run = kfp_client.create_run_from_pipeline_package(ir_file)
+#     # url = f'{endpoint}/#/runs/details/{run.run_id}'
+#     # print(url)
+#     # webbrowser.open_new_tab(url)
