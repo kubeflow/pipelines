a74eec2eb85b8d7d0077fbae0b904edf1d8fdae5
Chen Sun <chensun@users.noreply.github.com>
2023-02-14 11:01:58 -0800

    chore(test): revert api changes to deprecated client (#8841)
    * revert api changes to deprecated client
    
    * list experiments
    
    * fix
    
    * fix sample v2 test

--- backend/api/v1beta1/python_http_client/setup.py
+++ backend/api/v1beta1/python_http_client/setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "kfp-server-api"
-VERSION = "2.0.0-alpha.6"
+VERSION = "unreleased"
 # To install the library, run the following
 #
 # python setup.py install
