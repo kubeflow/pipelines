8fb292f147e5b036eb66e220bf70bc92c508d45a
Changyu Zhu <changyuz@google.com>
2023-04-04 09:55:58 -0700

    feat(components): Add machine type and encryption key for Error Analysis Component
    PiperOrigin-RevId: 521791590

--- components/google-cloud/google_cloud_pipeline_components/experimental/evaluation/error_analysis/error_analysis_annotation/component.py
+++ components/google-cloud/google_cloud_pipeline_components/experimental/evaluation/error_analysis/error_analysis_annotation/component.py
@@ -28,7 +28,9 @@ def error_analysis_annotation(
     embeddings_dir: str,
     root_dir: str,
     location: str = 'us-central1',
+    machine_type: str = 'n1-standard-8',
     num_neighbors: int = 5,
+    encryption_spec_key_name: str = '',
 ):
   """Computes error analysis annotations from image embeddings.
 
@@ -40,8 +42,15 @@ def error_analysis_annotation(
         output from the embeddings component.
       root_dir (str): Required. The GCS directory for storing error analysis
         annotation output.
+      machine_type (Optional[str]): The machine type computing error analysis
+        annotations. If not set, defaulted to
+        `n1-standard-8`. More details:
+        https://cloud.google.com/compute/docs/machine-resource
       num_neighbors (Optional[int]): Number of nearest neighbors to look for. If
         not set, defaulted to `5`.
+      encryption_spec_key_name (Optional[str]): Customer-managed encryption key
+        options for the CustomJob. If this is set, then all resources created by
+        the CustomJob will be encrypted with the provided encryption key.
 
   Returns:
       error_analysis_output_uri (str):
@@ -76,7 +85,7 @@ def error_analysis_annotation(
               f' "error-analysis-annotation-{PIPELINE_JOB_ID_PLACEHOLDER}',
               f'-{PIPELINE_TASK_ID_PLACEHOLDER}", ',
               '"job_spec": {"worker_pool_specs": [{"replica_count":"1',
-              '", "machine_spec": {"machine_type": "n1-standard-8"},',
+              '", "machine_spec": {"machine_type": "', machine_type, '"},',
               ' "container_spec": {"image_uri":"',
               'us-docker.pkg.dev/vertex-ai-restricted/vision-error-analysis/error-analysis:v0.2',
               '", "args": ["--embeddings_dir=', embeddings_dir,
@@ -84,7 +93,11 @@ def error_analysis_annotation(
               f'{root_dir}/{PIPELINE_JOB_ID_PLACEHOLDER}-{PIPELINE_TASK_ID_PLACEHOLDER}',
               '", "--num_neighbors=', num_neighbors,
               '", "--error_analysis_output_uri=', error_analysis_output_uri,
-              '", "--executor_input={{$.json_escape[1]}}"]}}]}}',
+              '", "--executor_input={{$.json_escape[1]}}"]}}]}',
+              ', "encryption_spec": {"kms_key_name":"',
+              encryption_spec_key_name,
+              '"}',
+              '}',
           ]),
       ],
   )
