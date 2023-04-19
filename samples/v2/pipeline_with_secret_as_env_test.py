448c06715e81548861fed51f739bba12d909b209
Linchin <lingqing.gan@gmail.com>
2023-04-19 20:28:01 +0000

    fix comments

--- samples/v2/pipeline_with_secret_as_env_test.py
+++ samples/v2/pipeline_with_secret_as_env_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Pipeline with secret as volume."""
+"""A pipeline that passes a secret as an env variable to a container."""
 
 from __future__ import annotations
 
