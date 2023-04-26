18c9dcb063c009c0550383f7d4593a325ea04cfc
Connor McCarthy <cjmccarthy@google.com>
2023-04-26 16:36:38 -0700

    chore(components): internal
    PiperOrigin-RevId: 527408534

--- components/google-cloud/google_cloud_pipeline_components/v1/bigquery/create_model/component.py
+++ components/google-cloud/google_cloud_pipeline_components/v1/bigquery/create_model/component.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+print('trigger tests')
 from typing import Dict, List
 
 from google_cloud_pipeline_components.types.artifact_types import BQMLModel
--- components/google-cloud/tests/v1/bigquery/integration/test_bigquery_query_job_compile.py
+++ components/google-cloud/tests/v1/bigquery/integration/test_bigquery_query_job_compile.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Test google-cloud-pipeline-Components to ensure they compile correctly."""
+print('trigger test')
 
 import json
 import os
