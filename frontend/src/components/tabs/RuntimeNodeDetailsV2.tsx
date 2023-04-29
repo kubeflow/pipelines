bdd4358f2d06fd5c05d5addf0a6251b1b44ba36b
Joe Li <yyjoeli@google.com>
2023-04-29 01:04:39 +0000

    Remove repeated TODO

--- frontend/src/components/tabs/RuntimeNodeDetailsV2.tsx
+++ frontend/src/components/tabs/RuntimeNodeDetailsV2.tsx
@@ -258,7 +258,6 @@ async function getLogsInfo(execution: Execution, runId?: string): Promise<Map<st
   // Early return if it is from cache.
   // TODO(jlyaoyuli): Consider to link to the cached execution.
   if (cachedExecutionId) {
-    // TODO(jlyaoyuli): show cached exection details
     logsInfo.set(LogsInfoKeys.LOGS_DETAILS, 'This step output is taken from cache.');
     return logsInfo;
   }
