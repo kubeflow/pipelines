4818e849f8e0cf7ddbb4c008fc43ec6f367d48cb
ananth102 <abashyam@amazon.com>
2023-05-03 10:56:15 -0700

    feat(components): Sagemaker V2 Hosting components and tests (#9243)
    * Hosting Components and test
    
    * update dependency
    
    * Regenerating with spec trimming
    
    * handle None case
    
    * adress pr comments
    
    * another way of handling update not supported
    
    * test changes
    
    * removing unused logic
    
    * Staging pr
    
    * Added READMEs
    
    * Main doc changes
    
    * minor edit

--- components/aws/sagemaker/commonv2/__init__.py
+++ components/aws/sagemaker/commonv2/__init__.py
@@ -10,10 +10,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import re
 
 def snake_to_camel(name):
     """Convert snake case to camel case."""
-    if name == "role_arn":
-        return "roleARN"
+    overrides = {
+        "role_arn": "roleARN",
+        "execution_role_arn" : "executionRoleARN"
+    }
+    if name in overrides:
+        return overrides[name]
     temp = name.split("_")
     return temp[0] + "".join(ele.title() for ele in temp[1:])
+
+def is_ack_requeue_error(error_msg):
+    # ^(start) [one or more alphanumeric characters] in [one or more alphanumeric characters] state cannot be modified or deleted. $(end)
+    # Alternative was using substring "state cannot be modified or deleted.", but there is a risk of validation errors including it too.
+    requeue_regex =  re.compile(r"^(\w+) in (\w+) state cannot be modified or deleted.$")
+    matches = requeue_regex.fullmatch(error_msg)
+    if matches is not None:
+        return True
+    return False
