c8c41b3df6947e56291ccf5256f8e63e37d0bf48
Connor McCarthy <cjmccarthy@google.com>
2023-05-05 12:40:40 -0700

    chore(components): convert experimental.notebooks component YAML to Python container component
    PiperOrigin-RevId: 529787983

--- components/google-cloud/google_cloud_pipeline_components/experimental/notebooks/executor/__init__.py
+++ components/google-cloud/google_cloud_pipeline_components/experimental/notebooks/executor/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2021 The Kubeflow Authors. All Rights Reserved.
+# Copyright 2023 The Kubeflow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,10 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Google Cloud Pipeline Experimental Forecasting Components."""
-
-from google_cloud_pipeline_components.experimental.notebooks.executor.component import notebooks_executor as NotebooksExecutorOp
-
-__all__ = [
-    'NotebooksExecutorOp',
-]
+"""Notebook Executor Component."""
