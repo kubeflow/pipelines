e822f01e367fff977f9c048c761c43414709a072
Connor McCarthy <cjmccarthy@google.com>
2023-05-05 12:50:33 -0700

    chore(components): convert v1.forecasting component YAML to Python container components
    PiperOrigin-RevId: 529790400

--- components/google-cloud/google_cloud_pipeline_components/v1/forecasting/prepare_data_for_train/component.py
+++ components/google-cloud/google_cloud_pipeline_components/v1/forecasting/prepare_data_for_train/component.py
@@ -1,16 +1,28 @@
+# Copyright 2023 The Kubeflow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Module for preparing the training input."""
 
 from typing import NamedTuple, Optional
 
-import kfp
+from kfp import dsl
 
 
+# define component without decorator so that we can test function implementation independently
 def prepare_data_for_train(
-    # pylint: disable=g-bare-generic
     input_tables: list,
     preprocess_metadata: dict,
     model_feature_columns: Optional[list] = None,
-    # pylint: enable=g-bare-generic
 ) -> NamedTuple(
     'Outputs',
     [
@@ -179,9 +191,8 @@ def prepare_data_for_train(
   )
 
 
-if __name__ == '__main__':
-  prepare_data_for_train_op = kfp.components.component_decorator.component(
-      prepare_data_for_train,
-      base_image='python:3.8',
-      output_component_file='component.yaml',
-  )
+prepare_data_for_train_implementation = prepare_data_for_train
+
+prepare_data_for_train = dsl.component(base_image='python:3.8')(
+    prepare_data_for_train
+)
