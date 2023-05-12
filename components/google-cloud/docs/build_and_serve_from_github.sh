afd628c5c33d8d70100835540bded6f3202943f9
Connor McCarthy <cjmccarthy@google.com>
2023-05-12 15:49:30 -0700

    chore(components): simplify build and serve from github script
    PiperOrigin-RevId: 531624069

--- components/google-cloud/docs/build_and_serve_from_github.sh
+++ components/google-cloud/docs/build_and_serve_from_github.sh
@@ -1,11 +1,5 @@
-# run from google3/third_party/py/google_cloud_pipeline_components/docs
-pushd ..
-python setup.py bdist_wheel
-WHEEL_FILE=$(find "dist/" -name "google_cloud_pipeline_components*.whl")
-pip3 install --upgrade $WHEEL_FILE
-popd
-pip install -r requirements.txt
-builddir=$(mktemp -d)
+# run from components/google-cloud
+pip3 install -e "..[docs]"
 sphinx-build -M html "source" $builddir
 pushd $builddir/html
 python3 -m http.server
