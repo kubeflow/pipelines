4df60da81d89b3f2265712dd90ec4823c626996c
Googler <nobody@google.com>
2023-05-19 00:23:01 -0700

    feat(components): Support Instance Config in kubeflow model_batch_predict component
    PiperOrigin-RevId: 533374983

--- components/google-cloud/google_cloud_pipeline_components/v1/batch_predict_job/component.py
+++ components/google-cloud/google_cloud_pipeline_components/v1/batch_predict_job/component.py
@@ -15,6 +15,7 @@
 from typing import Dict, List
 
 from google_cloud_pipeline_components import _image
+from google_cloud_pipeline_components import utils
 from google_cloud_pipeline_components.types.artifact_types import BQTable
 from google_cloud_pipeline_components.types.artifact_types import UnmanagedContainerModel
 from google_cloud_pipeline_components.types.artifact_types import VertexBatchPredictionJob
@@ -44,6 +45,10 @@ def model_batch_predict(
     predictions_format: str = 'jsonl',
     gcs_source_uris: List[str] = [],
     bigquery_source_input_uri: str = '',
+    instance_type: str = '',
+    key_field: str = '',
+    included_fields: List[str] = [],
+    excluded_fields: List[str] = [],
     model_parameters: Dict[str, str] = {},
     gcs_destination_output_uri_prefix: str = '',
     bigquery_destination_output_uri: str = '',
@@ -98,6 +103,71 @@ def model_batch_predict(
         config, see
           https://cloud.google.com/vertex-ai/docs/reference/rest/v1/projects.locations.batchPredictionJobs#InputConfig.
       model_parameters: The parameters that govern the predictions. The schema of the parameters
+      instance_type (Optional[str]):
+        The format of the instance that the Model accepts. Vertex AI will
+        convert compatible
+        [batch prediction input instance formats][InputConfig.instances_format]
+        to the specified format.
+        Supported values are:
+        ** `object`: Each input is converted to JSON object format.
+        * For `bigquery`, each row is converted to an object.
+        * For `jsonl`, each line of the JSONL input must be an object.
+        * Does not apply to `csv`, `file-list`, `tf-record`, or
+        `tf-record-gzip`.
+        ** `array`: Each input is converted to JSON array format.
+        * For `bigquery`, each row is converted to an array. The order
+          of columns is determined by the BigQuery column order, unless
+          [included_fields][] is populated.
+          [included_fields][] must be populated for specifying field orders.
+        * For `jsonl`, if each line of the JSONL input is an object,
+          [included_fields][] must be populated for specifying field orders.
+        * Does not apply to `csv`, `file-list`, `tf-record`, or
+          `tf-record-gzip`.
+        If not specified, Vertex AI converts the batch prediction input as
+        follows:
+         * For `bigquery` and `csv`, the behavior is the same as `array`. The
+           order of columns is the same as defined in the file or table, unless
+           [included_fields][] is populated.
+         * For `jsonl`, the prediction instance format is determined by
+           each line of the input.
+         * For `tf-record`/`tf-record-gzip`, each record will be converted to
+           an object in the format of `{"b64": <value>}`, where `<value>` is
+           the Base64-encoded string of the content of the record.
+         * For `file-list`, each file in the list will be converted to an
+           object in the format of `{"b64": <value>}`, where `<value>` is
+           the Base64-encoded string of the content of the file.
+        (-- api-linter: core::0140::base64=disabled
+            aip.dev/not-precedent: Base64 is not for this field. --)
+      key_field (Optional[str]):
+        The name of the field that is considered as a key.
+        The values identified by the key field is not included in the
+        transformed instances that is sent to the Model. This is similar to
+        specifying this name of the field in [excluded_fields][]. In addition,
+        the batch prediction output will not include the instances. Instead the
+        output will only include the value of the key field, in a field named
+        `key` in the output:
+         * For `jsonl` output format, the output will have a `key` field
+           instead of the `instance` field.
+         * For `csv`/`bigquery` output format, the output will have have a `key`
+           column instead of the instance feature columns.
+        The input must be JSONL with objects at each line, CSV, BigQuery
+        or TfRecord.
+      included_fields (Optional[Sequence[str]]):
+        Fields that will be included in the prediction instance that is
+        sent to the Model.
+        If [instance_type][] is `array`, the order of field names in
+        included_fields also determines the order of the values in the array.
+        When included_fields is populated, [excluded_fields][] must be empty.
+        The input must be JSONL with objects at each line, CSV, BigQuery
+        or TfRecord.
+      excluded_fields (Optional[Sequence[str]]):
+        Fields that will be excluded in the prediction instance that is
+        sent to the Model.
+        Excluded will be attached to the batch prediction output if
+        [key_field][] is not specified.
+        When excluded_fields is populated, [included_fields][] must be empty.
+        The input must be JSONL with objects at each line, CSV, BigQuery
+        or TfRecord.
         may be specified via the Model's `parameters_schema_uri`.
       gcs_destination_output_uri_prefix: The Google Cloud
         Storage location of the directory where the output is to be written
@@ -263,6 +333,28 @@ def model_batch_predict(
               bigquery_source_input_uri,
               '"',
               '}',
+              '}',
+              ', "instance_config": {',
+              '"instance_type": "',
+              instance_type,
+              '"',
+              ', "key_field": "',
+              key_field,
+              '", ',
+              IfPresentPlaceholder(
+                  input_name='included_fields',
+                  then=ConcatPlaceholder([
+                      '"included_fields": ',
+                      included_fields,
+                  ]),
+              ),
+              IfPresentPlaceholder(
+                  input_name='excluded_fields',
+                  then=ConcatPlaceholder([
+                      '"excluded_fields": ',
+                      excluded_fields,
+                  ]),
+              ),
               '}',
               ', "model_parameters": ',
               model_parameters,
