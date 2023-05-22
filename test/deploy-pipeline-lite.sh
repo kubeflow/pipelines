fb1d7ac23782d275bb59da09e7d33d13763b0753
Chen Sun <chensun@users.noreply.github.com>
2023-05-22 16:11:30 -0700

    Update deploy-pipeline-lite.sh

--- test/deploy-pipeline-lite.sh
+++ test/deploy-pipeline-lite.sh
@@ -67,7 +67,7 @@ if [ -z "$KFP_DEPLOY_RELEASE" ]; then
   kubectl apply -k .
   popd
 else
-  KFP_LATEST_RELEASE=$(git tag --sort=v:refname | tail -1)
+  KFP_LATEST_RELEASE=$(git tag --sort=v:refname | grep -v "sdk-" | tail -1)
   echo "Deploying KFP release $KFP_LATEST_RELEASE"
 
   # temporarily checkout last release tag
