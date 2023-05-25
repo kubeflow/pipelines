74c4c6a34fd14e32314e65fed12e1298c13be83e
Joe Li <56132941+jlyaoyuli@users.noreply.github.com>
2023-05-25 14:37:27 -0700

    fix(frontend): Toolbar in pipeline details (from run) should only have "Clone run" button (#9467)
    * Fix the toolbars button for pipeline details (from run)
    Create run -> Clone run
    Upload pipeline is removed
    
    * Update unit test.

--- frontend/src/pages/PipelineDetails.tsx
+++ frontend/src/pages/PipelineDetails.tsx
@@ -104,20 +104,11 @@ class PipelineDetails extends Page<{}, PipelineDetailsState> {
     const origin = this.getOrigin();
     const pipelineIdFromParams = this.props.match.params[RouteParams.pipelineId];
     const pipelineVersionIdFromParams = this.props.match.params[RouteParams.pipelineVersionId];
-    buttons
-      .newRunFromPipelineVersion(
-        () => {
-          return pipelineIdFromParams ? pipelineIdFromParams : '';
-        },
-        () => {
-          return pipelineVersionIdFromParams ? pipelineVersionIdFromParams : '';
-        },
-      )
-      .newPipelineVersion('Upload version', () =>
-        pipelineIdFromParams ? pipelineIdFromParams : '',
-      );
 
     if (origin) {
+      const getRunIdList = () => [origin.runId!];
+      buttons.cloneRun(getRunIdList, true);
+
       return {
         actions: buttons.getToolbarActionMap(),
         breadcrumbs: [
@@ -136,6 +127,17 @@ class PipelineDetails extends Page<{}, PipelineDetailsState> {
     } else {
       // Add buttons for creating experiment and deleting pipeline version
       buttons
+        .newRunFromPipelineVersion(
+          () => {
+            return pipelineIdFromParams ? pipelineIdFromParams : '';
+          },
+          () => {
+            return pipelineVersionIdFromParams ? pipelineVersionIdFromParams : '';
+          },
+        )
+        .newPipelineVersion('Upload version', () =>
+          pipelineIdFromParams ? pipelineIdFromParams : '',
+        )
         .newExperiment(() =>
           this.state.v1Pipeline
             ? this.state.v1Pipeline.id!
