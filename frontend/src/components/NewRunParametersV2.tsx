44e66fde75f9e0f6a4be4729b141ee591a3269a2
Joe Li <joe.yaoyuli@gmail.com>
2023-06-02 22:00:08 +0000

    Embed pipelineRoot in v2 new run object.

--- frontend/src/components/NewRunParametersV2.tsx
+++ frontend/src/components/NewRunParametersV2.tsx
@@ -55,7 +55,7 @@ interface NewRunParametersProps {
   // ComponentInputsSpec_ParameterSpec
   specParameters: SpecParameters;
   clonedRuntimeConfig?: PipelineSpecRuntimeConfig;
-  handlePipelineRootChange?: (pipelineRoot: string) => void;
+  handlePipelineRootChange?: (pipelineRoot?: string) => void;
   handleParameterChange?: (parameters: RuntimeParameters) => void;
   setIsValidInput?: (isValid: boolean) => void;
 }
@@ -166,7 +166,13 @@ function convertNonUserInputParamToString(
 }
 
 function NewRunParametersV2(props: NewRunParametersProps) {
-  const { specParameters, clonedRuntimeConfig, handleParameterChange, setIsValidInput } = props;
+  const {
+    specParameters,
+    clonedRuntimeConfig,
+    handlePipelineRootChange,
+    handleParameterChange,
+    setIsValidInput,
+  } = props;
   const [customPipelineRootChecked, setCustomPipelineRootChecked] = useState(false);
   const [customPipelineRoot, setCustomPipelineRoot] = useState(props.pipelineRoot);
   const [errorMessages, setErrorMessages] = useState<string[]>([]);
@@ -242,6 +248,9 @@ function NewRunParametersV2(props: NewRunParametersProps) {
                 setCustomPipelineRootChecked(checked);
                 if (!checked) {
                   setCustomPipelineRoot(undefined);
+                  if (handlePipelineRootChange) {
+                    handlePipelineRootChange(undefined);
+                  }
                 }
               }}
               inputProps={{ 'aria-label': 'Set custom pipeline root.' }}
@@ -257,6 +266,9 @@ function NewRunParametersV2(props: NewRunParametersProps) {
           value={customPipelineRoot || ''}
           onChange={ev => {
             setCustomPipelineRoot(ev.target.value);
+            if (handlePipelineRootChange) {
+              handlePipelineRootChange(ev.target.value);
+            }
           }}
           className={classes(commonCss.textField, css.textfield)}
         />
