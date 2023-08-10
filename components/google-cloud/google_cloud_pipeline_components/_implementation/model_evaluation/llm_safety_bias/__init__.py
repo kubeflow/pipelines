6e218a4d5187ca6b552b1632d1b75fef9f778067
Jason Dai <jsndai@google.com>
2023-08-10 12:56:11 -0700

    feat(components): add Vertex RAI safety bias evaluation pipeline
    PiperOrigin-RevId: 555615709

--- components/google-cloud/google_cloud_pipeline_components/_implementation/model_evaluation/llm_safety_bias/__init__.py
+++ components/google-cloud/google_cloud_pipeline_components/_implementation/model_evaluation/llm_safety_bias/__init__.py
@@ -11,4 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Google Cloud Pipeline LLM Safety Bias Components."""
+"""Google Cloud Pipeline LLM Safety Bias Evaluation Components."""
