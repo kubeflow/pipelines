432c442a1104648bbef8d6b1e4f3942f463db4ce
ryanjung-jm <ryanjung@google.com>
2023-08-12 03:52:52 +0900

    lint

--- samples/core/caching/caching_sample.py
+++ samples/core/caching/caching_sample.py
@@ -9,20 +9,20 @@ from kfp import dsl
 
 @dsl.component
 def do_work_op(seconds: float = 60) -> str:
-  import datetime
-  import time
-  print(f"Working for {seconds} seconds.")
-  for i in range(int(seconds)):
-    print(f"Working: {i}.")
-    time.sleep(1)
-  print("Done.")
-  return datetime.datetime.now().isoformat()
+    import datetime
+    import time
+    print(f"Working for {seconds} seconds.")
+    for i in range(int(seconds)):
+        print(f"Working: {i}.")
+        time.sleep(1)
+    print("Done.")
+    return datetime.datetime.now().isoformat()
 
 
 @kfp.dsl.pipeline(name='caching-pipeline')
 def caching_pipeline(seconds: float = 60):
-  # All outputs of successful executions are cached
-  work_task = do_work_op(seconds=seconds)
+    # All outputs of successful executions are cached
+    work_task = do_work_op(seconds=seconds)
 
 
 # Test 1
@@ -52,28 +52,26 @@ elapsed_time = datetime.datetime.now() - start_time
 print(f"Total run time: {int(elapsed_time.total_seconds())} seconds")
 
 if elapsed_time.total_seconds() > 60:
-  raise RuntimeError(
-      "The cached execution was not re-used or pipeline run took to long to complete.")
+    raise RuntimeError("The cached execution was not re-used or pipeline run took to long to complete.")
 
 
 # Test 3
 # In this case, the pipeline should not reuse the cached result, since they are
 # disabled.
-@kfp.dsl.pipeline(name='caching-pipeline2')
-def caching_pipeline2(seconds: float = 60):
-  work_task = do_work_op(seconds=seconds)
-  work_task.set_caching_options(enable_caching=False)
+@kfp.dsl.pipeline(name='caching-pipeline3')
+def caching_pipeline3(seconds: float = 60):
+    work_task = do_work_op(seconds=seconds)
+    work_task.set_caching_options(enable_caching=False)
 
 
 print("Starting test 3")
 start_time = datetime.datetime.now()
 kfp.Client(host=kfp_endpoint).create_run_from_pipeline_func(
-    caching_pipeline2,
+    caching_pipeline3,
     arguments=dict(seconds=60),
 ).wait_for_run_completion(timeout=999)
 elapsed_time = datetime.datetime.now() - start_time
 print(f"Total run time: {int(elapsed_time.total_seconds())} seconds")
 
 if elapsed_time.total_seconds() < 60:
-  raise RuntimeError(
-      "The cached execution was apparently re-used, but that should not happen.")
+    raise RuntimeError("The cached execution was apparently re-used, but that should not happen.")
