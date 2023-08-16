7ff7d9686ed22f0997db6a7d01ada93b1146cbf8
Junggil Lee <junggill@google.com>
2023-08-16 23:37:03 +0900

    fix(samples): Update execution_order, loop_output samples to v2 pipelines (#9867)
    * Update execution_order, loop_output samples to v2 pipelines
    
    * Update execution_order, loop_output samples to v2 pipelines

--- samples/core/loop_output/loop_output.py
+++ samples/core/loop_output/loop_output.py
@@ -12,22 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from kfp.deprecated import components
-from kfp.deprecated import dsl
+from kfp import compiler, dsl
 
 
-@components.create_component_from_func
+@dsl.component
 def args_generator_op() -> str:
     return '[1.1, 1.2, 1.3]'
 
 
-@components.create_component_from_func
-def print_op(s: float):
+# TODO(Bobgy): how can we make this component with type float?
+# got error: kfp.components.types.type_utils.InconsistentTypeException:
+# Incompatible argument passed to the input "s" of component "Print op": Argument
+# type "STRING" is incompatible with the input type "NUMBER_DOUBLE"
+@dsl.component
+def print_op(s: str):
     print(s)
 
 
-@dsl.pipeline(name='pipeline-with-loop-output')
+@dsl.pipeline(name='pipeline-with-loop-output-v2')
 def my_pipeline():
     args_generator = args_generator_op()
     with dsl.ParallelFor(args_generator.output) as item:
-        print_op(item)
+        print_op(s=item)
+
+if __name__ == '__main__':
+    compiler.Compiler().compile(my_pipeline, __file__ + '.yaml')
