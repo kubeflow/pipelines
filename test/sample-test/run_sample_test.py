f985672c75ea87d513f3dbd7040f4d73c19f3da8
Chen Sun <chensun@users.noreply.github.com>
2023-08-17 02:33:38 -0700

    test: upgrade e2e sample tests to using sdk v2 (#9885)
    * install kfp sdk 2.0 in sample test
    
    * remove reference to GCSHelper from deprecated module
    
    * exclude obsolete samples
    
    * update test script
    
    * update client usage
    
    * revert recent changes to exit_handler.py

--- test/sample-test/run_sample_test.py
+++ test/sample-test/run_sample_test.py
@@ -12,40 +12,41 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import kfp
+from datetime import datetime
 import os
 import tarfile
 import time
+
+from constants import CONFIG_DIR
+from constants import DEFAULT_CONFIG
+from constants import SCHEMA_CONFIG
+import kfp
+from kfp import Client
 import utils
 import yamale
 import yaml
-from datetime import datetime
-from kfp import Client
-from constants import CONFIG_DIR, DEFAULT_CONFIG, SCHEMA_CONFIG
 
 
 class PySampleChecker(object):
 
-    def __init__(
-            self,
-            testname,
-            input,
-            output,
-            result,
-            experiment_name,
-            host,
-            namespace='kubeflow'
-    ):
+    def __init__(self,
+                 testname,
+                 input,
+                 output,
+                 result,
+                 experiment_name,
+                 host,
+                 namespace='kubeflow'):
         """Util class for checking python sample test running results.
 
-    :param testname: test name.
-    :param input: The path of a pipeline file that will be submitted.
-    :param output: The path of the test output.
-    :param result: The path of the test result that will be exported.
-    :param host: The hostname of KFP API endpoint.
-    :param namespace: namespace of the deployed pipeline system. Default: kubeflow
-    :param experiment_name: Name of the experiment to monitor
-    """
+        :param testname: test name.
+        :param input: The path of a pipeline file that will be submitted.
+        :param output: The path of the test output.
+        :param result: The path of the test result that will be exported.
+        :param host: The hostname of KFP API endpoint.
+        :param namespace: namespace of the deployed pipeline system. Default: kubeflow
+        :param experiment_name: Name of the experiment to monitor
+        """
         self._testname = testname
         self._experiment_name = experiment_name
         self._input = input
@@ -72,20 +73,18 @@ class PySampleChecker(object):
         self._client = Client(host=self._host)
 
         ###### Check Input File ######
-        utils.add_junit_test(
-            self._test_cases, 'input generated yaml file',
-            os.path.exists(self._input), 'yaml file is not generated'
-        )
+        utils.add_junit_test(self._test_cases, 'input generated yaml file',
+                             os.path.exists(self._input),
+                             'yaml file is not generated')
         if not os.path.exists(self._input):
-            utils.write_junit_xml(
-                self._test_name, self._result, self._test_cases
-            )
+            utils.write_junit_xml(self._test_name, self._result,
+                                  self._test_cases)
             print('Error: job not found.')
             exit(1)
 
         ###### Create Experiment ######
         response = self._client.create_experiment(self._experiment_name)
-        self._experiment_id = response.id
+        self._experiment_id = response.experiment_id
         utils.add_junit_test(self._test_cases, 'create experiment', True)
 
         ###### Create Job ######
@@ -98,8 +97,8 @@ class PySampleChecker(object):
                 raw_args = yaml.safe_load(f)
             default_config = yamale.make_data(DEFAULT_CONFIG)
             yamale.validate(
-                config_schema, default_config
-            )  # If fails, a ValueError will be raised.
+                config_schema,
+                default_config)  # If fails, a ValueError will be raised.
         except yaml.YAMLError as yamlerr:
             raise RuntimeError('Illegal default config:{}'.format(yamlerr))
         except OSError as ose:
@@ -109,25 +108,21 @@ class PySampleChecker(object):
             self._run_pipeline = raw_args['run_pipeline']
 
         try:
-            config_file = os.path.join(
-                CONFIG_DIR, '%s.config.yaml' % self._testname
-            )
+            config_file = os.path.join(CONFIG_DIR,
+                                       '%s.config.yaml' % self._testname)
             with open(config_file, 'r') as f:
                 raw_args = yaml.safe_load(f)
             test_config = yamale.make_data(config_file)
             yamale.validate(
-                config_schema, test_config
-            )  # If fails, a ValueError will be raised.
+                config_schema,
+                test_config)  # If fails, a ValueError will be raised.
         except yaml.YAMLError as yamlerr:
-            print(
-                'No legit yaml config file found, use default args:{}'.
-                format(yamlerr)
-            )
+            print('No legit yaml config file found, use default args:{}'.format(
+                yamlerr))
         except OSError as ose:
             print(
-                'Config file with the same name not found, use default args:{}'.
-                format(ose)
-            )
+                'Config file with the same name not found, use default args:{}'
+                .format(ose))
         else:
             if 'arguments' in raw_args.keys() and raw_args['arguments']:
                 self._test_args.update(raw_args['arguments'])
@@ -143,17 +138,15 @@ class PySampleChecker(object):
         if self._testname == 'parameterized_tfx_oss':
             self._test_args['pipeline-root'] = os.path.join(
                 self._test_args['output'],
-                'tfx_taxi_simple_' + kfp.dsl.RUN_ID_PLACEHOLDER
-            )
+                'tfx_taxi_simple_' + kfp.dsl.RUN_ID_PLACEHOLDER)
             del self._test_args['output']
 
         # Submit for pipeline running.
         if self._run_pipeline:
-            response = self._client.run_pipeline(
-                self._experiment_id, self._job_name, self._input,
-                self._test_args
-            )
-            self._run_id = response.id
+            response = self._client.run_pipeline(self._experiment_id,
+                                                 self._job_name, self._input,
+                                                 self._test_args)
+            self._run_id = response.run_id
             utils.add_junit_test(self._test_cases, 'create pipeline run', True)
 
     def check(self):
@@ -163,53 +156,21 @@ class PySampleChecker(object):
             try:
                 start_time = datetime.now()
                 response = self._client.wait_for_run_completion(
-                    self._run_id, self._test_timeout
-                )
-                succ = (response.run.status.lower() == 'succeeded')
+                    self._run_id, self._test_timeout)
+                succ = (response.state.lower() == 'succeeded')
                 end_time = datetime.now()
                 elapsed_time = (end_time - start_time).seconds
-                utils.add_junit_test(
-                    self._test_cases, 'job completion', succ,
-                    'waiting for job completion failure', elapsed_time
-                )
+                utils.add_junit_test(self._test_cases, 'job completion', succ,
+                                     'waiting for job completion failure',
+                                     elapsed_time)
             finally:
-                ###### Output Argo Log for Debugging ######
-                workflow_json = self._client._get_workflow_json(self._run_id)
-                workflow_id = workflow_json['metadata']['name']
-                print("Argo Workflow Name: ", workflow_id)
-                argo_log, _ = utils.run_bash_command(
-                    'argo logs {} -n {}'.format(
-                        workflow_id, self._namespace
-                    )
-                )
-                print('=========Argo Workflow Log=========')
-                print(argo_log)
+                # TODO(chensun): print log for debugging
+                pass
 
             if not succ:
-                utils.write_junit_xml(
-                    self._test_name, self._result, self._test_cases
-                )
+                utils.write_junit_xml(self._test_name, self._result,
+                                      self._test_cases)
                 exit(1)
-
-            ###### Validate the results for specific test cases ######
-            if self._testname == 'xgboost_training_cm':
-                # For xgboost sample, check its confusion matrix.
-                cm_tar_path = './confusion_matrix.tar.gz'
-                utils.get_artifact_in_minio(
-                    workflow_json, 'confusion-matrix', cm_tar_path,
-                    'mlpipeline-ui-metadata'
-                )
-                with tarfile.open(cm_tar_path) as tar_handle:
-                    file_handles = tar_handle.getmembers()
-                    assert len(file_handles) == 1
-
-                    with tar_handle.extractfile(file_handles[0]) as f:
-                        cm_data = f.read()
-                        utils.add_junit_test(
-                            self._test_cases, 'confusion matrix format',
-                            (len(cm_data) > 0),
-                            'the confusion matrix file is empty'
-                        )
 
         ###### Delete Job ######
         #TODO: add deletion when the backend API offers the interface.
