b056868db644cbb76ac0b8302f39517d8eaf9cee
Googler <nobody@google.com>
2023-08-24 14:51:14 -0700

    chore(components): internal
    PiperOrigin-RevId: 559877231

--- components/google-cloud/google_cloud_pipeline_components/v1/dataflow/python_job/component.py
+++ components/google-cloud/google_cloud_pipeline_components/v1/dataflow/python_job/component.py
@@ -11,17 +11,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import List
+from typing import List, NamedTuple
 
 from google_cloud_pipeline_components import _image
 from google_cloud_pipeline_components import _placeholders
+from google_cloud_pipeline_components.v1.cancel_job import CancelJobOp
 from kfp.dsl import container_component
 from kfp.dsl import ContainerSpec
 from kfp.dsl import OutputPath
+from kfp.dsl import pipeline
 
 
 @container_component
-def dataflow_python(
+def dataflow_python_component(
     python_module_path: str,
     temp_location: str,
     gcp_resources: OutputPath(str),
@@ -75,3 +77,46 @@ def dataflow_python(
           gcp_resources,
       ],
   )
+
+
+@pipeline
+def dataflow_python(
+    project: str,
+    python_module_path: str,
+    temp_location: str,
+    # gcp_resources: OutputPath(str),
+    location: str = 'us-central1',
+    requirements_file_path: str = '',
+    args: List[str] = [],
+) -> NamedTuple('outputs', gcp_resources=str):
+  # fmt: off
+  """Launch a self-executing Beam Python file on Google Cloud using the
+  Dataflow Runner and cancel underlying gcp resource if job is cancelled.
+
+  Args:
+      project: Project to create the Dataflow job.
+      location: Location of the Dataflow job. If not set, defaults to
+        ``'us-central1'``.
+      python_module_path: The GCS path to the Python file to run.
+      temp_location: A GCS path for Dataflow to stage temporary job files
+        created during the execution of the pipeline.
+      requirements_file_path: The GCS path to the pip requirements file.
+      args: The list of args to pass to the Python file. Can include additional
+        parameters for the Dataflow Runner.
+  """
+  # fmt: on
+  dataflow_python_task = dataflow_python_component(
+      project=project,
+      python_module_path=python_module_path,
+      temp_location=temp_location,
+      location=location,
+      requirements_file_path=requirements_file_path,
+      args=args,
+  )
+
+  cancel_job_op = CancelJobOp(
+      gcp_resources=dataflow_python_task.outputs['gcp_resources']
+  ).ignore_upstream_failure()
+
+  outputs = NamedTuple('outputs', gcp_resources=str)
+  return outputs(dataflow_python_task.outputs['gcp_resources'])
