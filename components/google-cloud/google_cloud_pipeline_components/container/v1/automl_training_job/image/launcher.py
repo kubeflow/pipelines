15c24e344f4c624d151116e548c470be935b8e70
Changyu Zhu <changyuz@google.com>
2023-10-09 16:38:43 -0700

    feat(components): Switch v1 AutoMLImageTrainingJob to use the pipeline remote runner
    PiperOrigin-RevId: 572073436

--- components/google-cloud/google_cloud_pipeline_components/container/v1/automl_training_job/image/launcher.py
+++ components/google-cloud/google_cloud_pipeline_components/container/v1/automl_training_job/image/launcher.py
@@ -13,20 +13,197 @@
 # limitations under the License.
 """GCP launcher for AutoML image training jobs based on the AI Platform SDK."""
 
+import argparse
+import json
 import logging
 import sys
+from typing import List
 
 from google_cloud_pipeline_components.container.v1.automl_training_job.image import remote_runner
 from google_cloud_pipeline_components.container.v1.gcp_launcher.utils import parser_util
 
 
-def _parse_args(args):
+def _parse_args(args: List[str]):
   """Parse command line arguments."""
-  _, parsed_args = parser_util.parse_default_args(args)
-  return vars(parsed_args)
+  args.append('--payload')
+  args.append('"{}"')  # Unused but required by parser_util.
+  parser, _ = parser_util.parse_default_args(args)
+  # Parse the conditionally required arguments
+  parser.add_argument(
+      '--display_name',
+      dest='display_name',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--prediction_type',
+      dest='prediction_type',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--multi_label',
+      dest='multi_label',
+      type=parser_util.parse_bool,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--model_type',
+      dest='model_type',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--labels',
+      dest='labels',
+      type=json.loads,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--dataset',
+      dest='dataset',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--disable_early_stopping',
+      dest='disable_early_stopping',
+      type=parser_util.parse_bool,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--training_encryption_spec_key_name',
+      dest='training_encryption_spec_key_name',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--model_encryption_spec_key_name',
+      dest='model_encryption_spec_key_name',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--model_display_name',
+      dest='model_display_name',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--training_fraction_split',
+      dest='training_fraction_split',
+      type=float,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--validation_fraction_split',
+      dest='validation_fraction_split',
+      type=float,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--test_fraction_split',
+      dest='test_fraction_split',
+      type=float,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--budget_milli_node_hours',
+      dest='budget_milli_node_hours',
+      type=int,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--training_filter_split',
+      dest='training_filter_split',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--validation_filter_split',
+      dest='validation_filter_split',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--test_filter_split',
+      dest='test_filter_split',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--base_model',
+      dest='base_model',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--incremental_train_base_model',
+      dest='incremental_train_base_model',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--parent_model',
+      dest='parent_model',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--is_default_version',
+      dest='is_default_version',
+      type=parser_util.parse_bool,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--model_version_aliases',
+      dest='model_version_aliases',
+      type=json.loads,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--model_version_description',
+      dest='model_version_description',
+      type=str,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parser.add_argument(
+      '--model_labels',
+      dest='model_labels',
+      type=json.loads,
+      required=False,
+      default=argparse.SUPPRESS,
+  )
+  parsed_args, _ = parser.parse_known_args(args)
+  args_dict = vars(parsed_args)
+  del args_dict['payload']
+  return args_dict
 
 
-def main(argv):
+def main(argv: List[str]):
   """Main entry.
 
   Expected input args are as follows:
@@ -34,9 +211,9 @@ def main(argv):
     Region - Required. The region of which the resource will be launched.
     Type - Required. GCP launcher is a single container. This Enum will
         specify which resource to be launched.
-    Request payload - Required. The full serialized json of the resource spec.
-        Note this can contain the Pipeline Placeholders.
     gcp_resources - placeholder output for returning job_id.
+    Extra arguments - For constructing request payload. See remote_runner.py for
+    more information.
 
   Args:
     argv: A list of system arguments.
