244de1ac34bbbfce802b0dfbb227638bd00eaa07
Googler <nobody@google.com>
2023-10-12 15:17:03 -0700

    feat(components): [text2sql] Initialize  preprocess component and integrate with text2sql pipeline
    PiperOrigin-RevId: 573025616

--- components/google-cloud/google_cloud_pipeline_components/_implementation/model_evaluation/text2sql/evaluation_llm_text2sql_pipeline.py
+++ components/google-cloud/google_cloud_pipeline_components/_implementation/model_evaluation/text2sql/evaluation_llm_text2sql_pipeline.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 """Text2SQL evaluation pipeline."""
 
+from google_cloud_pipeline_components import _placeholders
+from google_cloud_pipeline_components._implementation.model_evaluation.text2sql_preprocess.component import text2sql_evaluation_preprocess as Text2SQLEvaluationPreprocessOp
 from google_cloud_pipeline_components.types import artifact_types
 import kfp
 
@@ -22,14 +24,48 @@ _PIPELINE_NAME = 'evaluation_llm_text2sql_pipeline'
 
 @kfp.dsl.pipeline(name=_PIPELINE_NAME)
 def evaluation_llm_text2sql_pipeline(
-    location: str,
     model_name: str,
+    evaluation_data_source_path: str,
+    tables_metadata_path: str,
+    prompt_template_path: str = '',
+    project: str = _placeholders.PROJECT_ID_PLACEHOLDER,
+    location: str = _placeholders.LOCATION_PLACEHOLDER,
+    machine_type: str = 'e2-highmem-16',
+    service_account: str = '',
+    network: str = '',
+    encryption_spec_key_name: str = '',
 ):
   """The LLM Evaluation Text2SQL Pipeline.
 
   Args:
-    location: Required. The GCP region that runs the pipeline components.
-    model_name: The path for model to generate embeddings.
+    model_name: The Model used to run text2sql evaluation. Must be a publisher
+      model or a managed Model sharing the same ancestor location. Starting this
+      job has no impact on any existing deployments of the Model and their
+      resources. Supported model is publishers/google/models/text-bison.
+    evaluation_data_source_path: Required. The path for json file containing
+      text2sql evaluation input dataset, including natural language question,
+      ground truth SQL / SQL results.
+    tables_metadata_path: Required. The path for json file containing database
+      metadata, including table names, schema fields.
+    prompt_template_path: Required. The path for json file containing prompt
+      template. Will provide default value if users do not sepecify.
+    project: Optional. The GCP project that runs the pipeline components.
+      Default value is the same project used to run the pipeline.
+    location: Optional. The GCP region that runs the pipeline components.
+      Default value is the same location used to run the pipeline.
+    machine_type: The machine type of this custom job. If not set, defaulted to
+      `e2-highmem-16`. More details:
+      https://cloud.google.com/compute/docs/machine-resource
+    service_account: Service account to run the dataflow job. If not set,
+      dataflow will use the default worker service account. For more details,
+      see
+      https://cloud.google.com/dataflow/docs/concepts/security-and-permissions#default_worker_service_account
+    network: Dataflow's fully qualified subnetwork name, when empty the default
+      subnetwork will be used. More details:
+      https://cloud.google.com/dataflow/docs/guides/specifying-networks#example_network_and_subnetwork_specifications
+    encryption_spec_key_name: Customer-managed encryption key options for the
+      CustomJob. If this is set, then all resources created by the CustomJob
+      will be encrypted with the provided encryption key.
   """
 
   get_vertex_model_task = kfp.dsl.importer(
@@ -40,3 +76,15 @@ def evaluation_llm_text2sql_pipeline(
       metadata={'resourceName': model_name},
   )
   get_vertex_model_task.set_display_name('get-vertex-model')
+
+  _ = Text2SQLEvaluationPreprocessOp(
+      project=project,
+      location=location,
+      evaluation_data_source_path=evaluation_data_source_path,
+      tables_metadata_path=tables_metadata_path,
+      prompt_template_path=prompt_template_path,
+      machine_type=machine_type,
+      service_account=service_account,
+      network=network,
+      encryption_spec_key_name=encryption_spec_key_name,
+  )
