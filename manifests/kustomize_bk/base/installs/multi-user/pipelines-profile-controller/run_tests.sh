1a4d1101a841c91bdd105206b05fc8af62e7c7ed
Alin Spinu <spinualin@gmail.com>
2023-10-12 16:37:43 +0300

    rm bk folder
    Signed-off-by: Alin Spinu <spinualin@gmail.com>

--- manifests/kustomize_bk/base/installs/multi-user/pipelines-profile-controller/run_tests.sh
+++
@@ -1,9 +0,0 @@
-# Build venv with required packages
-VENV=".venv"
-PYTHON_VENV="${VENV}/bin/python"
-python -m venv $VENV
-$PYTHON_VENV -m pip install -U pip
-$PYTHON_VENV -m pip install -r requirements-dev.txt
-
-# Run tests
-$PYTHON_VENV -m pytest ./test_sync.py
