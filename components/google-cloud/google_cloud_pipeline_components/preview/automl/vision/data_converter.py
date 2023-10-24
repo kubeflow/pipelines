11b47cffb61c38c28541c11556518e19049ad15f
Changyu Zhu <changyuz@google.com>
2023-10-24 13:57:24 -0700

    chore(components): Update AutoML Vision data converter component
    PiperOrigin-RevId: 576269528

--- components/google-cloud/google_cloud_pipeline_components/preview/automl/vision/data_converter.py
+++ components/google-cloud/google_cloud_pipeline_components/preview/automl/vision/data_converter.py
@@ -27,7 +27,7 @@ def data_converter(
     input_file_path: str,
     input_file_type: str,
     objective: str,
-    output_dir: str,
+    output_dir: dsl.OutputPath(str),
     gcp_resources: dsl.OutputPath(str),
     location: str = 'us-central1',
     timeout: str = '604800s',
@@ -36,10 +36,6 @@ def data_converter(
     output_shape: Optional[str] = None,
     split_ratio: Optional[str] = None,
     num_shard: Optional[str] = None,
-    output_fps: Optional[int] = None,
-    num_frames: Optional[int] = None,
-    min_duration_sec: Optional[float] = None,
-    pos_neg_ratio: Optional[float] = None,
     encryption_spec_key_name: str = '',
     project: str = _placeholders.PROJECT_ID_PLACEHOLDER,
 ):
@@ -51,21 +47,17 @@ def data_converter(
     input_file_path: Input file path. Please refer to different input formats in Vertex AI Documentation. For example, [image classification prepare data](https://cloud.google.com/vertex-ai/docs/image-data/classification/prepare-data) page.
     input_file_type: 'csv', 'jsonl', or 'coco_json'. Must be one of the input file types supported by the objective.
     objective: One of 'icn', 'iod', 'isg', 'vcn', or 'var'.
-    output_dir: Cloud Storage directory for storing converted data and pipeline information.
     location: Location for creating the custom training job. If not set, default to us-central1.
     timeout: The maximum job running time. The default is 7 days. A duration in seconds with up to nine fractional digits, terminated by 's', for example: "3.5s".
     service_account: Sets the default service account for workload run-as account. The [service account](https://cloud.google.com/vertex-ai/docs/pipelines/configure-project#service-account) running the pipeline submitting jobs must have act-as permission on this run-as account. If unspecified, the Vertex AI Custom Code [Service Agent ](https://cloud.google.com/vertex-ai/docs/general/access-control#service-agents) for the CustomJob's project.
     machine_type: [Machine type](https://cloud.google.com/vertex-ai/docs/training/configure-compute#machine-types) for the CustomJob. If conversion failed, consider using a machine type with more RAM or splitting dataset into smaller pieces.
-    output_shape: Video only. Output shape (height,width) for video frames.
+    output_shape: Output shape (height,width) for images.
     split_ratio: Proportion of data to split into train/validation/test, separated by comma.
     num_shard: Number of train/validation/test shards, separated by comma.
-    output_fps: Video only. Output frames per second.
-    num_frames: VAR only. Number of frames inside a single video clip window.
-    min_duration_sec: VAR only. Minimum duration of a video clip annotation in seconds.
-    pos_neg_ratio: VAR only. Sampling ratio between positive and negative segments.
     encryption_spec_key_name: Customer-managed encryption key options for the CustomJob. If this is set, then all resources created by the CustomJob will be encrypted with the provided encryption key.
     project: Project to create the custom training job in. Defaults to the project in which the PipelineJob is run.
   Returns:
+    output_dir: Cloud Storage directory storing converted data and pipeline information.
     gcp_resources: Serialized JSON of `gcp_resources` [proto](https://github.com/kubeflow/pipelines/tree/master/components/google-cloud/google_cloud_pipeline_components/proto) which tracks the CustomJob.
   """
   # fmt: on
@@ -99,6 +91,7 @@ def data_converter(
                   ' "us-docker.pkg.dev/vertex-ai/vertex-vision-model-garden-dockers/data-converter",'
               ),
               '"args": [',
+              '"--enable_input_validation","true",',
               '"--input_file_path", "',
               input_file_path,
               '",',
@@ -129,30 +122,6 @@ def data_converter(
                       [',"--num_shard","', num_shard, '"']
                   ),
               ),
-              dsl.IfPresentPlaceholder(
-                  input_name='output_fps',
-                  then=dsl.ConcatPlaceholder(
-                      [',"--output_fps","', output_fps, '"']
-                  ),
-              ),
-              dsl.IfPresentPlaceholder(
-                  input_name='num_frames',
-                  then=dsl.ConcatPlaceholder(
-                      [',"--num_frames","', num_frames, '"']
-                  ),
-              ),
-              dsl.IfPresentPlaceholder(
-                  input_name='min_duration_sec',
-                  then=dsl.ConcatPlaceholder(
-                      [',"--min_duration_sec","', min_duration_sec, '"']
-                  ),
-              ),
-              dsl.IfPresentPlaceholder(
-                  input_name='pos_neg_ratio',
-                  then=dsl.ConcatPlaceholder(
-                      [',"--pos_neg_ratio","', pos_neg_ratio, '"']
-                  ),
-              ),
               ']}}],',
               '"scheduling": {',
               '"timeout": "',
@@ -165,12 +134,8 @@ def data_converter(
                       ['"service_account": "', service_account, '",']
                   ),
               ),
-              '"enable_web_access": false,',
-              '"base_output_directory": {',
-              '"output_uri_prefix": "',
-              output_dir,
-              '"',
-              '}},',
+              '"enable_web_access": false',
+              '},',
               '"encryption_spec": {',
               '"kms_key_name": "',
               encryption_spec_key_name,
