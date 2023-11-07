480f30db5f52ae7d544ba33595acb30a88e7528f
Googler <nobody@google.com>
2023-11-07 11:56:02 -0800

    chore(components): Update image
    PiperOrigin-RevId: 580254777

--- components/google-cloud/google_cloud_pipeline_components/v1/automl/tabular/infra_validator.py
+++ components/google-cloud/google_cloud_pipeline_components/v1/automl/tabular/infra_validator.py
@@ -32,7 +32,7 @@ def automl_tabular_infra_validator(
   # fmt: on
 
   return dsl.ContainerSpec(
-      image='us-docker.pkg.dev/vertex-ai/automl-tabular/prediction-server:20231002_0125',
+      image='us-docker.pkg.dev/vertex-ai/automl-tabular/prediction-server:20231029_0125',
       command=[],
       args=['--executor_input', '{{$}}'],
   )
--- components/google-cloud/google_cloud_pipeline_components/v1/automl/tabular/training_configurator_and_validator.py
+++ components/google-cloud/google_cloud_pipeline_components/v1/automl/tabular/training_configurator_and_validator.py
@@ -97,7 +97,7 @@ def training_configurator_and_validator(
   # fmt: on
 
   return dsl.ContainerSpec(
-      image='us-docker.pkg.dev/vertex-ai/automl-tabular/feature-transform-engine:20231002_0125',
+      image='us-docker.pkg.dev/vertex-ai/automl-tabular/feature-transform-engine:20231029_0125',
       command=[],
       args=[
           'training_configurator_and_validator',
