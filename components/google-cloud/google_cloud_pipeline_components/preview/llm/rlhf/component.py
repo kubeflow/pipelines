2b769a956af35e8bb54f4f7b878c4b4ac94638ab
Googler <nobody@google.com>
2023-11-15 10:52:16 -0800

    feat(components): Group `preview.llm.rlhf_pipeline` components for more readability
    PiperOrigin-RevId: 582733817

--- components/google-cloud/google_cloud_pipeline_components/preview/llm/rlhf/component.py
+++ components/google-cloud/google_cloud_pipeline_components/preview/llm/rlhf/component.py
@@ -13,15 +13,13 @@
 # limitations under the License.
 """Defines a pipeline that performs reinforcement learning from human feedback."""
 
-import json
 from typing import NamedTuple, Optional
 
 from google_cloud_pipeline_components import _placeholders
-from google_cloud_pipeline_components._implementation.llm import deploy_llm_model
+from google_cloud_pipeline_components._implementation.llm import deployment_graph
 from google_cloud_pipeline_components._implementation.llm import function_based
 from google_cloud_pipeline_components._implementation.llm import reinforcement_learning_graph
 from google_cloud_pipeline_components._implementation.llm import reward_model_graph
-from google_cloud_pipeline_components._implementation.llm import upload_llm_model
 from google_cloud_pipeline_components.preview.llm.infer import component
 import kfp
 
@@ -80,11 +78,6 @@ def rlhf_pipeline(
     endpoint_resource_name: Path the Online Prediction Endpoint. This will be an empty string if the model was not deployed.
   """
   # fmt: on
-  upload_location = 'us-central1'
-  reference_model_metadata = function_based.resolve_reference_model_metadata(
-      large_model_reference=large_model_reference,
-  ).set_display_name('Resolve Model Metadata')
-
   reward_model_pipeline = (
       reward_model_graph.pipeline(
           preference_dataset=preference_dataset,
@@ -134,56 +127,16 @@ def rlhf_pipeline(
         instruction=instruction,
     )
 
-  adapter_artifact = kfp.dsl.importer(
-      artifact_uri=rl_model_pipeline.outputs['output_adapter_path'],
-      artifact_class=kfp.dsl.Artifact,
-  ).set_display_name('Import Tuned Adapter')
-  regional_endpoint = function_based.resolve_regional_endpoint(
-      upload_location=upload_location
-  ).set_display_name('Resolve Regional Endpoint')
-  display_name = function_based.resolve_model_display_name(
-      large_model_reference=reference_model_metadata.outputs[
-          'large_model_reference'
-      ],
+  llm_model_handler = deployment_graph.pipeline(
+      output_adapter_path=rl_model_pipeline.outputs['output_model_path'],
+      large_model_reference=large_model_reference,
       model_display_name=model_display_name,
-  ).set_display_name('Resolve Model Display Name')
-  upload_model = function_based.resolve_upload_model(
-      large_model_reference=reference_model_metadata.outputs[
-          'large_model_reference'
-      ]
-  ).set_display_name('Resolve Upload Model')
-  upload_task = (
-      upload_llm_model.upload_llm_model(
-          project=_placeholders.PROJECT_ID_PLACEHOLDER,
-          location=upload_location,
-          regional_endpoint=regional_endpoint.output,
-          artifact_uri=adapter_artifact.output,
-          model_display_name=display_name.output,
-          model_reference_name='text-bison@001',
-          upload_model=upload_model.output,
-      )
-      .set_env_variable(
-          name='VERTEX_AI_PIPELINES_RUN_LABELS',
-          value=json.dumps({'tune-type': 'rlhf'}),
-      )
-      .set_display_name('Upload Model')
-  )
-  deploy_model = function_based.resolve_deploy_model(
       deploy_model=deploy_model,
-      large_model_reference=reference_model_metadata.outputs[
-          'large_model_reference'
-      ],
-  ).set_display_name('Resolve Deploy Model')
-  deploy_task = deploy_llm_model.create_endpoint_and_deploy_model(
-      project=_placeholders.PROJECT_ID_PLACEHOLDER,
-      location=upload_location,
-      model_resource_name=upload_task.outputs['model_resource_name'],
-      display_name=display_name.output,
-      regional_endpoint=regional_endpoint.output,
-      deploy_model=deploy_model.output,
-  ).set_display_name('Deploy Model')
+  ).set_display_name('Upload and Deploy Tuned Model')
 
   return PipelineOutput(
-      model_resource_name=upload_task.outputs['model_resource_name'],
-      endpoint_resource_name=deploy_task.outputs['endpoint_resource_name'],
+      model_resource_name=llm_model_handler.outputs['model_resource_name'],
+      endpoint_resource_name=llm_model_handler.outputs[
+          'endpoint_resource_name'
+      ],
   )
