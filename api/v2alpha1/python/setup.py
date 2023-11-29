49243a382f53c624558275dfb2ced64f4828959e
Connor McCarthy <mccarthy.connor.james@gmail.com>
2023-11-29 18:18:03 -0500

    chore(sdk): support Python 3.12 (#10272)
    * chore(sdk): support Python 3.12
    
    * fix kfp-kubernetes test import error
    
    * use assertEqual instead of assertEquals
    
    * fix kfp-kubernetes snapshot yaml

--- api/v2alpha1/python/setup.py
+++ api/v2alpha1/python/setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
     author_email='kubeflow-pipelines@google.com',
     url='https://github.com/kubeflow/pipelines',
     packages=setuptools.find_namespace_packages(include=['kfp.*']),
-    python_requires='>=3.7.0',
+    python_requires='>=3.7.0,<3.13.0',
     install_requires=['protobuf>=3.13.0,<4'],
     include_package_data=True,
     license='Apache 2.0',
