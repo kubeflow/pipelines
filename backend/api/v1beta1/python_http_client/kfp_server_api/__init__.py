a226de2197f4188ecf066cecd13ad1027dd2f681
Chen Sun <chensun@users.noreply.github.com>
2023-12-01 00:04:16 +0000

    chore(release): bumped version to 2.0.4

--- backend/api/v1beta1/python_http_client/kfp_server_api/__init__.py
+++ backend/api/v1beta1/python_http_client/kfp_server_api/__init__.py
@@ -14,7 +14,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "2.0.3"
+__version__ = "2.0.4"
 
 # import apis into sdk package
 from kfp_server_api.api.experiment_service_api import ExperimentServiceApi
