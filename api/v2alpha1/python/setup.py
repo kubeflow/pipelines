61458ad524db4e4676f4c21cee9f05ca88677f71
connor-mccarthy <mccarthy.connor.james@gmail.com>
2023-12-12 17:41:20 -0500

    chore(sdk): depend on protobuf 4 in `kfp-pipeline-spec`

--- api/v2alpha1/python/setup.py
+++ api/v2alpha1/python/setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     url='https://github.com/kubeflow/pipelines',
     packages=setuptools.find_namespace_packages(include=['kfp.*']),
     python_requires='>=3.7.0,<3.13.0',
-    install_requires=['protobuf>=3.13.0,<4'],
+    install_requires=['protobuf>=4.21.1,<5'],
     include_package_data=True,
     license='Apache 2.0',
 )
