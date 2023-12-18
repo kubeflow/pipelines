5541992810b7aa37f1dd86fa93650c83039a8abc
Connor McCarthy <mccarthy.connor.james@gmail.com>
2023-12-18 18:50:37 -0500

    feat(sdk): add local execution logging #localexecution (#10326)

--- sdk/python/kfp/local/task_dispatcher.py
+++ sdk/python/kfp/local/task_dispatcher.py
@@ -12,12 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Code for dispatching a local task execution."""
+import logging
 from typing import Any, Dict
 
 from kfp import local
 from kfp.local import config
 from kfp.local import executor_input_utils
 from kfp.local import executor_output_utils
+from kfp.local import logging_utils
 from kfp.local import placeholder_utils
 from kfp.local import status
 from kfp.local import subprocess_task_handler
@@ -108,33 +110,57 @@ def _run_single_component_implementation(
                 subprocess_task_handler.SubprocessTaskHandler,
         }
     TaskHandler = task_handler_map[runner_type]
-    # TODO: add logging throughout for observability of state, execution progress, outputs, errors, etc.
-    task_handler = TaskHandler(
-        image=image,
-        full_command=full_command,
-        pipeline_root=pipeline_root,
-        runner=runner,
-    )
 
-    task_status = task_handler.run()
-
-    if task_status == status.Status.SUCCESS:
-        outputs = executor_output_utils.get_outputs_for_task(
-            executor_input=executor_input,
-            component_spec=component_spec,
+    with logging_utils.local_logger_context():
+        task_name_for_logs = logging_utils.color_text(
+            f'{task_resource_name!r}',
+            logging_utils.Color.CYAN,
         )
 
-    elif task_status == status.Status.FAILURE:
-        msg = f'Local execution exited with status {task_status.name}.'
-        if raise_on_error:
-            raise RuntimeError(msg)
+        logging.info(f'Executing task {task_name_for_logs}')
+        task_handler = TaskHandler(
+            image=image,
+            full_command=full_command,
+            pipeline_root=pipeline_root,
+            runner=runner,
+        )
+
+        # trailing newline helps visually separate subprocess logs
+        logging.info(f'Streamed logs:\n')
+
+        with logging_utils.indented_print():
+            # subprocess logs printed here
+            task_status = task_handler.run()
+
+        if task_status == status.Status.SUCCESS:
+            logging.info(
+                f'Task {task_name_for_logs} finished with status {logging_utils.color_text(task_status.value, logging_utils.Color.GREEN)}'
+            )
+
+            outputs = executor_output_utils.get_outputs_for_task(
+                executor_input=executor_input,
+                component_spec=component_spec,
+            )
+            if outputs:
+                output_string = [
+                    f'Task {task_name_for_logs} outputs:',
+                    *logging_utils.make_log_lines_for_outputs(outputs),
+                    '\n',
+                ]
+                logging.info('\n'.join(output_string))
+            else:
+                logging.info(f'Task {task_name_for_logs} has no outputs')
+
+        elif task_status == status.Status.FAILURE:
+            msg = f'Task {task_name_for_logs} finished with status {logging_utils.color_text(task_status.value, logging_utils.Color.RED)}'
+            if raise_on_error:
+                raise RuntimeError(msg)
+            else:
+                logging.error(msg)
+            outputs = {}
+
         else:
-            # TODO: replace with robust logging
-            print(msg)
-        outputs = {}
+            # for developers; user should never hit this
+            raise ValueError(f'Got unknown status: {task_status}')
 
-    else:
-        # for developers; user should never hit this
-        raise ValueError(f'Got unknown status: {task_status}')
-
-    return outputs
+        return outputs
