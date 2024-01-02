21631f064e0cad3ba3ceb11c611a06fa6eeab744
Connor McCarthy <mccarthy.connor.james@gmail.com>
2024-01-02 18:00:14 -0500

    fix(sdk): fix presentation of strings in local execution #localexecution (#10353)

--- sdk/python/kfp/local/logging_utils_test.py
+++ sdk/python/kfp/local/logging_utils_test.py
@@ -61,8 +61,8 @@ class TestRenderArtifact(unittest.TestCase):
     def test_empty(self):
         actual = logging_utils.make_log_lines_for_artifact(dsl.Artifact())
         expected = [
-            'Artifact( name=,',
-            '          uri=,',
+            "Artifact( name='',",
+            "          uri='',",
             '          metadata={} )',
         ]
         self.assertListEqual(actual, expected)
@@ -79,8 +79,8 @@ class TestRenderArtifact(unittest.TestCase):
                     'float_field': 3.14
                 }))
         expected = [
-            'Model( name=my_artifact,',
-            '       uri=/local/foo/bar,',
+            "Model( name='my_artifact',",
+            "       uri='/local/foo/bar',",
             "       metadata={'dict_field': {'baz': 'bat'}, 'float_field': 3.14} )",
         ]
         self.assertListEqual(actual, expected)
@@ -105,7 +105,7 @@ class TestMakeLogLinesForOutputs(unittest.TestCase):
             }
         })
         expected = [
-            '    foo: bar',
+            "    foo: 'bar'",
             '    baz: 100',
             '    bat: 1.0',
             '    brap: True',
@@ -136,14 +136,14 @@ class TestMakeLogLinesForOutputs(unittest.TestCase):
                 ),
         })
         expected = [
-            '    my_artifact: Artifact( name=,',
-            '                           uri=,',
+            "    my_artifact: Artifact( name='',",
+            "                           uri='',",
             '                           metadata={} )',
-            '    my_model: Model( name=my_artifact,',
-            '                     uri=/local/foo/bar/1234567890/1234567890/1234567890/1234567890/1234567890,',
+            "    my_model: Model( name='my_artifact',",
+            "                     uri='/local/foo/bar/1234567890/1234567890/1234567890/1234567890/1234567890',",
             "                     metadata={'dict_field': {'baz': 'bat'}, 'float_field': 3.14} )",
-            '    my_dataset: Dataset( name=my_dataset,',
-            '                         uri=/local/foo/baz,',
+            "    my_dataset: Dataset( name='my_dataset',",
+            "                         uri='/local/foo/baz',",
             '                         metadata={} )',
         ]
         self.assertListEqual(actual, expected)
@@ -182,20 +182,20 @@ class TestMakeLogLinesForOutputs(unittest.TestCase):
                 ),
         })
         expected = [
-            '    foo: bar',
+            "    foo: 'bar'",
             '    baz: 100',
             '    bat: 1.0',
             '    brap: True',
             '    my_list: [1, 2, 3]',
             "    my_dict: {'foo': 'bar'}",
-            '    my_artifact: Artifact( name=,',
-            '                           uri=,',
+            "    my_artifact: Artifact( name='',",
+            "                           uri='',",
             '                           metadata={} )',
-            '    my_model: Model( name=my_artifact,',
-            '                     uri=/local/foo/bar/1234567890/1234567890/1234567890/1234567890/1234567890,',
+            "    my_model: Model( name='my_artifact',",
+            "                     uri='/local/foo/bar/1234567890/1234567890/1234567890/1234567890/1234567890',",
             "                     metadata={'dict_field': {'baz': 'bat'}, 'float_field': 3.14} )",
-            '    my_dataset: Dataset( name=my_dataset,',
-            '                         uri=/local/foo/baz,',
+            "    my_dataset: Dataset( name='my_dataset',",
+            "                         uri='/local/foo/baz',",
             '                         metadata={} )',
         ]
 
