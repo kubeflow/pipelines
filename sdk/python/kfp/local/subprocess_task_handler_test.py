5a9a3b036c43522cf097f7d51507b13d8b1a23fe
Connor McCarthy <mccarthy.connor.james@gmail.com>
2024-01-05 15:52:08 -0500

    fix(sdk): permit empty local execution outputs #localexecution (#10338)

--- sdk/python/kfp/local/subprocess_task_handler_test.py
+++ sdk/python/kfp/local/subprocess_task_handler_test.py
@@ -404,6 +404,19 @@ class TestLightweightPythonComponentLogic(
         self.assertEqual(task.outputs['out_param'], 'HelloHello')
         self.assertEqual(task.outputs['Output'], 1)
 
+    def test_outputpath_result_not_written(self):
+        local.init(runner=local.SubprocessRunner(use_venv=True))
+
+        # use dsl.OutputPath(int) for more thorough testing
+        # want to ensure that the code that converts protobuf number to
+        # Python int permits unwritten outputs
+        @dsl.component
+        def my_comp(out_param: dsl.OutputPath(int)):
+            pass
+
+        task = my_comp()
+        self.assertEmpty(task.outputs)
+
 
 if __name__ == '__main__':
     unittest.main()
