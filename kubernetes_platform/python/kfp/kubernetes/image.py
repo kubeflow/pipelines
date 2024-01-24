d10b3c123422122a9121d961f12260f847e39ca6
Josep-Sampe-Domenech <josep.sampe.domenech@ibm.com>
2024-01-24 17:30:23 +0100

    Update kfp backend and kubernetes sdk to support ImagePullSecrets

---
+++ kubernetes_platform/python/kfp/kubernetes/image.py
@@ -0,0 +1,48 @@
+# Copyright 2024 The Kubeflow Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import List
+
+from google.protobuf import json_format
+from kfp.dsl import PipelineTask
+from kfp.kubernetes import common
+from kfp.kubernetes import kubernetes_executor_config_pb2 as pb
+
+
+def set_image_pull_secrets(
+    task: PipelineTask,
+    secret_names: List[str],
+) -> PipelineTask:
+    """Set image pull secrets for a Kubernetes task.
+
+    Args:
+        task: Pipeline task.
+        secret_names: List of image pull secret names.
+
+    Returns:
+        Task object with updated image pull secret configuration.
+    """
+
+    msg = common.get_existing_kubernetes_config_as_message(task)
+
+    # Assuming secret_names is a list of strings
+    image_pull_secrets = [
+        pb.ImagePullSecret(name=secret_name) for secret_name in secret_names
+    ]
+
+    msg.image_pull_secrets.extend(image_pull_secrets)
+
+    task.platform_config['kubernetes'] = json_format.MessageToDict(msg)
+
+    return task
