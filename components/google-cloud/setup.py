c803a69d85c4b09d3c04596c9a9b6ddd3923b124
Connor McCarthy <cjmccarthy@google.com>
2024-01-25 14:54:10 -0800

    chore(components): fix GCPC documentation dependencies issue
    PiperOrigin-RevId: 601574718

--- components/google-cloud/setup.py
+++ components/google-cloud/setup.py
@@ -69,10 +69,10 @@ setuptools.setup(
         ] + [
             "commonmark==0.9.1",
             "autodocsumm==0.2.9",
-            "sphinx==5.0.2",
+            "sphinx>=5.0.2,<6.0.0",
             "sphinx-immaterial==0.9.0",
-            "sphinx-rtd-theme==1.0.0",
-            "m2r2==0.3.3",
+            "sphinx-rtd-theme==2.0.0",
+            "m2r2==0.3.3.post2",
             "sphinx-notfound-page==0.8.3",
         ],
     },
