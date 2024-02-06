5ce793bb034d3b12d38af6a8112ca7c08336dbfa
Ricky(Yuxuan) Xie <rickyxie@google.com>
2024-02-05 22:20:29 -0800

    fix format sort

--- sdk/python/test_data/pipelines/pipeline_with_parallelfor_parallelism.py
+++ sdk/python/test_data/pipelines/pipeline_with_parallelfor_parallelism.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import tempfile
 import os
-
+import tempfile
 from typing import Dict, List
+
 from kfp import compiler
 from kfp import dsl
 from kfp.components import load_component_from_file
