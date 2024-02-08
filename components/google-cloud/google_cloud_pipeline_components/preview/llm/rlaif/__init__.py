6c943533748d2ddcd198af0cbdbf53233b0a9e78
Googler <nobody@google.com>
2024-02-08 12:36:00 -0800

    feat(components): Add RLAIF pipeline to preview
    PiperOrigin-RevId: 605396378

--- components/google-cloud/google_cloud_pipeline_components/preview/llm/rlaif/__init__.py
+++ components/google-cloud/google_cloud_pipeline_components/preview/llm/rlaif/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2023 The Kubeflow Authors. All Rights Reserved.
+# Copyright 2024 The Kubeflow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,14 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Large-language model preview components."""
-
-from google_cloud_pipeline_components.preview.llm.infer.component import infer_pipeline
-from google_cloud_pipeline_components.preview.llm.rlaif.component import rlaif_pipeline
-from google_cloud_pipeline_components.preview.llm.rlhf.component import rlhf_pipeline
-
-__all__ = [
-    'infer_pipeline',
-    'rlhf_pipeline',
-    'rlaif_pipeline',
-]
