b3978c1e98a6aa119d5411315dd6ebe8d79ef0f9
Tommy Li <Tommy.chaoping.li@ibm.com>
2024-02-09 13:15:01 -0800

    feat(Backend + SDK): Update kfp backend and kubernetes sdk to support pod labels and annotations (#10393)
    * update kfp kubernetes sdk to include pod labels and annotations
    
    * fix unit test output order
    
    * add podmetadata changes
    
    * update argo compiler to support pod metadata
    
    * update tests
    
    * update go mod to use the latest kubernetes_platform package
    
    * update licenses
    
    * address comments
    
    * update kubernetes_platform package to include the latest spec
    
    ---------
    
    Co-authored-by: Chen Sun <chensun@users.noreply.github.com>

--- kubernetes_platform/python/kfp/kubernetes/__init__.py
+++ kubernetes_platform/python/kfp/kubernetes/__init__.py
@@ -21,9 +21,13 @@ __all__ = [
     'use_secret_as_env',
     'use_secret_as_volume',
     'add_node_selector',
+    'add_pod_label',
+    'add_pod_annotation',
     'set_image_pull_secrets'
 ]
 
+from kfp.kubernetes.pod_metadata import add_pod_label
+from kfp.kubernetes.pod_metadata import add_pod_annotation
 from kfp.kubernetes.node_selector import add_node_selector
 from kfp.kubernetes.secret import use_secret_as_env
 from kfp.kubernetes.secret import use_secret_as_volume
