0d0d7ebbabbe840b830120a0539d438f0907fb4e
KevinGrantLee <kglee@google.com>
2024-02-12 15:35:29 -0800

    fix(sdk): Prevents dsl.ParallelFor over single paramter from compiling.

--- sdk/python/kfp/dsl/for_loop_test.py
+++ sdk/python/kfp/dsl/for_loop_test.py
@@ -147,6 +147,24 @@ class ForLoopTest(parameterized.TestCase):
     @parameterized.parameters(
         {
             'channel':
+                pipeline_channel.PipelineParameterChannel(
+                    name='param1',
+                    channel_type='String',
+                    task_name='task1',
+                ),
+        },)
+    def test_loop_parameter_argument_from_single_pipeline_channel_raises_error(
+            self, channel):
+        with self.assertRaisesRegex(
+                ValueError,
+                r'Cannot iterate over a single Parameter using `dsl\.ParallelFor`\. Expected a list of Parameters as argument to `items`\.'
+        ):
+            loop_argument = for_loop.LoopParameterArgument.from_pipeline_channel(
+                channel)
+
+    @parameterized.parameters(
+        {
+            'channel':
                 pipeline_channel.PipelineArtifactChannel(
                     name='param1',
                     channel_type='system.Artifact@0.0.1',
