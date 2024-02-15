81a8f2f5ead547d8bbdaae30eb8c7bec7dee16c0
Googler <nobody@google.com>
2024-02-15 13:23:27 -0800

    chore(components): Sync AutoML components
    PiperOrigin-RevId: 607435076

--- components/google-cloud/google_cloud_pipeline_components/v1/automl/tabular/infra_validator.py
+++ components/google-cloud/google_cloud_pipeline_components/v1/automl/tabular/infra_validator.py
@@ -32,7 +32,7 @@ def automl_tabular_infra_validator(
   # fmt: on
 
   return dsl.ContainerSpec(
-      image='us-docker.pkg.dev/vertex-ai/automl-tabular/prediction-server:20240119_0125',
+      image='us-docker.pkg.dev/vertex-ai/automl-tabular/prediction-server:20240214_1325',
       command=[],
       args=['--executor_input', '{{$}}'],
   )
