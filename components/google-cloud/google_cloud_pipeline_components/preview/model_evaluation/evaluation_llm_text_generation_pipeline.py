e664a461b440792d82f6a05051f62bdd37334a53
Googler <nobody@google.com>
2024-03-20 11:39:22 -0700

    fix(components): Fix missing pipeline parameters
    PiperOrigin-RevId: 617579452

--- components/google-cloud/google_cloud_pipeline_components/preview/model_evaluation/evaluation_llm_text_generation_pipeline.py
+++ components/google-cloud/google_cloud_pipeline_components/preview/model_evaluation/evaluation_llm_text_generation_pipeline.py
@@ -59,17 +59,17 @@ def evaluation_llm_text_generation_pipeline(  # pylint: disable=dangerous-defaul
     project: Required. The GCP project that runs the pipeline components.
     location: Required. The GCP region that runs the pipeline components.
     batch_predict_gcs_source_uris: Required. Google Cloud Storage URI(s) to your eval dataset instances data to run batch prediction on. The instances data should also contain the ground truth (target) data, used for evaluation. May contain wildcards. For more information on [wildcards](https://cloud.google.com/storage/docs/gsutil/addlhelp/WildcardNames). For more details about this [input config](https://cloud.google.com/vertex-ai/docs/reference/rest/v1/projects.locations.batchPredictionJobs#InputConfig). The content of gcs source files should be preset to one of the following formats:
-    1) Prediction & Evaluation Dataset format, guaranteeing "prompt" and "ground_truth" attributes are included
-    {
-      "prompt": "your input/prompt text",
-      "ground_truth": "your ground truth output text"
-    }
-    or
-    2) Tuning Dataset format, guaranteeing "input_text" and "output_text" attributes are included.
-    {
-      "input_text": "your input/prompt text",
-      "output_text": "your ground truth output text"
-    }
+        1) Prediction & Evaluation Dataset format, guaranteeing "prompt" and "ground_truth" attributes are included
+        {
+          "prompt": "your input/prompt text",
+          "ground_truth": "your ground truth output text"
+        }
+        or
+        2) Tuning Dataset format, guaranteeing "input_text" and "output_text" attributes are included.
+        {
+          "input_text": "your input/prompt text",
+          "output_text": "your ground truth output text"
+        }
     batch_predict_gcs_destination_output_uri: Required. The Google Cloud Storage location of the directory where the eval pipeline output is to be written to.
     model_name: The Model name used to run evaluation. Must be a publisher Model or a managed Model sharing the same ancestor location. Starting this job has no impact on any existing deployments of the Model and their resources.
     evaluation_task: The task that the large language model will be evaluated on. The evaluation component computes a set of metrics relevant to that specific task. Currently supported tasks are: `summarization`, `question-answering`, `text-generation`.
