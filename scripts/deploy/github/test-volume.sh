33c54867126946ddcd567fdfa424ee85f462f112
Ricardo M. Oliveira <rmartine@redhat.com>
2024-04-16 18:10:17 -0300

    Add kfp-tekton integration tests and manifests
    Signed-off-by: Ricardo M. Oliveira <rmartine@redhat.com>

---
+++ scripts/deploy/github/test-volume.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright 2023 kubeflow.org
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+KUBEFLOW_NS="${KUBEFLOW_NS:-kubeflow}"
+
+C_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$C_DIR" ]]; then C_DIR="$PWD"; fi
+source "${C_DIR}/test-pipeline.sh"
+
+# need kfp-kubernetes for this test case
+# unfortunately, we can't install it from kubernetes_platform/python
+pip install kfp-kubernetes
+
+RESULT=0
+run_test_case "use-volume" "samples/v2/pipeline_with_volume.py" "SUCCEEDED" 10 || RESULT=$?
+
+STATUS_MSG=PASSED
+if [[ "$RESULT" -ne 0 ]]; then
+  STATUS_MSG=FAILED
+fi
