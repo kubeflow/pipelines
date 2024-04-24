19b15284b200e56d3b08d564b4517314f3115ebd
rickyxie0929 <rickyxie@google.com>
2024-04-24 14:28:57 -0700

    fix test
    Signed-off-by: rickyxie0929 <rickyxie@google.com>

--- sdk/python/kfp/dsl/component_factory.py
+++ sdk/python/kfp/dsl/component_factory.py
@@ -537,7 +537,7 @@ def create_component_from_func(
     if base_image is None:
         base_image = _DEFAULT_BASE_IMAGE
         warnings.warn(
-            ("Python 3.8 has reached end-of-life. The default base_image used by the @dsl.component decorator will switch from 'python:3.8' to 'python:3.9' in Dec, 2024. To ensure your existing components work with versions of the KFP SDK released after that date, you should provide an explicit base_image argument and ensure your component works as intended on Python 3.9."
+            ("Python 3.8 has reached end-of-life. The default base_image used by the @dsl.component decorator will switch from 'python:3.8' to 'python:3.9' in Oct, 2024. To ensure your existing components work with versions of the KFP SDK released after that date, you should provide an explicit base_image argument and ensure your component works as intended on Python 3.9."
             ),
             FutureWarning,
             stacklevel=2,
