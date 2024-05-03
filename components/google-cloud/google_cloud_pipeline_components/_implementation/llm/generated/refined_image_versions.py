7f48b52e5e6aeb34957e1d141305c7dc2849096c
Googler <nobody@google.com>
2024-05-02 19:12:41 -0700

    feat(components): create utility class for preprocessors and use it in rlhf preprocessor and infer preprocessor
    PiperOrigin-RevId: 630245237

--- components/google-cloud/google_cloud_pipeline_components/_implementation/llm/generated/refined_image_versions.py
+++ components/google-cloud/google_cloud_pipeline_components/_implementation/llm/generated/refined_image_versions.py
@@ -17,4 +17,4 @@
 DO NOT EDIT - This file is generated, manual changes will be overridden.
 """
 
-IMAGE_TAG = '20240430_1158_RC00'
+IMAGE_TAG = '20240502_1327_RC00'
--- components/google-cloud/google_cloud_pipeline_components/_implementation/llm/infer_preprocessor.py
+++ components/google-cloud/google_cloud_pipeline_components/_implementation/llm/infer_preprocessor.py
@@ -21,17 +21,57 @@ from kfp import dsl
 
 @dsl.container_component
 def infer_preprocessor(
+    large_model_reference: str,
+    accelerator_type: str,
+    use_test_spec: bool,
+    project: str,
+    location: str,
+    artifact_registry: str,
+    tag: str,
     gcp_resources: dsl.OutputPath(str),  # pytype: disable=invalid-annotation
+    metadata_large_model_reference: dsl.OutputPath(str),  # pytype: disable=invalid-annotation
+    metadata_reference_model_path: dsl.OutputPath(str),  # pytype: disable=invalid-annotation
+    metadata_reward_model_reference: dsl.OutputPath(str),  # pytype: disable=invalid-annotation
+    metadata_reward_model_path: dsl.OutputPath(str),  # pytype: disable=invalid-annotation
+    metadata_machine_type: dsl.OutputPath(str),  # pytype: disable=invalid-annotation
+    metadata_tuning_location: dsl.OutputPath(str),  # pytype: disable=invalid-annotation
+    metadata_accelerator_type: dsl.OutputPath(str),  # pytype: disable=invalid-annotation
+    metadata_accelerator_count: dsl.OutputPath(int),  # pytype: disable=invalid-annotation
+    metadata_instruction: dsl.OutputPath(str),  # pytype: disable=invalid-annotation
+    metadata_refined_image_uri: dsl.OutputPath(str),  # pytype: disable=invalid-annotation
+    use_experimental_image: bool = False,
+    input_reference_model_path: str = '',
+    instruction: str = '',
     image_uri: str = utils.get_default_image_uri('refined_cpu', ''),
 ) -> dsl.ContainerSpec:  # pylint: disable=g-doc-args
   # fmt: off
   """Preprocess infer pipeline inputs.
 
   Args:
-    app_name: The preprocessor app name.
+    large_model_reference: The model for fine tuning.
+    accelerator_type: Specific accelerator type for the job.
+    use_test_spec: Whether to use a lower resource machine for testing.
+    project: Project that contains the artifact registry.
+    location: Region that contains the artifact registry.
+    artifact_registry: Registry that contains Docker images.
+    tag: Image tag.
+    use_experimental_image:  Whether to use refined experimental image.
+    input_reference_model_path: The model checkpoint path for the reference model
+    instruction: The instruction to let the model know what task it needs to perform.
+    image_uri: Docker image URI to use for the custom job.
 
   Returns:
     gcp_resources: GCP resources that can be used to track the custom job.
+    metadata_large_model_reference: The base model for fine tuning. The name should be in capitalized snake case format.
+    metadata_reference_model_path: The model checkpoint path for the reinforcer model
+    metadata_reward_model_reference:  The base model for training reward model. The name should be in capitalized snake case format.
+    metadata_reward_model_path: The model checkpoint path for the reward model.
+    metadata_machine_type: The type of the machine to provision for the custom job.
+    metadata_tuning_location: The GCP region to run the custom job.
+    metadata_accelerator_type: Specific accelerator type for the custom job.
+    metadata_accelerator_count: The number of accelerator.
+    metadata_instruction: The instruction to let the model know what task it needs to perform.
+    metadata_refined_image_uri: Docker image URI to use for the custom job.
   """
   # fmt: on
   return gcpc_utils.build_serverless_customjob_container_spec(
@@ -43,6 +83,26 @@ def infer_preprocessor(
           image_uri=image_uri,
           args=[
               '--app_name=infer_preprocessor',
+              f'--large_model_reference={large_model_reference}',
+              f'--input_reference_model_path={input_reference_model_path}',
+              f'--accelerator_type={accelerator_type}',
+              f'--use_test_spec={use_test_spec}',
+              f'--project={project}',
+              f'--location={location}',
+              f'--artifact_registry={artifact_registry}',
+              f'--tag={tag}',
+              f'--use_experimental_image={use_experimental_image}',
+              f'--instruction={instruction}',
+              f'--metadata_large_model_reference_path={metadata_large_model_reference}',
+              f'--metadata_reference_model_path_path={metadata_reference_model_path}',
+              f'--metadata_reward_model_reference_path={metadata_reward_model_reference}',
+              f'--metadata_reward_model_path_path={metadata_reward_model_path}',
+              f'--metadata_machine_type_path={metadata_machine_type}',
+              f'--metadata_tuning_location_path={metadata_tuning_location}',
+              f'--metadata_accelerator_type_path={metadata_accelerator_type}',
+              f'--metadata_accelerator_count_path={metadata_accelerator_count}',
+              f'--metadata_instruction_path={metadata_instruction}',
+              f'--metadata_refined_image_uri_path={metadata_refined_image_uri}',
           ],
       ),
       gcp_resources=gcp_resources,
