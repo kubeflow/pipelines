4c402d7f328467b89ab8c007838d26992299abb2
rchojn <rafalchojnacki0502@gmail.com>
2024-05-21 13:45:11 +0200

    fixes tests pass
    Signed-off-by: rchojn <rafalchojnacki0502@gmail.com>

--- sdk/python/kfp/client/client.py
+++ sdk/python/kfp/client/client.py
@@ -1444,7 +1444,8 @@ class Client:
             ``V2beta1PipelineVersion`` object.
         """
 
-        if not any([pipeline_id, pipeline_name]):
+        if all([pipeline_id, pipeline_name
+            ]) or not any([pipeline_id, pipeline_name]):
             raise ValueError('Either pipeline_id or pipeline_name is required.')
 
         if pipeline_name:
