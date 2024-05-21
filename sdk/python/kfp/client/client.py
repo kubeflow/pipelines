6c8e05dffed42c106eded8fdd4d7badb814a16af
rchojn <rafalchojnacki0502@gmail.com>
2024-05-21 14:31:58 +0200

    fixing formatting
    Signed-off-by: rchojn <rafalchojnacki0502@gmail.com>

--- sdk/python/kfp/client/client.py
+++ sdk/python/kfp/client/client.py
@@ -1445,7 +1445,7 @@ class Client:
         """
 
         if all([pipeline_id, pipeline_name
-            ]) or not any([pipeline_id, pipeline_name]):
+               ]) or not any([pipeline_id, pipeline_name]):
             raise ValueError('Either pipeline_id or pipeline_name is required.')
 
         if pipeline_name:
