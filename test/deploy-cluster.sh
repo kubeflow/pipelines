77a944d0fd6538167cb575cddc96f1119129faf1
hbelmiro <helber.belmiro@gmail.com>
2024-06-08 12:25:01 -0300

    Updated cluster version
    Signed-off-by: hbelmiro <helber.belmiro@gmail.com>

--- test/deploy-cluster.sh
+++ test/deploy-cluster.sh
@@ -86,15 +86,8 @@ else
     # reference: https://cloud.google.com/compute/docs/access/service-accounts#accesscopesiam
     SCOPE_ARG="--scopes=storage-rw,cloud-platform"
   fi
-  # Use regular release channel to keep up with newly created clusters in Google Cloud Marketplace.
-  # TODO(#9706): Switch back to regular channel once we stop building test images via dind.
-  # Temporarily use cos as image type until docker dependencies gets removed. 
-  # reference: https://github.com/kubeflow/pipelines/issues/6696
-  # Hard-coded GKE to 1.25.10-gke.1200 (the latest 1.25 in STABLE channel). Reference: 
-  # https://github.com/kubeflow/pipelines/issues/9704#issuecomment-1622310358
-  # 08/09/2023 update: 1.25.10-gke.1200 no longer supported, use 1.25.10-gke.2100 instead. Reference:
-  # https://cloud.google.com/kubernetes-engine/docs/release-notes-nochannel#2023-r17_version_updates
-  gcloud container clusters create ${TEST_CLUSTER} --image-type cos_containerd --release-channel stable --cluster-version 1.25 ${SCOPE_ARG} ${NODE_POOL_CONFIG_ARG} ${WI_ARG}
+  # Check the available versions in https://cloud.google.com/kubernetes-engine/docs/release-schedule#schedule-for-release-channels
+  gcloud container clusters create ${TEST_CLUSTER} --image-type cos_containerd --release-channel stable --cluster-version 1.26 ${SCOPE_ARG} ${NODE_POOL_CONFIG_ARG} ${WI_ARG}
 fi
 
 gcloud container clusters get-credentials ${TEST_CLUSTER}
