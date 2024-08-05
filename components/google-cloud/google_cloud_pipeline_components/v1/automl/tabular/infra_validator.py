65f6475156da446ea06d608159d5f84ce876f964
Googler <nobody@google.com>
2024-08-05 13:05:52 -0700

    chore(components): Bump image version for Structured Data pipelines
    Signed-off-by: Googler <nobody@google.com>
    PiperOrigin-RevId: 659655574

--- components/google-cloud/google_cloud_pipeline_components/v1/automl/tabular/infra_validator.py
+++ components/google-cloud/google_cloud_pipeline_components/v1/automl/tabular/infra_validator.py
@@ -32,7 +32,7 @@ def automl_tabular_infra_validator(
   # fmt: on
 
   return dsl.ContainerSpec(
-      image='us-docker.pkg.dev/vertex-ai/automl-tabular/prediction-server:20240710_0625',
+      image='us-docker.pkg.dev/vertex-ai/automl-tabular/prediction-server:20240804_0625',
       command=[],
       args=['--executor_input', '{{$}}'],
   )
