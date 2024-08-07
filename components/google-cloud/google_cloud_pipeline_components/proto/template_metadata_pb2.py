5d3c8343d488e47310bf9040af59ec972673d676
Googler <nobody@google.com>
2024-08-06 17:11:24 -0700

    feat(components): internal
    Signed-off-by: Googler <nobody@google.com>
    PiperOrigin-RevId: 660156029

--- components/google-cloud/google_cloud_pipeline_components/proto/template_metadata_pb2.py
+++ components/google-cloud/google_cloud_pipeline_components/proto/template_metadata_pb2.py
@@ -5,18 +5,9 @@
 """Generated protocol buffer code."""
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
-from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
 
-_runtime_version.ValidateProtobufRuntimeVersion(
-    _runtime_version.Domain.GOOGLE_INTERNAL,
-    0,
-    20240806,
-    0,
-    '',
-    './template_metadata.proto',
-)
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
