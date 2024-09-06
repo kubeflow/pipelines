4f526236ce5ba058b96e1e2f5e50b222ba661f14
violetzu <134059758+violetzu@users.noreply.github.com>
2024-09-07 01:10:44 +0800

    Update QPE.sh
    Signed-off-by: violetzu <134059758+violetzu@users.noreply.github.com>

--- samples/contrib/QPE_pipeline_example/QPE.sh
+++ samples/contrib/QPE_pipeline_example/QPE.sh
@@ -25,4 +25,4 @@ popd
 
 # The -u flag makes python output unbuffered, so that we can see real time log.
 # Reference: https://stackoverflow.com/a/107717
-python3 -u .samples/contrib/QPE_pipeline_example/QPE.py
+python3 -u ./samples/contrib/QPE_pipeline_example/QPE.py
