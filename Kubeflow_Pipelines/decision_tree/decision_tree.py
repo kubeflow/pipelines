e85997020d249d1528fdfad9738f9e4d8f4816ba
chris454656 <166201621+chris454656@users.noreply.github.com>
2024-09-18 10:45:48 +0800

    Add files via upload
    Signed-off-by: chris <chris20120826@gmail.com>

---
+++ Kubeflow_Pipelines/decision_tree/decision_tree.py
@@ -0,0 +1,54 @@
+import json
+
+import argparse
+from pathlib import Path
+
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+
+def _decision_tree(args):
+
+    # Open and reads file "data"
+    with open(args.data) as data_file:
+        data = json.load(data_file)
+    
+    # The excted data type is 'dict', however since the file
+    # was loaded as a json object, it is first loaded as a string
+    # thus we need to load again from such string in order to get 
+    # the dict-type object.
+    data = json.loads(data)
+
+    x_train = data['x_train']
+    y_train = data['y_train']
+    x_test = data['x_test']
+    y_test = data['y_test']
+    
+    # Initialize and train the model
+    model = DecisionTreeClassifier(max_depth=3)
+    model.fit(x_train, y_train)
+
+    # Get predictions
+    y_pred = model.predict(x_test)
+    
+    # Get accuracy
+    accuracy = accuracy_score(y_test, y_pred)
+
+    # Save output into file
+    with open(args.accuracy, 'w') as accuracy_file:
+        accuracy_file.write(str(accuracy))
+
+
+
+if __name__ == '__main__':
+
+    # Defining and parsing the command-line arguments
+    parser = argparse.ArgumentParser(description='My program description')
+    parser.add_argument('--data', type=str)
+    parser.add_argument('--accuracy', type=str)
+
+    args = parser.parse_args()
+
+    # Creating the directory where the output file will be created (the directory may or may not exist).
+    Path(args.accuracy).parent.mkdir(parents=True, exist_ok=True)
+    
+    _decision_tree(args)
