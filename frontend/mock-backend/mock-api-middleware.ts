90b4a374b512280570517d1b44d04de0920a4533
Elay Aharoni (EXT-Nokia) <elay.aharoni.ext@nokia.com>
2024-09-19 14:54:35 +0300

    Show Pod Events in KFP v2 UI
    Signed-off-by: Elay Aharoni (EXT-Nokia) <elay.aharoni.ext@nokia.com>

--- frontend/mock-backend/mock-api-middleware.ts
+++ frontend/mock-backend/mock-api-middleware.ts
@@ -727,6 +727,22 @@ export default (app: express.Application) => {
     }, 300);
   });
 
+  app.get('/k8s/pod/events', (req, res) => {
+    const podName = decodeURIComponent(req.query.podname);
+    if (podName === 'json-12abc') {
+      res.status(404).send('pod not found');
+      return;
+    }
+    if (podName === 'coinflip-recursive-q7dqb-3721646052') {
+      res.status(500).send('Failed to retrieve events');
+      return;
+    }
+    const events = {};
+    setTimeout(() => {
+      res.send(events);
+    }, 300);
+  });
+
   app.get('/visualizations/allowed', (req, res) => {
     res.send(true);
   });
