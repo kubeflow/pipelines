e92358b1d50d41ea68abcf42da43165acc5a09e0
Marie Stephen Leo <35026793+stephenleo@users.noreply.github.com>
2024-09-21 20:13:42 +0800

    Adding ability to deploy on spot instances
    Signed-off-by: Marie Stephen Leo <35026793+stephenleo@users.noreply.github.com>

--- components/google-cloud/google_cloud_pipeline_components/v1/endpoint/deploy_model/component.py
+++ components/google-cloud/google_cloud_pipeline_components/v1/endpoint/deploy_model/component.py
@@ -36,6 +36,7 @@ def model_deploy(
     dedicated_resources_max_replica_count: int = 0,
     dedicated_resources_accelerator_type: str = '',
     dedicated_resources_accelerator_count: int = 0,
+    dedicated_resources_spot: bool = False,
     automatic_resources_min_replica_count: int = 0,
     automatic_resources_max_replica_count: int = 0,
     service_account: str = '',
@@ -57,6 +58,7 @@ def model_deploy(
       dedicated_resources_accelerator_count: The number of accelerators to attach to a worker replica.
       dedicated_resources_min_replica_count: The minimum number of machine replicas this DeployedModel will be always deployed on. This value must be greater than or equal to 1. If traffic against the DeployedModel increases, it may dynamically be deployed onto more replicas, and as traffic decreases, some of these extra replicas may be freed.
       dedicated_resources_max_replica_count: The maximum number of replicas this deployed model may the larger value of min_replica_count or 1 will be used. If value provided is smaller than min_replica_count, it will automatically be increased to be min_replica_count. The maximum number of replicas this deployed model may be deployed on when the traffic against it increases. If requested value is too large, the deployment will error, but if deployment succeeds then the ability to scale the model to that many replicas is guaranteed (barring service outages). If traffic against the deployed model increases beyond what its replicas at maximum may handle, a portion of the traffic will be dropped. If this value is not provided, will use `dedicated_resources_min_replica_count` as the default value.
+      dedicated_resources_spot: If true, schedule the deployment workload on spot VMs.
       automatic_resources_min_replica_count: The minimum number of replicas this DeployedModel will be always deployed on. If traffic against it increases, it may dynamically be deployed onto more replicas up to `automatic_resources_max_replica_count`, and as traffic decreases, some of these extra replicas may be freed. If the requested value is too large, the deployment will error.  This field is required if `dedicated_resources_machine_type` is not specified.
       automatic_resources_max_replica_count: The maximum number of replicas this DeployedModel may be deployed on when the traffic against it increases. If the requested value is too large, the deployment will error, but if deployment succeeds then the ability to scale the model to that many replicas is guaranteed (barring service outages). If traffic against the DeployedModel increases beyond what its replicas at maximum may handle, a portion of the traffic will be dropped. If this value is not provided, a no upper bound for scaling under heavy traffic will be assume, though Vertex AI may be unable to scale beyond certain replica number.
       service_account: The service account that the DeployedModel's container runs as. Specify the email address of the service account. If this service account is not specified, the container runs as a service account that doesn't have access to the resource project.  Users deploying the Model must have the `iam.serviceAccounts.actAs` permission on this service account.
@@ -113,6 +115,8 @@ def model_deploy(
               automatic_resources_min_replica_count,
               ', "max_replica_count": ',
               automatic_resources_max_replica_count,
+              ', "spot": ',
+              dedicated_resources_spot,
               '}',
               ', "service_account": "',
               service_account,
