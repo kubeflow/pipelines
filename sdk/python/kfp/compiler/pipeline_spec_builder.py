04216812bb253c7100f519b60e53dbcfeda63d71
Greg Sheremeta <gshereme@redhat.com>
2024-10-29 14:08:11 -0400

    feat(sdk): add PipelineConfig to DSL to re-implement pipeline-level config (#11112)
    KFP v1 supported setting pipeline-level configuration via a
    `PipelineConf` class. This class was deprecated and no replacement
    was added to KFP v2.
    
    add new PipelineConfig class to support setting pipeline-level
    configuration in KFP v2.
    
    Signed-off-by: Greg Sheremeta <gshereme@redhat.com>

--- sdk/python/kfp/compiler/pipeline_spec_builder.py
+++ sdk/python/kfp/compiler/pipeline_spec_builder.py
@@ -28,6 +28,7 @@ from kfp.compiler import compiler_utils
 from kfp.dsl import component_factory
 from kfp.dsl import for_loop
 from kfp.dsl import pipeline_channel
+from kfp.dsl import pipeline_config
 from kfp.dsl import pipeline_context
 from kfp.dsl import pipeline_task
 from kfp.dsl import placeholders
@@ -1879,6 +1880,7 @@ def create_pipeline_spec(
     pipeline: pipeline_context.Pipeline,
     component_spec: structures.ComponentSpec,
     pipeline_outputs: Optional[Any] = None,
+    pipeline_config: pipeline_config.PipelineConfig = None,
 ) -> Tuple[pipeline_spec_pb2.PipelineSpec, pipeline_spec_pb2.PlatformSpec]:
     """Creates a pipeline spec object.
 
@@ -1886,6 +1888,7 @@ def create_pipeline_spec(
         pipeline: The instantiated pipeline object.
         component_spec: The component spec structures.
         pipeline_outputs: The pipeline outputs via return.
+        pipeline_config: The pipeline config object.
 
     Returns:
         A PipelineSpec proto representing the compiled pipeline.
@@ -1947,6 +1950,10 @@ def create_pipeline_spec(
     )
 
     platform_spec = pipeline_spec_pb2.PlatformSpec()
+    if pipeline_config is not None:
+        _merge_pipeline_config(
+            pipelineConfig=pipeline_config, platformSpec=platform_spec)
+
     for group in all_groups:
         build_spec_by_group(
             pipeline_spec=pipeline_spec,
@@ -2060,6 +2067,17 @@ def write_pipeline_spec_to_file(
     else:
         raise ValueError(
             f'The output path {package_path} should end with ".yaml".')
+
+
+def _merge_pipeline_config(pipelineConfig: pipeline_config.PipelineConfig,
+                           platformSpec: pipeline_spec_pb2.PlatformSpec):
+    # TODO: add pipeline config options (ttl, semaphore, etc.) to the dict
+    # json_format.ParseDict(
+    #     {'pipelineConfig': {
+    #         '<some pipeline config option>': pipelineConfig.<get that value>,
+    #     }}, platformSpec.platforms['kubernetes'])
+
+    return platformSpec
 
 
 def extract_comments_from_pipeline_spec(pipeline_spec: dict,
