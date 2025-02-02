18a54aabf2aea8188214990c1bdd40ce8471c2c2
chahatsagarmain <chahatsagar2003@gmail.com>
2025-02-02 21:10:06 +0530

    add workflow
    Signed-off-by: chahatsagarmain <chahatsagar2003@gmail.com>

--- backend/src/agent/persistence/persistence_agent.go
+++ backend/src/agent/persistence/persistence_agent.go
@@ -26,11 +26,13 @@ import (
 	"github.com/kubeflow/pipelines/backend/src/agent/persistence/worker"
 	"github.com/kubeflow/pipelines/backend/src/common/util"
 	log "github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	k8sclient "sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
@@ -84,7 +86,7 @@ func NewPersistenceAgent(
 	// Set up the controller to watch both ScheduledWorkflow and Workflow resources.
 	if err = ctrl.NewControllerManagedBy(mgr).
 		For(&util.ScheduledWorkflow{}).
-		// Watches(&source.Kind{Type: &workflowregister.Workflow{}}, &handler.EnqueueRequestForObject{}).
+		Watches(&Workflow{},&handler.EnqueueRequestForObject{}).
 		Complete(reconciler); err != nil {
 		return nil, err
 	}
@@ -118,22 +120,51 @@ func (r *PersistenceAgent) Reconcile(
 	}
 
 	// If not found, try to fetch a Workflow.
-	// wf := &util.Workflow{}
-	// err = r.Get(ctx, req.NamespacedName, wf)
-	// if err == nil {
-	//     r.logger.Info("Reconciling Workflow", "namespace", wf.Namespace, "name", wf.Name)
-	// 	key := fmt.Sprintf("%s/%s", wf.Namespace, wf.Name)
-	// 	if err := r.wfsaver.Save(key, wf.Namespace, wf.Name, nowEpoch); err != nil {
-	// 		if util.HasCustomCode(err, util.CUSTOM_CODE_TRANSIENT) {
-	// 			r.logger.Error(err, "Transient error saving Workflow; will retry", "key", key)
-	// 			return reconcile.Result{RequeueAfter: 1 * time.Second}, err
-	// 		}
-	// 		r.logger.Error(err, "Permanent error saving Workflow", "key", key)
-	// 		return reconcile.Result{}, nil
-	// 	}
-	// 	return reconcile.Result{}, nil
-	// }
+	wf := &Workflow{}
+	err = r.Get(ctx, req.NamespacedName, wf)
+	if err == nil {
+	    r.logger.Info("Reconciling Workflow", "namespace", wf.Namespace, "name", wf.Name)
+		key := fmt.Sprintf("%s/%s", wf.Namespace, wf.Name)
+		if err := r.wfsaver.Save(key, wf.Namespace, wf.Name, nowEpoch); err != nil {
+			if util.HasCustomCode(err, util.CUSTOM_CODE_TRANSIENT) {
+				r.logger.Error(err, "Transient error saving Workflow; will retry", "key", key)
+				return reconcile.Result{RequeueAfter: 1 * time.Second}, err
+			}
+			r.logger.Error(err, "Permanent error saving Workflow", "key", key)
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, nil
+	}
 
 	r.logger.Info("Object not found; may have been deleted", "key", req.NamespacedName)
 	return reconcile.Result{}, nil
+}
+
+type Workflow struct {
+	util.Workflow
+}
+
+// Override SetAnnotations on our Workflow type.
+// This method will be used instead of util.Workflow's version.
+func (w *Workflow) SetAnnotations(val map[string]string) {
+	if w.Annotations == nil {
+		w.Annotations = make(map[string]string)
+	}
+
+	for key, value := range val {
+		w.Annotations[key] = value
+	}
+}
+
+func (w *Workflow) SetLabels(val map[string]string) {
+	if w.Labels == nil {
+		w.Labels = make(map[string]string)
+	}
+	for key, value := range val {
+		w.Labels[key] = value
+	}
+}
+
+func (w *Workflow) SetOwnerReferences(refs []metav1.OwnerReference) {
+	w.ObjectMeta.OwnerReferences = refs
 }
