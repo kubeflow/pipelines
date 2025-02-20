ea29b2790cf27522020536c279ea9cd1b0a38804
Vani Haripriya Mudadla <vmudadla@redhat.com>
2025-02-19 23:49:42 -0600

    fix(backend): fixes DAG status update to reflect completion of all tasks (#11651)
    Signed-off-by: VaniHaripriya <vmudadla@redhat.com>

--- backend/src/v2/driver/driver.go
+++ backend/src/v2/driver/driver.go
@@ -979,6 +979,10 @@ func DAG(ctx context.Context, opts Options, mlmd *metadata.Client) (execution *E
 	ecfg.OutputArtifacts = opts.Component.GetDag().GetOutputs().GetArtifacts()
 	glog.V(4).Info("outputArtifacts: ", ecfg.OutputArtifacts)
 
+	totalDagTasks := len(opts.Component.GetDag().GetTasks())
+	ecfg.TotalDagTasks = &totalDagTasks
+	glog.V(4).Info("totalDagTasks: ", *ecfg.TotalDagTasks)
+
 	if opts.Task.GetArtifactIterator() != nil {
 		return execution, fmt.Errorf("ArtifactIterator is not implemented")
 	}
