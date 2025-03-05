3a89bd8564e12891a25161568680c4ec3a018631
Humair Khan <HumairAK@users.noreply.github.com>
2025-03-04 19:07:06 -0500

    chore(release): bumped version to 2.4.1 (#11718)
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

--- backend/api/v2beta1/python_http_client/kfp_server_api/configuration.py
+++ backend/api/v2beta1/python_http_client/kfp_server_api/configuration.py
@@ -351,8 +351,8 @@ conf = kfp_server_api.Configuration(
         return "Python SDK Debug Report:\n"\
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
-               "Version of the API: 2.4.0\n"\
-               "SDK Package Version: 2.4.0".\
+               "Version of the API: 2.4.1\n"\
+               "SDK Package Version: 2.4.1".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
