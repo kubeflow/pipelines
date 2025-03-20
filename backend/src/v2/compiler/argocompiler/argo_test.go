61a051c1bdd74d93e23bc358a0a8912f90be3f01
VaniHaripriya <vmudadla@redhat.com>
2025-03-20 11:55:55 -0500

    test: Add test for pipeline TTL in PlatformSpec
    Signed-off-by: VaniHaripriya <vmudadla@redhat.com>

--- backend/src/v2/compiler/argocompiler/argo_test.go
+++ backend/src/v2/compiler/argocompiler/argo_test.go
@@ -37,6 +37,7 @@ func Test_argo_compiler(t *testing.T) {
 		platformSpecPath string // path of possible input PlatformSpec to compile
 		argoYAMLPath     string // path of expected output argo workflow YAML
 		envVars          map[string]string
+		opts             *argocompiler.Options
 	}{
 		{
 			jobPath:          "../testdata/hello_world.json",
@@ -80,6 +81,12 @@ func Test_argo_compiler(t *testing.T) {
 			argoYAMLPath:     "testdata/hello_world_log_level.yaml",
 			envVars:          map[string]string{"PIPELINE_LOG_LEVEL": "3"},
 		},
+		{
+			jobPath:          "../testdata/hello_world.json",
+			platformSpecPath: "../testdata/hello_world_ttl_platform.json",
+			argoYAMLPath:     "testdata/hello_world_ttl.yaml",
+			opts:             &argocompiler.Options{TtlSeconds: 60},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(fmt.Sprintf("%+v", tt), func(t *testing.T) {
@@ -100,10 +107,10 @@ func Test_argo_compiler(t *testing.T) {
 					}
 				}
 			}()
-
+			var opts *argocompiler.Options = tt.opts
 			job, platformSpec := load(t, tt.jobPath, tt.platformSpecPath)
 			if *update {
-				wf, err := argocompiler.Compile(job, platformSpec, nil)
+				wf, err := argocompiler.Compile(job, platformSpec, opts)
 				if err != nil {
 					t.Fatal(err)
 				}
@@ -120,7 +127,7 @@ func Test_argo_compiler(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
-			wf, err := argocompiler.Compile(job, platformSpec, nil)
+			wf, err := argocompiler.Compile(job, platformSpec, opts)
 			if err != nil {
 				t.Error(err)
 			}
