2369c541a1a164fe6174c79d5ba8db61577b142b
Humair Khan <HumairAK@users.noreply.github.com>
2025-03-28 16:30:29 -0400

    feat(backend/sdk): Add input parameterization for various k8s resources  (#11770)
    * add backend support for k8s platform inputs
    
    This change adds driver support for input parameter support for the
    kubernetes platform spec. Input resolution change is extracted and made
    more generic so it may be re-used when building out the container spec
    for the k8s config.
    
    Also add unit tests for constant & runtime input parameters.
    TaskOutput parameter support are omitted due to a lack of appropriate
    mlmd mock framework.
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * add sdk implementation for k8s params inputs
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * add tests for k8s input params
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * add setup/teardown of prereqs and secret tests
    
    and update/re-enable secret env tests
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * have kfp sample tests use local python pkgs
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * use a better configmap k8s name input...
    
    Add support for multiple input types for pull secrets.
    Clarify toleration docstring
    Remove unnecessary resolve function
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

--- samples/v2/pipeline_with_secret_as_env.py
+++ samples/v2/pipeline_with_secret_as_env.py
@@ -14,6 +14,8 @@
 """A pipeline that passes a secret as an env variable to a container."""
 from kfp import dsl
 from kfp import kubernetes
+from kfp.dsl import OutputPath
+import os
 
 # Note: this sample will only work if this secret is pre-created before running this pipeline.
 # Is is pre-created by default only in the Google Cloud distribution listed here:
@@ -25,24 +27,39 @@ SECRET_NAME = "user-gcp-sa"
 @dsl.component
 def comp():
     import os
-    import sys
-    if os.environ['SECRET_VAR'] == "service_account":
-        print("Success")
-        return 0
-    else:
-        print(os.environ['SECRET_VAR']  + " is not service_account")
-        sys.exit("Failure: cannot access secret as env variable")
+    username = os.getenv("USER_NAME", "")
+    psw1 = os.getenv("PASSWORD_VAR1", "")
+    psw2 = os.getenv("PASSWORD_VAR2", "")
+    assert username == "user1"
+    assert psw1 == "psw1"
+    assert psw2 == "psw2"
 
 
+@dsl.component
+def generate_secret_name(some_output: OutputPath(str)):
+    secret_name = "test-secret-3"
+    with open(some_output, 'w') as f:
+        f.write(secret_name)
 
+# Secrets are referenced from samples/v2/pre-requisites/test-secrets.yaml
 @dsl.pipeline
-def pipeline_secret_env():
+def pipeline_secret_env(secret_parm: str = "test-secret-1"):
     task = comp()
     kubernetes.use_secret_as_env(
         task,
-        secret_name=SECRET_NAME,
-        secret_key_to_env={'type': 'SECRET_VAR'})
+        secret_name=secret_parm,
+        secret_key_to_env={'username': 'USER_NAME'})
 
+    kubernetes.use_secret_as_env(
+        task,
+        secret_name="test-secret-2",
+        secret_key_to_env={'password': 'PASSWORD_VAR1'})
+
+    task2 = generate_secret_name()
+    kubernetes.use_secret_as_env(
+        task,
+        secret_name=task2.output,
+        secret_key_to_env={'password': 'PASSWORD_VAR2'})
 
 if __name__ == '__main__':
     from kfp import compiler
