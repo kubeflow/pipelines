fd1b48b4712038afe8a78e37843672d4773dc080
Humair Khan <HumairAK@users.noreply.github.com>
2025-03-28 16:30:29 -0400

    feat(backend/sdk): Add input parameterization for various k8s resources  (#11770)
    * add backend support for k8s platform inputs
    
    This change adds driver support for input parameter support for the
    kubernetes platform spec. Input resolution change is extracted and made
    more generic so it may be re-used when building out the container spec
    for the k8s config.
    
    Also add unit tests for constant & runtime input parameters.
    TaskOutput parameter support are omitted due to a lack of appropriate
    mlmd mock framework.
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * add sdk implementation for k8s params inputs
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * add tests for k8s input params
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * add setup/teardown of prereqs and secret tests
    
    and update/re-enable secret env tests
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * have kfp sample tests use local python pkgs
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * use a better configmap k8s name input...
    
    Add support for multiple input types for pull secrets.
    Clarify toleration docstring
    Remove unnecessary resolve function
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

--- kubernetes_platform/python/kfp/kubernetes/common.py
+++ kubernetes_platform/python/kfp/kubernetes/common.py
@@ -12,12 +12,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Union
+
+from kfp.dsl import pipeline_channel
+from kfp.compiler.pipeline_spec_builder import to_protobuf_value
+from kfp.dsl import PipelineTask
 from google.protobuf import json_format
 from kfp.kubernetes import kubernetes_executor_config_pb2 as pb
-
+import pipeline_spec_pb2
 
 def get_existing_kubernetes_config_as_message(
         task: 'PipelineTask') -> pb.KubernetesExecutorConfig:
     cur_k8_config_dict = task.platform_config.get('kubernetes', {})
     k8_config_msg = pb.KubernetesExecutorConfig()
     return json_format.ParseDict(cur_k8_config_dict, k8_config_msg)
+
+
+def parse_k8s_parameter_input(
+        input_param: Union[pipeline_channel.PipelineParameterChannel, str, dict],
+        task: PipelineTask,
+) -> pipeline_spec_pb2.TaskInputsSpec.InputParameterSpec:
+    param_spec = pipeline_spec_pb2.TaskInputsSpec.InputParameterSpec()
+
+    if isinstance(input_param, (str, dict)):
+        param_spec.runtime_value.constant.CopyFrom(to_protobuf_value(input_param))
+    elif isinstance(input_param, pipeline_channel.PipelineParameterChannel):
+        if input_param.task_name is None:
+            param_spec.component_input_parameter = input_param.full_name
+
+        else:
+            param_spec.task_output_parameter.producer_task = input_param.task_name
+            param_spec.task_output_parameter.output_parameter_key = input_param.name
+            if input_param.task:
+                task.after(input_param.task)
+    else:
+        raise ValueError(
+            f'Argument for {"input_param"!r} must be an instance of str, dict, or PipelineChannel. '
+            f'Got unknown input type: {type(input_param)!r}.'
+        )
+
+    return param_spec
