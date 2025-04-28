b44a3900211edd6ae624e9361402dcc3ad19d002
Humair Khan <HumairAK@users.noreply.github.com>
2025-04-28 15:44:53 -0400

    chore(sdk): kfp and kfp-kubernetes 2.13 & 1.5
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

--- kubernetes_platform/python/docs/conf.py
+++ kubernetes_platform/python/docs/conf.py
@@ -140,10 +140,17 @@ html_theme_options = {
     'version_info': [
         {
             'version':
+                'https://kfp-kubernetes.readthedocs.io/en/kfp-kubernetes-1.5.0/',
+            'title':
+                '1.5.0',
+            'aliases': ['stable'],
+        },
+        {
+            'version':
                 'https://kfp-kubernetes.readthedocs.io/en/kfp-kubernetes-1.4.0/',
             'title':
                 '1.4.0',
-            'aliases': ['stable'],
+            'aliases': [],
         },
         {
             'version':
