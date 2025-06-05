8103b1ba0f043737d3814b19ce956182ee2828e7
Nelesh Singla <neleshsingla86@gmail.com>
2025-06-05 13:33:13 -0400

    adding pipeline upload version api tests
    Signed-off-by: Nelesh Singla <117123879+nsingla@users.noreply.github.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED
    
    Signed-off-by: Nelesh Singla <117123879+nsingla@users.noreply.github.com>

--- backend/test/v2/api/matcher/pipeline_matcher.go
+++ backend/test/v2/api/matcher/pipeline_matcher.go
@@ -8,11 +8,21 @@ import (
 
 func MatchPipelines(actual *model.V2beta1Pipeline, expected *model.V2beta1Pipeline) {
 	Expect(actual.PipelineID).To(Not(BeEmpty()), "Pipeline ID is empty")
-	expected.PipelineID = actual.PipelineID
 	actualTime := time.Time(actual.CreatedAt).UTC()
 	expectedTime := time.Time(expected.CreatedAt).UTC()
 	Expect(actualTime.After(expectedTime)).To(BeTrue(), "Actual Pipeline creation time is not as expected")
-	expected.CreatedAt = actual.CreatedAt
-	Expect(actual).To(Equal(expected), "Pipeline Object not matching")
+	Expect(actual.DisplayName).To(Equal(expected.DisplayName), "Pipeline name not matching")
+	Expect(actual.Namespace).To(Equal(expected.Namespace), "Pipeline Namespace not matching")
+	Expect(actual.Description).To(Equal(expected.Description), "Pipeline Description not matching")
 
+}
+
+func MatchPipelineVersions(actual *model.V2beta1PipelineVersion, expected *model.V2beta1PipelineVersion) {
+	Expect(actual.PipelineVersionID).To(Not(Equal(expected.PipelineVersionID)), "Pipeline Version ID is empty")
+	actualTime := time.Time(actual.CreatedAt).UTC()
+	expectedTime := time.Time(expected.CreatedAt).UTC()
+	Expect(actualTime.After(expectedTime)).To(BeTrue(), "Actual Pipeline Version creation time is not as expected")
+	Expect(actual.DisplayName).To(Equal(expected.DisplayName), "Pipeline Display Name not matching")
+	Expect(actual.Description).To(Equal(expected.Description), "Pipeline Description not matching")
+	Expect(actual.PipelineSpec).To(Equal(expected.PipelineSpec), "Pipeline Spec not matching")
 }
