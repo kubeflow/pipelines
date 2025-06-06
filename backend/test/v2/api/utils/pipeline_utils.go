4f980e7ed28610bc7805abe1d8bb6bbce8241464
Nelesh Singla <neleshsingla86@gmail.com>
2025-06-06 14:23:48 -0400

    adding custom report and suite config and moving some utils methods to utility files
    Signed-off-by: Nelesh Singla <117123879+nsingla@users.noreply.github.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- backend/test/v2/api/utils/pipeline_utils.go
+++ backend/test/v2/api/utils/pipeline_utils.go
@@ -18,27 +18,36 @@ import (
 	"fmt"
 	pipeline_params "github.com/kubeflow/pipelines/backend/api/v2beta1/go_http_client/pipeline_client/pipeline_service"
 	"github.com/kubeflow/pipelines/backend/api/v2beta1/go_http_client/pipeline_model"
+	model "github.com/kubeflow/pipelines/backend/api/v2beta1/go_http_client/pipeline_upload_model"
 	api_server "github.com/kubeflow/pipelines/backend/src/common/client/api_server/v2"
+	"github.com/kubeflow/pipelines/backend/src/common/util"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"log"
 )
 
 func ListPipelines(client *api_server.PipelineClient) (
 	[]*pipeline_model.V2beta1Pipeline, int, string, error,
 ) {
 	parameters := &pipeline_params.PipelineServiceListPipelinesParams{}
+	GinkgoWriter.Printf("Listing all pipelines")
 	return client.List(parameters)
 }
 
+/*
+Delete a pipeline by id
+*/
 func DeletePipeline(client *api_server.PipelineClient, pipelineId string) {
-	log.Printf("Deleting all pipeline version of pipeline with id=%s", pipelineId)
+	GinkgoWriter.Printf("Deleting all pipeline version of pipeline with id=%s", pipelineId)
 	DeleteAllPipelineVersions(client, pipelineId)
-	log.Printf("Deleting pipeline with id=%s", pipelineId)
+	GinkgoWriter.Printf("Deleting pipeline with id=%s", pipelineId)
 	err := client.Delete(&pipeline_params.PipelineServiceDeletePipelineParams{PipelineID: pipelineId})
 	Expect(err).NotTo(HaveOccurred(), fmt.Sprintf("Error occurred while deleting pipeline with id=%s", pipelineId))
-	log.Printf("Pipeline with id=%s, DELETED", pipelineId)
+	GinkgoWriter.Printf("Pipeline with id=%s, DELETED", pipelineId)
 }
 
+/*
+Delete all pipelines
+*/
 func DeleteAllPipelines(client *api_server.PipelineClient) {
 	pipelines, _, _, err := ListPipelines(client)
 	Expect(err).NotTo(HaveOccurred(), "Error occurred while listing pipelines")
@@ -51,9 +60,51 @@ func DeleteAllPipelines(client *api_server.PipelineClient) {
 			DeleteAllPipelineVersions(client, pId)
 			deletedPipelines[pId] = true
 		}
+		GinkgoWriter.Printf("Deleting pipeline with id=%s", pId)
 		Expect(client.Delete(&pipeline_params.PipelineServiceDeletePipelineParams{PipelineID: pId})).NotTo(HaveOccurred(), fmt.Sprintf("Error occurred while deleting pipeline with id=%s", pId))
 	}
 	for _, isRemoved := range deletedPipelines {
 		Expect(isRemoved).To(BeTrue())
 	}
+}
+
+/*
+Get pipeline via GET pipeline end point call, so that we retreive the values from DB
+*/
+func GetPipeline(client *api_server.PipelineClient, pipelineId string) model.V2beta1Pipeline {
+	params := new(pipeline_params.PipelineServiceGetPipelineParams)
+	params.PipelineID = pipelineId
+	GinkgoWriter.Printf("Get pipeline with id=%s", pipelineId)
+	pipeline, err := client.Get(params)
+	Expect(err).NotTo(HaveOccurred())
+	return model.V2beta1Pipeline{
+		DisplayName: pipeline.DisplayName,
+		Description: pipeline.Description,
+		PipelineID:  pipeline.PipelineID,
+		CreatedAt:   pipeline.CreatedAt,
+		Namespace:   pipeline.Namespace,
+	}
+}
+
+/*
+Get all pipelines (upto 1000) and filter by name, if the pipeline exists, return true otherwise false
+*/
+func FindPipelineByName(client *api_server.PipelineClient, pipelineName string) bool {
+	requestedNumberOfPipelinesPerPage := 1000
+	params := new(pipeline_params.PipelineServiceListPipelinesParams)
+	params.PageSize = util.Int32Pointer(int32(requestedNumberOfPipelinesPerPage))
+	GinkgoWriter.Printf("Get all pipelines")
+	pipelines, size, _, err := client.List(params)
+	Expect(err).NotTo(HaveOccurred())
+	GinkgoWriter.Printf("Finding pipeline with name=%s", pipelineName)
+	if size < requestedNumberOfPipelinesPerPage {
+		for _, pipeline := range pipelines {
+			if pipeline.DisplayName == pipelineName {
+				return true
+			} else {
+				return false
+			}
+		}
+	}
+	return false
 }
