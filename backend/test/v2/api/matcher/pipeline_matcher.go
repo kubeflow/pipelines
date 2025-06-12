3fe898a7ea5beb7480aba436a8243d09bb7112ae
Nelesh Singla <117123879+nsingla@users.noreply.github.com>
2025-06-12 13:30:38 -0400

    moving pipeline files to a common directory so that every test in the project can refer it and adding a check to verify that the uploaded pipeline spec matches the input file
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED
    
    Signed-off-by: Nelesh Singla <117123879+nsingla@users.noreply.github.com>

--- backend/test/v2/api/matcher/pipeline_matcher.go
+++ backend/test/v2/api/matcher/pipeline_matcher.go
@@ -1,9 +1,12 @@
 package api
 
 import (
+	"fmt"
+	"reflect"
+	"time"
+
 	model "github.com/kubeflow/pipelines/backend/api/v2beta1/go_http_client/pipeline_upload_model"
 	. "github.com/onsi/gomega"
-	"time"
 )
 
 func MatchPipelines(actual *model.V2beta1Pipeline, expected *model.V2beta1Pipeline) {
@@ -24,5 +27,13 @@ func MatchPipelineVersions(actual *model.V2beta1PipelineVersion, expected *model
 	Expect(actualTime.After(expectedTime)).To(BeTrue(), "Actual Pipeline Version creation time is not as expected")
 	Expect(actual.DisplayName).To(Equal(expected.DisplayName), "Pipeline Display Name not matching")
 	Expect(actual.Description).To(Equal(expected.Description), "Pipeline Description not matching")
-	Expect(actual.PipelineSpec).To(Equal(expected.PipelineSpec), "Pipeline Spec not matching")
+	MatchMaps(actual.PipelineSpec, expected.PipelineSpec, "Pipeline Spec")
+}
+
+func MatchPipelineVersionSpec(actual any, expected map[string]interface{}) {
+	if reflect.TypeOf(actual).Kind() == reflect.Map {
+		for key, value := range expected {
+			Expect(actual).To(HaveKeyWithValue(key, value), fmt.Sprintf("%s value not matching", key))
+		}
+	}
 }
