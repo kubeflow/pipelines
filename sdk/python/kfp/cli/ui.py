97e995506a19f50afdfd131171673e7f26e5c9dd
Francisco Javier Arceo <farceo@redhat.com>
2025-06-16 15:20:19 -0400

    yapf fix
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- sdk/python/kfp/cli/ui.py
+++ sdk/python/kfp/cli/ui.py
@@ -18,6 +18,10 @@ def ui(port, host, api_server):
     1. Remote mode: When --api-server is provided, pipeline runs are submitted to a remote KFP cluster
     2. Local mode: When --api-server is not provided, pipelines are executed locally using kfp.local
     """
-    logging.warn("\033[91mThe local `kfp ui` is in early development and still considered alpha/experimental. \x1b[0m")
-    logging.warn("\033[93mPlease report any bugs if you encounter them https://github.com/kubeflow/pipelines/issues/new?template=BUG_FRONTEND.md \033[93m")
+    logging.warn(
+        "\033[91mThe local `kfp ui` is in early development and still considered alpha/experimental. \x1b[0m"
+    )
+    logging.warn(
+        "\033[93mPlease report any bugs if you encounter them https://github.com/kubeflow/pipelines/issues/new?template=BUG_FRONTEND.md \033[93m"
+    )
     start_ui_server(host=host, port=port, api_server=api_server)
