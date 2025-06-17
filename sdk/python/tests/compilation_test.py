efe8c7a1c682f62a0da653da8161112a716343d4
Nelesh Singla <117123879+nsingla@users.noreply.github.com>
2025-06-17 11:09:15 -0400

    adding yaml validation
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED
    
    Signed-off-by: Nelesh Singla <117123879+nsingla@users.noreply.github.com>

--- sdk/python/tests/compilation_test.py
+++ sdk/python/tests/compilation_test.py
@@ -1,3 +1,4 @@
+import kfp
 from kfp import compiler
 from kfp.dsl import Output
 from kfp.dsl import pipeline
@@ -6,36 +7,45 @@ from test_data.components import add_numbers
 from test_data.components import dict_input
 from test_data.components.containerized_python_component import concat_message
 from test_data.datamodels.node import Node
+import yaml
 
 
 class TestPipelineCompilation:
 
-    @pytest.mark.parametrize('pipeline_name, output_pipeline_file_name, node', [
-        ('Add Numbers', 'add_numbers.yaml',
-         Node(data=(add_numbers.add_numbers, {
-             'a': 6,
-             'b': 4
-         }))),
-        ('Add Numbers No Input', 'add_numbers.yaml',
-         Node(data=add_numbers.add_numbers)),
-        ('Dict input', 'dict_input.yaml',
-         Node(
-             data=(dict_input.dict_input, {
-                 'struct': {
-                     'number1': 6,
-                     'number2': 4
-                 }
-             }))),
-        ('Containerized Component - Concat Message',
-         'containerized_concat_message.yaml',
-         Node(
-             data=(concat_message, {
-                 'message1': 'Hello',
-                 'message2': 'World'
-             }))),
-    ])
-    def test_compilation(self, pipeline_name, output_pipeline_file_name, node):
+    keys_to_ignore_for_comparison = ['displayName', 'name', 'sdkVersion']
 
+    @pytest.mark.parametrize(
+        'pipeline_display_name, pipeline_name, output_pipeline_file_name, node, expected_file',
+        [
+            ('Add Numbers', 'add_numbers', 'add_numbers.yaml',
+             Node(data=(add_numbers.add_numbers, {
+                 'a': 6,
+                 'b': 4
+             })), 'add_numbers.yaml'),
+            ('Add Numbers no input',
+             'add_numbers_no_input', 'add_numbers_no_input.yaml',
+             Node(data=add_numbers.add_numbers), 'add_numbers_no_input.yaml'),
+            ('Dict input', 'dict_input', 'dict_input.yaml',
+             Node(
+                 data=(dict_input.dict_input, {
+                     'struct': {
+                         'number1': 6,
+                         'number2': 4
+                     }
+                 })), 'dict_input.yaml'),
+            ('Containerized Component - Concat Message',
+             'containerized_concat_message',
+             'containerized_concat_message.yaml',
+             Node(
+                 data=(concat_message, {
+                     'message1': 'Hello',
+                     'message2': 'World'
+                 })), 'containerized_concat_message.yaml'),
+        ])
+    def test_compilation(self, pipeline_display_name, pipeline_name,
+                         output_pipeline_file_name, node, expected_file):
+
+        print('Compiling Pipeline')
         if not node.next and type(node.data) != tuple:
             compiler.Compiler().compile(
                 pipeline_func=node.data,
@@ -43,7 +53,7 @@ class TestPipelineCompilation:
                 package_path=output_pipeline_file_name)
         else:
 
-            @pipeline(display_name=pipeline_name)
+            @pipeline(display_name=pipeline_display_name)
             def create_pipeline():
                 self.create_components(node=node)
 
@@ -52,6 +62,17 @@ class TestPipelineCompilation:
                 pipeline_name=pipeline_name,
                 package_path=output_pipeline_file_name)
         print('Pipeline Created')
+        print(f'Parsing expected yaml {expected_file} for comparison')
+        expected_yaml = self.read_yaml_file(expected_file)
+        print(
+            f'Parsing expected yaml {output_pipeline_file_name} for comparison')
+        generated_yaml = self.read_yaml_file(output_pipeline_file_name)
+        print('Verify that the generated yaml matches expected yaml or not')
+        self.compare_dict(
+            actual=generated_yaml,
+            expected=expected_yaml,
+            display_name=pipeline_display_name,
+            name=pipeline_name)
 
     def create_components(self, node: Node):
         current_node = node
@@ -78,3 +99,31 @@ class TestPipelineCompilation:
             else:
                 component = current_data()
             current_node = current_node.next
+
+    import yaml
+
+    def read_yaml_file(self, filepath) -> dict:
+        with open(filepath, 'r') as file:
+            try:
+                yaml_data: dict = yaml.safe_load(file)
+                return yaml_data
+            except yaml.YAMLError as ex:
+                print(f'Error parsing YAML file: {ex}')
+                raise f'Could not load yaml file: {filepath} due to {ex}'
+
+    def compare_dict(self, actual: dict, expected: dict, **kwargs):
+        for key, value in expected.items():
+            if type(value) == dict:
+                self.compare_dict(actual[key], value, **kwargs)
+            else:
+                if key in self.keys_to_ignore_for_comparison:
+                    if key == 'sdkVersion':
+                        expected['sdkVersion'] = f'kfp-{kfp.__version__}'
+                    elif key == 'displayName':
+                        pipeline_display_name = kwargs['name'] if kwargs[
+                            'display_name'] is None else kwargs['display_name']
+                        expected['displayName'] = pipeline_display_name
+                    elif key == 'name':
+                        expected['name'] = kwargs['name']
+                assert value == actual[
+                    key], f'Value for "{key}" is not the same'
