88870823fa5e152a643db67c69ed91b4dc16b961
Michael <m.zazula@gmail.com>
2025-06-19 20:00:12 -0400

    chore: update workflows to test proto changes in ci (#11995)
    Signed-off-by: zazulam <m.zazula@gmail.com>

--- test/presubmit-tests-sdk.sh
+++ test/presubmit-tests-sdk.sh
@@ -23,11 +23,17 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r sdk/python/requirements.txt
 python3 -m pip install -r sdk/python/requirements-dev.txt
 python3 -m pip install setuptools
+python3 -m pip install wheel==0.42.0
 python3 -m pip install coveralls==1.9.2
 python3 -m pip install --upgrade protobuf
 
 python3 -m pip install sdk/python
-
+# regenerate the kfp-pipeline-spec
+cd api/
+make clean python
+cd ..
+# install the local kfp-pipeline-spec
+python3 -m pip install -I api/v2alpha1/python 
 pytest sdk/python/kfp --cov=kfp
 
 set +x
