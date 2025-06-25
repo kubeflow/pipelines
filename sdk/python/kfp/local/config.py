268e0898ecdd3f9447988b6e675676eb21c584f0
Alexander Bieniek <BieniekAlexander@gmail.com>
2025-06-25 14:19:16 -0400

    feat(sdk): Add Support for Docker Container Run Arguments (#12006)
    Signed-off-by: Alex Bieniek <BieniekAlexander@gmail.com>

--- sdk/python/kfp/local/config.py
+++ sdk/python/kfp/local/config.py
@@ -45,18 +45,143 @@ class SubprocessRunner:
     use_venv: bool = True
 
 
-@dataclasses.dataclass
 class DockerRunner:
     """Runner that indicates that local tasks should be run as a Docker
     container."""
+    DOCKER_CONTAINER_RUN_ARGS = {
+        # for available run parameters:
+        # https://docker-py.readthedocs.io/en/stable/containers.html
+        'image',
+        'command',
+        'auto_remove',
+        'blkio_weight_device',
+        'blkio_weight',
+        'cap_add',
+        'cap_drop',
+        'cgroup_parent',
+        'cgroupns',
+        'cpu_count',
+        'cpu_percent',
+        'cpu_period',
+        'cpu_quota',
+        'cpu_rt_period',
+        'cpu_rt_runtime',
+        'cpu_shares',
+        'cpuset_cpus',
+        'cpuset_mems',
+        'detach',
+        'device_cgroup_rules',
+        'device_read_bps',
+        'device_read_iops',
+        'device_write_bps',
+        'device_write_iops',
+        'devices',
+        'device_requests',
+        'dns',
+        'dns_opt',
+        'dns_search',
+        'domainname',
+        'entrypoint',
+        'environment',
+        'extra_hosts',
+        'group_add',
+        'healthcheck',
+        'hostname',
+        'init',
+        'init_path',
+        'ipc_mode',
+        'isolation',
+        'kernel_memory',
+        'labels',
+        'links',
+        'log_config',
+        'lxc_conf',
+        'mac_address',
+        'mem_limit',
+        'mem_reservation',
+        'mem_swappiness',
+        'memswap_limit',
+        'mounts',
+        'name',
+        'nano_cpus',
+        'network',
+        'network_disabled',
+        'network_mode',
+        'networking_config',
+        'oom_kill_disable',
+        'oom_score_adj',
+        'pid_mode',
+        'pids_limit',
+        'platform',
+        'ports',
+        'privileged',
+        'publish_all_ports',
+        'read_only',
+        'remove',
+        'restart_policy',
+        'runtime',
+        'security_opt',
+        'shm_size',
+        'stdin_open',
+        'stdout',
+        'stderr',
+        'stop_signal',
+        'storage_opt',
+        'stream',
+        'sysctls',
+        'tmpfs',
+        'tty',
+        'ulimits',
+        'use_config_proxy',
+        'user',
+        'userns_mode',
+        'uts_mode',
+        'version',
+        'volume_driver',
+        'volumes',
+        'working_dir',
+    }
 
-    def __post_init__(self):
+    def __init__(self, **container_run_args):
+        """Runner constructor, taking any arguments to propagate to
+        `containers.run` in the `docker` SDK.
+
+        Args:
+            **container_run_args: Keyword arguments that comport with `containers.run` in the `docker` SDK, with some exceptions (see below).
+
+        `containers.run` arguments are supported with the following exceptions:
+        - image
+        - command
+        - volumes
+
+        Raises:
+            ImportError: Raised when `docker` is not installed
+            ValueError: Raised when unsupported `containers.run` arguments are supplied
+        """
         try:
             import docker  # noqa
         except ImportError as e:
             raise ImportError(
                 f"Package 'docker' must be installed to use {DockerRunner.__name__!r}. Install it using 'pip install docker'."
             ) from e
+
+        unsupported_args = container_run_args.keys(
+        ) - self.DOCKER_CONTAINER_RUN_ARGS
+
+        if unsupported_args:
+            raise ValueError(
+                f"Unsupported `docker run` arguments, see Package `docker` for details: {', '.join(unsupported_args)}"
+            )
+
+        excess_args: set = set(['image', 'command', 'volumes'
+                               ]) & container_run_args.keys()
+
+        if excess_args:
+            raise ValueError(
+                f"The following docker run arguments should not be specififed: {', '.join(excess_args)}"
+            )
+
+        self.container_run_args = container_run_args
 
 
 class LocalExecutionConfig:
