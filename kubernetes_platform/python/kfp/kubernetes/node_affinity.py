1c59ce630857c1d7667dc4dc6ce62d55afbb6939
VaniHaripriya <vmudadla@redhat.com>
2025-07-10 17:54:47 -0500

    Add driver and sdk changes to support adding nodeAffinity
    Signed-off-by: VaniHaripriya <vmudadla@redhat.com>

---
+++ kubernetes_platform/python/kfp/kubernetes/node_affinity.py
@@ -0,0 +1,93 @@
+# Copyright 2025 The Kubeflow Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import List, Optional, Union
+from google.protobuf import json_format
+from kfp.dsl import PipelineTask, pipeline_channel
+from kfp.kubernetes import common
+from kfp.kubernetes import kubernetes_executor_config_pb2 as pb
+
+
+def add_node_affinity(
+    task: PipelineTask,
+    match_expressions: Optional[List[dict]] = None,
+    match_fields: Optional[List[dict]] = None,
+    weight: Optional[int] = None,
+) -> PipelineTask:
+    """Add a constraint to the task Pod's `nodeAffinity
+    <https://kubernetes.io/docs/concepts/scheduling-eviction/assign-pod-node/#node-affinity>`_.
+
+    Each constraint is specified as a match expression (key, operator, values) or match field,
+    corresponding to the PodSpec's `nodeAffinity <https://kubernetes.io/docs/reference/kubernetes-api/workload-resources/pod-v1/#NodeAffinity>`_ field.
+
+    Args:
+        task: Pipeline task.
+        match_expressions: List of dicts for matchExpressions (keys: key, operator, values).
+        match_fields: List of dicts for matchFields (keys: key, operator, values).
+        weight: If set, this affinity is preferred (K8s weight 1-100); otherwise required.
+
+    Returns:
+        Task object with added node affinity.
+    """
+    msg = common.get_existing_kubernetes_config_as_message(task)
+    affinity_term = pb.NodeAffinityTerm()
+    if match_expressions:
+        for expr in match_expressions:
+            affinity_term.match_expressions.add(
+                key=expr.get("key"),
+                operator=expr.get("operator"),
+                values=expr.get("values", []),
+            )
+    if match_fields:
+        for field in match_fields:
+            affinity_term.match_fields.add(
+                key=field.get("key"),
+                operator=field.get("operator"),
+                values=field.get("values", []),
+            )
+    if weight is not None:
+        affinity_term.weight = weight
+    msg.node_affinity.append(affinity_term)
+    task.platform_config["kubernetes"] = json_format.MessageToDict(msg)
+    return task
+
+def add_node_affinity_json(
+    task: PipelineTask,
+    node_affinity_json: Union[pipeline_channel.PipelineParameterChannel, dict],
+) -> PipelineTask:
+    """Add a node affinity constraint to the task Pod's `nodeAffinity`
+    using a JSON struct or pipeline parameter.
+
+    This allows parameterized node affinity to be specified,
+    matching the Kubernetes NodeAffinity schema:
+    https://kubernetes.io/docs/reference/kubernetes-api/workload-resources/pod-v1/#nodeaffinity
+
+    Args:
+        task: Pipeline task.
+        node_affinity_json: Dict or pipeline parameter for node affinity. Should match K8s NodeAffinity schema.
+
+    Returns:
+        Task object with added node affinity.
+    """
+    msg = common.get_existing_kubernetes_config_as_message(task)
+    # Remove any previous JSON-based node affinity terms
+    for i in range(len(msg.node_affinity) - 1, -1, -1):
+        if msg.node_affinity[i].HasField("node_affinity_json"):
+            del msg.node_affinity[i]
+    affinity_term = pb.NodeAffinityTerm()
+    input_param_spec = common.parse_k8s_parameter_input(node_affinity_json, task)
+    affinity_term.node_affinity_json.CopyFrom(input_param_spec)
+    msg.node_affinity.append(affinity_term)
+    task.platform_config["kubernetes"] = json_format.MessageToDict(msg)
+    return task
