VaniHaripriya/data-science-pipelines

feat(backend): Update Argo Workflow Compiler to create workspace PVCs
Signed-off-by: VaniHaripriya <vmudadla@redhat.com>

backend/src/apiserver/template/template.go
@@ -32,6 +32,7 @@ import (
 	"google.golang.org/protobuf/encoding/protojson"
 	structpb "google.golang.org/protobuf/types/known/structpb"
 	goyaml "gopkg.in/yaml.v3"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/yaml"
 )
@@ -134,18 +135,19 @@ type Template interface {
 }
 
 type RunWorkflowOptions struct {
-	RunId         string
-	RunAt         int64
-	CacheDisabled bool
+	RunId          string
+	RunAt          int64
+	CacheDisabled  bool
+	DefaultPVCSpec *corev1.PersistentVolumeClaimSpec
 }
 
-func New(bytes []byte, cacheDisabled bool) (Template, error) {
+func New(bytes []byte, cacheDisabled bool, defaultPVCSpec *corev1.PersistentVolumeClaimSpec) (Template, error) {
 	format := inferTemplateFormat(bytes)
 	switch format {
 	case V1:
 		return NewArgoTemplate(bytes)
 	case V2:
-		return NewV2SpecTemplate(bytes, cacheDisabled)
+		return NewV2SpecTemplate(bytes, cacheDisabled, defaultPVCSpec)
 	default:
 		return nil, util.NewInvalidInputErrorWithDetails(ErrorInvalidPipelineSpec, "unknown template format")
 	}

kubeflow/pipelines

backend/src/apiserver/template/template.go
@@ -32,6 +32,7 @@ import (
 	"google.golang.org/protobuf/encoding/protojson"
 	structpb "google.golang.org/protobuf/types/known/structpb"
 	goyaml "gopkg.in/yaml.v3"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/yaml"
 )
@@ -134,18 +135,19 @@ type Template interface {
 }
 
 type RunWorkflowOptions struct {
-	RunId         string
-	RunAt         int64
-	CacheDisabled bool
+	RunId            string
+	RunAt            int64
+	CacheDisabled    bool
+	DefaultWorkspace *corev1.PersistentVolumeClaimSpec
 }
 
-func New(bytes []byte, cacheDisabled bool) (Template, error) {
+func New(bytes []byte, cacheDisabled bool, defaultWorkspace *corev1.PersistentVolumeClaimSpec) (Template, error) {
 	format := inferTemplateFormat(bytes)
 	switch format {
 	case V1:
 		return NewArgoTemplate(bytes)
 	case V2:
-		return NewV2SpecTemplate(bytes, cacheDisabled)
+		return NewV2SpecTemplate(bytes, cacheDisabled, defaultWorkspace)
 	default:
 		return nil, util.NewInvalidInputErrorWithDetails(ErrorInvalidPipelineSpec, "unknown template format")
 	}