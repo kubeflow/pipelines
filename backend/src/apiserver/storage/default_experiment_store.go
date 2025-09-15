2a08179b23915e444074b43c26d2bcd54f8c5f42
kaikaila <lyk2772@126.com>
2025-09-15 02:15:18 -0700

    refactor(backend): integrate postgresql
    Signed-off-by: kaikaila <lyk2772@126.com>

--- backend/src/apiserver/storage/default_experiment_store.go
+++ backend/src/apiserver/storage/default_experiment_store.go
@@ -19,10 +19,9 @@ import (
 
 	sq "github.com/Masterminds/squirrel"
 	"github.com/golang/glog"
+	"github.com/kubeflow/pipelines/backend/src/apiserver/common/sql/dialect"
 	"github.com/kubeflow/pipelines/backend/src/common/util"
 )
-
-var defaultExperimentDBValue = sq.Eq{"DefaultExperimentId": ""}
 
 type DefaultExperimentStoreInterface interface {
 	GetDefaultExperimentId() (string, error)
@@ -32,7 +31,8 @@ type DefaultExperimentStoreInterface interface {
 // Implementation of a DefaultExperimentStoreInterface. This stores the default experiment's ID,
 // which is created the first time the API server is initialized.
 type DefaultExperimentStore struct {
-	db *DB
+	db        *sql.DB
+	dbDialect dialect.DBDialect
 }
 
 func (s *DefaultExperimentStore) initializeDefaultExperimentTable() error {
@@ -41,7 +41,13 @@ func (s *DefaultExperimentStore) initializeDefaultExperimentTable() error {
 	if err != nil {
 		return util.NewInternalServerError(err, "Failed to create a new transaction to initialize default experiment table")
 	}
-	rows, err := tx.Query("SELECT * FROM default_experiments")
+	q := s.dbDialect.QuoteIdentifier
+	qb := s.dbDialect.QueryBuilder()
+	sql, args, err := qb.Select("*").From(q("default_experiments")).ToSql()
+	if err != nil {
+		return util.NewInternalServerError(err, "Failed to create query to check default experiment table")
+	}
+	rows, err := tx.Query(sql, args...)
 	if err != nil {
 		tx.Rollback()
 		return util.NewInternalServerError(err, "Failed to get default experiment")
@@ -55,9 +61,11 @@ func (s *DefaultExperimentStore) initializeDefaultExperimentTable() error {
 
 	// If the table is not initialized, then set the default value.
 	if !next {
-		sql, args, queryErr := sq.
-			Insert("default_experiments").
-			SetMap(defaultExperimentDBValue).
+		sql, args, queryErr := qb.
+			Insert(q("default_experiments")).
+			SetMap(map[string]interface{}{
+				q("DefaultExperimentId"): "",
+			}).
 			ToSql()
 
 		if queryErr != nil {
@@ -80,10 +88,16 @@ func (s *DefaultExperimentStore) initializeDefaultExperimentTable() error {
 }
 
 func (s *DefaultExperimentStore) SetDefaultExperimentId(id string) error {
-	sql, args, err := sq.
-		Update("default_experiments").
-		SetMap(sq.Eq{"DefaultExperimentId": id}).
-		Where(sq.Eq{"DefaultExperimentId": ""}).
+	q := s.dbDialect.QuoteIdentifier
+	qb := s.dbDialect.QueryBuilder()
+	sql, args, err := qb.
+		Update(q("default_experiments")).
+		SetMap(map[string]interface{}{
+			q("DefaultExperimentId"): id,
+		}).
+		Where(sq.Eq{
+			q("DefaultExperimentId"): "",
+		}).
 		ToSql()
 	if err != nil {
 		return util.NewInternalServerError(err, "Error creating query to set default experiment ID")
@@ -96,8 +110,10 @@ func (s *DefaultExperimentStore) SetDefaultExperimentId(id string) error {
 }
 
 func (s *DefaultExperimentStore) GetDefaultExperimentId() (string, error) {
+	q := s.dbDialect.QuoteIdentifier
+	qb := s.dbDialect.QueryBuilder()
 	var defaultExperimentId string
-	sql, args, err := sq.Select("DefaultExperimentId").From("default_experiments").ToSql()
+	sql, args, err := qb.Select(q("DefaultExperimentId")).From(q("default_experiments")).ToSql()
 	if err != nil {
 		return "", util.NewInternalServerError(err, "Error creating query to get default experiment ID")
 	}
@@ -129,10 +145,16 @@ func (s *DefaultExperimentStore) GetDefaultExperimentId() (string, error) {
 // Update is used instead of delete so that we don't need to first check that the experiment ID is
 // there.
 func (s *DefaultExperimentStore) UnsetDefaultExperimentIdIfIdMatches(tx *sql.Tx, id string) error {
-	sql, args, err := sq.
-		Update("default_experiments").
-		SetMap(sq.Eq{"DefaultExperimentId": ""}).
-		Where(sq.Eq{"DefaultExperimentId": id}).
+	q := s.dbDialect.QuoteIdentifier
+	qb := s.dbDialect.QueryBuilder()
+	sql, args, err := qb.
+		Update(q("default_experiments")).
+		SetMap(map[string]interface{}{
+			q("DefaultExperimentId"): "",
+		}).
+		Where(sq.Eq{
+			q("DefaultExperimentId"): id,
+		}).
 		ToSql()
 	if err != nil {
 		return util.NewInternalServerError(err, "Failed to create command to clear default experiment with ID: %s", id)
@@ -146,9 +168,8 @@ func (s *DefaultExperimentStore) UnsetDefaultExperimentIdIfIdMatches(tx *sql.Tx,
 }
 
 // factory function for creating default experiment store.
-func NewDefaultExperimentStore(db *DB) *DefaultExperimentStore {
-	s := &DefaultExperimentStore{db: db}
-	// Initialize default experiment table
+func NewDefaultExperimentStore(db *sql.DB, d dialect.DBDialect) *DefaultExperimentStore {
+	s := &DefaultExperimentStore{db: db, dbDialect: d}
 	s.initializeDefaultExperimentTable()
 	return s
 }
---
+++ backend/src/apiserver/storage/sql_dialect_util.go
@@ -0,0 +1,114 @@
+// Copyright 2025 The Kubeflow Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage
+
+import (
+	"errors"
+	"strings"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/go-sql-driver/mysql"
+	"github.com/jackc/pgerrcode"
+	"github.com/jackc/pgx/v5/pgconn"
+	sqlite3 "github.com/mattn/go-sqlite3"
+
+	"github.com/kubeflow/pipelines/backend/src/apiserver/common/sql/dialect"
+)
+
+// ---- Duplicate error detection ------------------------------------------------
+
+// isDuplicateError returns true if err indicates a unique/duplicate violation
+// for the given dialect.
+func isDuplicateError(d dialect.DBDialect, err error) bool {
+	if err == nil {
+		return false
+	}
+	switch d.Name() { // use exported Name(); do NOT rely on unexported fields
+	case "mysql":
+		var me *mysql.MySQLError
+		if errors.As(err, &me) {
+			// 1062 = ER_DUP_ENTRY
+			return me.Number == 1062
+		}
+		return false
+	case "pgx":
+		var pe *pgconn.PgError
+		if errors.As(err, &pe) {
+			// Unique violation
+			return pe.Code == pgerrcode.UniqueViolation
+		}
+		return false
+	default:
+		// Optional: for unit tests that still use sqlite.
+		var se sqlite3.Error
+		if errors.As(err, &se) {
+			return se.Code == sqlite3.ErrConstraint
+		}
+		return false
+	}
+}
+
+// ---- Upsert (INSERT ... ON DUPLICATE KEY / ON CONFLICT) ----------------------
+
+// insertUpsert starts an INSERT builder and appends the dialect-specific upsert
+// clause. Callers should continue chaining .Columns(...).Values(...).
+func insertUpsert(d dialect.DBDialect, table string, keyCols []string, overwrite bool, updateCols []string) sq.InsertBuilder {
+	q := d.QuoteIdentifier
+	ib := d.QueryBuilder().Insert(q(table))
+
+	switch d.Name() {
+	case "pgx", "sqlite":
+		sets := make([]string, 0, len(updateCols))
+		for _, c := range updateCols {
+			if overwrite {
+				sets = append(sets, q(c)+"=excluded."+q(c))
+			} else {
+				sets = append(sets, q(c)+"="+q(c))
+			}
+		}
+		suffix := "ON CONFLICT (" + joinQuoted(q, keyCols) + ") DO UPDATE SET " + strings.Join(sets, ", ")
+		return ib.Suffix(suffix)
+	default: // mysql
+		sets := make([]string, 0, len(updateCols))
+		for _, c := range updateCols {
+			if overwrite {
+				sets = append(sets, q(c)+"=VALUES("+q(c)+")")
+			} else {
+				sets = append(sets, q(c)+"="+q(c))
+			}
+		}
+		suffix := "ON DUPLICATE KEY UPDATE " + strings.Join(sets, ", ")
+		return ib.Suffix(suffix)
+	}
+}
+
+func joinQuoted(q func(string) string, cols []string) string {
+	out := make([]string, len(cols))
+	for i, c := range cols {
+		out[i] = q(c)
+	}
+	return strings.Join(out, ", ")
+}
+
+// quoteAll applies the dialect's QuoteIdentifier (q) to each column name and returns a new slice.
+// Use this when passing columns into squirrel.Select(...), so each identifier is properly quoted
+// for the current SQL dialect (e.g., Postgres requires double quotes to preserve case).
+func quoteAll(q func(string) string, cols []string) []string {
+	out := make([]string, len(cols))
+	for i, c := range cols {
+		out[i] = q(c)
+	}
+	return out
+}
