648fa4d30a5d88f738dd666c4a006a137bdd43c0
arpechenin <arpechenin@avito.ru>
2025-09-22 21:03:09 +0300

    Central driver POC https://github.com/kubeflow/pipelines/pull/12023
    - Modify Argo compiler: generate a plugin template instead of a container
    - driver as a http server
    
    Signed-off-by: arpechenin <arpechenin@avito.ru>

---
+++ backend/src/driver/api/request.go
@@ -0,0 +1,44 @@
+package api
+
+type DriverPluginArgs struct {
+	CachedDecisionPath string `json:"cached_decision_path"`
+	Component          string `json:"component,omitempty"`
+	Container          string `json:"container,omitempty"`
+	RunMetadata        string `json:"run_metadata,omitempty"`
+	DagExecutionID     string `json:"dag_execution_id"`
+	IterationIndex     string `json:"iteration_index"`
+	HttpProxy          string `json:"http_proxy"`
+	HttpsProxy         string `json:"https_proxy"`
+	NoProxy            string `json:"no_proxy"`
+	KubernetesConfig   string `json:"kubernetes_config,omitempty"`
+	RuntimeConfig      string `json:"runtime_config,omitempty"`
+	PipelineName       string `json:"pipeline_name"`
+	RunID              string `json:"run_id"`
+	RunName            string `json:"run_name"`
+	RunDisplayName     string `json:"run_display_name"`
+	TaskName           string `json:"task_name"`
+	Task               string `json:"task"`
+	Type               string `json:"type"`
+	CacheDisabledFlag  bool   `json:"cache_disabled"`
+	PublishLogs        string `json:"publish_logs"`
+	ExecutionIdPath    string `json:"execution_id_path"`
+	IterationCountPath string `json:"iteration_count_path"`
+	ConditionPath      string `json:"condition_path"`
+	PodSpecPathPath    string `json:"pod_spec_patch_path"`
+}
+
+type DriverPlugin struct {
+	DriverPlugin *DriverPluginContainer `json:"driver-plugin"`
+}
+
+type DriverPluginContainer struct {
+	Args *DriverPluginArgs `json:"args"`
+}
+
+type DriverTemplate struct {
+	Plugin *DriverPlugin `json:"plugin"`
+}
+
+type DriverRequest struct {
+	Template *DriverTemplate `json:"template"`
+}
