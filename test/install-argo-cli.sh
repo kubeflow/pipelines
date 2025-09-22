a4c669249ec8530a30b9b8d7a71546d0730b1940
Giulio Frasca <gfrasca@redhat.com>
2025-09-22 16:27:34 -0400

    chore: Improve argo upgrade Makefile to update all other refs
    Signed-off-by: Giulio Frasca <gfrasca@redhat.com>

--- test/install-argo-cli.sh
+++ test/install-argo-cli.sh
@@ -19,7 +19,7 @@ set -ex
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 REPO_ROOT="${DIR}/.."
 ARGO_VERSION="$(cat ${REPO_ROOT}/third_party/argo/VERSION)"
-# ARGO_VERSION=v3.6.7
+# ARGO_VERSION=v3.7.0
 OS=${OS:-"linux-amd64"}
 
 # if argo is not installed
