c02a78329de41ce019774f3b315b45ff243e237d
Alvaro Alonso <112358.fn@gmail.com>
2025-10-04 13:58:18 +0200

    fix(backend): RetrievePodName with POD_NAMES v1
    Avoid panic if the workflow annotation does not exists
    Signed-off-by: Alvaro Alonso <112358.fn@gmail.com>

--- backend/src/common/util/workflow.go
+++ backend/src/common/util/workflow.go
@@ -384,7 +384,10 @@ func (w *Workflow) ScheduledWorkflowUUIDAsStringOrEmpty() string {
 // This behaviour can be reverted by setting POD_NAMES=v1 on the workflow controller
 // https://argo-workflows.readthedocs.io/en/release-3.5/upgrading/#e5b131a33-feat-add-template-node-to-pod-name-fixes-1319-6712
 func RetrievePodName(wf workflowapi.Workflow, node workflowapi.NodeStatus) string {
-	if wf.APIVersion == "v1" || wf.Annotations["workflows.argoproj.io/pod-name-format"] == "v1" {
+	if wf.APIVersion == "v1" {
+		return node.ID
+	}
+	if v, ok := wf.Annotations["workflows.argoproj.io/pod-name-format"]; ok && v == "v1" {
 		return node.ID
 	}
 	if wf.Name == node.Name {
