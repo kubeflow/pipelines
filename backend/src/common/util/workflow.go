8dc3bd518596cdea0ee5e680d5ba0920ade23bcf
Alvaro Alonso <112358.fn@gmail.com>
2025-10-06 14:19:58 +0200

    fix: linting errors
    fix: Stylistic issues ST1000 ST1020
    fix: Omit embedded fields from selector expression
    fix: Use strings.ReplaceAll
    
    Signed-off-by: Alvaro Alonso <112358.fn@gmail.com>

--- backend/src/common/util/workflow.go
+++ backend/src/common/util/workflow.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package util
 package util
 
 import (
@@ -122,7 +123,7 @@ func UnmarshParametersWorkflow(paramsString string) (SpecParameters, error) {
 	return rev, nil
 }
 
-// Marshal parameters to JSON encoded string.
+// MarshalParametersWorkflow marshal parameters to JSON encoded string.
 // This also checks result is not longer than a limit.
 func MarshalParametersWorkflow(params SpecParameters) (string, error) {
 	if params == nil {
@@ -148,13 +149,12 @@ func MarshalParametersWorkflow(params SpecParameters) (string, error) {
 	return string(paramBytes), nil
 }
 
-// Get ExecutionType: ArgoWorkflow
+// ExecutionType ArgoWorkflow
 func (w *Workflow) ExecutionType() ExecutionType {
 	return ArgoWorkflow
 }
 
-// ExecutionSpec interface: Get ExecutionStatus which can be used to
-// access status related information
+// ExecutionStatus access status related information
 func (w *Workflow) ExecutionStatus() ExecutionStatus {
 	return w
 }
@@ -201,12 +201,12 @@ func (w *Workflow) GenerateRetryExecution() (ExecutionSpec, []string, error) {
 		return nil, nil, NewBadRequestError(errors.New("workflow cannot be retried"), "Workflow must be Failed/Error to retry")
 	}
 
-	newWF := w.Workflow.DeepCopy()
+	newWF := w.DeepCopy()
 	// Delete/reset fields which indicate workflow completed
 	delete(newWF.Labels, common.LabelKeyCompleted)
 	// Delete/reset fields which indicate workflow is finished being persisted to the database
 	delete(newWF.Labels, LabelKeyWorkflowPersistedFinalState)
-	newWF.ObjectMeta.Labels[common.LabelKeyPhase] = string(workflowapi.NodeRunning)
+	newWF.Labels[common.LabelKeyPhase] = string(workflowapi.NodeRunning)
 	newWF.Status.Phase = workflowapi.WorkflowRunning
 	newWF.Status.Conditions.UpsertCondition(workflowapi.Condition{Status: metav1.ConditionFalse, Type: workflowapi.ConditionTypeCompleted})
 	newWF.Status.Message = ""
@@ -218,7 +218,7 @@ func (w *Workflow) GenerateRetryExecution() (ExecutionSpec, []string, error) {
 
 	// Iterate the previous nodes. If it was successful Pod carry it forward
 	newWF.Status.Nodes = make(map[string]workflowapi.NodeStatus)
-	onExitNodeName := w.ObjectMeta.Name + ".onExit"
+	onExitNodeName := w.Name + ".onExit"
 	var podsToDelete []string
 	for _, node := range w.Status.Nodes {
 		oldNodeID := RetrievePodName(*w.Workflow, node)
@@ -280,7 +280,7 @@ func (w *Workflow) ExecutionName() string {
 	return w.Name
 }
 
-// OverrideName sets the name of a Workflow.
+// SetExecutionName sets the name of a Workflow.
 func (w *Workflow) SetExecutionName(name string) {
 	w.GenerateName = ""
 	w.Name = name
@@ -378,10 +378,10 @@ func (w *Workflow) ScheduledWorkflowUUIDAsStringOrEmpty() string {
 	return ""
 }
 
-// Derives the Pod name from a given workflowapi.Workflow and workflowapi.NodeStatus
+// RetrievePodName derives the Pod name from a given workflowapi.Workflow and workflowapi.NodeStatus
 // This is a workaround for an upstream breaking change with node.ID and node.Name mismatches,
 // see https://github.com/argoproj/argo-workflows/issues/10107#issuecomment-1536113642
-// This behaviour can be reverted by setting POD_NAMES=v1 on the workflow controller
+// This behavior can be reverted by setting POD_NAMES=v1 on the workflow controller
 // https://argo-workflows.readthedocs.io/en/release-3.5/upgrading/#e5b131a33-feat-add-template-node-to-pod-name-fixes-1319-6712
 func RetrievePodName(wf workflowapi.Workflow, node workflowapi.NodeStatus) string {
 	if wf.APIVersion == "v1" || wf.Annotations["workflows.argoproj.io/pod-name-format"] == "v1" {
@@ -708,7 +708,7 @@ func (w *Workflow) SetPodMetadataLabels(key string, value string) {
 }
 
 func (w *Workflow) ReplaceUID(id string) error {
-	newWorkflowString := strings.Replace(w.ToStringForStore(), "{{workflow.uid}}", id, -1)
+	newWorkflowString := strings.ReplaceAll(w.ToStringForStore(), "{{workflow.uid}}", id)
 	var workflow *workflowapi.Workflow
 	if err := json.Unmarshal([]byte(newWorkflowString), &workflow); err != nil {
 		return NewInternalServerError(err,
