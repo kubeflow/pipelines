c20fecb518c1fc56ae4a8b0ef4690a574838c81c
Humair Khan <HumairAK@users.noreply.github.com>
2025-10-09 14:11:15 -0400

    chore: release 2.14.5
    automate requirements updates
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

--- kubernetes_platform/python/docs/conf.py
+++ kubernetes_platform/python/docs/conf.py
@@ -140,10 +140,17 @@ html_theme_options = {
     'version_info': [
         {
             'version':
+                'https://kfp-kubernetes.readthedocs.io/en/kfp-kubernetes-2.14.5/',
+            'title':
+                '2.14.5',
+            'aliases': ['stable'],
+        },
+        {
+            'version':
                 'https://kfp-kubernetes.readthedocs.io/en/kfp-kubernetes-2.14.4/',
             'title':
                 '2.14.4',
-            'aliases': ['stable'],
+            'aliases': [],
         },
         {
             'version':
