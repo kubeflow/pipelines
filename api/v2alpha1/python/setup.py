4063bf6f2033e5931fe145c7322a75be08bfd5a4
Humair Khan <HumairAK@users.noreply.github.com>
2025-10-13 15:52:06 -0400

    chore: release 2.14.6 (#12355)
    * chore: add publish package workflow (#12344)
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    (cherry picked from commit 08ff96b153ad6ea77e4ad4e53bf4dfafe24574c8)
    
    * chore: regenerate files for ver upgrade (#12341)
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    (cherry picked from commit f689e86f69f1a1f81908a0db1cbad1c9dabcfadc)
    
    * chore: update versioning policy (#12342)
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    (cherry picked from commit d0e85fad14c1718d11a8b62f9da7442c5d6f388f)
    
    * chore: remove pin on protobuf 6 ver (#12345)
    
    * remove pin on protobuf 6 ver
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * use requirements.in for kfp-k8s and spec
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    (cherry picked from commit 06e6a44ffd767c5c404a38468cd85e2c0faef0d9)
    
    * test for kfp-server-api codegen in ci
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    (cherry picked from commit b23aac8bcab194a88b0f9f1bc85833e47f5c5f01)
    
    * chore: Release 2.14.6
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * build wheels from sdist
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    * revert click version change
    
    Pin click on 8.1.8. Due to compatibility issues, cli tests fail on click>8.1.8 in Python >= 3.13.
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

--- api/v2alpha1/python/setup.py
+++ api/v2alpha1/python/setup.py
@@ -17,7 +17,7 @@ import os
 from typing import List
 
 NAME = 'kfp-pipeline-spec'
-VERSION = '2.14.3'
+VERSION = '2.14.6'
 
 def get_requirements(requirements_file: str) -> List[str]:
     """Read requirements from requirements.in."""
