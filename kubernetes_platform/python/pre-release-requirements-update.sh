336c7cbbdddf08170c51cacba85e30d43bda2261
Humair Khan <HumairAK@users.noreply.github.com>
2025-10-13 17:31:41 -0400

    update requirements
    Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

--- kubernetes_platform/python/pre-release-requirements-update.sh
+++ kubernetes_platform/python/pre-release-requirements-update.sh
@@ -4,5 +4,5 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 
 pip-compile --no-emit-find-links --no-header --no-emit-index-url requirements.in \
   --find-links="${GIT_ROOT}/sdk/python/dist" \
-  --find-links="${GIT_ROOT}/api/v2alpha1/python/dist" \
+  --find-links="${GIT_ROOT}/api/v2alpha1/python/dist" > requirements.txt
 
