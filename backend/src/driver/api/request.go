a00b468452f1cc85763af756f058d08fe8f1b114
arpechenin <arpechenin@avito.ru>
2025-11-17 23:22:56 +0300

    - merge tests - fix formatting
    Signed-off-by: arpechenin <arpechenin@avito.ru>

--- backend/src/driver/api/request.go
+++ backend/src/driver/api/request.go
@@ -1,3 +1,18 @@
+// Copyright 2025 The Kubeflow Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package api provides HTTP DTOs used by the driver server.
 package api
 
 type DriverPluginArgs struct {
@@ -6,8 +21,8 @@ type DriverPluginArgs struct {
 	Container            string `json:"container,omitempty"`
 	DagExecutionID       string `json:"dag_execution_id"`
 	IterationIndex       string `json:"iteration_index"`
-	HttpProxy            string `json:"http_proxy"`
-	HttpsProxy           string `json:"https_proxy"`
+	HTTPProxy            string `json:"http_proxy"`
+	HTTPSProxy           string `json:"https_proxy"`
 	NoProxy              string `json:"no_proxy"`
 	KubernetesConfig     string `json:"kubernetes_config,omitempty"`
 	RuntimeConfig        string `json:"runtime_config,omitempty"`
@@ -20,7 +35,7 @@ type DriverPluginArgs struct {
 	Task                 string `json:"task"`
 	Type                 string `json:"type"`
 	CacheDisabledFlag    bool   `json:"cache_disabled"`
-	ExecutionIdPath      string `json:"execution_id_path"`
+	ExecutionIDPath      string `json:"execution_id_path"`
 	IterationCountPath   string `json:"iteration_count_path"`
 	ConditionPath        string `json:"condition_path"`
 	PodSpecPathPath      string `json:"pod_spec_patch_path"`
