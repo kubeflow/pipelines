ffbd69f4b3533e0986dc87ae59ab30b6c8a56446
Siva <siva.explores06@proton.me>
2025-12-19 09:39:11 +0530

    fix(backend): add formatting to make parameter macros such as [[CurrentTime]] forward compatible from v1 to v2 (#12497)
    * Adds formatting to make Parameter macros such as [[CurrentTime]] forward compatible with v2 from v1 ,matching the behaviour of v1 templates
    
    Signed-off-by: Siva-Sainath <siva.explores06@proton.me>
    
    * Fix gofmt formatting issue
    
    Signed-off-by: Siva-Sainath <siva.explores06@proton.me>
    
    * solved a go linting pre-commit test error
    
    Signed-off-by: Siva <siva.explores06@proton.me>
    Signed-off-by: Siva-Sainath <siva.explores06@proton.me>
    
    * fix(backend): add formatting to make parameter macros such as [[CurrentTime]] forward compatible from v1 to v2
    
    Signed-off-by: Siva-Sainath <siva.explores06@proton.me>
    
    ---------
    
    Signed-off-by: Siva-Sainath <siva.explores06@proton.me>
    Signed-off-by: Siva <siva.explores06@proton.me>

--- backend/src/apiserver/template/v2_template.go
+++ backend/src/apiserver/template/v2_template.go
@@ -105,6 +105,9 @@ func (t *V2Spec) ScheduledWorkflow(modelJob *model.Job) (*scheduledworkflow.Sche
 		return nil, util.Wrap(err, "Failed to convert runtime config")
 	}
 	job.RuntimeConfig = jobRuntimeConfig
+
+	// Parameter macros like [[CurrentTime]], [[RunUUID]], [[ScheduledTime]], [[Index]] are not formatted here.
+	// They remain unformatted in the ScheduledWorkflow spec and will be formatted by the scheduled workflow controller
 	if err = t.validatePipelineJobInputs(job); err != nil {
 		return nil, util.Wrap(err, "invalid pipeline job inputs")
 	}
@@ -316,6 +319,37 @@ func (t *V2Spec) RunWorkflow(modelRun *model.Run, options RunWorkflowOptions) (u
 		return nil, util.NewInternalServerError(err, "Failed to convert to PipelineJob RuntimeConfig")
 	}
 	job.RuntimeConfig = jobRuntimeConfig
+
+	// Format parameters to expand macros like [[CurrentTime]], [[RunUUID]], [[ScheduledTime]], [[Index]] (V1 forward compatibility).
+	// Uses NewSWFParameterFormatter to support all macros. For standalone runs, [[ScheduledTime]] and [[Index]] remain unformatted
+
+	if job.RuntimeConfig != nil && len(job.RuntimeConfig.GetParameterValues()) > 0 {
+		scheduledEpoch := int64(-1) // disabled by default
+
+		if modelRun.ScheduledAtInSec > 0 {
+			scheduledEpoch = modelRun.ScheduledAtInSec
+		}
+		formatter := util.NewSWFParameterFormatter(
+			options.RunID,
+			scheduledEpoch,
+			options.RunAt,
+			-1,
+		)
+		// Convert structpb.Value to strings, format, convert back
+		paramValues := job.RuntimeConfig.GetParameterValues()
+		stringParams := make(map[string]string)
+		for key, val := range paramValues {
+			if strVal := val.GetStringValue(); strVal != "" {
+				stringParams[key] = strVal
+			}
+		}
+		// Format the string parameters
+		formattedParams := formatter.FormatWorkflowParameters(stringParams)
+		// Convert formatted strings back to structpb.Value
+		for key, formattedVal := range formattedParams {
+			paramValues[key] = structpb.NewStringValue(formattedVal)
+		}
+	}
 	if err = t.validatePipelineJobInputs(job); err != nil {
 		return nil, util.Wrap(err, "invalid pipeline job inputs")
 	}
