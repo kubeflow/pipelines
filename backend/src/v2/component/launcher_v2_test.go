65171f87601f50c662bf153a4a28d92edee63e9f
sduvvuri1603 <sduvvuri@redhat.com>
2026-01-09 13:06:25 -0500

    Replace deprecated NewSimpleClientset() with NewClientset()
    Replace all instances of NewSimpleClientset() with NewClientset() which
    is available in client-go v0.33.1 and is a drop-in replacement. This
    removes the need for //nolint: staticcheck directives.
    
    Signed-off-by: sduvvuri1603 <sduvvuri@redhat.com>

--- backend/src/v2/component/launcher_v2_test.go
+++ backend/src/v2/component/launcher_v2_test.go
@@ -348,7 +348,7 @@ func Test_NewLauncherV2(t *testing.T) {
 
 	disabledCacheClient, _ := cacheutils.NewClient("ml-pipeline.kubeflow", "8887", true, &tls.Config{})
 	var testLauncherV2Deps = client_manager.NewFakeClientManager(
-		fake.NewSimpleClientset(),
+		fake.NewClientset(),
 		metadata.NewFakeClient(),
 		disabledCacheClient,
 	)
