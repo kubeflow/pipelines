7b9763787975d1b894e8f281c7be8bac92e8f2ec
sduvvuri1603 <sduvvuri@redhat.com>
2026-01-15 10:30:56 -0500

    fix: Add validation to FakeWorkflowClient ConfigMap creation
    Match the real WorkflowInterface.configureWorkflowParallelism() behavior.
    
    Signed-off-by: sduvvuri1603 <sduvvuri@redhat.com>

--- backend/src/apiserver/client/workflow_fake.go
+++ backend/src/apiserver/client/workflow_fake.go
@@ -63,16 +63,26 @@ func (c *FakeWorkflowClient) Create(ctx context.Context, execSpec util.Execution
 	if c.configMapClient != nil && workflow.Workflow != nil && workflow.Annotations != nil {
 		pipelineVersionID := workflow.Annotations[util.AnnotationKeyPipelineVersionID]
 		maxActiveRunsStr := workflow.Annotations[util.AnnotationKeyMaxActiveRuns]
+
 		if pipelineVersionID != "" && maxActiveRunsStr != "" {
-			namespace := workflow.Namespace
-			if namespace != "" {
-				value := maxActiveRunsStr
-				configMapApply := applyv1.ConfigMap(pipelineParallelismConfigMapName, namespace).
-					WithData(map[string]string{pipelineVersionID: value})
-				_, err := c.configMapClient.Apply(ctx, configMapApply, v1.ApplyOptions{FieldManager: "kubeflow-pipelines", Force: false})
-				if err != nil {
-					glog.Warningf("Failed to persist max_active_runs configuration for pipeline version %s in namespace %s: %v", pipelineVersionID, namespace, err)
+			maxActiveRuns, err := strconv.Atoi(maxActiveRunsStr)
+			if err != nil {
+				glog.Warningf("Invalid max_active_runs annotation value: %v", err)
+			} else if maxActiveRuns <= 0 {
+				glog.Warningf("max_active_runs must be greater than 0, got %d", maxActiveRuns)
+			} else {
+				namespace := workflow.Namespace
+				if namespace == "" {
+					glog.Warningf("Workflow namespace is required for ConfigMap operations")
 					// Don't fail the workflow creation, just log the warning
+				} else {
+					value := strconv.Itoa(maxActiveRuns)
+					configMapApply := applyv1.ConfigMap(pipelineParallelismConfigMapName, namespace).
+						WithData(map[string]string{pipelineVersionID: value})
+					_, err := c.configMapClient.Apply(ctx, configMapApply, v1.ApplyOptions{FieldManager: "kubeflow-pipelines", Force: false})
+					if err != nil {
+						glog.Warningf("Failed to persist max_active_runs configuration for pipeline version %s in namespace %s: %v", pipelineVersionID, namespace, err)
+					}
 				}
 			}
 		}
