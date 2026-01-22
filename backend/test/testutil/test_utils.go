9dc08224c0dd61fa031d19bb6f4e4589265a2fe4
sduvvuri1603 <sduvvuri@redhat.com>
2026-01-22 06:11:23 -0500

    test: shorten pipeline parallelism runtimes
    Signed-off-by: sduvvuri1603 <sduvvuri@redhat.com>

--- backend/test/testutil/test_utils.go
+++ backend/test/testutil/test_utils.go
@@ -141,6 +141,12 @@ func ReplaceSDKInPipelineSpec(pipelineFilePath string) []byte {
 	newPackagePath := getPackagePath("sdk/python")
 	modifiedPipelineSpec := kfpPattern.ReplaceAllString(pipelineFileString, newPackagePath)
 
+	// Shorten default sleep durations so parallelism tests complete faster.
+	sleepDefaultPattern := regexp.MustCompile(`sleep_seconds:\s*int\s*=\s*20`)
+	modifiedPipelineSpec = sleepDefaultPattern.ReplaceAllString(modifiedPipelineSpec, "sleep_seconds: int = 5")
+	sleepYamlDefaultPattern := regexp.MustCompile(`defaultValue:\s*20\.0?`)
+	modifiedPipelineSpec = sleepYamlDefaultPattern.ReplaceAllString(modifiedPipelineSpec, "defaultValue: 5.0")
+
 	return []byte(modifiedPipelineSpec)
 }
 
