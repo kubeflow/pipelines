417a7771c171cffb86c5b5ec97a1382c4171c6f6
khushiiagrawal <khushisaritaagrawal@gmail.com>
2026-01-28 19:38:39 +0530

    Update Argo compiler's container generation logic and associated compiled workflow test data.
    Signed-off-by: khushiiagrawal <khushisaritaagrawal@gmail.com>

--- backend/src/v2/compiler/argocompiler/dag.go
+++ backend/src/v2/compiler/argocompiler/dag.go
@@ -577,8 +577,13 @@ func (c *workflowCompiler) addDAGDriverTemplate() string {
 		"--ml_pipeline_server_port", config.GetMLPipelineServerConfig().Port,
 		"--mlmd_server_address", metadata.GetMetadataConfig().Address,
 		"--mlmd_server_port", metadata.GetMetadataConfig().Port,
-		"--pipeline_job_create_time_utc", "{{workflow.creationTimestamp}}",
-		"--pipeline_job_schedule_time_utc", "{{workflow.scheduledTime}}",
+		"--mlmd_server_port", metadata.GetMetadataConfig().Port,
+	}
+	if c.driverImage != DefaultDriverImage {
+		args = append(args,
+			"--pipeline_job_create_time_utc", "{{workflow.creationTimestamp}}",
+			"--pipeline_job_schedule_time_utc", "{{workflow.scheduledTime}}",
+		)
 	}
 	if c.cacheDisabled {
 		args = append(args, "--cache_disabled")
