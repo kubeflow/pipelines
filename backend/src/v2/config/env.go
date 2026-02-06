c3e6d46adf844087d5ec803957e90fda40a89a86
Priyanshu-u07 <connect.priyanshu8271@gmail.com>
2026-02-07 01:53:12 +0530

    add CLUSTER_DOMAIN env var to deployment and unit tests for GetClusterDomain
    Signed-off-by: Priyanshu-u07 <connect.priyanshu8271@gmail.com>

--- backend/src/v2/config/env.go
+++ backend/src/v2/config/env.go
@@ -194,7 +194,11 @@ func getDefaultMinioSessionInfo() (objectstore.SessionInfo, error) {
 
 func GetMLPipelineServerConfig() *ServerConfig {
 	return &ServerConfig{
+<<<<<<< HEAD
 		Address: common.GetMLPipelineServiceName() + "." + common.GetPodNamespace() + ".svc.cluster.local",
+=======
+		Address: common.GetMLPipelineServiceName() + "." + common.GetPodNamespace() + ".svc." + common.GetClusterDomain(),
+>>>>>>> c3d669c52 (add CLUSTER_DOMAIN env var to deployment and unit tests for GetClusterDomain)
 		Port:    mlPipelineGrpcServicePort,
 	}
 }
