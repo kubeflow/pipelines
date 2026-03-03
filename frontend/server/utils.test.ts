f7c34421abab7e8ad68cfe9ae726567826199410
Jeff Spahr <spahrj@gmail.com>
2026-03-03 14:52:53 -0500

    style(frontend): format KFP error parsing test
    Signed-off-by: Jeff Spahr <spahrj@gmail.com>

--- frontend/server/utils.test.ts
+++ frontend/server/utils.test.ts
@@ -12,7 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import { PassThrough } from 'stream';
-import { PreviewStream, findFileOnPodVolume, parseError, resolveFilePathOnVolume } from './utils.js';
+import {
+  PreviewStream,
+  findFileOnPodVolume,
+  parseError,
+  resolveFilePathOnVolume,
+} from './utils.js';
 
 describe('utils', () => {
   describe('PreviewStream', () => {
