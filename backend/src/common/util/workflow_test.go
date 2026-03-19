7148bf5c5e282d809f21917153053d3a8285dba4
khushiiagrawal <khushisaritaagrawal@gmail.com>
2026-03-20 00:29:28 +0530

    test(util): enhance workflow string representation tests and add JSON normalization utility
    Signed-off-by: khushiiagrawal <khushisaritaagrawal@gmail.com>

--- backend/src/common/util/workflow_test.go
+++ backend/src/common/util/workflow_test.go
@@ -16,6 +16,7 @@ package util
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"testing"
 	"time"
@@ -242,9 +243,10 @@ func TestToStringForStore(t *testing.T) {
 			Name: "WORKFLOW_NAME",
 		},
 	})
+	expected := "{\"metadata\":{\"name\":\"WORKFLOW_NAME\"},\"spec\":{\"arguments\":{}},\"status\":{\"startedAt\":null,\"finishedAt\":null}}"
 	assert.Equal(t,
-		"{\"metadata\":{\"name\":\"WORKFLOW_NAME\",\"creationTimestamp\":null},\"spec\":{\"arguments\":{}},\"status\":{\"startedAt\":null,\"finishedAt\":null}}",
-		workflow.ToStringForStore())
+		normalizeJSONForNullFieldComparison(t, expected),
+		normalizeJSONForNullFieldComparison(t, workflow.ToStringForStore()))
 }
 
 func TestToStringForSchedule(t *testing.T) {
@@ -253,9 +255,40 @@ func TestToStringForSchedule(t *testing.T) {
 			Name: "WORKFLOW_NAME",
 		},
 	})
+	expected := "{\"metadata\":{\"name\":\"WORKFLOW_NAME\"},\"spec\":{\"arguments\":{}},\"status\":{\"startedAt\":null,\"finishedAt\":null}}"
 	assert.Equal(t,
-		"{\"metadata\":{\"name\":\"WORKFLOW_NAME\",\"creationTimestamp\":null},\"spec\":{\"arguments\":{}},\"status\":{\"startedAt\":null,\"finishedAt\":null}}",
-		workflow.ToStringForSchedule())
+		normalizeJSONForNullFieldComparison(t, expected),
+		normalizeJSONForNullFieldComparison(t, workflow.ToStringForSchedule()))
+}
+
+func normalizeJSONForNullFieldComparison(t *testing.T, jsonString string) interface{} {
+	t.Helper()
+
+	var decoded interface{}
+	err := json.Unmarshal([]byte(jsonString), &decoded)
+	assert.NoError(t, err)
+	return removeNilMapFields(decoded)
+}
+
+func removeNilMapFields(value interface{}) interface{} {
+	switch typedValue := value.(type) {
+	case map[string]interface{}:
+		for key, nestedValue := range typedValue {
+			if nestedValue == nil {
+				delete(typedValue, key)
+				continue
+			}
+			typedValue[key] = removeNilMapFields(nestedValue)
+		}
+		return typedValue
+	case []interface{}:
+		for index, nestedValue := range typedValue {
+			typedValue[index] = removeNilMapFields(nestedValue)
+		}
+		return typedValue
+	default:
+		return value
+	}
 }
 
 func TestWorkflow_OverrideName(t *testing.T) {
