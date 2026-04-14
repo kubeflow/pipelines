6d6a66bd8912ee190e56519c52f4153b4983b1b5
Alyssa Goins <agoins@redhat.com>
2026-04-14 14:39:23 -0400

    test-commit
    Signed-off-by: Alyssa Goins <agoins@redhat.com>

--- backend/src/v2/cmd/driver/main.go
+++ backend/src/v2/cmd/driver/main.go
@@ -96,6 +96,7 @@ var (
 	defaultRunAsUser     = flag.Int64("default_run_as_user", -1, "Admin-configured default runAsUser for user containers. -1 means not set.")
 	defaultRunAsGroup    = flag.Int64("default_run_as_group", -1, "Admin-configured default runAsGroup for user containers. -1 means not set.")
 	defaultRunAsNonRoot  = flag.String("default_run_as_non_root", "", "Admin-configured default runAsNonRoot for user containers. Empty means not set.")
+	testFlag             = flag.Bool("test_flag", false, "Test flag, do not use.")
 )
 
 // func RootDAG(pipelineName string, runID string, component *pipelinespec.ComponentSpec, task *pipelinespec.PipelineTaskSpec, mlmd *metadata.Client) (*Execution, error) {
