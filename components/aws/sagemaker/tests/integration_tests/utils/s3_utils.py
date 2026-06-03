e8e305d764e9c21a886a463e6dc25e4415443fce
mprahl <mprahl@users.noreply.github.com>
2026-06-03 14:07:31 -0400

    Remove the components directory except for google-cloud
    The KFP components now live at:
    https://github.com/kubeflow/pipelines-components
    
    This deletion is in accordance to and several discussions on the KFP
    community calls:
    https://github.com/kubeflow/pipelines/discussions/13051
    
    This also removes the docs/code-tree directory that seems to have been
    accidentally committed and is now stale.
    
    Signed-off-by: mprahl <mprahl@users.noreply.github.com>

--- components/aws/sagemaker/tests/integration_tests/utils/s3_utils.py
+++
@@ -1,4 +0,0 @@
-def check_object_exists(client, bucket, key):
-    waiter = client.get_waiter("object_exists")
-    waiter.wait(Bucket=bucket, Key=key)
-    return True
--- components/aws/sagemaker/tests/unit_tests/run_unit_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pushd ../..
-
-coverage run -m pytest tests/unit_tests/tests --junitxml ./unit_tests.log
-coverage report -m --omit "*/usr/*,tests/*,*__init__*,*/Python/*"
-
-popd
