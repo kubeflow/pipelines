5df3d53775a10b52280a51d9af7bec29692db160
Helber Belmiro <helber.belmiro@gmail.com>
2026-06-19 11:39:48 -0300

    chore: update obsolete generated API and model files (#13501)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- backend/api/v2beta1/go_http_client/recurring_run_client/recurring_run_service/recurring_run_service_enable_recurring_run_responses.go
+++ backend/api/v2beta1/go_http_client/recurring_run_client/recurring_run_service/recurring_run_service_enable_recurring_run_responses.go
@@ -125,7 +125,7 @@ An unexpected error response.
 type RecurringRunServiceEnableRecurringRunDefault struct {
 	_statusCode int
 
-	Payload *recurring_run_model.GooglerpcStatus
+	Payload *recurring_run_model.GoogleRPCStatus
 }
 
 // IsSuccess returns true when this recurring run service enable recurring run default response has a 2xx status code
@@ -168,13 +168,13 @@ func (o *RecurringRunServiceEnableRecurringRunDefault) String() string {
 	return fmt.Sprintf("[POST /apis/v2beta1/recurringruns/{recurring_run_id}:enable][%d] RecurringRunService_EnableRecurringRun default %s", o._statusCode, payload)
 }
 
-func (o *RecurringRunServiceEnableRecurringRunDefault) GetPayload() *recurring_run_model.GooglerpcStatus {
+func (o *RecurringRunServiceEnableRecurringRunDefault) GetPayload() *recurring_run_model.GoogleRPCStatus {
 	return o.Payload
 }
 
 func (o *RecurringRunServiceEnableRecurringRunDefault) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
 
-	o.Payload = new(recurring_run_model.GooglerpcStatus)
+	o.Payload = new(recurring_run_model.GoogleRPCStatus)
 
 	// response payload
 	if err := consumer.Consume(response.Body(), o.Payload); err != nil && err != io.EOF {
