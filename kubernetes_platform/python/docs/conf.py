53910ff4f7a149153d20122fdf44205230cbb52c
Michael <m.zazula@gmail.com>
2026-08-26 18:19:36 -0400

    chore(release): bump version to 2.17.1 (#14182)
    Signed-off-by: zazulam <m.zazula@gmail.com>

--- kubernetes_platform/python/docs/conf.py
+++ kubernetes_platform/python/docs/conf.py
@@ -140,10 +140,17 @@ html_theme_options = {
     'version_info': [
         {
             'version':
+                'https://kfp-kubernetes.readthedocs.io/en/kfp-kubernetes-2.17/',
+            'title':
+                '2.17',
+            'aliases': ['stable'],
+        },
+        {
+            'version':
                 'https://kfp-kubernetes.readthedocs.io/en/kfp-kubernetes-2.17.0/',
             'title':
                 '2.17.0',
-            'aliases': ['stable'],
+            'aliases': [],
         },
         {
             'version':
