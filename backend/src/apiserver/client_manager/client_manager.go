HumairAK/data-science-pipelines

feat(backend): extract MLMD removal API foundation
Split the server-side artifact and task API foundation into a reviewable first slice so follow-on runtime and cleanup work can build on a stable contract.

Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

fix(backend): preserve legacy task detail compatibility

Keep the deprecated v2beta1 PipelineTaskDetail wire-compatible while moving the richer task payload to a new PipelineTask message. This lets the new task APIs evolve without breaking existing clients before the planned major-version cleanup.

Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

fix(backend): restore runtime state wire values

Preserve the original v2beta1 RuntimeState numeric assignments so existing protobuf clients and stored payloads keep decoding correctly. This keeps the current contract stable until the planned major-version cleanup removes the legacy surface.

Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

fix(backend): drop unreleased artifact field reserve

Remove the unnecessary reserved tag for the unreleased CreateArtifactRequest field so the artifact API stays flexible before release.

Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

fix(backend): remove unused artifact link type input

Drop CreateArtifactRequest.type from the v2beta1 artifact API because artifact creation always persists output links regardless of the request body. This aligns the proto contract with the behavior we actually support and avoids validating a field that has no effect.

Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

fix(backend): validate artifact task run ownership

Require artifact_task.run_id to match the owning task's run before persisting create requests and bulk links. This prevents forged run associations from leaking artifact-task rows across run filters.

Signed-off-by: Humair Khan <HumairAK@users.noreply.github.com>

fix(backend): enforce artifact namespace ownership

backend/src/apiserver/client_manager/client_manager.go
@@ -37,6 +37,7 @@ import (
 	"github.com/kubeflow/pipelines/backend/src/apiserver/client"
 	"github.com/kubeflow/pipelines/backend/src/apiserver/common"
 	"github.com/kubeflow/pipelines/backend/src/apiserver/model"
+	"github.com/kubeflow/pipelines/backend/src/apiserver/resource"
 	"github.com/kubeflow/pipelines/backend/src/apiserver/storage"
 	"github.com/kubeflow/pipelines/backend/src/apiserver/validation"
 	"github.com/kubeflow/pipelines/backend/src/common/util"
@@ -93,7 +94,10 @@ func init() {
 	}
 }
 
-// Container for all service clients.
+// Ensure that ClientManager implements the resource.ClientManagerInterface interface.
+var _ resource.ClientManagerInterface = &ClientManager{}
+
+// ClientManager Container for all service clients.
 type ClientManager struct {
 	db                        *storage.DB
 	experimentStore           storage.ExperimentStoreInterface
@@ -101,6 +105,8 @@ type ClientManager struct {
 	jobStore                  storage.JobStoreInterface
 	runStore                  storage.RunStoreInterface
 	taskStore                 storage.TaskStoreInterface
+	artifactStore             storage.ArtifactStoreInterface
+	artifactTaskStore         storage.ArtifactTaskStoreInterface
 	resourceReferenceStore    storage.ResourceReferenceStoreInterface
 	dBStatusStore             storage.DBStatusStoreInterface
 	defaultExperimentStore    storage.DefaultExperimentStoreInterface
@@ -152,6 +158,14 @@ func (c *ClientManager) JobStore() storage.JobStoreInterface {
 
 func (c *ClientManager) RunStore() storage.RunStoreInterface {
 	return c.runStore
+}
+
+func (c *ClientManager) ArtifactStore() storage.ArtifactStoreInterface {
+	return c.artifactStore
+}
+
+func (c *ClientManager) ArtifactTaskStore() storage.ArtifactTaskStoreInterface {
+	return c.artifactTaskStore
 }
 
 func (c *ClientManager) ResourceReferenceStore() storage.ResourceReferenceStoreInterface {
@@ -312,6 +326,8 @@ func (c *ClientManager) init(options *Options) error {
 
 	runStore := storage.NewRunStore(db, c.time)
 	c.runStore = runStore
+	c.artifactStore = storage.NewArtifactStore(db, c.time, c.uuid)
+	c.artifactTaskStore = storage.NewArtifactTaskStore(db, c.uuid)
 
 	// Log archive
 	c.logArchive = initLogArchive()
@@ -570,6 +586,8 @@ func autoMigrate(db *gorm.DB) error {
 	glog.Infof("Running AutoMigrate.")
 
 	if err := db.AutoMigrate(
+		&model.Artifact{},
+		&model.ArtifactTask{},
 		&model.DBStatus{},
 		&model.DefaultExperiment{},
 		&model.Experiment{},
@@ -579,7 +597,7 @@ func autoMigrate(db *gorm.DB) error {
 		&model.PipelineVersionTag{},
 		&model.Job{},
 		&model.Run{},
-		&model.RunMetric{},
+		&model.RunMetricV1{},
 		&model.Task{},
 		&model.ResourceReference{},
 	); err != nil {

kubeflow/pipelines

backend/src/apiserver/client_manager/client_manager.go
@@ -42,6 +42,7 @@ import (
 	commonsql "github.com/kubeflow/pipelines/backend/src/apiserver/common/sql"
 	sqldrv "github.com/kubeflow/pipelines/backend/src/apiserver/common/sql/dialect"
 	"github.com/kubeflow/pipelines/backend/src/apiserver/model"
+	"github.com/kubeflow/pipelines/backend/src/apiserver/resource"
 	"github.com/kubeflow/pipelines/backend/src/apiserver/storage"
 	"github.com/kubeflow/pipelines/backend/src/apiserver/validation"
 	"github.com/kubeflow/pipelines/backend/src/common/util"
@@ -100,7 +101,10 @@ func init() {
 	}
 }
 
-// Container for all service clients.
+// Ensure that ClientManager implements the resource.ClientManagerInterface interface.
+var _ resource.ClientManagerInterface = &ClientManager{}
+
+// ClientManager Container for all service clients.
 type ClientManager struct {
 	db                        *sql.DB
 	dbDialect                 sqldrv.DBDialect
@@ -109,6 +113,8 @@ type ClientManager struct {
 	jobStore                  storage.JobStoreInterface
 	runStore                  storage.RunStoreInterface
 	taskStore                 storage.TaskStoreInterface
+	artifactStore             storage.ArtifactStoreInterface
+	artifactTaskStore         storage.ArtifactTaskStoreInterface
 	resourceReferenceStore    storage.ResourceReferenceStoreInterface
 	dBStatusStore             storage.DBStatusStoreInterface
 	defaultExperimentStore    storage.DefaultExperimentStoreInterface
@@ -169,6 +175,14 @@ func (c *ClientManager) JobStore() storage.JobStoreInterface {
 
 func (c *ClientManager) RunStore() storage.RunStoreInterface {
 	return c.runStore
+}
+
+func (c *ClientManager) ArtifactStore() storage.ArtifactStoreInterface {
+	return c.artifactStore
+}
+
+func (c *ClientManager) ArtifactTaskStore() storage.ArtifactTaskStoreInterface {
+	return c.artifactTaskStore
 }
 
 func (c *ClientManager) ResourceReferenceStore() storage.ResourceReferenceStoreInterface {
@@ -342,6 +356,8 @@ func (c *ClientManager) init(options *Options) error {
 
 	runStore := storage.NewRunStore(db, c.time, c.dbDialect)
 	c.runStore = runStore
+	c.artifactStore = storage.NewArtifactStore(db, c.time, c.uuid, c.dbDialect)
+	c.artifactTaskStore = storage.NewArtifactTaskStore(db, c.uuid, c.dbDialect)
 
 	// Log archive
 	c.logArchive = initLogArchive()