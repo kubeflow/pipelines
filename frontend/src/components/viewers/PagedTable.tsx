1120d3c42cb502d366ceafc03e33b0675db3f7b9
Jeff Spahr <spahrj@gmail.com>
2026-09-23 14:07:34 -0400

    fix(frontend): restore top border in label-free artifact tables (#14490)
    * fix(frontend): restore top border in label-free artifact tables
    
    Signed-off-by: Jeff Spahr <spahrj@gmail.com>
    
    * fix(frontend): omit empty table headers to preserve border painting
    
    Signed-off-by: Jeff Spahr <spahrj@gmail.com>
    
    ---------
    
    Signed-off-by: Jeff Spahr <spahrj@gmail.com>

--- frontend/src/components/viewers/PagedTable.tsx
+++ frontend/src/components/viewers/PagedTable.tsx
@@ -17,7 +17,7 @@
 import * as React from 'react';
 import Viewer, { ViewerConfig, PlotType } from './Viewer';
 import { color, fontsize, commonCss } from '../../Css';
-import { stylesheet } from 'typestyle';
+import { classes, stylesheet } from 'typestyle';
 
 import {
   Table,
@@ -76,6 +76,9 @@ class PagedTable extends Viewer<PagedTableProps, PagedTableState> {
       fontWeight: 'bold',
       paddingLeft: this._isSmall() ? 5 : 'invalid',
     },
+    topBorder: {
+      borderTop: 'solid 1px ' + color.divider,
+    },
     row: {
       borderBottom: '1px solid #ddd',
       height: this._isSmall() ? 25 : this._rowHeight,
@@ -111,29 +114,32 @@ class PagedTable extends Viewer<PagedTableProps, PagedTableState> {
     return (
       <div style={{ width: '100%' }} className={commonCss.page}>
         <Table style={{ display: 'block', overflow: 'auto' }}>
-          <TableHead>
-            <TableRow>
-              {labels.map((label, i) => {
-                return (
-                  <TableCell
-                    className={this._css.columnName}
-                    key={i}
-                    sortDirection={orderBy === i ? order : false}
-                  >
-                    <Tooltip title='Sort' enterDelay={300}>
-                      <TableSortLabel
-                        active={orderBy === i}
-                        direction={order}
-                        onClick={this._handleSort(i)}
-                      >
-                        {label}
-                      </TableSortLabel>
-                    </Tooltip>
-                  </TableCell>
-                );
-              }, this)}
-            </TableRow>
-          </TableHead>
+          {/* An empty header clips the body's collapsed top border in the scrollable table. */}
+          {labels.length > 0 && (
+            <TableHead>
+              <TableRow>
+                {labels.map((label, i) => {
+                  return (
+                    <TableCell
+                      className={this._css.columnName}
+                      key={i}
+                      sortDirection={orderBy === i ? order : false}
+                    >
+                      <Tooltip title='Sort' enterDelay={300}>
+                        <TableSortLabel
+                          active={orderBy === i}
+                          direction={order}
+                          onClick={this._handleSort(i)}
+                        >
+                          {label}
+                        </TableSortLabel>
+                      </Tooltip>
+                    </TableCell>
+                  );
+                }, this)}
+              </TableRow>
+            </TableHead>
+          )}
 
           <TableBody>
             {this._stableSort(data)
@@ -142,7 +148,13 @@ class PagedTable extends Viewer<PagedTableProps, PagedTableState> {
                 return (
                   <TableRow hover={true} tabIndex={-1} key={index} className={this._css.row}>
                     {row.map((cell, i) => (
-                      <TableCell key={i} className={this._css.cell}>
+                      <TableCell
+                        key={i}
+                        className={classes(
+                          this._css.cell,
+                          labels.length === 0 && index === 0 ? this._css.topBorder : '',
+                        )}
+                      >
                         {cell}
                       </TableCell>
                     ))}
